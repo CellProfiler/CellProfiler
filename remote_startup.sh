578b904d71b284cfde831d9b80ce0ab797256bee
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-09-08 21:27:52 +0000

    kill MATLAB as well
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@18 1fc53939-2000-0410-845c-e8453a809027

--- remote_startup.sh
+++ remote_startup.sh
@@ -6,5 +6,5 @@ echo -n "Hit Return to kill remote matlab shells..."
 read
 
 for i in p0 p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 p12 p13 p14 p15; do
-    ssh -x $i killall matlab
+    ssh -x $i killall matlab MATLAB
 done

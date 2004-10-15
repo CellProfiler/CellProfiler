e176bece581ec18752369dd7ba3056019a0d5ae3
inthek <inthek@1fc53939-2000-0410-845c-e8453a809027>
2004-10-15 04:00:03 +0000

    Added Boilerplate to all the *.m files
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@113 1fc53939-2000-0410-845c-e8453a809027

--- Modules/AlignImages.m
+++ Modules/AlignImages.m
@@ -499,3 +499,29 @@ I = entropy(X) + entropy(Y) - entropy2(X,Y);
 %%%%% Which image is displayed as which color can be changed by going into the module's ".m" file
 %%%%% and changing the lines after "FOR DISPLAY PURPOSES ONLY".  The first
 %%%%% line in each set is red, then green, then blue.
+
+
+% The contents of this file are subject to the Mozilla Public License Version 
+% 1.1 (the "License"); you may not use this file except in compliance with 
+% the License. You may obtain a copy of the License at 
+% http://www.mozilla.org/MPL/
+% 
+% Software distributed under the License is distributed on an "AS IS" basis,
+% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+% for the specific language governing rights and limitations under the
+% License.
+% 
+% 
+% The Original Code is the ______________________.
+% 
+% The Initial Developer of the Original Code is
+% Whitehead Institute for Biomedical Research
+% Portions created by the Initial Developer are Copyright (C) 2003,2004
+% the Initial Developer. All Rights Reserved.
+% 
+% Contributor(s):
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$

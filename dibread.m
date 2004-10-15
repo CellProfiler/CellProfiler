04f36a5c8d2af059ff1b450073c0d75b4f8ba093
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2004-10-15 16:05:06 +0000

    modified to have a bit-depth argument, and return doubles
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@120 1fc53939-2000-0410-845c-e8453a809027

--- dibread.m
+++ dibread.m
@@ -1,4 +1,4 @@
-function im = dibread(filename, width, height, channels)
+function im = dibread(filename, width, height, channels, depth)
 
 fid = fopen(filename, 'r');
 if (fid == -1),
@@ -7,15 +7,15 @@ end
 
 ignore = fread(fid, 52, 'uchar');
 
-im = zeros(height,width,channels, 'uint16');
+im = zeros(height,width,channels);
 
 for c=1:channels,
-  [data, count] = fread(fid, width * height, 'uint16=>uint16', 0, 'l');
+  [data, count] = fread(fid, width * height, 'uint16', 0, 'l');
   if count < (width * height),
     fclose(fid);
     error(['end-of-file encountered in DIBREAD while reading ' filename]);
   end
-  im(:,:,c) = reshape(data, [width height])';
+  im(:,:,c) = reshape(data, [width height])' / (2^depth - 1);
 end
 
 fclose(fid);

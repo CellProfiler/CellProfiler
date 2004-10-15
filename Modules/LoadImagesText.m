e233abbcdab137dc14e4179b1aa2b248f997d013
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-10-15 19:47:56 +0000

    Compatible with DIB image files now
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@133 1fc53939-2000-0410-845c-e8453a809027

--- Modules/LoadImagesText.m
+++ Modules/LoadImagesText.m
@@ -1,4 +1,29 @@
-function handles = AlgLoadImagesText6(handles)
+function handles = AlgLoadImagesText(handles)
+
+% The contents of this file are subject to the Mozilla Public License Version 
+% 1.1 (the "License"); you may not use this file except in compliance with 
+% the License. You may obtain a copy of the License at 
+% http://www.mozilla.org/MPL/
+% 
+% Software distributed under the License is distributed on an "AS IS" basis,
+% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+% for the specific language governing rights and limitations under the
+% License.
+% 
+% 
+% The Original Code is the Load Images Text module.
+% 
+% The Initial Developer of the Original Code is
+% Whitehead Institute for Biomedical Research
+% Portions created by the Initial Developer are Copyright (C) 2003,2004
+% the Initial Developer. All Rights Reserved.
+% 
+% Contributor(s):
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
 
 %%% Reads the current algorithm number, since this is needed to find 
 %%% the variable values that the user entered.
@@ -79,12 +104,21 @@ if SetBeingAnalyzed == 1
     %%% Checks whether the file format the user entered is readable by Matlab.
     IsFormat = imformats(FileFormat);
     if isempty(IsFormat) == 1
-        error('The image file type entered in the Load Images Text module is not recognized by Matlab. Or, you may have entered a period in the box. For a list of recognizable image file formats, type "imformats" (no quotes) at the command line in Matlab.','Error')
+        %%% Checks if the image is a DIB image file.
+        if strcmp(upper(FileFormat),'DIB') == 1
+            Answers = inputdlg({'Enter the width of the images in pixels','Enter the height of the images in pixels','Enter the bit depth of the camera','Enter the number of channels'},'Enter DIB file information',1,{'512','512','12','1'});
+            handles.dOTDIBwidth = str2num(Answers{1});
+            handles.dOTDIBheight = str2num(Answers{2});
+            handles.dOTDIBbitdepth = str2num(Answers{3});
+            handles.dOTDIBchannels = str2num(Answers{4});
+        else
+            error('The image file type entered in the Load Images Text module is not recognized by Matlab. Or, you may have entered a period in the box. For a list of recognizable image file formats, type "imformats" (no quotes) at the command line in Matlab.','Error')
+        end
     end
     %%% If the user did not enter any data in the first slot (they put
     %%% a slash in either box), no images are retrieved.
     if strcmp(TextToFind{1}, '/') == 1 | strcmp(ImageName{1}, '/') == 1
-        error('Image processing was canceled because the first image slot in the Load Images module was left blank.')
+        error('Image processing was canceled because the first image slot in the Load Images Text module was left blank.')
     end
     %%% For all 4 image slots, the file names are extracted.
     for n = 1:4
@@ -197,8 +231,6 @@ if SetBeingAnalyzed == 1
         end
     end
     handles.Vnumberimagesets = NumberOfImageSets;
-    %%% Update the handles structure.
-    %%% Removed for parallel: guidata(gcbo, handles);
 end % Goes with: if SetBeingAnalyzed == 1
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -207,7 +239,7 @@ end % Goes with: if SetBeingAnalyzed == 1
 
 for n = 1:4
     %%% This try/catch will catch any problems in the load images module.
-    try 
+    try
         if strcmp(TextToFind{n}, '/') == 0 & strcmp(ImageName{n}, '/') == 0
             %%% The following runs every time through this module (i.e. for
             %%% every image set).
@@ -218,21 +250,44 @@ for n = 1:4
             CurrentFileName = FileList(SetBeingAnalyzed);
             %%% Determine the directory to switch to.
             if (~ isfield(handles, 'parallel_machines')),
-              fieldname = ['dOTPathName', ImageName{n}];
-              PathName = handles.(fieldname);
+                fieldname = ['dOTPathName', ImageName{n}];
+                PathName = handles.(fieldname);
             else
-              PathName = handles.RemoteImagePathName;
+                PathName = handles.RemoteImagePathName;
             end
             %%% Switch to the directory
             cd(PathName);
-            %%% Read (open) the image you want to analyze and assign it to a variable,
-            %%% "LoadedImage".
-            try
-                LoadedImage = im2double(imread(char(CurrentFileName),FileFormat));
-            catch error(['Image processing was canceled because the Load Images Text module could not load the image "', char(CurrentFileName), '" which you specified is in "', FileFormat, '" file format.'])
+            %%% Handles a non-Matlab readable file format.
+            if isfield(handles, 'dOTDIBwidth') == 1
+                %%% Opens this non-Matlab readable file format.
+                Width = handles.dOTDIBwidth;
+                Height = handles.dOTDIBheight;
+                Channels = handles.dOTDIBchannels;
+                BitDepth = handles.dOTDIBbitdepth;
+                fid = fopen(char(CurrentFileName), 'r');
+                if (fid == -1),
+                    error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
+                end
+                Ignore = fread(fid, 52, 'uchar');
+                LoadedImage = zeros(Height,Width,Channels);
+                for c=1:Channels,
+                    [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
+                    if Count < (Width * Height),
+                        fclose(fid);
+                        error(['End-of-file encountered while reading ', char(CurrentFileName), '. Have you entered the proper size and number of channels for these images?']);
+                    end
+                    LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
+                end
+                fclose(fid);
+            else
+                %%% Opens Matlab-readable file formats.
+                try
+                    LoadedImage = im2double(imread(char(CurrentFileName),FileFormat));
+                catch error(['Image processing was canceled because the Load Images Text module could not load the image "', char(CurrentFileName), '" which you specified is in "', FileFormat, '" file format.'])
+                end
             end
             %%% Saves the original image file name to the handles structure.  The field
-            %%% is named 
+            %%% is named
             %%% appropriately based on the user's input, with the 'dOT' prefix added so
             %%% that this field will be deleted at the end of the analysis batch.
             fieldname = ['dOTFilename', ImageName{n}];
@@ -240,10 +295,10 @@ for n = 1:4
             %%% Saves the loaded image to the handles structure.The field is named
             %%% appropriately based on the user's input.The prefix 'dOT' is added to
             %%% the beginning of the measurement name so that this field will be
-            %%% deleted at the end of the analysis batch.  
+            %%% deleted at the end of the analysis batch.
             fieldname = ['dOT',ImageName{n}];
             handles.(fieldname) = LoadedImage;
-        end    
+        end
     catch ErrorMessage = lasterr;
         ErrorNumber(1) = {'first'};
         ErrorNumber(2) = {'second'};
@@ -254,23 +309,21 @@ for n = 1:4
 end
 %%% Changes back to the original directory.
 cd(CurrentDirectory)
-%%% Update the handles structure.
-%%% Removed for parallel: guidata(gcbo, handles);
 
 %%%%%%%%%%%%%%%%%%%%
 %%% FIGURE WINDOW %%%
 %%%%%%%%%%%%%%%%%%%%
 
 if SetBeingAnalyzed == 1
-%%% The figure window display is unnecessary for this module, so the figure
-%%% window is closed the first time through the module.
-%%% Determines the figure number.
-fieldname = ['figurealgorithm',CurrentAlgorithm];
-ThisAlgFigureNumber = handles.(fieldname);
-%%% If the window is open, it is closed.
-if any(findobj == ThisAlgFigureNumber) == 1;
-    close(ThisAlgFigureNumber)
-end
+    %%% The figure window display is unnecessary for this module, so the figure
+    %%% window is closed the first time through the module.
+    %%% Determines the figure number.
+    fieldname = ['figurealgorithm',CurrentAlgorithm];
+    ThisAlgFigureNumber = handles.(fieldname);
+    %%% If the window is open, it is closed.
+    if any(findobj == ThisAlgFigureNumber) == 1;
+        close(ThisAlgFigureNumber)
+    end
 end
 
 %%%%%%%%%%%
@@ -285,42 +338,16 @@ end
 %%%%% the proper image.  If more than five images per set must be loaded,
 %%%%% more than one Load Images module can be run sequentially. 
 %%%%% . 
-%%%%% This module is different from the Load Images module because Search
-%%%%% Load Images can be used to load images that are not in a defined
-%%%%% order.  That is, Load Images is useful when images are present in a
-%%%%% repeating order, like DAPI, FITC, Red, DAPI, FITC, Red, and so on,
-%%%%% where images are selected based on how many images are in each set
-%%%%% and what position within each set a particular color is located (e.g.
-%%%%% three images per set, DAPI is always first).  Load Images Text is
-%%%%% used instead to load images that have a particular piece of text in
-%%%%% the name.
+%%%%% This module is different from the Load Images Order module because
+%%%%% Load Images Text can be used to load images that are not in a defined
+%%%%% order.  That is, Load Images Order is useful when images are present
+%%%%% in a repeating order, like DAPI, FITC, Red, DAPI, FITC, Red, and so
+%%%%% on, where images are selected based on how many images are in each
+%%%%% set and what position within each set a particular color is located
+%%%%% (e.g. three images per set, DAPI is always first).  Load Images Text
+%%%%% is used instead to load images that have a particular piece of text
+%%%%% in the name.
 %%%%% .
 %%%%% You may have folders within the directory that is being searched, but
 %%%%% these folders must not contain the text you are searching for or an
 %%%%% error will result.
-
-
-% The contents of this file are subject to the Mozilla Public License Version 
-% 1.1 (the "License"); you may not use this file except in compliance with 
-% the License. You may obtain a copy of the License at 
-% http://www.mozilla.org/MPL/
-% 
-% Software distributed under the License is distributed on an "AS IS" basis,
-% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-% for the specific language governing rights and limitations under the
-% License.
-% 
-% 
-% The Original Code is the ______________________.
-% 
-% The Initial Developer of the Original Code is
-% Whitehead Institute for Biomedical Research
-% Portions created by the Initial Developer are Copyright (C) 2003,2004
-% the Initial Developer. All Rights Reserved.
-% 
-% Contributor(s):
-%   Anne Carpenter <carpenter@wi.mit.edu>
-%   Thouis Jones   <thouis@csail.mit.edu>
-%   In Han Kang    <inthek@mit.edu>
-%
-% $Revision$

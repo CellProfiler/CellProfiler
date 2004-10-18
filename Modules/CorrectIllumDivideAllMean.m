e256febcd2bafd1009b853e5fd72024bb3b375ee
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-10-18 20:05:40 +0000

    Polished by Anne
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@168 1fc53939-2000-0410-845c-e8453a809027

--- Modules/CorrectIllumDivideAllMean.m
+++ Modules/CorrectIllumDivideAllMean.m
@@ -1,31 +1,69 @@
 function handles = AlgCorrectIlluminationDivideAllMean(handles)
 
-%%% Reads the current algorithm number, since this is needed to find 
-%%% the variable values that the user entered.
-CurrentAlgorithm = handles.currentalgorithm;
-CurrentAlgorithmNum = str2num(handles.currentalgorithm);
+% Help for the Correct Illumination Divide All Mean module: 
+%
+% This module corrects for uneven illumination of each image, based on
+% information from a set of images collected at the same time. This module
+% works by averaging together all of the images, then smoothing this image
+% by fitting a low-order polynomial to the resulting average image and
+% rescaling it.  This produces an image that represents the variation in
+% illumination across the field of view.  This process is carried out
+% before the first image set is processed; subsequent image sets use the
+% already calculated image. Each image is divided by this illumination
+% image to produce the corrected image.
+%
+% If you want to run this module only to calculate the mean and
+% illumination images and not to correct every image in the directory,
+% simply run the module as usual and use the button on the Timer to
+% stop processing after the first image set.
+
+% The contents of this file are subject to the Mozilla Public License Version 
+% 1.1 (the "License"); you may not use this file except in compliance with 
+% the License. You may obtain a copy of the License at 
+% http://www.mozilla.org/MPL/
+% 
+% Software distributed under the License is distributed on an "AS IS" basis,
+% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+% for the specific language governing rights and limitations under the
+% License.
+% 
+% 
+% The Original Code is the Correct Illumination Divide All Mean module.
+% 
+% The Initial Developer of the Original Code is
+% Whitehead Institute for Biomedical Research
+% Portions created by the Initial Developer are Copyright (C) 2003,2004
+% the Initial Developer. All Rights Reserved.
+% 
+% Contributor(s):
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
 
 %%%%%%%%%%%%%%%%
 %%% VARIABLES %%%
 %%%%%%%%%%%%%%%%
 drawnow
 
+%%% Reads the current algorithm number, since this is needed to find 
+%%% the variable values that the user entered.
+CurrentAlgorithm = handles.currentalgorithm;
+CurrentAlgorithmNum = str2num(handles.currentalgorithm);
+
 %textVAR01 = What did you call the image to be corrected?
 %defaultVAR01 = OrigBlue
 ImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,1});
+
 %textVAR02 = What do you want to call the corrected image?
 %defaultVAR02 = CorrBlue
 CorrectedImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,2});
-%textVAR04 = To save each corrected image, enter text to append to the image name 
-%defaultVAR04 = N
-SaveImage = char(handles.Settings.Vvariable{CurrentAlgorithmNum,4});
-%textVAR05 =  Otherwise, leave as "N". To save or display other images, press Help button
-%textVAR06 = In what file format do you want to save images? Do not include a period
-%defaultVAR06 = tif
-FileFormat = char(handles.Settings.Vvariable{CurrentAlgorithmNum,6});
+
 %textVAR08 = To save the illum. corr. image to use later, type a file name + .mat. Else, 'N'
 %defaultVAR08 = N
 IllumCorrectFileName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,8});
+
 %textVAR09 = If you have already created an illumination corrrection image to be used, enter the 
 %textVAR10 = path & file name of the image below. To calculate the illumination correction image 
 %textVAR11 = from all the images of this color that will be processed, leave a slash in the box below.
@@ -36,15 +74,11 @@ IllumCorrectPathAndFileName = char(handles.Settings.Vvariable{CurrentAlgorithmNu
 %%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
-%%% Checks whether the file format the user entered is readable by Matlab.
-IsFormat = imformats(FileFormat);
-if isempty(IsFormat) == 1
-    error('The image file type entered in the Correct Illumination module is not recognized by Matlab. Or, you may have entered a period in the box. For a list of recognizable image file formats, type "imformats" (no quotes) at the command line in Matlab.','Error')
-end
-%%% Read (open) the image you want to analyze and assign it to a variable,
+
+%%% Reads (opens) the image you want to analyze and assigns it to a variable,
 %%% "OrigImage".
 fieldname = ['dOT', ImageName];
-%%% Check whether the image to be analyzed exists in the handles structure.
+%%% Checks whether the image to be analyzed exists in the handles structure.
 if isfield(handles, fieldname) == 0
     %%% If the image is not there, an error message is produced.  The error
     %%% is not displayed: The error function halts the current function and
@@ -54,46 +88,13 @@ if isfield(handles, fieldname) == 0
     %%% analysis loop without attempting further modules.
     error(['Image processing was canceled because the Correct Illumination module could not find the input image.  It was supposed to be named ', ImageName, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
 end
-%%% Read the image.
+%%% Reads the image.
 OrigImage = handles.(fieldname);
-        % figure, imshow(OrigImage), title('OrigImage')
         
-%%% Check whether the appendages to be added to the file names of images
-%%% will result in overwriting the original file, or in a file name that
-%%% contains spaces.
-%%% Determine the filename of the image to be analyzed.
-fieldname = ['dOTFilename', ImageName];
-FileName = handles.(fieldname)(handles.setbeinganalyzed);
-%%% Find and remove the file format extension within the original file
-%%% name, but only if it is at the end. Strip the original file format extension 
-%%% off of the file name, if it is present, otherwise, leave the original
-%%% name intact.
-CharFileName = char(FileName);
-PotentialDot = CharFileName(end-3:end-3);
-if strcmp(PotentialDot,'.') == 1
-    BareFileName = CharFileName(1:end-4);
-else BareFileName = CharFileName;
-end
-%%% Assemble the new image name.
-NewImageName = [BareFileName,SaveImage,'.',FileFormat];
-%%% Check whether the new image name is going to result in a name with
-%%% spaces.
-A = isspace(SaveImage);
-if any(A) == 1
-    error('Image processing was canceled because you have entered one or more spaces in the box of text to append to the object outlines image name in the Correct Illumination All Divide module.  If you do not want to save the object outlines image to the hard drive, type "N" into the appropriate box.')
-    return
-end
-%%% Check whether the new image name is going to result in overwriting the
-%%% original file.
-B = strcmp(upper(CharFileName), upper(NewImageName));
-if B == 1
-    error('Image processing was canceled because you have not entered text to append to the object outlines image name in the Correct Illumination All Divide module.  If you do not want to save the object outlines image to the hard drive, type "N" into the appropriate box.')
-    return
-end
-
 %%%%%%%%%%%%%%%%%%%%%
 %%% IMAGE ANALYSIS %%%
 %%%%%%%%%%%%%%%%%%%%%
+drawnow
 
 %%% Checks that the original image is two-dimensional (i.e. not a color
 %%% image), which would disrupt several of the image functions.
@@ -113,7 +114,6 @@ if handles.setbeinganalyzed == 1
     if strcmp(IllumCorrectPathAndFileName, '/') ~= 1
         try StructureIlluminationImage = load(IllumCorrectPathAndFileName);
              IlluminationImage = StructureIlluminationImage.IlluminationImage;
-            % figure, imagesc(IlluminationImage), colormap('gray'), title('Loaded Illumination Correction Image')
         catch error(['Image processing was canceled because there was a problem loading the image ', IllumCorrectPathAndFileName, '. Check that the full path and file name has been typed correctly.'])
         end
         %%% Otherwise, the illumination correction image is calculated based on all
@@ -159,6 +159,7 @@ if handles.setbeinganalyzed == 1
                 WaitbarText = {'Calculating the illumination function for the';'Correct Illumination All Divide module.'; 'Subsequent image sets will be processed';'more quickly than the first image set.'; ['Seconds remaining: ', num2str(TimeRemaining),]};
                 WaitbarText = char(WaitbarText);
                 waitbar(i/NumberOfImages, WaitbarHandle, WaitbarText)
+                drawnow
             end
             if length(FileList) == 1
                 CurrentTime = clock;
@@ -168,8 +169,6 @@ if handles.setbeinganalyzed == 1
             WaitbarText = char(WaitbarText);
             waitbar(i/NumberOfImages, WaitbarHandle, WaitbarText)
             MeanImage = TotalImage / length(FileList);
-            %    figure, imagesc(MeanImage), colormap(gray)
-            % imwrite(MeanImage/256, [ImageName,'MeanImage2','.',FileFormat], FileFormat);
             %%% The following is used to fit a low-dimensional polynomial to the mean image.
             %%% The result, IlluminationImage, is an image of the smooth illumination function.
             [x,y] = meshgrid(1:size(MeanImage,2), 1:size(MeanImage,1));
@@ -179,8 +178,8 @@ if handles.setbeinganalyzed == 1
             o = ones(size(MeanImage));
             Ind = find(MeanImage > 0);
             Coeffs = [x2(Ind) y2(Ind) xy(Ind) x(Ind) y(Ind) o(Ind)] \ double(MeanImage(Ind));
+            drawnow
             IlluminationImage1 = reshape([x2(:) y2(:) xy(:) x(:) y(:) o(:)] * Coeffs, size(MeanImage));
-               % figure, imagesc(IlluminationImage), colormap(gray), title('Calculated illumination correction image')
             %%% The final IlluminationImage is produced by dividing each
             %%% pixel of the illumination image by a scalar: the minimum
             %%% pixel value anywhere in the illumination image. (If the
@@ -188,6 +187,7 @@ if handles.setbeinganalyzed == 1
             %%% This rescales the IlluminationImage from 1 to some number.
             %%% This ensures that the final, corrected image will be in a
             %%% reasonable range, from zero to 1.
+            drawnow
             IlluminationImage = IlluminationImage1 ./ max([min(min(IlluminationImage1)); .00000001]);
                %%% Note: the following "imwrite" saves the illumination
             %%% correction image in TIF format, but the image is compressed
@@ -196,7 +196,7 @@ if handles.setbeinganalyzed == 1
             %%% format.
             % imwrite(IlluminationImage, 'IlluminationImage.tif', 'tif')
             
-            %%% The illumination correction image is saved to the hard
+            %%% Saves the illumination correction image to the hard
             %%% drive if requested.
             if strcmp(upper(IllumCorrectFileName), 'N') == 0
                 try
@@ -208,20 +208,17 @@ if handles.setbeinganalyzed == 1
             error(['An error occurred in the Correct Illumination module. Matlab says the problem is: ', ErrorMessage, ErrorMessage2])
         end
     end    
-    %%% Store the mean image and the Illumination image to the handles
-    %%% structure. I have given it a specific name to ensure that the
-    %%% user hasn't chosen this exact name for something else.
+    %%% Stores the mean image and the Illumination image to the handles
+    %%% structure.
     if exist('MeanImage') == 1
         fieldname = ['dOTMeanImageAD', ImageName];
         handles.(fieldname) = MeanImage;        
     end
     fieldname = ['dOTIllumImageAD', ImageName];
     handles.(fieldname) = IlluminationImage;
-    %%% Update the handles structure.
-    %%% Removed for parallel: guidata(gcbo, handles);
 end
 
-%%% The following is run for every image set. Retrieve the mean image
+%%% The following is run for every image set. Retrieves the mean image
 %%% and illumination image from the handles structure.  The mean image is
 %%% retrieved just for display purposes.
 fieldname = ['dOTMeanImageAD', ImageName];
@@ -230,17 +227,9 @@ if isfield(handles, fieldname) == 1
 end
 fieldname = ['dOTIllumImageAD', ImageName];
 IlluminationImage = handles.(fieldname);
-%%% The original image is corrected based on the IlluminationImage,
+%%% Corrects the original image based on the IlluminationImage,
 %%% by dividing each pixel by the value in the IlluminationImage.
 CorrectedImage = OrigImage ./ IlluminationImage;
-
-%%% Checking to see whether the rescaling makes sense:
-% MAX(1) = max(max(OrigImage));
-% MIN(1) = min(min(OrigImage));
-% MAX(2) = max(max(IlluminationImage));
-% MIN(2) = min(min(IlluminationImage));
-% MAX(3) = max(max(CorrectedImage))
-% MIN(3) = min(min(CorrectedImage))
 
 %%% Returns to the original directory.
 cd(CurrentDirectory)
@@ -250,18 +239,16 @@ cd(CurrentDirectory)
 %%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
-%%% Note: Everything between the "if" and "end" is not carried out if the 
-%%% user has closed
-%%% the figure window, so do not do any important calculations here.
-%%% Otherwise an error message will be produced if the user has closed the
-%%% window but you have attempted to access data that was supposed to be
-%%% produced by this part of the code.
-
 %%% Determines the figure number to display in.
 fieldname = ['figurealgorithm',CurrentAlgorithm];
 ThisAlgFigureNumber = handles.(fieldname);
-%%% Check whether that figure is open. This checks all the figure handles
+%%% Checks whether that figure is open. This checks all the figure handles
 %%% for one whose handle is equal to the figure number for this algorithm.
+%%% Note: Everything between the "if" and "end" is not carried out if the 
+%%% user has closed the figure window, so do not do any important
+%%% calculations here. Otherwise an error message will be produced if the
+%%% user has closed the window but you have attempted to access data that
+%%% was supposed to be produced by this part of the code.
 if any(findobj == ThisAlgFigureNumber) == 1;
     %%% The "drawnow" function executes any pending figure window-related
     %%% commands.  In general, Matlab does not update figure windows
@@ -297,113 +284,21 @@ if any(findobj == ThisAlgFigureNumber) == 1;
     subplot(2,2,4); imagesc(IlluminationImage); 
     title('Illumination Function'); colormap(gray)
 end
-%%% Executes pending figure-related commands so that the results are
-%%% displayed.
-drawnow
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% SAVE DATA TO HANDLES STRUCTURE %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
 
-%%% The corrected image is saved to the
+%%% Saves the corrected image to the
 %%% handles structure so it can be used by subsequent algorithms.
 fieldname = ['dOT', CorrectedImageName];
 handles.(fieldname) = CorrectedImage;
-%%% Removed for parallel: guidata(gcbo, handles);
 
-%%% The original file name is saved to the handles structure in a
+%%% Determines the filename of the image to be analyzed.
+fieldname = ['dOTFilename', ImageName];
+FileName = handles.(fieldname)(handles.setbeinganalyzed);
+%%% Saves the original file name to the handles structure in a
 %%% field named after the corrected image name.
 fieldname = ['dOTFilename', CorrectedImageName];
 handles.(fieldname)(handles.setbeinganalyzed) = FileName;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% SAVE PROCESSED IMAGE TO HARD DRIVE %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%% Determine whether the user wanted to save the corrected image
-%%% by comparing their entry "SaveImage" with "N" (after
-%%% converting SaveImage to uppercase).
-if strcmp(upper(SaveImage),'N') ~= 1
-%%% Save the image to the hard drive.    
-imwrite(CorrectedImage, NewImageName, FileFormat);
-min(min(CorrectedImage))
-max(max(CorrectedImage))
-min(min(uint8(CorrectedImage)))
-max(max(uint8(CorrectedImage)))
-
-end
-
-drawnow
-
-%%%%%%%%%%%
-%%% HELP %%%
-%%%%%%%%%%%
-
-%%%%% Help for the Correct Illumination Divide All Mean module: 
-
-%%%%%   This module corrects for uneven illumination of each image, based on
-%%%%% information from a set of images collected at the same time.
-%%%%%   This module works by averaging together all of the images, then
-%%%%% smoothing this image by fitting a low-order polynomial to the
-%%%%% resulting average image and rescaling it.  This produces an image
-%%%%% that represents the variation in illumination across the field of
-%%%%% view.  This process is carried out before the first image set is
-%%%%% processed; subsequent image sets use the already calculated image.
-%%%%% Each image is divided by this illumination image to produce the
-%%%%% corrected image.
-%%%%% .
-%%%%% If you want to run this module only to calculate the mean and
-%%%%% illumination images and not to correct every image in the directory,
-%%%%% simply run the module as usual and use the button on the Timer to
-%%%%% stop processing after the first image set.
-%%%%% . 
-%%%%% DISPLAYING AND SAVING PROCESSED IMAGES 
-%%%%% PRODUCED BY THIS IMAGE ANALYSIS MODULE:
-%%%%% Note: Images saved using the boxes in the main CellProfiler window
-%%%%% will be saved in the default directory specified at the top of the
-%%%%% CellProfiler window.
-%%%%% .
-%%%%% If you want to save other processed images, open the m-file for this 
-%%%%% image analysis module, go to the line in the
-%%%%% m-file where the image is generated, and there should be 2 lines
-%%%%% which have been inactivated.  These are green comment lines that are
-%%%%% indented. To display an image, remove the percent sign before
-%%%%% the line that says "figure, imshow...". This will cause the image to
-%%%%% appear in a fresh display window for every image set. To save an
-%%%%% image to the hard drive, remove the percent sign before the line
-%%%%% that says "imwrite..." and adjust the file type and appendage to the
-%%%%% file name as desired.  When you have finished removing the percent
-%%%%% signs, go to File > Save As and save the m file with a new name.
-%%%%% Then load the new image analysis module into the CellProfiler as
-%%%%% usual.
-%%%%% Please note that not all of these imwrite lines have been checked for
-%%%%% functionality: it may be that you will have to alter the format of
-%%%%% the image before saving.  Try, for example, adding the uint8 command:
-%%%%% uint8(Image) surrounding the image prior to using the imwrite command
-%%%%% if the image is not saved correctly.
-
-
-% The contents of this file are subject to the Mozilla Public License Version 
-% 1.1 (the "License"); you may not use this file except in compliance with 
-% the License. You may obtain a copy of the License at 
-% http://www.mozilla.org/MPL/
-% 
-% Software distributed under the License is distributed on an "AS IS" basis,
-% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-% for the specific language governing rights and limitations under the
-% License.
-% 
-% 
-% The Original Code is the ______________________.
-% 
-% The Initial Developer of the Original Code is
-% Whitehead Institute for Biomedical Research
-% Portions created by the Initial Developer are Copyright (C) 2003,2004
-% the Initial Developer. All Rights Reserved.
-% 
-% Contributor(s):
-%   Anne Carpenter <carpenter@wi.mit.edu>
-%   Thouis Jones   <thouis@csail.mit.edu>
-%   In Han Kang    <inthek@mit.edu>
-%
-% $Revision$

d82cf8cf390d701fd538594f478b080f8bf5562b
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-10-20 18:31:44 +0000

    Polished by Anne
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@189 1fc53939-2000-0410-845c-e8453a809027

--- Modules/ImageTiler.m
+++ Modules/ImageTiler.m
@@ -1,60 +1,84 @@
-function handles = AlgImageTiler5(handles)
+function handles = AlgImageTiler(handles)
 
-%%% Reads the current algorithm number, since this is needed to find 
-%%% the variable values that the user entered.
-CurrentAlgorithm = handles.currentalgorithm;
-CurrentAlgorithmNum = str2num(handles.currentalgorithm);
+% Help for the Image Tiler module:
+% Sorry, this module has not yet been documented.
+
+% The contents of this file are subject to the Mozilla Public License Version 
+% 1.1 (the "License"); you may not use this file except in compliance with 
+% the License. You may obtain a copy of the License at 
+% http://www.mozilla.org/MPL/
+% 
+% Software distributed under the License is distributed on an "AS IS" basis,
+% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+% for the specific language governing rights and limitations under the
+% License.
+% 
+% 
+% The Original Code is the Image Tiler module.
+% 
+% The Initial Developer of the Original Code is
+% Whitehead Institute for Biomedical Research
+% Portions created by the Initial Developer are Copyright (C) 2003,2004
+% the Initial Developer. All Rights Reserved.
+% 
+% Contributor(s):
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
 
 %%%%%%%%%%%%%%%%
 %%% VARIABLES %%%
 %%%%%%%%%%%%%%%%
 drawnow
 
+%%% Reads the current algorithm number, since this is needed to find 
+%%% the variable values that the user entered.
+CurrentAlgorithm = handles.currentalgorithm;
+CurrentAlgorithmNum = str2num(handles.currentalgorithm);
+
 %textVAR01 = What did you call the images to be tiled?
 %defaultVAR01 = OrigBlue
 ImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,1});
+
 %textVAR02 = What do you want to call the tiled image?
 %defaultVAR02 = TiledImage
 TiledImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,2});
+
 %textVAR03 = Number of rows to display (leave "A" to calculate automatically)
 %defaultVAR03 = A
 NumberRows = char(handles.Settings.Vvariable{CurrentAlgorithmNum,3});
+
 %textVAR04 = Number of columns to display (leave "A" to calculate automatically)
 %defaultVAR04 = A
 NumberColumns = char(handles.Settings.Vvariable{CurrentAlgorithmNum,4});
+
 %textVAR05 = Are the first two images arranged in a row or a column?
 %defaultVAR05 = C
 RowOrColumn = char(handles.Settings.Vvariable{CurrentAlgorithmNum,5});
+
 %textVAR06 = Is the first image at the bottom or the top?
 %defaultVAR06 = T
 TopOrBottom = char(handles.Settings.Vvariable{CurrentAlgorithmNum,6});
+
 %textVAR07 = Is the first image at the left or the right?
 %defaultVAR07 = L
 LeftOrRight = char(handles.Settings.Vvariable{CurrentAlgorithmNum,7});
+
 %textVAR08 = What fraction should the images be sized (the resolution will be changed)?
 %defaultVAR08 = .1
 SizeChange = char(handles.Settings.Vvariable{CurrentAlgorithmNum,8});
-%textVAR09 = To save the resulting tiled image, enter a filename (no extension) 
-%defaultVAR09 = N
-SaveImage = char(handles.Settings.Vvariable{CurrentAlgorithmNum,9});
-%textVAR10 =  Otherwise, leave as "N". To save or display other images, press Help button
-%textVAR11 = In what file format do you want to save the image? Do not include a period
-%defaultVAR11 = tif
-FileFormat = char(handles.Settings.Vvariable{CurrentAlgorithmNum,11});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
-%%% Checks whether the file format the user entered is readable by Matlab.
-IsFormat = imformats(FileFormat);
-if isempty(IsFormat) == 1
-    error('The image file type entered in the Image Tiler module is not recognized by Matlab. Or, you may have entered a period in the box. For a list of recognizable image file formats, type "imformats" (no quotes) at the command line in Matlab.','Error')
-end
-%%% Read (open) the image you want to analyze and assign it to a variable,
+
+%%% Reads (opens) the image you want to analyze and assigns it to a variable,
 %%% "OrigImage".
 fieldname = ['dOT', ImageName];
-%%% Check whether the image to be analyzed exists in the handles structure.
+%%% Checks whether the image to be analyzed exists in the handles structure.
 if isfield(handles, fieldname) == 0
     %%% If the image is not there, an error message is produced.  The error
     %%% is not displayed: The error function halts the current function and
@@ -65,53 +89,31 @@ if isfield(handles, fieldname) == 0
     error(['Image processing was canceled because the Image Tiler module could not find the input image.  It was supposed to be named ', ImageName, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
 end
 
-%%% If the resulting tiled image is to be saved, check whether the file
-%%% name of images will result in overwriting a file, or in a file name
-%%% that contains spaces.
-if strcmp(upper(SaveImage),'N') ~= 1
-    A = isspace(SaveImage);
-    if any(A) == 1
-        error('Image processing was canceled because you have entered one or more spaces for the tiled image file name in the Image Tiler module.  If you do not want to save the object outlines image to the hard drive, type "N" into the appropriate box.')
-        return
-    end
-    %%% Checks whether the specified file name will overwrite an
-    %%% existing file. 
-    OutputFileOverwrite = exist([cd,'/',SaveImage]);
-    if OutputFileOverwrite ~= 0
-        Answer = questdlg(['A file with the name ', SaveImage, ' already exists. Do you want to overwrite it?'],'Confirm file overwrite','Yes','No','No');
-        if strcmp(Answer,'No') == 1;
-            error('Image processing was canceled.')
-            return
-        end
-    end
-end
-
 %%%%%%%%%%%%%%%%%%%%%
 %%% IMAGE ANALYSIS %%%
 %%%%%%%%%%%%%%%%%%%%%
+drawnow
 
 %%% The first time the module is run, the tiling is performed.
 if handles.setbeinganalyzed == 1
-    %%% Make note of the current directory so the module can return to it
+    %%% Makes note of the current directory so the module can return to it
     %%% at the end of this module.
     CurrentDirectory = cd;
-    
     %%% Checks whether any sample info has been loaded.
     if isfield(handles, 'headings') == 1
         %%% Retrieves the Sample Info (only the first column is used).
         SampleInfo = handles.headings(1);
     else SampleInfo = [];
     end
-    
-    %%% Retrieve the path where the images are stored from the handles
+    %%% Retrieves the path where the images are stored from the handles
     %%% structure.
     fieldname = ['dOTPathName', ImageName];
     try PathName = handles.(fieldname);
     catch error('Image processing was canceled because the Image Tiler module must be run using images straight from a load images module (i.e. the images cannot have been altered by other image processing modules). This is because the Image Tiler module calculates an illumination correction image based on all of the images before correcting each individual image as CellProfiler cycles through them. One solution is to process the entire batch of images using the image analysis modules preceding this module and save the resulting images to the hard drive, then start a new stage of processing from this Image Tiler module onward.')
     end
-    %%% Change to that directory.
+    %%% Changes to that directory.
     cd(PathName)
-    %%% Retrieve the list of filenames where the images are stored from the
+    %%% Retrieves the list of filenames where the images are stored from the
     %%% handles structure.
     fieldname = ['dOTFileList', ImageName];
     FileList = handles.(fieldname);
@@ -134,7 +136,7 @@ if handles.setbeinganalyzed == 1
     elseif strcmp(upper(NumberRows),'A') == 1
         NumberColumns = str2num(NumberColumns);
         NumberRows = ceil((NumberOfImages)/NumberColumns);
-    elseif strcmp(upper(NumberColumns),'A') == 1 
+    elseif strcmp(upper(NumberColumns),'A') == 1
         NumberRows = str2num(NumberRows);
         NumberColumns = ceil((NumberOfImages)/NumberRows);
     else NumberColumns = str2num(NumberColumns);
@@ -145,7 +147,7 @@ if handles.setbeinganalyzed == 1
         if strcmp(Answer,'No') == 1
             %%% This line will "cancel" processing after the first time through this
             %%% module.  Without the following cancel line, the module will run X
-            %%% times, where X is the number of files in the current directory.  
+            %%% times, where X is the number of files in the current directory.
             set(handles.timertexthandle,'string','Cancel')
             return
         end
@@ -156,7 +158,7 @@ if handles.setbeinganalyzed == 1
         if strcmp(Answer,'No') == 1
             %%% This line will "cancel" processing after the first time through this
             %%% module.  Without the following cancel line, the module will run X
-            %%% times, where X is the number of files in the current directory.  
+            %%% times, where X is the number of files in the current directory.
             set(handles.timertexthandle,'string','Cancel')
             return
         end
@@ -165,7 +167,6 @@ if handles.setbeinganalyzed == 1
             SampleInfo(length(FileList)+1:NumberRows*NumberColumns) = {'none'};
         end
     end
-    
     if strcmp(upper(RowOrColumn),'R') == 1
         NewFileList = reshape(FileList, NumberColumns, NumberRows);
         NewFileList = NewFileList';
@@ -176,7 +177,7 @@ if handles.setbeinganalyzed == 1
     elseif strcmp(upper(RowOrColumn),'C') == 1
         NewFileList = reshape(FileList, NumberRows, NumberColumns);
         if isempty(SampleInfo) ~= 1
-            
+
             NewSampleInfo = reshape(SampleInfo, NumberRows, NumberColumns);
         end
     else error('You must enter "R" or "C" to select whether the first two images are in a row or a column relative to each other')
@@ -196,7 +197,7 @@ if handles.setbeinganalyzed == 1
     ImageWidth = ImageSize(2);
     TotalWidth = NumberColumns*ImageWidth;
     TotalHeight = NumberRows*ImageHeight;
-    TiledImage(TotalHeight,TotalWidth) = 0;  
+    TiledImage(TotalHeight,TotalWidth) = 0;
 
     for i = 1:NumberRows,
         for j = 1:NumberColumns,
@@ -218,7 +219,7 @@ if handles.setbeinganalyzed == 1
                 end
             end
             TiledImage((ImageHeight*(i-1))+(1:ImageHeight),(ImageWidth*(j-1))+(1:ImageWidth)) = CurrentImage;
-            
+
             ImageNumber = (i-1)*NumberColumns + j;
             ImagesRemaining = NumberOfImages - ImageNumber;
             waitbar(ImageNumber/NumberOfImages, WaitbarHandle)
@@ -232,12 +233,12 @@ if handles.setbeinganalyzed == 1
                 error('Image tiling was canceled')
                 return
             end
+            drawnow
         end
     end
     if strcmp(LeftOrRight,'R') == 1
         NewFileList = fliplr(NewFileList);
         if isempty(SampleInfo) ~= 1
-            
             NewSampleInfo = fliplr(NewSampleInfo);
         end
         TiledImage = fliplr(TiledImage);
@@ -249,33 +250,30 @@ if handles.setbeinganalyzed == 1
         end
         TiledImage = flipud(TiledImage);
     end
-    
     delete(WaitbarHandle)
     %%% This line will "cancel" processing after the first time through this
     %%% module.  Without the following cancel line, the module will run X
-    %%% times, where X is the number of files in the current directory.  
+    %%% times, where X is the number of files in the current directory.
     set(handles.timertexthandle,'string','Cancel')
-    
     %%% Returns to the original directory.
     cd(CurrentDirectory)
 end
+
 %%%%%%%%%%%%%%%%%%%%%%
 %%% DISPLAY RESULTS %%%
 %%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
-%%% Note: Everything between the "if" and "end" is not carried out if the 
-%%% user has closed
-%%% the figure window, so do not do any important calculations here.
-%%% Otherwise an error message will be produced if the user has closed the
-%%% window but you have attempted to access data that was supposed to be
-%%% produced by this part of the code.
-
 %%% Determines the figure number to display in.
 fieldname = ['figurealgorithm',CurrentAlgorithm];
 ThisAlgFigureNumber = handles.(fieldname);
-%%% Check whether that figure is open. This checks all the figure handles
+%%% Checks whether that figure is open. This checks all the figure handles
 %%% for one whose handle is equal to the figure number for this algorithm.
+%%% Note: Everything between the "if" and "end" is not carried out if the
+%%% user has closed the figure window, so do not do any important
+%%% calculations here. Otherwise an error message will be produced if the
+%%% user has closed the window but you have attempted to access data that
+%%% was supposed to be produced by this part of the code.
 if any(findobj == ThisAlgFigureNumber) == 1;
     %%% The "drawnow" function executes any pending figure window-related
     %%% commands.  In general, Matlab does not update figure windows
@@ -313,6 +311,7 @@ if any(findobj == ThisAlgFigureNumber) == 1;
     ChangeGridButton_handle = uicontrol('Style', 'pushbutton', ...
         'String', 'Change', 'Position', [110 6 45 20], ...
         'Callback', ChangeGridButtonFunction, 'parent',ThisAlgFigureNumber);
+    
     ShowFileNamesButtonFunction = 'Handles = findobj(''UserData'',''FileNameTextHandles''); set(Handles,''visible'',''on''); clear Handles';
     ShowFileNamesButton_handle = uicontrol('Style', 'pushbutton', ...
         'String', 'Show', 'Position', [170 6 45 20], ...
@@ -339,7 +338,6 @@ if any(findobj == ThisAlgFigureNumber) == 1;
         'String', 'Change', 'Position', [430 6 45 20], ...
         'Callback', ChangeSampleInfoButtonFunction, 'parent',ThisAlgFigureNumber);
 
-    
     ChangeColormapButtonFunction = 'ImageHandle = findobj(gca, ''type'',''image''); propedit(ImageHandle)';
     ChangeColormapButton_handle = uicontrol('Style', 'pushbutton', ...
         'String', 'Change', 'Position', [490 6 45 20], ...
@@ -445,88 +443,13 @@ if any(findobj == ThisAlgFigureNumber) == 1;
     end
     set(ThisAlgFigureNumber,'toolbar','figure')
 end
-%%% Executes pending figure-related commands so that the results are
-%%% displayed.
-drawnow
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% SAVE DATA TO HANDLES STRUCTURE %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%% The tiled image is saved to the
-%%% handles structure so it can be used by subsequent algorithms.
-fieldname = ['dOT', TiledImageName];
-handles.(fieldname) = TiledImage;
-%%% Removed for parallel: guidata(gcbo, handles);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% SAVE PROCESSED IMAGE TO HARD DRIVE %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%% Determine whether the user wanted to save the corrected image
-%%% by comparing their entry "SaveImage" with "N" (after
-%%% converting SaveImage to uppercase).
-if strcmp(upper(SaveImage),'N') ~= 1
-%%% Save the image to the hard drive.    
-imwrite(TiledImage, [SaveImage,'.',FileFormat], FileFormat);
-end
-
 drawnow
 
-%%%%%%%%%%%
-%%% HELP %%%
-%%%%%%%%%%%
-
-%%%%% Help for the Image Tiler module: 
-
-%%%%% . 
-%%%%% DISPLAYING AND SAVING PROCESSED IMAGES 
-%%%%% PRODUCED BY THIS IMAGE ANALYSIS MODULE:
-%%%%% Note: Images saved using the boxes in the main CellProfiler window
-%%%%% will be saved in the default directory specified at the top of the
-%%%%% CellProfiler window.
-%%%%% .
-%%%%% If you want to save other processed images, open the m-file for this 
-%%%%% image analysis module, go to the line in the
-%%%%% m-file where the image is generated, and there should be 2 lines
-%%%%% which have been inactivated.  These are green comment lines that are
-%%%%% indented. To display an image, remove the percent sign before
-%%%%% the line that says "figure, imshow...". This will cause the image to
-%%%%% appear in a fresh display window for every image set. To save an
-%%%%% image to the hard drive, remove the percent sign before the line
-%%%%% that says "imwrite..." and adjust the file type and appendage to the
-%%%%% file name as desired.  When you have finished removing the percent
-%%%%% signs, go to File > Save As and save the m file with a new name.
-%%%%% Then load the new image analysis module into the CellProfiler as
-%%%%% usual.
-%%%%% Please note that not all of these imwrite lines have been checked for
-%%%%% functionality: it may be that you will have to alter the format of
-%%%%% the image before saving.  Try, for example, adding the uint8 command:
-%%%%% uint8(Image) surrounding the image prior to using the imwrite command
-%%%%% if the image is not saved correctly.
-
-
-% The contents of this file are subject to the Mozilla Public License Version 
-% 1.1 (the "License"); you may not use this file except in compliance with 
-% the License. You may obtain a copy of the License at 
-% http://www.mozilla.org/MPL/
-% 
-% Software distributed under the License is distributed on an "AS IS" basis,
-% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-% for the specific language governing rights and limitations under the
-% License.
-% 
-% 
-% The Original Code is the ______________________.
-% 
-% The Initial Developer of the Original Code is
-% Whitehead Institute for Biomedical Research
-% Portions created by the Initial Developer are Copyright (C) 2003,2004
-% the Initial Developer. All Rights Reserved.
-% 
-% Contributor(s):
-%   Anne Carpenter <carpenter@wi.mit.edu>
-%   Thouis Jones   <thouis@csail.mit.edu>
-%   In Han Kang    <inthek@mit.edu>
-%
-% $Revision$
+%%% Saves the tiled image to the handles structure so it can be used by
+%%% subsequent algorithms.
+fieldname = ['dOT', TiledImageName];
+handles.(fieldname) = TiledImage;

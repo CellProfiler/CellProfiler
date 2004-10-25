52c86ec2bf2ce940bde512b440303fc3bb0a5193
inthek <inthek@1fc53939-2000-0410-845c-e8453a809027>
2004-10-25 21:59:14 +0000

    Adding grayscale module
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@228 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/RGBToGray.m
@@ -0,0 +1,160 @@
+function handles = AlgRGBToGray(handles)
+
+% The contents of this file are subject to the Mozilla Public License Version 
+% 1.1 (the "License"); you may not use this file except in compliance with 
+% the License. You may obtain a copy of the License at 
+% http://www.mozilla.org/MPL/
+% 
+% Software distributed under the License is distributed on an "AS IS" basis,
+% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+% for the specific language governing rights and limitations under the
+% License.
+% 
+% 
+% The Original Code is the Apply Threshold module.
+% 
+% The Initial Developer of the Original Code is
+% Whitehead Institute for Biomedical Research
+% Portions created by the Initial Developer are Copyright (C) 2003,2004
+% the Initial Developer. All Rights Reserved.
+% 
+% Contributor(s):
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
+
+%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Reads the current algorithm number, since this is needed to find 
+%%% the variable values that the user entered.
+CurrentAlgorithm = handles.currentalgorithm;
+CurrentAlgorithmNum = str2double(handles.currentalgorithm);
+
+%textVAR01 = What did you call the image to be converted to Gray?
+%defaultVAR01 = OrigBlue
+ImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,1});
+
+%textVAR02 = What do you want to call the grayscale image?
+%defaultVAR02 = GrayscaleBlue
+GrayscaleImageName = char(handles.Settings.Vvariable{CurrentAlgorithmNum,2});
+
+%textVAR03 = What do you want the red intensity to be?
+%defaultVAR03 = 0.30
+redIntensity = str2num(char(handles.Settings.Vvariable{CurrentAlgorithmNum,3}));
+
+%textVAR04 = What do you want the green intensity to be?
+%defaultVAR04 = 0.59
+greenIntensity = str2num(char(handles.Settings.Vvariable{CurrentAlgorithmNum,4}));
+
+%textVAR05 = What do you want the blue intensity to be?
+%defaultVAR05 = 0.11
+blueIntensity = str2num(char(handles.Settings.Vvariable{CurrentAlgorithmNum,5}));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Reads (opens) the image you want to analyze and assigns it to a variable,
+%%% "OrigImage".
+fieldname = ['dOT', ImageName];
+%%% Checks whether the image to be analyzed exists in the handles structure.
+if isfield(handles, fieldname) == 0
+    %%% If the image is not there, an error message is produced.  The error
+    %%% is not displayed: The error function halts the current function and
+    %%% returns control to the calling function (the analyze all images
+    %%% button callback.)  That callback recognizes that an error was
+    %%% produced because of its try/catch loop and breaks out of the image
+    %%% analysis loop without attempting further modules.
+    error(['Image processing was canceled because the RGB to Gray module could not find the input image.  It was supposed to be named ', ImageName, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
+end
+%%% Reads the image.
+OrigImage = handles.(fieldname);
+
+%%% Checks that the original image is three-dimensional (i.e. a color
+%%% image)
+if ndims(OrigImage) ~= 3
+    error('Image processing was canceled because the RGBtoGrayscale module requires an input image that is three-dimensional, but the image loaded does not fit this requirement.  This may be because the image is a grayscale image already.')
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%
+%%% IMAGE ANALYSIS%%%
+%%%%%%%%%%%%%%%%%%%%%
+
+%%% Converts Image to Gray
+
+GrayscaleImage = OrigImage(:,:,1)*redIntensity+OrigImage(:,:,2)*greenIntensity+OrigImage(:,:,3)*blueIntensity;
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Determines the figure number to display in.
+fieldname = ['figurealgorithm',CurrentAlgorithm];
+ThisAlgFigureNumber = handles.(fieldname);
+%%% Checks whether that figure is open. This checks all the figure handles
+%%% for one whose handle is equal to the figure number for this algorithm.
+%%% Note: Everything between the "if" and "end" is not carried out if the
+%%% user has closed the figure window, so do not do any important
+%%% calculations here. Otherwise an error message will be produced if the
+%%% user has closed the window but you have attempted to access data that
+%%% was supposed to be produced by this part of the code.
+if any(findobj == ThisAlgFigureNumber) == 1;
+    %%% The "drawnow" function executes any pending figure window-related
+    %%% commands.  In general, Matlab does not update figure windows
+    %%% until breaks between image analysis modules, or when a few select
+    %%% commands are used. "figure" and "drawnow" are two of the commands
+    %%% that allow Matlab to pause and carry out any pending figure window-
+    %%% related commands (like zooming, or pressing timer pause or cancel
+    %%% buttons or pressing a help button.)  If the drawnow command is not
+    %%% used immediately prior to the figure(ThisAlgFigureNumber) line,
+    %%% then immediately after the figure line executes, the other commands
+    %%% that have been waiting are executed in the other windows.  Then,
+    %%% when Matlab returns to this module and goes to the subplot line,
+    %%% the figure which is active is not necessarily the correct one.
+    %%% This results in strange things like the subplots appearing in the
+    %%% timer window or in the wrong figure window, or in help dialog boxes.
+    drawnow
+    %%% Sets the width of the figure window to be appropriate (half width).
+    if handles.setbeinganalyzed == 1
+        originalsize = get(ThisAlgFigureNumber, 'position');
+        newsize = originalsize;
+        newsize(3) = 0.5*originalsize(3);
+        set(ThisAlgFigureNumber, 'position', newsize);
+    end
+    %%% Activates the appropriate figure window.
+    figure(ThisAlgFigureNumber);
+    %%% A subplot of the figure window is set to display the original image.
+    subplot(2,1,1); imagesc(OrigImage);colormap(gray);
+    title(['Input Image, Image Set # ',num2str(handles.setbeinganalyzed)]);
+    %%% A subplot of the figure window is set to display the Inverted
+    %%% Image.
+    subplot(2,1,2); imagesc(GrayscaleImage); title('Grayscale Image');
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% SAVE DATA TO HANDLES STRUCTURE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Saves the Grayscaled image to the
+%%% handles structure so it can be used by subsequent algorithms.
+fieldname = ['dOT', GrayscaleImageName];
+handles.(fieldname) = GrayscaleImage;
+
+
+%%% Determines the filename of the image to be analyzed.
+fieldname = ['dOTFilename', ImageName];
+FileName = handles.(fieldname)(handles.setbeinganalyzed);
+%%% Saves the original file name to the handles structure in a
+%%% field named after the Inverted image name.
+fieldname = ['dOTFilename', GrayscaleImageName];
+handles.(fieldname)(handles.setbeinganalyzed) = FileName;

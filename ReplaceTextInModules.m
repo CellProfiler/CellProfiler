103fd4ac231341efdea9af256ed0c3b25393cbab
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-10-28 23:10:41 +0000

    Updated
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@266 1fc53939-2000-0410-845c-e8453a809027

--- ReplaceTextInModules.m
+++ ReplaceTextInModules.m
@@ -1,70 +1,54 @@
-function ReplaceTextInModules
+function ReplaceTextInModules(Multiple)
 
-[FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the old text')
-TextToRemove = retrievetextfromfile([PathName,FileName])
-
-[FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the new text')
-TextToAddInItsPlace = retrievetextfromfile([PathName,FileName])
-
+[FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the old text');
+TextToRemove = retrievetextfromfile([PathName,FileName]);
+[FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the new text');
+TextToAddInItsPlace = retrievetextfromfile([PathName,FileName]);
+PathName = uigetdir(pwd,'Choose the folder in which you want to search and replace')
+cd(PathName)
 %%% Retrieves every file in the current directory that begins with
 %%% "Alg".
-PathName = pwd;
 FilesAndDirsStructure = dir(PathName);
 FileAndDirNames = sortrows({FilesAndDirsStructure.name}');
 LogicalIsDirectory = [FilesAndDirsStructure.isdir];
 FileNamesNoDir = FileAndDirNames(~LogicalIsDirectory);
-AlgorithmFileNames = FileNamesNoDir(strncmp(FileNamesNoDir,'Alg',3))
+AlgorithmFileNames = FileNamesNoDir(strncmp(FileNamesNoDir,'Alg',3));
 NumberOfAlgorithmFiles = size(AlgorithmFileNames,1)
-
-%%% Determines features of the text being searched.
-FirstLineOfTextToRemove = TextToRemove(1,:)
-NumberOfLinesOfTextToRemove = size(TextToRemove,1)
-
-% %%% Loops through each Algorithm.
-% for i = 1:NumberOfAlgorithmFiles
-%     %%% Opens each file.
-%     fid=fopen(AlgorithmFileNames(i,:));
-%     while 1;
-%         %%% Reads each line.
-%         output = fgetl(fid);
-%         %%% Ignores the line if it is not text.
-%         if ~ischar(output);
-%             break;
-%         end;
-%         %%% Compares the line to desired text.
-%         if strcmp(output,FirstLineOfTextToRemove) == 1;
-%             %%% Extract that line plus the next N lines of text
-%             %%% (DON'T KNOW HOW TO DO THIS)
-%             WholeBlockOfPotentiallyMatchingText = ???;
-%             for i = 1:NumberOfLinesOfTextToRemove
-%                 if strcmp(TextToRemove(i,:), WholeBlockOfMatchingText(i,:))
-%                     MatchingLines(i) = 1;
-%                 end
-%             end
-%             if sum(MatchingLines) == NumberOfLinesOfTextToRemove
-%                 %%% Need to remove the text here and replace it.
-%                 Result = ['Replacement successful for ', AlgorithmFileNames(i,:)]
-%             else
-%             end
-% 
-%             break;
-%         end
-%     end
-%     fclose(fid);
-%     %%% IF A MATCH WAS NEVER FOUND, run the following line.
-%     Result = ['Replacement FAILED for ', AlgorithmFileNames(i,:)]
-% end
-
+%%% Loops through each Algorithm.
+for i = 1:NumberOfAlgorithmFiles
+    %%% Opens each file & reads its contents as a string.
+    OriginalAlgorithmContents = retrievetextfromfile(cell2mat([AlgorithmFileNames(i,:)]));
+    PositionsOfLocatedText = strfind(OriginalAlgorithmContents,TextToRemove);
+    if isempty(PositionsOfLocatedText)==1
+        %%% If a match was not found, run the following line.
+        Result(i,:) = {['NO replacement for ', cell2mat(AlgorithmFileNames(i,:))]};
+    else
+        if strcmpi(Multiple, 'all') == 1
+            LimitToReplace = length(PositionsOfLocatedText);
+        else LimitToReplace = 1;
+        end
+        NewAlgorithmContents = OriginalAlgorithmContents;
+        for j = 1:LimitToReplace
+            Number = LimitToReplace+1-j
+            PositionToReplace = PositionsOfLocatedText(Number);
+            %%% Piece together the file with the beginning part and the
+            %%% ending part and the text to add in the middle.
+            PreReplacementText = NewAlgorithmContents(1:PositionToReplace-1);
+            PostReplacementText = NewAlgorithmContents(PositionToReplace + length(TextToRemove):end);
+            NewAlgorithmContents = [PreReplacementText,TextToAddInItsPlace,PostReplacementText];
+            fid=fopen(cell2mat([AlgorithmFileNames(i,:)]),'w');
+            fwrite(fid,NewAlgorithmContents,'char');
+            fclose(fid);
+        end
+        Result(i,:) = {[num2str(LimitToReplace),' successful replacement(s) for ', cell2mat(AlgorithmFileNames(i,:))]};
+    end
+end
+%%% Prints the results at the command line.
+Result
 
 %%% SUBFUNCTION
-ExtractedText = retrievetextfromfile(PathAndFileName)
+function ExtractedText = retrievetextfromfile(PathAndFileName)
 %%% Opens the file and retrieves the TextToRemove.
 fid=fopen(PathAndFileName);
-while 1;
-    %%% Reads each line. NEED TO DEFINE LINE NUMBER SOMEHOW.
-    ExtractedText(LineNumber) = fgetl(fid);
-    %%% Ignores the line if it is not text.
-    if ~ischar(output);
-        break;
-    end;
-end
+ExtractedText = char(fread(fid,inf,'char')');
+fclose(fid);

2bc92929c33283219bcb681da5a9e1027bad237f
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-10-29 13:56:01 +0000

    Totally functional now
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@270 1fc53939-2000-0410-845c-e8453a809027

--- ReplaceTextInModules.m
+++ ReplaceTextInModules.m
@@ -1,10 +1,19 @@
 function ReplaceTextInModules(Multiple)
 
 [FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the old text');
+if FileName == 0
+    return
+end
 TextToRemove = retrievetextfromfile([PathName,FileName]);
 [FileName,PathName] = uigetfile('*.m', 'Choose the M-file with the new text');
+if FileName == 0
+    return
+end
 TextToAddInItsPlace = retrievetextfromfile([PathName,FileName]);
 PathName = uigetdir(pwd,'Choose the folder in which you want to search and replace')
+if PathName == 0
+    return
+end
 cd(PathName)
 %%% Retrieves every file in the current directory that begins with
 %%% "Alg".
@@ -14,6 +23,13 @@ LogicalIsDirectory = [FilesAndDirsStructure.isdir];
 FileNamesNoDir = FileAndDirNames(~LogicalIsDirectory);
 AlgorithmFileNames = FileNamesNoDir(strncmp(FileNamesNoDir,'Alg',3));
 NumberOfAlgorithmFiles = size(AlgorithmFileNames,1)
+Answer = questdlg('Do you want to replace all instances of the text or just the first?','','All','First','Cancel','All');
+if strcmp(Answer,'All') == 1
+Multiple = 1;
+elseif strcmp(Answer,'First') == 1
+    Multiple = 0;
+else return   
+end
 %%% Loops through each Algorithm.
 for i = 1:NumberOfAlgorithmFiles
     %%% Opens each file & reads its contents as a string.
@@ -23,13 +39,13 @@ for i = 1:NumberOfAlgorithmFiles
         %%% If a match was not found, run the following line.
         Result(i,:) = {['NO replacement for ', cell2mat(AlgorithmFileNames(i,:))]};
     else
-        if strcmpi(Multiple, 'all') == 1
+        if Multiple == 1
             LimitToReplace = length(PositionsOfLocatedText);
         else LimitToReplace = 1;
         end
         NewAlgorithmContents = OriginalAlgorithmContents;
         for j = 1:LimitToReplace
-            Number = LimitToReplace+1-j
+            Number = LimitToReplace+1-j;
             PositionToReplace = PositionsOfLocatedText(Number);
             %%% Piece together the file with the beginning part and the
             %%% ending part and the text to add in the middle.

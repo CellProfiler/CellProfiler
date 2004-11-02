d2f1f115e7e04134c046a4a80b02baf4a6bdbb6b
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2004-11-02 19:36:55 +0000

    Changed license to GPL
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@323 1fc53939-2000-0410-845c-e8453a809027

--- Modules/FileRenamer.m
+++ Modules/FileRenamer.m
@@ -8,30 +8,18 @@ function handles = AlgFileRenamer(handles)
 %
 % See also ALGFILERENUMBER.
 
-% The contents of this file are subject to the Mozilla Public License Version 
-% 1.1 (the "License"); you may not use this file except in compliance with 
-% the License. You may obtain a copy of the License at 
-% http://www.mozilla.org/MPL/
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
 % 
-% Software distributed under the License is distributed on an "AS IS" basis,
-% WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-% for the specific language governing rights and limitations under the
-% License.
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
 % 
-% 
-% The Original Code is the File Renamer module.
-% 
-% The Initial Developer of the Original Code is
-% Whitehead Institute for Biomedical Research
-% Portions created by the Initial Developer are Copyright (C) 2003,2004
-% the Initial Developer. All Rights Reserved.
-% 
-% Contributor(s):
+% Authors:
 %   Anne Carpenter <carpenter@wi.mit.edu>
 %   Thouis Jones   <thouis@csail.mit.edu>
 %   In Han Kang    <inthek@mit.edu>
 %
-% $Revision$
+% $Revision 1.15 $
 
 % PROGRAMMING NOTE
 % HELP:
--- Modules/IdentifySecPropagateSubfunction.cpp
+++ Modules/IdentifySecPropagateSubfunction.cpp
@@ -1,27 +1,15 @@
 /* propagate.c - */
 
-/* The contents of this file are subject to the Mozilla Public License Version 
- * 1.1 (the "License"); you may not use this file except in compliance with 
- * the License. You may obtain a copy of the License at 
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- * 
- * 
- * The Original Code is the Identify Secondary Objects Propagate Subfunction.
- * 
- * The Initial Developer of the Original Code is
- * Whitehead Institute for Biomedical Research
- * Portions created by the Initial Developer are Copyright (C) 2003,2004
- * the Initial Developer. All Rights Reserved.
- * 
- * Contributor(s):
- *   Anne Carpenter <carpenter@wi.mit.edu>
- *   Thouis Jones   <thouis@csail.mit.edu>
- *   In Han Kang    <inthek@mit.edu>
+/* CellProfiler is distributed under the GNU General Public License.
+ * See the accompanying file LICENSE for details.                   
+ *                                                                  
+ * Developed by the Whitehead Institute for Biomedical Research.    
+ * Copyright 2003,2004,2005.                                        
+ *                                                                  
+ * Authors:                                                         
+ *   Anne Carpenter <carpenter@wi.mit.edu>                          
+ *   Thouis Jones   <thouis@csail.mit.edu>                          
+ *   In Han Kang    <inthek@mit.edu>                                
  *
  * $Revision$
  */

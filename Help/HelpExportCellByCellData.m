b94df3383d7fa4079a578ea05ea5efd77bab4fbd
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2004-12-17 16:38:30 +0000

    Removed Tool-specific help files.  The help information is now within each tool's m-file.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@476 1fc53939-2000-0410-845c-e8453a809027

--- Help/HelpExportCellByCellData.m
+++
@@ -1,8 +0,0 @@
-function HelpExportCellByCellData
-
-% Help for EXPORT CELL BY CELL DATA: 
-% This button allows individual cell measurements to be extracted for
-% a particular image and for a particular measurement parameter. The
-% data will be converted to a delimited text file which can be read by
-% most programs.  By naming the file with the extension for Microsoft
-% Excel (.xls), the file is usually easily openable by Excel.
--- Help/HelpExportMeanData.m
+++
@@ -1,9 +0,0 @@
-function HelpExportMeanData
-
-% Help for EXPORT MEAN DATA: 
-% Once image analysis is complete, click this button and select the
-% output file to extract the measurements and other information about
-% the analysis.  The data will be converted to a delimited text file
-% which can be read by most programs.  By naming the file with the
-% extension for Microsoft Excel (.xls), the file is usually easily
-% openable by Excel.

413290e699640a5f911a8f1ec4b91a55df71bace
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2005-02-02 21:03:34 +0000

    Standardized the help and formatting.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@637 1fc53939-2000-0410-845c-e8453a809027

--- Modules/MeasureObjectNeighbors.m
+++ Modules/MeasureObjectNeighbors.m
@@ -175,9 +175,142 @@ for k = 1:max(IncomingLabelMatrixImage(:))
     ImOfNeighbors(sub2ind([sr sc],r,c)) = NrOfNeighbors; 
 end
 
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%% MAKE MEASUREMENTS & SAVE TO HANDLES STRUCTURE %%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    drawnow
+
+% PROGRAMMING NOTE
+% HANDLES STRUCTURE:
+%       In CellProfiler (and Matlab in general), each independent
+% function (module) has its own workspace and is not able to 'see'
+% variables produced by other modules. For data or images to be shared
+% from one module to the next, they must be saved to what is called
+% the 'handles structure'. This is a variable, whose class is
+% 'structure', and whose name is handles. The contents of the handles
+% structure are printed out at the command line of Matlab using the
+% Tech Diagnosis button. The only variables present in the main
+% handles structure are handles to figures and gui elements.
+% Everything else should be saved in one of the following
+% substructures:
+%
+% handles.Settings:
+%       Everything in handles.Settings is stored when the user uses
+% the Save pipeline button, and these data are loaded into
+% CellProfiler when the user uses the Load pipeline button. This
+% substructure contains all necessary information to re-create a
+% pipeline, including which modules were used (including variable
+% revision numbers), their setting (variables), and the pixel size.
+%   Fields currently in handles.Settings: PixelSize, ModuleNames,
+% VariableValues, NumbersOfVariables, VariableRevisionNumbers.
+%
+% handles.Pipeline:
+%       This substructure is deleted at the beginning of the
+% analysis run (see 'Which substructures are deleted prior to an
+% analysis run?' below). handles.Pipeline is for storing data which
+% must be retrieved by other modules. This data can be overwritten as
+% each image set is processed, or it can be generated once and then
+% retrieved during every subsequent image set's processing, or it can
+% be saved for each image set by saving it according to which image
+% set is being analyzed, depending on how it will be used by other
+% modules. Any module which produces or passes on an image needs to
+% also pass along the original filename of the image, named after the
+% new image name, so that if the SaveImages module attempts to save
+% the resulting image, it can be named by appending text to the
+% original file name.
+%   Example fields in handles.Pipeline: FileListOrigBlue,
+% PathnameOrigBlue, FilenameOrigBlue, OrigBlue (which contains the actual image).
+%
+% handles.Current:
+%       This substructure contains information needed for the main
+% CellProfiler window display and for the various modules to
+% function. It does not contain any module-specific data (which is in
+% handles.Pipeline).
+%   Example fields in handles.Current: NumberOfModules,
+% StartupDirectory, DefaultOutputDirectory, DefaultImageDirectory,
+% FilenamesInImageDir, CellProfilerPathname, ImageToolHelp,
+% DataToolHelp, FigureNumberForModule01, NumberOfImageSets,
+% SetBeingAnalyzed, TimeStarted, CurrentModuleNumber.
+%
+% handles.Preferences: 
+%       Everything in handles.Preferences is stored in the file
+% CellProfilerPreferences.mat when the user uses the Set Preferences
+% button. These preferences are loaded upon launching CellProfiler.
+% The PixelSize, DefaultImageDirectory, and DefaultOutputDirectory
+% fields can be changed for the current session by the user using edit
+% boxes in the main CellProfiler window, which changes their values in
+% handles.Current. Therefore, handles.Current is most likely where you
+% should retrieve this information if needed within a module.
+%   Fields currently in handles.Preferences: PixelSize, FontSize,
+% DefaultModuleDirectory, DefaultOutputDirectory,
+% DefaultImageDirectory.
+%
+% handles.Measurements:
+%       Everything in handles.Measurements contains data specific to each
+% image set analyzed for exporting. It is used by the ExportMeanImage
+% and ExportCellByCell data tools. This substructure is deleted at the
+% beginning of the analysis run (see 'Which substructures are deleted
+% prior to an analysis run?' below).
+%    Note that two types of measurements are typically made: Object
+% and Image measurements.  Object measurements have one number for
+% every object in the image (e.g. ObjectArea) and image measurements
+% have one number for the entire image, which could come from one
+% measurement from the entire image (e.g. ImageTotalIntensity), or
+% which could be an aggregate measurement based on individual object
+% measurements (e.g. ImageMeanArea).  Use the appropriate prefix to
+% ensure that your data will be extracted properly. It is likely that
+% Subobject will become a new prefix, when measurements will be
+% collected for objects contained within other objects. 
+%       Saving measurements: The data extraction functions of
+% CellProfiler are designed to deal with only one "column" of data per
+% named measurement field. So, for example, instead of creating a
+% field of XY locations stored in pairs, they should be split into a
+% field of X locations and a field of Y locations. It is wise to
+% include the user's input for 'ObjectName' or 'ImageName' as part of
+% the fieldname in the handles structure so that multiple modules can
+% be run and their data will not overwrite each other.
+%   Example fields in handles.Measurements: ImageCountNuclei,
+% ObjectAreaCytoplasm, FilenameOrigBlue, PathnameOrigBlue,
+% TimeElapsed.
+%
+% Which substructures are deleted prior to an analysis run?
+%       Anything stored in handles.Measurements or handles.Pipeline
+% will be deleted at the beginning of the analysis run, whereas
+% anything stored in handles.Settings, handles.Preferences, and
+% handles.Current will be retained from one analysis to the next. It
+% is important to think about which of these data should be deleted at
+% the end of an analysis run because of the way Matlab saves
+% variables: For example, a user might process 12 image sets of nuclei
+% which results in a set of 12 measurements ("ImageTotalNucArea")
+% stored in handles.Measurements. In addition, a processed image of
+% nuclei from the last image set is left in the handles structure
+% ("SegmNucImg"). Now, if the user uses a different module which
+% happens to have the same measurement output name "ImageTotalNucArea"
+% to analyze 4 image sets, the 4 measurements will overwrite the first
+% 4 measurements of the previous analysis, but the remaining 8
+% measurements will still be present. So, the user will end up with 12
+% measurements from the 4 sets. Another potential problem is that if,
+% in the second analysis run, the user runs only a module which
+% depends on the output "SegmNucImg" but does not run a module that
+% produces an image by that name, the module will run just fine: it
+% will just repeatedly use the processed image of nuclei leftover from
+% the last image set, which was left in handles.Pipeline.
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+% PROGRAMMING NOTE
+% DISPLAYING RESULTS:
+% Some calculations produce images that are used only for display or
+% for saving to the hard drive, and are not used by downstream
+% modules. To speed processing, these calculations are omitted if the
+% figure window is closed and the user does not want to save the
+% images.
 fieldname = ['FigureNumberForModule',CurrentModule];
 ThisModuleFigureNumber = handles.Current.(fieldname);
-if any(findobj == ThisModuleFigureNumber)
+if any(findobj == ThisModuleFigureNumber) == 1
     %%% Sets the width of the figure window to be appropriate (half width).
     if handles.Current.SetBeingAnalyzed == 1
         originalsize = get(ThisModuleFigureNumber, 'position');
@@ -196,3 +329,33 @@ if any(findobj == ThisModuleFigureNumber)
     imagesc(ColoredLabelMatrixImage)
     title('Cells colored according to their original colors')
 end
+
+% PROGRAMMING NOTES THAT ARE UNNECESSARY FOR THIS MODULE:
+% PROGRAMMING NOTE
+% TO TEMPORARILY SHOW IMAGES DURING DEBUGGING:
+% figure, imshow(BlurredImage, []), title('BlurredImage')
+% TO TEMPORARILY SAVE IMAGES DURING DEBUGGING:
+% imwrite(BlurredImage, FileName, FileFormat);
+% Note that you may have to alter the format of the image before
+% saving.  If the image is not saved correctly, for example, try
+% adding the uint8 command:
+% imwrite(uint8(BlurredImage), FileName, FileFormat);
+% To routinely save images produced by this module, see the help in
+% the SaveImages module.
+
+% PROGRAMMING NOTE
+% DRAWNOW BEFORE FIGURE COMMAND:
+% The "drawnow" function executes any pending figure window-related
+% commands.  In general, Matlab does not update figure windows until
+% breaks between image analysis modules, or when a few select commands
+% are used. "figure" and "drawnow" are two of the commands that allow
+% Matlab to pause and carry out any pending figure window- related
+% commands (like zooming, or pressing timer pause or cancel buttons or
+% pressing a help button.)  If the drawnow command is not used
+% immediately prior to the figure(ThisModuleFigureNumber) line, then
+% immediately after the figure line executes, the other commands that
+% have been waiting are executed in the other windows.  Then, when
+% Matlab returns to this module and goes to the subplot line, the
+% figure which is active is not necessarily the correct one. This
+% results in strange things like the subplots appearing in the timer
+% window or in the wrong figure window, or in help dialog boxes.

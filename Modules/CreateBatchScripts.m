66023016fabca94e1534948f8e4e48e182699d2f
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2005-02-04 16:54:03 +0000

    fixed bug in script creation
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@643 1fc53939-2000-0410-845c-e8453a809027

--- Modules/CreateBatchScripts.m
+++ Modules/CreateBatchScripts.m
@@ -222,7 +222,7 @@ for n = 2:BatchSize:handles.Current.NumberOfImageSets,
     fprintf(BatchFile, 'StartImage = %d;\n', StartImage);
     fprintf(BatchFile, 'EndImage = %d;\n', EndImage);
     fprintf(BatchFile, 'tic;\n');
-    fprintf(BatchFile, 'load([BatchRemotePath ''/'' BatchFilePrefix ''data.mat'']);\n');
+    fprintf(BatchFile, 'load([''%s/'' BatchFilePrefix ''data.mat'']);\n', BatchRemotePath);
     fprintf(BatchFile, 'for BatchSetBeingAnalyzed = StartImage:EndImage,\n');
     fprintf(BatchFile, '    disp(sprintf(''Analyzing set %%d'', BatchSetBeingAnalyzed));\n');
     fprintf(BatchFile, '    toc;\n');

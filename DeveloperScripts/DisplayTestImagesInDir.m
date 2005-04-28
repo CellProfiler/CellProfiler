4e7b1d1eca47b75e107c3122bd34c6860ba59250
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-04-28 22:06:49 +0000

    minor changes
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@944 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/DisplayTestImagesInDir.m
+++ DeveloperScripts/DisplayTestImagesInDir.m
@@ -1,9 +1,22 @@
-Filelist = dir('./*.mat');
+Filelist = dir('./*_OUT.mat');
 
 figure
 
 for i = 1:length(Filelist)
     Filename = Filelist(i).name;
-    Image = load(Filename); %%% NEED TO FIX VARIABLES!!!!!!!!!!!!
-    subplot(2,7,i), imagesc(Image.Image), colormap(gray), title(Filename), axis image
+    load(Filename);
+    subplot(7,5,5*(i-1)+1), imagesc(handles.Pipeline.ProjectedOrigBlue), colormap(gray), axis image, colorbar
+    if i == 1, title({' ProjectedOrigBlue'}), end    
+    subplot(7,5,5*(i-1)+2), imagesc(handles.Pipeline.SmoothProjectedOrigBlue), colormap(gray),  axis image, colorbar
+    if i == 1,     title({' SmoothProjectedOrigBlue'}), end
+    subplot(7,5,5*(i-1)+3), imagesc(handles.Pipeline.RawProjectedThreshBlue), colormap(gray),  axis image, colorbar
+    if i == 1,     title({' RawProjectedThreshBlue'}), end
+    subplot(7,5,5*(i-1)+4), imagesc(handles.Pipeline.ProjectedThreshBlue), colormap(gray),  axis image, colorbar
+    if i == 1,     title({' ProjectedThreshBlue'}),end
+    subplot(7,5,5*(i-1)+5), imagesc(handles.Pipeline.SmoothProjectedThreshBlue), colormap(gray),  axis image, colorbar
+    if i == 1,     title({' SmoothProjectedThreshBlue'}), end
 end
+
+%         if exist('Image','var')
+%         subplot(2,7,i), imagesc(Image), colormap(gray), title(Filename), axis image
+%     else

a64c810243949193e2b055023f5abe7aebb21c1f
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-05-18 18:53:06 +0000

    DIB check moved to CPimread from Loadimage modules
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@999 1fc53939-2000-0410-845c-e8453a809027

--- CPimread.m
+++ CPimread.m
@@ -4,29 +4,41 @@ if nargin == 2,
     CurrentFileName = varargin{1};
     handles = varargin{2};
     %%% Handles a non-Matlab readable file format.
-    if isfield(handles.Pipeline, 'DIBwidth') == 1
+    [Pathname, FileName, ext] = fileparts(char(CurrentFileName));
+    if strcmp('.DIB', upper(ext)),
+        while 1
         %%% Opens this non-Matlab readable file format.
-        Width = handles.Pipeline.DIBwidth;
-        Height = handles.Pipeline.DIBheight;
-        Channels = handles.Pipeline.DIBchannels;
-        BitDepth = handles.Pipeline.DIBbitdepth;
-        fid = fopen(char(CurrentFileName), 'r');
-        if (fid == -1),
-            error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
-        end
-        fread(fid, 52, 'uchar');
-        LoadedImage = zeros(Height,Width,Channels);
-        for c=1:Channels,
-            [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
-            if Count < (Width * Height),
+            try
+                Width = handles.Pipeline.DIBwidth;
+                Height = handles.Pipeline.DIBheight;
+                Channels = handles.Pipeline.DIBchannels;
+                BitDepth = handles.Pipeline.DIBbitdepth;
+                fid = fopen(char(CurrentFileName), 'r');
+                if (fid == -1),
+                    error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
+                end
+                fread(fid, 52, 'uchar');
+                LoadedImage = zeros(Height,Width,Channels);
+                for c=1:Channels,
+                    [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
+                    if Count < (Width * Height),
+                        fclose(fid);
+                        error(['End-of-file encountered while reading ', char(CurrentFileName), '. Have you entered the proper size and number of channels for these images?']);
+                    end
+                    LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
+                end
                 fclose(fid);
-                error(['End-of-file encountered while reading ', char(CurrentFileName), '. Have you entered the proper size and number of channels for these images?']);
+                break
+            catch
+                Answers = inputdlg({'Enter the width of the images in pixels','Enter the height of the images in pixels','Enter the bit depth of the camera','Enter the number of channels'},'Enter DIB file information',1,{'512','512','12','1'});
+                handles.Pipeline.DIBwidth = str2double(Answers{1});
+                handles.Pipeline.DIBheight = str2double(Answers{2});
+                handles.Pipeline.DIBbitdepth = str2double(Answers{3});
+                handles.Pipeline.DIBchannels = str2double(Answers{4});
             end
-            LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
         end
-        fclose(fid);
     else
-        %%% Opens Matlab-readable file formats.
+        %%% Opens Matlab-readable file formats. 
         try
             %%% Read (open) the image you want to analyze and assign it to a variable,
             %%% "LoadedImage".

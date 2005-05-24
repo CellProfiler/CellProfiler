1096d5bbccf7d732d3f3e445718baa38ba35222d
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-05-24 18:00:07 +0000

    Allowed for cancelling of SaveImageAs
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1027 1fc53939-2000-0410-845c-e8453a809027

--- ImageTools/SaveImageAs.m
+++ ImageTools/SaveImageAs.m
@@ -8,7 +8,7 @@ function handles = SaveImageAs(handles)
 % command window to see a list of acceptable file formats. To
 % routinely save images that result from running an image analysis
 % pipeline, use the SaveImages module.
-%
+% 
 % See also <nothing relevant>.
 
 % CellProfiler is distributed under the GNU General Public License.
@@ -24,32 +24,37 @@ function handles = SaveImageAs(handles)
 %
 % $Revision$
 
-MsgboxHandle = CPmsgbox('Click twice on the image you wish to save. This window will be closed automatically - do not close it or click OK.');
-%%% TODO: Should allow canceling.
-waitforbuttonpress
-ClickedImage = getimage(gca);
-delete(MsgboxHandle)
-Answers = inputdlg({'Enter file name (no extension)','Enter image file format (e.g. tif,jpg)','If compatible with that file format, save as 16-bit image?'},'Save Image As',1,{'A','tif','no'});
-if isempty(Answers) ~= 1
-    FileName = char(Answers{1});
-    Extension = char(Answers{2});
-    SixteenBit = char(Answers{3});
-    if strcmp(SixteenBit,'yes') == 1
-        ClickedImage = uint16(65535*ClickedImage);
-    end
-    CompleteFileName = [FileName,'.',Extension];
-    %%% Checks whether the specified file name will overwrite an
-    %%% existing file.
-    ProposedFileAndPathname = [handles.Current.DefaultOutputDirectory,'/',CompleteFileName];%%% TODO: Fix filename construction.
-    OutputFileOverwrite = exist(ProposedFileAndPathname,'file');
-    if OutputFileOverwrite ~= 0
-        Answer = CPquestdlg(['A file with the name ', CompleteFileName, ' already exists at ', handles.Current.DefaultOutputDirectory,'. Do you want to overwrite it?'],'Confirm file overwrite','Yes','No','No');
-        if strcmp(Answer,'Yes') == 1;
+MsgboxHandle = CPmsgbox('Click twice on the image you wish to save or hit anything on the keyboard to cancel. This window will be closed automatically.');
+keyPress=waitforbuttonpress;
+if keyPress == 0
+    ClickedImage = getimage(gca);
+    delete(MsgboxHandle)
+    Answers = inputdlg({'Enter file name (no extension)','Enter image file format (e.g. tif,jpg)','If compatible with that file format, save as 16-bit image?'},'Save Image As',1,{'A','tif','no'});
+    if isempty(Answers) ~= 1
+        FileName = char(Answers{1});
+        Extension = char(Answers{2});
+        SixteenBit = char(Answers{3});
+        if strcmp(SixteenBit,'yes') == 1
+            ClickedImage = uint16(65535*ClickedImage);
+        end
+        CompleteFileName = [FileName,'.',Extension];
+        %%% Checks whether the specified file name will overwrite an
+        %%% existing file.
+        ProposedFileAndPathname = [handles.Current.DefaultOutputDirectory,'/',CompleteFileName];%%% TODO: Fix filename construction.
+        OutputFileOverwrite = exist(ProposedFileAndPathname,'file');
+        if OutputFileOverwrite ~= 0
+            Answer = CPquestdlg(['A file with the name ', CompleteFileName, ' already exists at ', handles.Current.DefaultOutputDirectory,'. Do you want to overwrite it?'],'Confirm file overwrite','Yes','No','No');
+            if strcmp(Answer,'Yes') == 1;
+                imwrite(ClickedImage, ProposedFileAndPathname, Extension)
+                CPmsgbox(['The file ', CompleteFileName, ' has been saved to the default output directory.']);
+            end
+        else
             imwrite(ClickedImage, ProposedFileAndPathname, Extension)
             CPmsgbox(['The file ', CompleteFileName, ' has been saved to the default output directory.']);
         end
     else
-        imwrite(ClickedImage, ProposedFileAndPathname, Extension)
-        CPmsgbox(['The file ', CompleteFileName, ' has been saved to the default output directory.']);
+        try %MsgboxHandle might already be deleted if button pressed is esc
+            delete(MsgboxHandle);
+        end
     end
 end

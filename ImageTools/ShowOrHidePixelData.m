5cefcc5ba2b4859752701ea45d4f412e0b67e7be
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-06-02 21:24:08 +0000

    Pixel data toggles rather than just goes on
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1074 1fc53939-2000-0410-845c-e8453a809027

---
+++ ImageTools/ShowOrHidePixelData.m
@@ -0,0 +1,24 @@
+function ShowOrHidePixelData(handles)
+
+% Help for the Show Pixel Data tool:
+% Category: Image Tools
+%
+% Use this tool if you have an image already displayed in a figure
+% window and would like to determine the X, Y position or the
+% intensity at a particular pixel.
+%
+% See also SHOWIMAGE, SHOWDATAONIMAGE.
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
+pixval

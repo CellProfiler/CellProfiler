d240f04ece0242f17c1ec3a784cce4065e9f0667
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-06-03 15:37:13 +0000

    Fixed some bugs and allowed for cancelling. (Colin)
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1084 1fc53939-2000-0410-845c-e8453a809027

--- CPimread.m
+++ CPimread.m
@@ -6,44 +6,50 @@ if nargin == 2,
     %%% Handles a non-Matlab readable file format.
     [Pathname, FileName, ext] = fileparts(char(CurrentFileName));
     if strcmp('.DIB', upper(ext)),
-        while 1
-        %%% Opens this non-Matlab readable file format.
+    %%% Opens this non-Matlab readable file format.
+        try
+            Width = handles.Pipeline.DIBwidth;
+            Height = handles.Pipeline.DIBheight;
+            Channels = handles.Pipeline.DIBchannels;
+            BitDepth = handles.Pipeline.DIBbitdepth;
+        catch
+            Answers = inputdlg({'Enter the width of the images in pixels','Enter the height of the images in pixels','Enter the bit depth of the camera','Enter the number of channels'},'Enter DIB file information',1,{'512','512','12','1'});
             try
-                Width = handles.Pipeline.DIBwidth;
-                Height = handles.Pipeline.DIBheight;
-                Channels = handles.Pipeline.DIBchannels;
-                BitDepth = handles.Pipeline.DIBbitdepth;
-                fid = fopen(char(CurrentFileName), 'r');
-                if (fid == -1),
-                    error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
-                end
-                fread(fid, 52, 'uchar');
-                LoadedImage = zeros(Height,Width,Channels);
-                for c=1:Channels,
-                    [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
-                    if Count < (Width * Height),
-                        fclose(fid);
-                        error(['End-of-file encountered while reading ', char(CurrentFileName), '. Have you entered the proper size and number of channels for these images?']);
-                    end
-                    LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
-                end
-                fclose(fid);
-                break
-            catch
-                Answers = inputdlg({'Enter the width of the images in pixels','Enter the height of the images in pixels','Enter the bit depth of the camera','Enter the number of channels'},'Enter DIB file information',1,{'512','512','12','1'});
                 handles.Pipeline.DIBwidth = str2double(Answers{1});
+                Width=handles.Pipeline.DIBwidth;
                 handles.Pipeline.DIBheight = str2double(Answers{2});
+                Height=handles.Pipeline.DIBheight;
                 handles.Pipeline.DIBbitdepth = str2double(Answers{3});
+                BitDepth=handles.Pipeline.DIBbitdepth;
                 handles.Pipeline.DIBchannels = str2double(Answers{4});
+                Channels=handles.Pipeline.DIBchannels;
+            catch
+                return %If the user cancels or closes the window.
             end
         end
-    else
-        %%% Opens Matlab-readable file formats. 
+        fid = fopen(char(CurrentFileName), 'r');
+        if (fid == -1),
+            error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
+        end
+        fread(fid, 52, 'uchar');
+        LoadedImage = zeros(Height,Width,Channels);
+        for c=1:Channels,
+            [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
+            if Count < (Width * Height),
+                fclose(fid);
+                error(['End-of-file encountered while reading ', char(CurrentFileName), '. Have you entered the proper size and number of channels for these images?']);
+            end
+            LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
+        end
+        fclose(fid);
+    else 
         try
             %%% Read (open) the image you want to analyze and assign it to a variable,
             %%% "LoadedImage".
+            %%% Opens Matlab-readable file formats.
             LoadedImage = im2double(imread(char(CurrentFileName)));
-        catch error(['Image processing was canceled because the module could not load the image "', char(CurrentFileName), '" in directory "', pwd,'.  The error message was "', lasterr, '"'])
+        catch
+            error(['Image processing was canceled because the module could not load the image "', char(CurrentFileName), '" in directory "', pwd,'.  The error message was "', lasterr, '"'])
         end
     end
 else
@@ -52,10 +58,14 @@ else
     if strcmp('.DIB', upper(ext)),
         %%% Opens this non-Matlab readable file format.
         Answers = inputdlg({'Enter the width of the images in pixels','Enter the height of the images in pixels','Enter the bit depth of the camera','Enter the number of channels'},'Enter DIB file information',1,{'512','512','12','1'});
-        Width = str2double(Answers{1});
-        Height = str2double(Answers{2});
-        BitDepth = str2double(Answers{3});
-        Channels = str2double(Answers{4});
+        try
+            Width = str2double(Answers{1});
+            Height = str2double(Answers{2});
+            BitDepth = str2double(Answers{3});
+            Channels = str2double(Answers{4});
+        catch
+            return %If the user cancels or closes the window.
+        end
         fid = fopen(char(CurrentFileName), 'r');
         if (fid == -1),
             error(['The file ', FileName, ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
@@ -72,12 +82,13 @@ else
         end
         fclose(fid);
     else
-        %%% Opens Matlab-readable file formats.
         try
             %%% Read (open) the image you want to analyze and assign it to a variable,
             %%% "LoadedImage".
+            %%% Opens Matlab-readable file formats.
             LoadedImage = im2double(imread(char(CurrentFileName)));
-        catch error(['Unable to open the file, perhaps not a valid Image File.']);
+        catch
+            error(['Image processing was canceled because the module could not load the image "', char(CurrentFileName), '" in directory "', pwd,'.  The error message was "', lasterr, '"'])
         end
     end
 end

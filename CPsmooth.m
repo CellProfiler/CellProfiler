adef7910cdc55a09b6190c335d80220779ce1c9a
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2005-06-07 12:35:22 +0000

    The subfunction no longer rescales the image automatically. In the modules which used this CPsmooth function, it is now optional to rescale, using the CPrescale subfunction.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1101 1fc53939-2000-0410-845c-e8453a809027

--- CPsmooth.m
+++ CPsmooth.m
@@ -1,7 +1,10 @@
 function SmoothedImage = CPsmooth(OrigImage,SmoothingMethod)
 
-%%% This subfunction is used for several illumination correction
-%%% modules, including SmoothImageForIllumCorrection.
+%%% This subfunction is used for several modules, including
+%%% SMOOTHIMAGE, MAKEPROJECTION_AVERAGEIMAGES,
+%%% CORRECTILLUMINATION_APPLY,
+%%% CORRECTILLUMINATION_CALCULATEUSINGINTENSITIES,
+%%% CORRECTILLUMINATION_CALCULATEUSINGBACKGROUNDINTENSITIES.
 
 if strcmpi(SmoothingMethod,'N') == 1
 elseif strcmpi(SmoothingMethod,'P') == 1
@@ -11,27 +14,21 @@ elseif strcmpi(SmoothingMethod,'P') == 1
     y2 = y.*y;
     xy = x.*y;
     o = ones(size(OrigImage));
+    drawnow
     Ind = find(OrigImage > 0);
     Coeffs = [x2(Ind) y2(Ind) xy(Ind) x(Ind) y(Ind) o(Ind)] \ double(OrigImage(Ind));
     drawnow
-    SmoothedImage1 = reshape([x2(:) y2(:) xy(:) x(:) y(:) o(:)] * Coeffs, size(OrigImage));
-    %%% The final SmoothedImage is produced by dividing each
-    %%% pixel of the smoothed image by a scalar: the minimum
-    %%% pixel value anywhere in the smoothed image. (If the
-    %%% minimum value is zero, .00000001 is substituted instead.)
-    %%% This rescales the SmoothedImage from 1 to some number.
-    %%% This ensures that the final, corrected image will be in a
-    %%% reasonable range, from zero to 1.
-    drawnow
-    SmoothedImage = SmoothedImage1 ./ max([min(min(SmoothedImage1)); .0001]);
+    SmoothedImage = reshape([x2(:) y2(:) xy(:) x(:) y(:) o(:)] * Coeffs, size(OrigImage));
  else try ArtifactWidth = str2num(SmoothingMethod);
         ArtifactRadius = 0.5*ArtifactWidth;
         StructuringElementLogical = getnhood(strel('disk', ArtifactRadius));
 %         MsgBoxHandle = CPmsgbox('Now calculating the smoothed image, which may take a long time.');
-        SmoothedImage1 = ordfilt2(OrigImage, floor(sum(sum(StructuringElementLogical))/2), StructuringElementLogical, 'symmetric');
-        SmoothedImage = SmoothedImage1 ./ max([min(min(SmoothedImage1)); .0001]);
+        SmoothedImage = ordfilt2(OrigImage, floor(sum(sum(StructuringElementLogical))/2), StructuringElementLogical, 'symmetric');
 %         MsgBox = 'Calculations for smoothing are complete.';
     catch
-        error(['The text you entered for the smoothing method in the Smooth Image For Illum Correction module is unrecognizable for some reason. You must enter a positive, even number or the letter P.  Your entry was ',SmoothingMethod])
+        error(['The text you entered for the smoothing method is not valid for some reason. You must enter N, P, or a positive, even number.  Your entry was ',SmoothingMethod])
     end
 end
+
+
+

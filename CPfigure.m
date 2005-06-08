ff5d64f0929daae6f3b48bf6e8ced9ed50d0874b
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-06-08 15:01:09 +0000

    Fixed
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1112 1fc53939-2000-0410-845c-e8453a809027

--- CPfigure.m
+++ CPfigure.m
@@ -1,9 +1,10 @@
 function FigHandles=CPfigure(varargin);
-
 userData.Application = 'CellProfiler';
-if isfield(varargin,'handles')
-    userData.MyHandles=getfield(varargin,'handles');
-    varargin=rmfield(varargin,'handles');
+try
+    if nargin>0 && isfield(varargin{1},'Settings')
+        userData.MyHandles=varargin{1};
+        varargin=varargin(2:end);
+    end
 end
 FigHandles=figure(varargin{:});
 set(FigHandles,'UserData',userData);

5e84047dcb9da12b402ba3905cde84d9292e2283
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-06-10 14:53:00 +0000

    Accounts for if CPfigure is called in order to refer to an existing figure, rather than creating a new one
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1131 1fc53939-2000-0410-845c-e8453a809027

--- CPfigure.m
+++ CPfigure.m
@@ -3,10 +3,12 @@ userData.Application = 'CellProfiler';
 if nargin>0 && isfield(varargin{1},'ImageToolsPopUpMenu')
     userData.MyHandles=varargin{1};
     FigHandles=figure(varargin{2:end});
-    TempMenu = uimenu('Label','CellProfiler Image Tools');
-    ListOfImageTools=get(userData.MyHandles.ImageToolsPopUpMenu,'String');
-    for j=2:length(ListOfImageTools)
-        uimenu(TempMenu,'Label',char(ListOfImageTools(j)),'Callback',['UserData=get(gcf,''userData'');' char(ListOfImageTools(j)) '(UserData.MyHandles); clear UserData']);
+    if nargin~=2 %if nargin==2 then only referring to an existing figure
+        TempMenu = uimenu('Label','CellProfiler Image Tools');
+        ListOfImageTools=get(userData.MyHandles.ImageToolsPopUpMenu,'String');
+        for j=2:length(ListOfImageTools)
+            uimenu(TempMenu,'Label',char(ListOfImageTools(j)),'Callback',['UserData=get(gcf,''userData'');' char(ListOfImageTools(j)) '(UserData.MyHandles); clear UserData']);
+        end
     end
     set(FigHandles,'UserData',userData);
 else

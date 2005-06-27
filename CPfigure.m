ca5404f0b5ee03301d25906d24b3709801f7a748
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-06-27 14:45:16 +0000

    Interactive Zoom
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1266 1fc53939-2000-0410-845c-e8453a809027

--- CPfigure.m
+++ CPfigure.m
@@ -1,19 +1,30 @@
-function FigHandles=CPfigure(varargin);
+function FigHandle=CPfigure(varargin);
 userData.Application = 'CellProfiler';
 if nargin>0 && isfield(varargin{1},'ImageToolsPopUpMenu')
     userData.MyHandles=varargin{1};
-    FigHandles=figure(varargin{2:end});
+    FigHandle=figure(varargin{2:end});
     if nargin~=2 %if nargin==2 then only referring to an existing figure
         TempMenu = uimenu('Label','CellProfiler Image Tools');
         ListOfImageTools=get(userData.MyHandles.ImageToolsPopUpMenu,'String');
         for j=2:length(ListOfImageTools)
             uimenu(TempMenu,'Label',char(ListOfImageTools(j)),'Callback',['UserData=get(gcf,''userData'');' char(ListOfImageTools(j)) '(UserData.MyHandles); clear UserData ans;']);
         end
+        StdUnit = 'pixels';
+        PointsPerPixel = 72/get(0,'ScreenPixelsPerInch');
+        set(FigHandle, 'Unit',StdUnit)
+        FigurePosition = get(FigHandle, 'Position');
+        ZoomButtonCallback = 'try, CPInteractiveZoom; catch CPmsgbox(''Could not find the file called InteractiveZoomSubfunction.m which should be located in the CellProfiler folder.''); end';
+        uicontrol('Parent',FigHandle, ...
+            'CallBack',ZoomButtonCallback, ...
+            'BackgroundColor',[.7 .7 .9], ...
+            'Position',PointsPerPixel*[FigurePosition(3)-108 5 105 22], ...
+            'String','Interactive Zoom', ...
+            'Style','pushbutton');
     end
-    set(FigHandles,'UserData',userData);
-    set(FigHandles,'Color',[0.7 0.7 0.9]);
+    set(FigHandle,'UserData',userData);
+    set(FigHandle,'Color',[0.7 0.7 0.9]);
 else
-    FigHandles=figure(varargin{:});
-    set(FigHandles,'UserData',userData);
-    set(FigHandles,'Color',[0.7 0.7 0.9]);
+    FigHandle=figure(varargin{:});
+    set(FigHandle,'UserData',userData);
+    set(FigHandle,'Color',[0.7 0.7 0.9]);
 end
--- ImageTools/OpenNewImageFile.m
+++ ImageTools/OpenNewImageFile.m
@@ -39,40 +39,11 @@ cd(TempCD);
 %%% If the user presses "Cancel", the FileName will = 0 and nothing will
 %%% happen.
 if FileName == 0
-else
-%%% REMOVED DUE TO CONFLICTS WITH THE NORMAL ZOOM FUNCTION
-%%% SHOULD CONSIDER ADDING IT BACK.
-%     %%% Acquires basic screen info for making buttons in the
-%     %%% display window.
-%     StdUnit = 'point';
-%     StdColor = get(0,'DefaultUIcontrolBackgroundColor');
-%     PointsPerPixel = 72/get(0,'ScreenPixelsPerInch');
-%%% REMOVED DUE TO CONFLICTS WITH THE NORMAL ZOOM FUNCTION
-    
+else 
     %%% Reads the image.
     Image = CPimread(fullfile(Pathname, FileName));
-    CPfigure(handles);
+    FigureHandle = CPfigure(handles);
     imagesc(Image);
     colormap(gray);
-    pixval;
     title(FileName);
-
-    
-%%% REMOVED DUE TO CONFLICTS WITH THE NORMAL ZOOM FUNCTION
-%%% SHOULD CONSIDER ADDING IT BACK.
-%     %%% The following adds the Interactive Zoom button, which relies
-%     %%% on the InteractiveZoomSubfunction.m being in the CellProfiler
-%     %%% folder.
-%     set(FigureHandle, 'Unit',StdUnit)
-%     FigurePosition = get(FigureHandle, 'Position');
-%     %%% Specifies the function that will be run when the zoom button is
-%     %%% pressed.
-%     ZoomButtonCallback = 'try, InteractiveZoomSubfunction, catch CPmsgbox(''Could not find the file called InteractiveZoomSubfunction.m which should be located in the CellProfiler folder.''), end';
-%     uicontrol('Parent',FigureHandle, ...
-%         'CallBack',ZoomButtonCallback, ...
-%         'BackgroundColor',StdColor, ...
-%         'Position',PointsPerPixel*[FigurePosition(3)-108 5 105 22], ...
-%         'String','Interactive Zoom', ...
-%         'Style','pushbutton');
-%%% REMOVED DUE TO CONFLICTS WITH THE NORMAL ZOOM FUNCTION
 end

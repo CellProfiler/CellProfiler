b516dc76d3b07670e5560f7161f16c1d365c5db7
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2005-07-20 17:41:24 +0000

    updated
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1401 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/OldText.m
+++ DeveloperScripts/OldText.m
@@ -1,8 +1,1 @@
-if ndims(OrigImage) ~= 2
-    s = size(OrigImage);
-    if (length(s) == 3 && s(3) == 3)
-        OrigImage = OrigImage(:,:,1)+OrigImage(:,:,2)+OrigImage(:,:,3);
-    else
-        error('Image processing was canceled because the Identify Primary Intensity module requires an input image that is two-dimensional (i.e. X vs Y), but the image loaded does not fit this requirement.  This may be because the image is a color image.')
-    end
-end
+IllumFnc

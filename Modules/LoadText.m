081c5568400c3414515776036d09fd2b18f1c6d5
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-08-05 20:15:30 +0000

    New module which is used by AddData
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1459 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/LoadText.m
@@ -0,0 +1,147 @@
+function handles = AddTextInfo(handles)
+
+% Help for the Add Text Information:
+% Category: Other
+%
+% Use this tool if you would like text information about each image to
+% be recorded in the output file along with measurements (e.g. Gene
+% names, accession numbers, or sample numbers). 
+% The text information must be specified in a separate text file
+% with the following syntax:
+%
+% IDENTIFIER <identfier> 
+% DESCRIPTION <description>
+% <Text info for image set #1>
+% <Text info for image set #2>
+% <Text info for image set #3>
+%              .
+%              .
+%
+% <identifier> is used as field name when storing the text information in
+% the Matlab structure. It must be one word. <description> is a description
+% of the text information stored in the file. It can be a sentence.
+%
+% For example:
+%
+% IDENTIFIER GeneNames
+% DESCRIPTION Gene names
+% Gene X
+% Gene Y
+% Gene Z
+%
+%
+% See also ADDDATA.
+%
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne Carpenter <carpenter@wi.mit.edu>
+%   Thouis Jones   <thouis@csail.mit.edu>
+%   In Han Kang    <inthek@mit.edu>
+%
+% $Revision$
+
+
+
+%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%
+
+%%% Reads the current module number, because this is needed to find
+%%% the variable values that the user entered.
+CurrentModule = handles.Current.CurrentModuleNumber;
+CurrentModuleNum = str2double(CurrentModule);
+
+%textVAR01 = What is the textfile that you want to add?
+%choiceVAR01 = Browse while running
+TextFileName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+%inputtypeVAR01 = popupmenu custom
+
+%%%VariableRevisionNumber = 1
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% PRELIMINARY ERROR CHECKING & FILE HANDLING %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if handles.Current.SetBeingAnalyzed == handles.Current.NumberOfImageSets
+    %%% Select file with text information to be added
+    if strcmp(TextFileName,'Browse while running')
+        if exist(handles.Current.DefaultOutputDirectory, 'dir')
+            [filename, pathname] = uigetfile(fullfile(handles.Current.DefaultOutputDirectory,'*.*'),'Pick file with text information');
+        else
+            [filename, pathname] = uigetfile('*.*','Pick file with text information');
+        end
+
+        if filename == 0 %User canceled
+            return;
+        end
+    else
+        [pathname filename extension]=fileparts(TextFileName);
+        filename = [filename extension];
+    end
+
+    %%% Parse text file %%%
+    fid = fopen(fullfile(pathname,filename),'r');
+
+    if fid == -1
+        errordlg('Could not open file.  It might not exist or you might not have given its valid path.');
+    end
+
+    % The first 10 characters must equal 'IDENTIFIER'
+    s = fgets(fid,10);
+    if ~strcmp(s,'IDENTIFIER')
+        errordlg('The first line in the text information file must be of the format ''IDENTIFIER <identifiername>''.')
+    end
+    s = fgetl(fid); s = s(2:end);
+    if sum(isspace(s)) > 0
+        errordlg('Entry after IDENTIFIER on the first line contains white spaces.')
+    end
+    FieldName = s;
+
+    % Get description
+    s = fgets(fid,11);
+    if ~strcmp(s,'DESCRIPTION')
+        errordlg('The second line in the text information file must start with DESCRIPTION')
+    end
+    Description = fgetl(fid);
+    Description = Description(2:end);       % Remove space
+
+    % Read following lines into a cell array
+    Text = {};
+    while 1
+        s = fgetl(fid);
+        if ~ischar(s), break, end
+        if ~isempty(s)
+            Text{end+1} = s;
+        end
+    end
+    fclose(fid);
+
+    %%% Check that the number of processed file names equals the number of text entries
+    if handles.Current.NumberOfImageSets ~= length(Text)
+        error(sprintf('The number of processed image sets (%d) does not match the number of entries in text information file (%d)',handles.Current.NumberOfImageSets,length(Text)));
+    end
+
+    %%% Add the data
+    %%% If the entered field doesn't exist  (This is the convenient way of doing it. Takes time for large ouput files??)
+    if ~isfield(handles.Measurements.Image,FieldName)
+        handles.Measurements.Image.([FieldName,'Text']) = {Description};
+        for imageset = 1:length(Text)
+            handles.Measurements.Image.(FieldName){imageset} = Text(imageset);
+        end
+     %%% If the entered field already exists we have to append to this field
+    else
+        handles.Measurements.Image.([FieldName,'Text']) = cat(2,handles.Measurements.Image.([FieldName,'Text']),{Description});
+        for imageset = 1:length(Text)
+            handles.Measurements.Image.(FieldName){imageset} = cat(2,handles.Measurements.Image.(FieldName){imageset},Text(imageset));
+        end
+    end
+end
+
+
+

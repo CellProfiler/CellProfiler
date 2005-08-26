0dd0b46628621ca1277e27fa89475824090b0d0b
cellprofiler <cellprofiler@1fc53939-2000-0410-845c-e8453a809027>
2005-08-26 19:58:14 +0000

    updated
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1638 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPfigure.m
+++ CPsubfunctions/CPfigure.m
@@ -1,19 +1,20 @@
 function FigHandle=CPfigure(varargin);
 userData.Application = 'CellProfiler';
-if nargin>0 && isfield(varargin{1},'ImageToolsPopUpMenu')
+if nargin>0 && isfield(varargin{1},'Pipeline')
     userData.MyHandles=varargin{1};
     FigHandle=figure(varargin{2:end});
     if nargin~=2 %if nargin==2 then only referring to an existing figure
         ZoomButtonCallback = 'try, CPInteractiveZoom; catch CPmsgbox(''Could not find the file called InteractiveZoomSubfunction.m which should be located in the CellProfiler folder.''); end';
         uimenu('Label','Interactive Zoom','Callback',ZoomButtonCallback);
         TempMenu = uimenu('Label','CellProfiler Image Tools');
-        ListOfImageTools=get(userData.MyHandles.ImageToolsPopUpMenu,'String');
+        ListOfImageTools=userData.MyHandles.Current.ImageToolsFilenames;
         for j=2:length(ListOfImageTools)
             uimenu(TempMenu,'Label',char(ListOfImageTools(j)),'Callback',['UserData=get(gcf,''userData'');' char(ListOfImageTools(j)) '(UserData.MyHandles); clear UserData ans;']);
         end
         Font = userData.MyHandles.Current.FontSize;
         set(FigHandle,'Toolbar','figure');
-        ToggleColorR = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorR'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,1);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,1)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,1)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;clear data AllData button ImageHandles;';
+        
+        ToggleColorR = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;if ~isempty(ImageHandles),AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorR'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,1);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,1)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,1)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;end;clear data AllData button ImageHandles;';
         uicontrol('Style', 'checkbox', ...
             'Units','normalized',...
             'Position', [.6 .02 .06 .04], ...
@@ -21,7 +22,7 @@ if nargin>0 && isfield(varargin{1},'ImageToolsPopUpMenu')
             'FontSize',Font,'BackgroundColor',[.7,.7,.9],'min',0,...
             'max',1,'value',1,'tag','ToggleColorR','string','R');
 
-        ToggleColorG = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorG'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,2);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,2)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,2)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;clear data AllData button ImageHandles;';
+        ToggleColorG = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;if ~isempty(ImageHandles),AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorG'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,2);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,2)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,2)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;end;clear data AllData button ImageHandles;';
         uicontrol('Style', 'checkbox', ...
             'Units','normalized',...
             'Position', [.66 .02 .06 .04], ...
@@ -29,7 +30,7 @@ if nargin>0 && isfield(varargin{1},'ImageToolsPopUpMenu')
             'FontSize',Font,'BackgroundColor',[.7,.7,.9],'min',0,...
             'max',1,'value',1,'tag','ToggleColorG','string','G');
 
-        ToggleColorB = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorB'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,3);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,3)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,3)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;clear data AllData button ImageHandles;';
+        ToggleColorB = 'ImageHandles = findobj(gcf,''Type'',''Image'');for i = length(ImageHandles):-1:1,if size(size(get(ImageHandles(i),''CData'')),2)~=3,ImageHandles(i)=[];end;end;if ~isempty(ImageHandles),AllData=get(ImageHandles,''CData'');ImageHandles = num2cell(ImageHandles);if ~iscell(AllData), AllData={AllData};end;button=findobj(gcf,''tag'',''ToggleColorB'');for i = 1:length(AllData),tempdata{i}=AllData{i}(:,:,3);end;for i = 1:length(AllData), data=AllData{i}; if get(button,''value'')==0,set(button,''UserData'',tempdata);data(:,:,3)=0;set(ImageHandles{i},''CData'',data);else,tempdata=get(button,''UserData'');if ~iscell(tempdata),tempdata={tempdata};end;data(:,:,3)=tempdata{i};AllData{i}=data;set(ImageHandles{i},''CData'',data);end;end;end;clear data AllData button ImageHandles;';
         uicontrol('Style', 'checkbox', ...
             'Units','normalized',...
             'Position', [.72 .02 .06 .04], ...

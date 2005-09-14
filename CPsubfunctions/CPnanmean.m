4e7a4fe7bb2844d1efbf5275ed157cdaf3df4947
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-09-14 14:26:28 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1906 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPnanmean.m
@@ -0,0 +1,33 @@
+function m = CPnanmean(x,dim)
+%NANMEAN Mean value, ignoring NaNs.
+%   M = NANMEAN(X) returns the sample mean of X, treating NaNs as missing
+%   values.  For vector input, M is the mean value of the non-NaN elements
+%   in X.  For matrix input, M is a row vector containing the mean value of
+%   non-NaN elements in each column.  For N-D arrays, NANMEAN operates
+%   along the first non-singleton dimension.
+%
+%   NANMEAN(X,DIM) takes the mean along dimension DIM of X.
+%
+%   See also MEAN, NANMEDIAN, NANSTD, NANVAR, NANMIN, NANMAX, NANSUM.
+
+%   Copyright 1993-2004 The MathWorks, Inc.
+%   $Revision: 2.13.4.3 $  $Date: 2004/07/28 04:38:41 $
+
+% Find NaNs and set them to zero
+nans = isnan(x);
+x(nans) = 0;
+
+if nargin == 1 % let sum deal with figuring out which dimension to use
+    % Count up non-NaNs.
+    n = sum(~nans);
+    n(n==0) = NaN; % prevent divideByZero warnings
+    % Sum up non-NaNs, and divide by the number of non-NaNs.
+    m = sum(x) ./ n;
+else
+    % Count up non-NaNs.
+    n = sum(~nans,dim);
+    n(n==0) = NaN; % prevent divideByZero warnings
+    % Sum up non-NaNs, and divide by the number of non-NaNs.
+    m = sum(x,dim) ./ n;
+end
+

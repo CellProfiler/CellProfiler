8c0cb3f1b29651fcf5b16d1121548cb2fc45bbeb
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-09-19 17:26:53 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@1934 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPRgSmartDilate.m
@@ -0,0 +1,40 @@
+function rgOut = RgSmartDilate(rgIn,n)
+%Accepts labelled regions and dilates them with radius 1 diamond, disallowing region overlap. 
+%rgIn is a labelled region image, n is number of iterations.
+
+global rg4trans
+
+intwarning('off');
+
+rgOut=rgIn;
+for i=1:n
+    %generate array of neighboring values 
+    %Note unstylish kluge at start of RunBatch.  For a standalone
+    %function (costs 1 second) use
+    %   rg4trans=uint16(zeros([size(rgOut) 4]));
+    rg4trans=uint16(zeros([size(rgOut) 4]));
+    rg4trans(:,:,:)=0;
+    %Shift up
+    rg4trans(1:end-1,1:end  ,1)=rgOut(2:end  ,1:end);
+    %Shift down
+    rg4trans(2:end  ,1:end  ,2)=rgOut(1:end-1,1:end);
+    %Shift left
+    rg4trans(1:end  ,1:end-1,3)=rgOut(1:end  ,2:end);
+    %Shift right
+    rg4trans(1:end  ,2:end  ,4)=rgOut(1:end  ,1:end-1);
+    
+    
+    %find maximal neigbor value
+    rgMax=max(rg4trans,[],3);
+    %find minimal nonzero neighbor value
+    rg4trans(rg4trans==0)=Inf;
+    rgMin=min(rg4trans,[],3);
+
+    %if only one nonzero neighbor value, return
+    rgMax(rgMax~=rgMin)=0;
+    
+    %if original value was nonzero, return
+    rgMax(rgOut>0)=0;
+    rgOut=uint16(double(rgOut)+double(rgMax));
+    %disp(['Dilated Smartly. Time: ' num2str(toc)])
+end

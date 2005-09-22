1662e12815a471c3d93744f5a2027b654a700407
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-09-22 19:47:44 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2002 1fc53939-2000-0410-845c-e8453a809027

--- Modules/PlaceAdjacent.m
+++ Modules/PlaceAdjacent.m
@@ -25,9 +25,6 @@ function handles = PlaceAdjacent(handles)
 %
 % $Revision$
 
-
-
-
 drawnow
 
 %%%%%%%%%%%%%%%%
@@ -43,110 +40,147 @@ CurrentModuleNum = str2double(CurrentModule);
 
 %textVAR01 = What did you call the first image to be placed?
 %infotypeVAR01 = imagegroup
-ImageName1 = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+ImageName{1} = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 %inputtypeVAR01 = popupmenu
 
 %textVAR02 = What did you call the second image to be placed?
 %infotypeVAR02 = imagegroup
-ImageName2 = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+ImageName{2} = char(handles.Settings.VariableValues{CurrentModuleNum,2});
 %inputtypeVAR02 = popupmenu
 
-%textVAR03 = What do you want to call the resulting image?
-%defaultVAR03 = AdjacentImage
-%infotypeVAR03 = imagegroup indep
-AdjacentImageName = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+%textVAR03 = What did you call the first image to be placed?
+%choiceVAR03 = Do not use
+%infotypeVAR03 = imagegroup
+ImageName{3} = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+%inputtypeVAR03 = popupmenu
 
-%textVAR04 = Placement Type.
-%choiceVAR04 = Horizontal
-%choiceVAR04 = Vertical
-HorizontalOrVertical = char(handles.Settings.VariableValues{CurrentModuleNum,4});
-HorizontalOrVertical = HorizontalOrVertical(1);
+%textVAR04 = What did you call the second image to be placed?
+%choiceVAR04 = Do not use
+%infotypeVAR04 = imagegroup
+ImageName{4} = char(handles.Settings.VariableValues{CurrentModuleNum,4});
 %inputtypeVAR04 = popupmenu
 
-%textVAR05 = Can the incoming images be deleted from the pipeline after they are placed adjacent (this saves memory, but prevents you from using the incoming images later in the pipeline)?
-%choiceVAR05 = No
-%choiceVAR05 = Yes
-DeletePipeline = char(handles.Settings.VariableValues{CurrentModuleNum,5});
-DeletePipeline = DeletePipeline(1);
+%textVAR05 = What did you call the first image to be placed?
+%choiceVAR05 = Do not use
+%infotypeVAR05 = imagegroup
+ImageName{5} = char(handles.Settings.VariableValues{CurrentModuleNum,5});
 %inputtypeVAR05 = popupmenu
 
-%%%VariableRevisionNumber = 2
+%textVAR06 = What did you call the second image to be placed?
+%choiceVAR06 = Do not use
+%infotypeVAR06 = imagegroup
+ImageName{6} = char(handles.Settings.VariableValues{CurrentModuleNum,6});
+%inputtypeVAR06 = popupmenu
+
+%textVAR07 = What do you want to call the resulting image?
+%defaultVAR07 = AdjacentImage
+%infotypeVAR07 = imagegroup indep
+AdjacentImageName = char(handles.Settings.VariableValues{CurrentModuleNum,7});
+
+%textVAR08 = Placement Type.
+%choiceVAR08 = Horizontal
+%choiceVAR08 = Vertical
+HorizontalOrVertical = char(handles.Settings.VariableValues{CurrentModuleNum,8});
+HorizontalOrVertical = HorizontalOrVertical(1);
+%inputtypeVAR08 = popupmenu
+
+%textVAR09 = Can the incoming images be deleted from the pipeline after they are placed adjacent (this saves memory, but prevents you from using the incoming images later in the pipeline)?
+%choiceVAR09 = No
+%choiceVAR09 = Yes
+DeletePipeline = char(handles.Settings.VariableValues{CurrentModuleNum,9});
+DeletePipeline = DeletePipeline(1);
+%inputtypeVAR09 = popupmenu
+
+%%%VariableRevisionNumber = 3
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
-%%% Reads (opens) the image you want to analyze and assigns it to a
-%%% variable.
-fieldname = ['', ImageName1];
-%%% Checks whether the image to be analyzed exists in the handles structure.
-if isfield(handles.Pipeline, fieldname)==0,
-    %%% If the image is not there, an error message is produced.  The error
-    %%% is not displayed: The error function halts the current function and
-    %%% returns control to the calling function (the analyze all images
-    %%% button callback.)  That callback recognizes that an error was
-    %%% produced because of its try/catch loop and breaks out of the image
-    %%% analysis loop without attempting further modules.
-    error(['Image processing was canceled because the Place Adjacent module could not find the input image.  It was supposed to be named ', ImageName1, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
+tmp1 = {};
+for n = 1:6
+    if ~strcmp(ImageName{n}, 'Do not use')
+        tmp1{end+1} = ImageName{n};
+    end
 end
-%%% Reads the image.
-OrigImage1 = handles.Pipeline.(fieldname);
+ImageName = tmp1;
 
-%%% Removes the image from the pipeline to save memory if requested.
-if strncmpi(DeletePipeline,'Y',1) == 1
-   handles.Pipeline = rmfield(handles.Pipeline,fieldname);
-end
-%%% Repeat for the second image.
-fieldname = ['', ImageName2];
-if isfield(handles.Pipeline, fieldname)==0,
-    error(['Image processing was canceled because the Place Adjacent module could not find the input image.  It was supposed to be named ', ImageName1, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
-end
-OrigImage2 = handles.Pipeline.(fieldname);
-%%% Removes the image from the pipeline to save memory if requested.
-if strncmpi(DeletePipeline,'Y',1) == 1
-handles.Pipeline = rmfield(handles.Pipeline,fieldname);
+OrigImage = {};
+for i=1:length(ImageName)
+    %%% Reads (opens) the image you want to analyze and assigns it to a
+    %%% variable.
+    fieldname = ['', ImageName{i}];
+    %%% Checks whether the image to be analyzed exists in the handles structure.
+    if isfield(handles.Pipeline, fieldname)==0,
+        %%% If the image is not there, an error message is produced.  The error
+        %%% is not displayed: The error function halts the current function and
+        %%% returns control to the calling function (the analyze all images
+        %%% button callback.)  That callback recognizes that an error was
+        %%% produced because of its try/catch loop and breaks out of the image
+        %%% analysis loop without attempting further modules.
+        error(['Image processing was canceled because the Place Adjacent module could not find the input image.  It was supposed to be named ', ImageName1, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
+    end
+    %%% Reads the image.
+    OrigImage{i} = handles.Pipeline.(fieldname);
+
+    %%% Removes the image from the pipeline to save memory if requested.
+    if strncmpi(DeletePipeline,'Y',1) == 1
+        handles.Pipeline = rmfield(handles.Pipeline,fieldname);
+    end
 end
 
 %%%%%%%%%%%%%%%%%%%%%
 %%% IMAGE ANALYSIS %%%
 %%%%%%%%%%%%%%%%%%%%%
 
-
-
 %%% Check that the images are the same height or width and place them
 %%% adjacent to each other.
 
-if strcmpi(HorizontalOrVertical,'H') == 1
-    if size(OrigImage1,1) ~= size(OrigImage2,1)
-        error('Image processing was canceled because the two input images must have the same height if they are to be placed horizontally adjacent to each other.')
-    end
-    %%% If one of the images is multidimensional (color), the other
-    %%% one is replicated to match its dimensions.
-    if size(OrigImage1,3) ~= size(OrigImage2,3)
-        DesiredLayers = max(size(OrigImage1,3),size(OrigImage2,3));
-        if size(OrigImage1,3) > size(OrigImage2,3)
-            for i = 1:DesiredLayers, OrigImage2(:,:,i) = OrigImage2(:,:,1); end
+if strcmpi(HorizontalOrVertical,'H')
+    for i=1:(length(OrigImage)-1)
+        if size(OrigImage{i},1) ~= size(OrigImage{i+1},1)
+            error('Image processing was canceled because the two input images must have the same height if they are to be placed horizontally adjacent to each other.')
+        end
+        %%% If one of the images is multidimensional (color), the other
+        %%% one is replicated to match its dimensions.
+        if size(OrigImage{i},3) ~= size(OrigImage{i+1},3)
+            DesiredLayers = max(size(OrigImage{i},3),size(OrigImage{i+1},3));
+            if size(OrigImage{i},3) > size(OrigImage{i+1},3)
+                for i = 1:DesiredLayers, OrigImage{i+1}(:,:,i) = OrigImage{i+1}(:,:,1); end
+            else
+                for i = 1:DesiredLayers, OrigImage{i}(:,:,i) = OrigImage{i}(:,:,1); end
+            end
+        end
+        if i == 1
+            TempAdjacentImage = cat(2,OrigImage{i},OrigImage{i+1});
         else
-            for i = 1:DesiredLayers, OrigImage1(:,:,i) = OrigImage1(:,:,1); end
+            TempAdjacentImage = cat(2,TempAdjacentImage,OrigImage{i+1});
         end
     end
-    AdjacentImage = cat(2,OrigImage1,OrigImage2);
-elseif strcmpi(HorizontalOrVertical,'V') == 1
-    if size(OrigImage1,2) ~= size(OrigImage2,2)
-        error('Image processing was canceled because the two input images must have the same height if they are to be placed horizontally adjacent to each other.')
-    end
-    %%% If one of the images is multidimensional (color), the other
-    %%% one is replicated to match its dimensions.
-    if size(OrigImage1,3) ~= size(OrigImage2,3)
-        DesiredLayers = max(size(OrigImage1,3),size(OrigImage2,3));
-        if size(OrigImage1,3) > size(OrigImage2,3)
-            for i = 1:DesiredLayers, OrigImage2(:,:,i) = OrigImage2(:,:,1); end
+    AdjacentImage = TempAdjacentImage;
+elseif strcmpi(HorizontalOrVertical,'V')
+    for i=1:(length(OrigImage)-1)
+        if size(OrigImage{i},2) ~= size(OrigImage{i+1},2)
+            error('Image processing was canceled because the two input images must have the same height if they are to be placed horizontally adjacent to each other.')
+        end
+        %%% If one of the images is multidimensional (color), the other
+        %%% one is replicated to match its dimensions.
+        if size(OrigImage{i},3) ~= size(OrigImage{i+1},3)
+            DesiredLayers = max(size(OrigImage{i},3),size(OrigImage{i+1},3));
+            if size(OrigImage{i},3) > size(OrigImage{i+1},3)
+                for i = 1:DesiredLayers, OrigImage{i+1}(:,:,i) = OrigImage{i+1}(:,:,1); end
+            else
+                for i = 1:DesiredLayers, OrigImage{i}(:,:,i) = OrigImage{i}(:,:,1); end
+            end
+        end
+        if i == 1
+            TempAdjacentImage = cat(1,OrigImage{i},OrigImage{i+1});
         else
-            for i = 1:DesiredLayers, OrigImage1(:,:,i) = OrigImage1(:,:,1); end
+            TempAdjacentImage = cat(1,TempAdjacentImage,OrigImage{i+1});
         end
     end
-    AdjacentImage = cat(1,OrigImage1,OrigImage2);
+    AdjacentImage = TempAdjacentImage;
 else
     error('Image processing was canceled because you must enter H or V to specify whether to place the images adjacent to each other horizontally or vertically.')
 end
@@ -156,8 +190,6 @@ end
 %%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
-
-
 fieldname = ['FigureNumberForModule',CurrentModule];
 ThisModuleFigureNumber = handles.Current.(fieldname);
 if any(findobj == ThisModuleFigureNumber) == 1;
@@ -166,9 +198,9 @@ if any(findobj == ThisModuleFigureNumber) == 1;
     %%% Activates the appropriate figure window.
     CPfigure(handles,ThisModuleFigureNumber);
     %%% A subplot of the figure window is set to display the original image.
-    subplot(2,2,1); imagesc(OrigImage1);
+    subplot(2,2,1); imagesc(OrigImage{1});
     title(['First input Image, Image Set # ',num2str(handles.Current.SetBeingAnalyzed)]);
-    subplot(2,2,3); imagesc(OrigImage2);
+    subplot(2,2,3); imagesc(OrigImage{2});
     title(['Second input Image, Image Set # ',num2str(handles.Current.SetBeingAnalyzed)]);
     %%% A subplot of the figure window is set to display the Adjacent
     %%% Image.
@@ -179,10 +211,6 @@ end
 %%% SAVE DATA TO HANDLES STRUCTURE %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
-
-
-
-
 
 %%% Saves the Adjacent Image to the
 %%% handles structure so it can be used by subsequent modules.

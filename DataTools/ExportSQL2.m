081a02fc5b2a43fa03362d769dd1f54eae706cba
xuefang_ma <xuefang_ma@1fc53939-2000-0410-845c-e8453a809027>
2005-09-28 16:13:28 +0000

    change ExportSQL2 to use CPConvertSQL -susan
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2047 1fc53939-2000-0410-845c-e8453a809027

--- DataTools/ExportSQL2.m
+++ DataTools/ExportSQL2.m
@@ -5,13 +5,10 @@ function ExportSQL2(handles)
 %
 % This tool exports measurements from one or several CellProfiler
 % output files to delimited text files. It also creates an SQL
-% script that puts the measurements into an SQL database. This function
-% is also called from the WriteSQLFiles module.
+% script that puts the measurements into an SQL database. This tool calls CPConvertSQL 
+% function to do the actuall exporting, which is same as WriteSQL module.
 %
 % Current known limitations and things to consider:
-%
-% - No check is performed that the selected files actually are CellProfiler
-%   output files. .
 %
 % - No check is performed that the selected files are compatible, i.e.
 %   were produced with the same pipeline of modules.
@@ -37,23 +34,8 @@ function ExportSQL2(handles)
 %
 % $Revision$
 
-%%% Determine if this function is called from the WriteSQLFiles module
-%%% In this case the required input data can be found in the handles structure
-if isfield(handles,'DatabaseName')
-    ModuleCall = 1;
-    DatabaseName = handles.DatabaseName;
-    DataPath = handles.DataPath;
-    CellProfilerDataFileNames = {get(handles.OutputFileNameEditBox,'string')};
 
-    %%% Generate SQL by removing the 'OUT' and appending '_SQLScript.SQL'.
-    filename = get(handles.OutputFileNameEditBox,'string');
-    index = strfind(filename,'OUT');
-    if ~isempty(index)
-        filename = [filename(1:index-1),filename(index+3:end)];
-    end
-    SQLScriptFileName = fullfile(DataPath,[filename(1:end-4),'_SQLScript.SQL']);
-else
-    % This function is not called from WriteSQLFiles but from the Data tool menu
+    % This function is not called from WriteSQLFiles ModuleCall but from the Data tool menu
     ModuleCall = 0;
 
     %%% Let the user select one output file to indicate the directory and how the
@@ -64,7 +46,7 @@ else
         [ExampleFile, DataPath] = uigetfile('*.mat','Select one CellProfiler output file');
     end
     if ~DataPath;return;end
-
+     %CurrentDataPath = handles.Current.DefaultOutputDirectory %current dir
 
     %%% Get all files with .mat extension in the chosen directory.
     %%% If the selected file name contains an 'OUT', it is assumed
@@ -83,28 +65,33 @@ else
     CellProfilerDataFileNames = Files(selection);
 
     %%% Ask for database name and name of SQL script
-    answer = inputdlg({'Database to use:','SQL script name:'},'Export SQL',1,{'Default','SQLScript.SQL'});
+    answer = inputdlg({'Database to use:','SQL script name:'},'Export SQL',1,{'Default','SQLScript_'});
     if isempty(answer),return;end
     DatabaseName = answer{1};
-    SQLScriptFileName = fullfile(DataPath,answer{2});
+    SQLScriptFileName = answer{2};%fullfile(DataPath,answer{2});
     if isempty(DatabaseName) | isempty(SQLScriptFileName)
         errordlg('A database name and an SQL script name must be specified!');
         return
     end
-end
+%end
 
+%check whether have write permission in current dir
 SQLScriptFid = fopen(SQLScriptFileName, 'wt');
 if SQLScriptFid == -1, error(['Could not open ' SQLScriptFileName ' for writing.']); end
-fprintf(SQLScriptFid, 'USE %s;\n', DatabaseName);
+ 
 
-GlobalImageSetNo = 0;                                                                % This variable is used for numbering the image sets when several input files are exported
+%display waiting bar
 waitbarhandle = waitbar(0,'Exporting SQL files');
-drawnow         
+drawnow   
+
+   
+    
 
 for FileNo = 1:length(CellProfilerDataFileNames)
 
     % If called as a Data tool, load handles structure from file
     if ~ModuleCall,load(fullfile(DataPath,CellProfilerDataFileNames{FileNo}));end
+
 
     % Get the object types, e.g. 'Image', 'Cells', 'Nuclei',...
     try
@@ -114,7 +101,19 @@ for FileNo = 1:length(CellProfilerDataFileNames)
         return;
     end
 
-    for ObjectTypeNo = 1:length(ObjectTypes)                                          % Loop over the objects
+    %get the output file's prefix as .sql file's fileprefix, so if mutliple
+    %files are selected the sql files will be named differently, even the
+    %out put files are exactly the same except the file names
+    filename = CellProfilerDataFileNames{FileNo};
+    index = strfind(filename,'.');
+    if ~isempty(index)
+        filename = [filename(1:index-1)]%,everything before . will be kept
+
+    end
+
+
+
+    for ObjectTypeNo = 1:length(ObjectTypes)    % Loop over the objects
 
         % Get the object type in a variable for convenience
         ObjectType = ObjectTypes{ObjectTypeNo};
@@ -124,112 +123,26 @@ for FileNo = 1:length(CellProfilerDataFileNames)
         total = length(CellProfilerDataFileNames)*length(ObjectTypes);
         waitbar(done/total,waitbarhandle);drawnow
 
-
-        %%% For each image set, construct a big matrix of size [NbrOfObjects x Total number of features]
-        %%% and corresponding cell array of size [1 x Total number of features] with the measurement names.
-        MeasurementNames = {};
-        Measurements = {};
-        AllFields = fieldnames(handles.Measurements.(ObjectType));     % Get all existing fields for this object
-        for j = 1:length(AllFields)
-            if ~isempty(strfind(AllFields{j},'Features'))              % Found a field with feature names
-
-                % Get the corresponding measurements
-                CellArray = handles.Measurements.(ObjectType).(AllFields{j}(1:end-8));
-
-                % Concatenate measurements
-                if length(Measurements) == 0
-                    Measurements = CellArray;                             % The first measurement structure encounterered
-                else
-                    for k = 1:length(CellArray)                           % Loop over the image sets and concatenate
-                        Measurements{k} = cat(2,Measurements{k},CellArray{k});
-                    end
-                end
-
-                % Fix and concatenate measurement names
-                FeatureNames = handles.Measurements.(ObjectType).(AllFields{j});      % Cell array with measurement names
-                Prefix = sprintf('%s_%s_',ObjectType,AllFields{j}(1:end-8));          % E.g. Cells_Shape_, Nuclei_Texture_OrigGreen,..
-                for k = 1:length(FeatureNames)                                            % Loop over the measurement names...
-                    FeatureName = FeatureNames{k};                                        %
-                    FeatureNames{k} = [Prefix FeatureName(~isspace(FeatureName))];        % ... to add prefix and remove spaces in the names
-                end
-                MeasurementNames = cat(2,MeasurementNames,FeatureNames);                  % Add the measurement names to the cell array
-            end
-        end
-
-        %%% Get the fields where the filenames are stored, these must be written to the SQL database in order
-        %%% to identify the image sets
-        FilenameFields = fieldnames(handles.Measurements.Image);
-        FilenameFields = FilenameFields(~cellfun('isempty',strfind(FilenameFields,'Filename')));
-
-        %%% Generate SQL data filenames by removing a potential 'OUT' and appending '_SQLData.SQL'.
-        filename = CellProfilerDataFileNames{FileNo};
-        index = strfind(filename,'OUT');
-        if ~isempty(index)
-            filename = [filename(1:index-1),filename(index+3:end)];
-        end
-        SQLDataFileName = [filename(1:end-4),'_',ObjectTypes{ObjectTypeNo},'_SQLData.SQL'];
-        SQLMeanDataFileName = [filename(1:end-4),'_',ObjectTypes{ObjectTypeNo},'_SQLMeanData.SQL'];
-
-        % Write lines in the SQL script for mean data (i.e. average object data)
-        % Add ImageSetNo and ObjectNo entries in the table
-        % Write lines for object data
-        fprintf(SQLScriptFid, 'CREATE TABLE IF NOT EXISTS %s (ImageSetNo INTEGER, ObjectNo INTEGER',ObjectType);
-        for k = 1:length(MeasurementNames),
-            fprintf(SQLScriptFid, ', %s FLOAT', MeasurementNames{k});
-        end
-        fprintf(SQLScriptFid, ', PRIMARY KEY (ImageSetNo, ObjectNo));\n');
-        fprintf(SQLScriptFid, 'LOAD DATA LOCAL INFILE ''%s'' REPLACE INTO TABLE %s FIELDS TERMINATED BY ''|'';\n', fullfile(DataPath,SQLMeanDataFileName),ObjectType);
-
-        % Write lines for average data
-        fprintf(SQLScriptFid, 'CREATE TABLE IF NOT EXISTS Mean%s (ImageSetNo INTEGER PRIMARY KEY',ObjectType);
-        for k = 1:length(MeasurementNames),
-            fprintf(SQLScriptFid, ', %s FLOAT', MeasurementNames{k});
-        end
-        for k = 1:length(FilenameFields),
-            fprintf(SQLScriptFid, ', %s CHAR(50)', FilenameFields{k});                                   % Write the filename fields names
-        end
-        fprintf(SQLScriptFid,');\n');
-        fprintf(SQLScriptFid, 'LOAD DATA LOCAL INFILE ''%s'' REPLACE INTO TABLE Mean%s FIELDS TERMINATED BY ''|'';\n', fullfile(DataPath,SQLDataFileName),ObjectType);
-
-        %%% Open the SQL data files for writing
-        SQLDataFid = fopen(fullfile(DataPath,SQLDataFileName), 'wt');
-        if SQLDataFid == -1, fclose(SQLScriptFid); error(['Could not open ' SQLDataFileName ' for writing.']);end
-        SQLMeanDataFid = fopen(fullfile(DataPath,SQLMeanDataFileName), 'wt');
-        if SQLMeanDataFid == -1, fclose(SQLScriptFid); error(['Could not open ' SQLMeanDataFileName ' for writing.']);end
+    end %endloop over objtype
+    %figure out the first and last set
+    if isfield(handles.Measurements, 'BatchInfo'),
+        FirstSet = handles.Measurements.BatchInfo.Start;
+        LastSet = handles.Measurements.BatchInfo.End;
+    else
+        FirstSet = 1;
+        LastSet = length(handles.Measurements.Image.FileNames);
+    end
 
 
-        %%% Loop over the image sets and write the data into the SQL data files
-        for ImageSetNo = 1:length(Measurements)
+    %%% This is necessary to make sure the export works with the last
+    %%% set.  Otherwise, the TimeElapsed array is missing the last
+    %%% element.  The corresponding 'tic' is in CellProfiler.m.
 
-            % Calculate mean data
-            AverageData = mean(Measurements{ImageSetNo},1);
+    handles.Measurements.Image.TimeElapsed{handles.Current.SetBeingAnalyzed} = toc;
+    % for calling from data tool, no tableprefix is asked from user, leave
+    % it as blank
+    CPConvertSQL(handles, DataPath, [filename,SQLScriptFileName], DatabaseName,'',FirstSet, LastSet);
 
-            % Create a cell array where every other cell contains
-            % the delimiter '|'
-            str = cell(2*length(AverageData),1);
-            str(1:2:end) = {'|'};
-
-            % Write the object data
-            for ObjectNo = 1:size(Measurements{ImageSetNo},1)
-                str(2:2:end) = cellstr(num2str(Measurements{ImageSetNo}(ObjectNo,:)','%g'));  % Create cell array with measurements
-                fprintf(SQLDataFid,'%d|%d',GlobalImageSetNo+ImageSetNo,ObjectNo);        % Write the image set number and object number
-                fprintf(SQLDataFid,sprintf('%s\n',cat(2,str{:})));            % Write measurements
-            end
-
-            % Write the average data
-            str(2:2:end) = cellstr(num2str(AverageData','%g'));
-            fprintf(SQLMeanDataFid,'%d',GlobalImageSetNo+ImageSetNo);                    % Write the image set number
-            fprintf(SQLMeanDataFid,sprintf('%s',cat(2,str{:})));              % Write mean measurements
-            for k = 1:length(FilenameFields)                                  % Write image file names
-                fprintf(SQLMeanDataFid,'|%s',handles.Measurements.Image.(FilenameFields{k}){ImageSetNo});
-            end
-            fprintf(SQLMeanDataFid,'\n');
-
-        end % End loop over image sets
-        fclose(SQLDataFid);
-        fclose(SQLMeanDataFid);
-    end % End loop over object type, e.g. 'Cells', 'Nuclei', 'Image',...
-    GlobalImageSetNo = GlobalImageSetNo + length(Measurements);
 end % End loop over data files
 
 %%% Done, let the user know if this function was called as a data tool and restore the handles structure
@@ -238,4 +151,3 @@ if ~ModuleCall
     CPmsgbox('Exporting is completed.')
 end
 fclose(SQLScriptFid);
-

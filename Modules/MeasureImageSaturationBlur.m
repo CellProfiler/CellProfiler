e1ac7eae6b39cb6b458bf6ffe3dab58af0158acd
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-09-30 13:07:52 +0000

    Fixed how MeasureImageSaturationBlur stores handles
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2086 1fc53939-2000-0410-845c-e8453a809027

--- Modules/MeasureImageSaturationBlur.m
+++ Modules/MeasureImageSaturationBlur.m
@@ -16,7 +16,7 @@ function handles = MeasureImageSaturationBlur(handles)
 % RightImage = Image(:,2:end)
 % LeftImage = Image(:,1:end-1)
 % MeanImageValue = mean(Image(:))
-% FocusScore = std(RightImage(:) - LeftImage(:)) / MeanImageValue
+% BlurScore = std(RightImage(:) - LeftImage(:)) / MeanImageValue
 %
 % SAVING IMAGES: If you want to save images produced by this module,
 % alter the code for this module to save those images to the handles
@@ -99,64 +99,64 @@ BlurCheck = BlurCheck(1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
-for ImageNumber = 1:6;
-    %%% Reads (opens) the images you want to analyze and assigns them to
-    %%% variables.
-    if strcmp(NameImageToCheck{ImageNumber}, 'Do not use') ~= 1
-        fieldname = ['', NameImageToCheck{ImageNumber}];
-        %%% Checks whether the image to be analyzed exists in the handles structure.
-        if isfield(handles.Pipeline, fieldname)==0,
-            %%% If the image is not there, an error message is produced.  The error
-            %%% is not displayed: The error function halts the current function and
-            %%% returns control to the calling function (the analyze all images
-            %%% button callback.)  That callback recognizes that an error was
-            %%% produced because of its try/catch loop and breaks out of the image
-            %%% analysis loop without attempting further modules.
-            error(['Image processing was canceled because the Saturation & Blur Check module could not find the input image.  It was supposed to be named ', NameImageToCheck{ImageNumber}, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
-        end
-        %%% Reads the image.
-        ImageToCheck{ImageNumber} = handles.Pipeline.(fieldname);
-
-        %%% Checks that the original image is two-dimensional (i.e. not a color
-        %%% image), which would disrupt several of the image functions.
-        if ndims(ImageToCheck{ImageNumber}) ~= 2
-            error('Image processing was canceled because the Saturation Blur Check module requires an input image that is two-dimensional (i.e. X vs Y), but the image loaded does not fit this requirement.  This may be because the image is a color image. You can run an RGB Split module or RGB to Grayscale module to convert your image to grayscale. Also, you can modify the code to handle each channel of a color image; we just have not done it yet.  This requires making the proper headings in the measurements file and displaying the results properly.')
-        end
-        NumberPixelsSaturated = sum(sum(ImageToCheck{ImageNumber} == 1));
-        [m,n] = size(ImageToCheck{ImageNumber});
-        TotalPixels = m*n;
-        PercentPixelsSaturated = 100*NumberPixelsSaturated/TotalPixels;
-        PercentSaturation{ImageNumber} = PercentPixelsSaturated;
-
-        %%% Checks the focus of the images, if desired.
-        if strcmp(upper(BlurCheck), 'N') ~= 1
-            RightImage = ImageToCheck{ImageNumber}(:,2:end);
-            LeftImage = ImageToCheck{ImageNumber}(:,1:end-1);
-            MeanImageValue = mean(ImageToCheck{ImageNumber}(:));
-            if MeanImageValue == 0
-                FocusScore{ImageNumber} = 0;
-            else
-                FocusScore{ImageNumber} = std(RightImage(:) - LeftImage(:)) / MeanImageValue;
-            end
-            FocusScoreFeaturenames(ImageNumber) = {['FocusScore ', NameImageToCheck{ImageNumber}]};
-        end
-        PercentSaturationFeaturenames(ImageNumber) = {['PercentSaturation ', NameImageToCheck{ImageNumber}]};
+tmp1 = {};
+for n = 1:6
+    if ~strcmp(NameImageToCheck{n}, 'Do not use')
+        tmp1{end+1} = NameImageToCheck{n};
     end
 end
+NameImageToCheck = tmp1;
 
-% Remove empty cells, in case there is an 'N' in between two actual images
-if strcmp(upper(BlurCheck), 'N') ~= 1
-    FocusScoreFeaturenames = cellstr(strvcat(FocusScoreFeaturenames{:}))';
-end
-PercentSaturationFeaturenames = cellstr(strvcat(PercentSaturationFeaturenames{:}))';
+for ImageNumber = 1:length(NameImageToCheck);
+    %%% Reads (opens) the images you want to analyze and assigns them to
+    %%% variables.
+    fieldname = ['', NameImageToCheck{ImageNumber}];
+    %%% Checks whether the image to be analyzed exists in the handles structure.
+    if ~isfield(handles.Pipeline, fieldname),
+        %%% If the image is not there, an error message is produced.  The error
+        %%% is not displayed: The error function halts the current function and
+        %%% returns control to the calling function (the analyze all images
+        %%% button callback.)  That callback recognizes that an error was
+        %%% produced because of its try/catch loop and breaks out of the image
+        %%% analysis loop without attempting further modules.
+        error(['Image processing was canceled because the Saturation & Blur Check module could not find the input image.  It was supposed to be named ', NameImageToCheck{ImageNumber}, ' but an image with that name does not exist.  Perhaps there is a typo in the name.'])
+    end
+    %%% Reads the image.
+    ImageToCheck{ImageNumber} = handles.Pipeline.(fieldname);
 
-% Store measurements
-if strcmp(upper(BlurCheck), 'N') ~= 1
-    handles.Measurements.Image.FocusScoreFeatures = FocusScoreFeaturenames;
-    handles.Measurements.Image.FocusScore{handles.Current.SetBeingAnalyzed} = cat(2,FocusScore{:});;
+    %%% Checks that the original image is two-dimensional (i.e. not a color
+    %%% image), which would disrupt several of the image functions.
+    if ndims(ImageToCheck{ImageNumber}) ~= 2
+        error('Image processing was canceled because the Saturation Blur Check module requires an input image that is two-dimensional (i.e. X vs Y), but the image loaded does not fit this requirement.  This may be because the image is a color image. You can run an RGB Split module or RGB to Grayscale module to convert your image to grayscale. Also, you can modify the code to handle each channel of a color image; we just have not done it yet.  This requires making the proper headings in the measurements file and displaying the results properly.')
+    end
+    NumberPixelsSaturated = sum(sum(ImageToCheck{ImageNumber} == 1));
+    [m,n] = size(ImageToCheck{ImageNumber});
+    TotalPixels = m*n;
+    PercentPixelsSaturated = 100*NumberPixelsSaturated/TotalPixels;
+    PercentSaturation{ImageNumber} = PercentPixelsSaturated;
+
+    Measurefieldname = ['SaturationBlur_',NameImageToCheck{ImageNumber}];
+    Featurefieldname = ['SaturationBlur_',NameImageToCheck{ImageNumber},'Features'];
+    %%% Checks the focus of the images, if desired.
+    if ~strcmp(upper(BlurCheck),'N')
+        RightImage = ImageToCheck{ImageNumber}(:,2:end);
+        LeftImage = ImageToCheck{ImageNumber}(:,1:end-1);
+        MeanImageValue = mean(ImageToCheck{ImageNumber}(:));
+        if MeanImageValue == 0
+            BlurScore{ImageNumber} = 0;
+        else
+            BlurScore{ImageNumber} = std(RightImage(:) - LeftImage(:)) / MeanImageValue;
+        end
+        Featurenames = {'BlurScore','PercentSaturated'};
+        handles.Measurements.Image.(Featurefieldname) = Featurenames;
+        handles.Measurements.Image.(Measurefieldname){handles.Current.SetBeingAnalyzed}(:,1) = BlurScore{ImageNumber};
+        handles.Measurements.Image.(Measurefieldname){handles.Current.SetBeingAnalyzed}(:,2) = PercentSaturation{ImageNumber};
+    else
+        Featurenames = {'PercentSaturated'};
+        handles.Measurements.Image.(Featurefieldname) = Featurenames;
+        handles.Measurements.Image.(Measurefieldname){handles.Current.SetBeingAnalyzed}(:,1) = PercentSaturation{ImageNumber};
+    end
 end
-handles.Measurements.Image.PercentSaturationFeatures = PercentSaturationFeaturenames;
-handles.Measurements.Image.PercentSaturation{handles.Current.SetBeingAnalyzed} = cat(2,PercentSaturation{:});
 
 %%%%%%%%%%%%%%%%%%%%%%
 %%% DISPLAY RESULTS %%%
@@ -167,19 +167,15 @@ ThisModuleFigureNumber = handles.Current.(fieldname);
 if any(findobj == ThisModuleFigureNumber) == 1;
     CPfigure(handles,ThisModuleFigureNumber);
     originalsize = get(ThisModuleFigureNumber, 'position');
-    newsize = originalsize;
-    newsize(1) = 0;
-    newsize(2) = 0;
     if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
-        newsize(3) = originalsize(3)*.5;
         originalsize(3) = originalsize(3)*.5;
-        set(ThisModuleFigureNumber, 'position', originalsize,'color',[1 1 1]);
+        set(ThisModuleFigureNumber, 'position', originalsize,'color',[.7 .7 .9]);
     end
 
-    delete(findobj('Parent',ThisModuleFigureNumber));
+    %delete(findobj('Parent',ThisModuleFigureNumber));
 
-    displaytexthandle = uicontrol(ThisModuleFigureNumber,'style','text', 'units','normalized','position', [0.1 0.1 0.8 0.8],...
-        'fontname','times','fontsize',handles.Current.FontSize,'backgroundcolor',[1 1 1],'horizontalalignment','left');
+    displaytexthandle = uicontrol(ThisModuleFigureNumber,'style','text', 'units','normalized','position',[0.1 0.1 0.8 0.8],...
+        'fontname','times','fontsize',handles.Current.FontSize,'backgroundcolor',[.7 .7 .9],'horizontalalignment','left');
     DisplayText = strvcat(['    Image Set # ',num2str(handles.Current.SetBeingAnalyzed)],... %#ok We want to ignore MLint error checking for this line.
         '      ',...
         'Percent of pixels that are Saturated:');
@@ -192,33 +188,13 @@ if any(findobj == ThisModuleFigureNumber) == 1;
     end
     DisplayText = strvcat(DisplayText, '      ','      ','Focus Score:'); %#ok We want to ignore MLint error checking for this line.
     if strcmp(upper(BlurCheck), 'N') ~= 1
-        for ImageNumber = 1:length(FocusScore)
-            if ~isempty(FocusScore{ImageNumber})
+        for ImageNumber = 1:length(BlurScore)
+            if ~isempty(BlurScore{ImageNumber})
                 try DisplayText = strvcat(DisplayText, ... %#ok We want to ignore MLint error checking for this line.
-                        [NameImageToCheck{ImageNumber}, ':    ', num2str(FocusScore{ImageNumber})]);%#ok We want to ignore MLint error checking for this line.
+                        [NameImageToCheck{ImageNumber}, ':    ', num2str(BlurScore{ImageNumber})]);%#ok We want to ignore MLint error checking for this line.
                 end
             end
         end
     end
     set(displaytexthandle,'string',DisplayText)
 end
-
-%%% ABANDONED WAYS TO MEASURE FOCUS:
-%             eval(['ImageToCheck',ImageNumber,' = histeq(eval([''ImageToCheck'',ImageNumber]));'])
-%             if str2num(BlurRadius) == 0
-%                 BlurredImage = eval(['ImageToCheck',ImageNumber]);
-%             else
-%                 %%% Blurs the image.
-%                 %%% Note: using filter2 is much faster than imfilter (e.g. 14.5 sec vs. 99.1 sec).
-%                 FiltSize = max(3,ceil(4*BlurRadius));
-%                 BlurredImage = filter2(fspecial('gaussian',FiltSize, str2num(BlurRadius)), eval(['ImageToCheck',ImageNumber]));
-%                 % figure, imshow(BlurredImage, []), title('BlurredImage')
-%                 % imwrite(BlurredImage, [BareFileName,'BI','.',FileFormat], FileFormat);
-%             end
-%             %%% Subtracts the BlurredImage from the original.
-%             SubtractedImage = imsubtract(eval(['ImageToCheck',ImageNumber]), BlurredImage);
-%             handles.FocusScore(handles.Current.SetBeingAnalyzed) = std(SubtractedImage(:));
-%             handles.FocusScore2(handles.Current.SetBeingAnalyzed) = sum(sum(SubtractedImage.*SubtractedImage));
-%             FocusScore = handles.FocusScore
-%             FocusScore2 = handles.FocusScore2
-%

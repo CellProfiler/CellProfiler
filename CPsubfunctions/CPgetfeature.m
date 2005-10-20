6bb5bd09c663c9fbd473dd707960100d3dd08a8f
(no author) <(no author)@1fc53939-2000-0410-845c-e8453a809027>
2005-10-20 21:52:33 +0000

    re-add Image
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2258 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPgetfeature.m
+++ CPsubfunctions/CPgetfeature.m
@@ -24,11 +24,11 @@ end
 
 %%% Extract the fieldnames of measurements from the handles structure.
 MeasFieldnames = fieldnames(handles.Measurements);
-for i=1:length(handles.Measurements)
-    if MeasFieldnames{i}=='Image'
-        MeasFieldnames(i) = [];
-    end;
-end;
+% for i=1:length(handles.Measurements)
+%     if MeasFieldnames{i}=='Image'
+%         MeasFieldnames(i) = [];
+%     end;
+% end;
 
 %%% Error detection.
 if isempty(MeasFieldnames)

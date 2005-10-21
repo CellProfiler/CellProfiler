5a605d786c3b7a71c8846c0b8987d0818bff772a
(no author) <(no author)@1fc53939-2000-0410-845c-e8453a809027>
2005-10-21 14:32:19 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2261 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPmakegrid.m
@@ -0,0 +1,79 @@
+function Grid = CPmakegrid(GridInfo)
+%%% This subfunction will take GridInfo and create the grid lines
+%%% neccessary for any module which requires a grid
+
+XLocationOfLowestXSpot = GridInfo.XLocationOfLowestXSpot;
+YLocationOfLowestYSpot= GridInfo.YLocationOfLowestYSpot;
+XSpacing = GridInfo.XSpacing;
+YSpacing = GridInfo.YSpacing;
+Rows = GridInfo.Rows;
+Columns = GridInfo.Columns;
+TotalHeight = GridInfo.TotalHeight;
+TotalWidth = GridInfo.TotalWidth;
+LeftOrRight = GridInfo.LeftOrRight;
+TopOrBottom = GridInfo.TopOrBottom;
+RowsOrColumns = GridInfo.RowsOrColumns;
+
+%%% Calculates and arranges the integer numbers for labeling.
+LinearNumbers = 1:Rows*Columns;
+if strcmp(RowsOrColumns,'Columns')
+    SpotTable = reshape(LinearNumbers,Rows,Columns);
+elseif strcmp(RowsOrColumns,'Rows')
+    SpotTable = reshape(LinearNumbers,Columns,Rows);
+    SpotTable = SpotTable';
+end
+if strcmp(LeftOrRight,'Right')
+    SpotTable = fliplr(SpotTable);
+end
+if strcmp(TopOrBottom,'Bottom')
+    SpotTable = flipud(SpotTable);
+end
+
+%%% Calculates the locations for all the sample labelings (whether it is
+%%% numbers, spot identifying information, or coordinates).
+GridXLocations = SpotTable;
+for g = 1:size(GridXLocations,2)
+    GridXLocations(:,g) = XLocationOfLowestXSpot + (g-1)*XSpacing - round(XSpacing/2);
+end
+%%% Converts to a single column.
+XLocations = reshape(GridXLocations, 1, []);
+%%% Same routine for Y.
+GridYLocations = SpotTable;
+for h = 1:size(GridYLocations,1)
+    GridYLocations(h,:) = YLocationOfLowestYSpot + (h-1)*YSpacing - round(YSpacing/2);
+end
+YLocations = reshape(GridYLocations, 1, []);
+
+%%% Calculates the lines.
+TotalHeight = size(ImageToDisplay,1);
+TotalWidth = size(ImageToDisplay,2);
+%%% Adds extra spaced line to end of X locations
+VertLinesX(1,:) = [GridXLocations(1,:),GridXLocations(1,end)+XSpacing];
+VertLinesX(2,:) = [GridXLocations(1,:),GridXLocations(1,end)+XSpacing];
+VertLinesY(1,:) = repmat(0,1,size(GridXLocations,2)+1); %%% Same as zeros(1,size(GridXLocations,2)+1)
+VertLinesY(2,:) = repmat(TotalHeight,1,size(GridXLocations,2)+1);
+%%% Adds extra spaced line to end of X locations
+HorizLinesY(1,:) = [GridYLocations(:,1)',GridYLocations(end,1)+YSpacing];
+HorizLinesY(2,:) = [GridYLocations(:,1)',GridYLocations(end,1)+YSpacing];
+HorizLinesX(1,:) = repmat(0,1,size(GridYLocations,1)+1); %%% Same as zeros(1,size(GridYLocations,2)+1)
+HorizLinesX(2,:) = repmat(TotalWidth,1,size(GridYLocations,1)+1);
+
+Grid.XLocationOfLowestXSpot = XLocationOfLowestXSpot;
+Grid.YLocationOfLowestYSpot = YLocationOfLowestYSpot;
+Grid.XSpacing = XSpacing;
+Grid.YSpacing = YSpacing;
+Grid.Rows = Rows;
+Grid.Columns = Columns;
+Grid.VertLinesX = VertLinesX;
+Grid.VertLinesY = VertLinesY;
+Grid.HorizLinesX = HorizLinesX;
+Grid.HorizLinesY = HorizLinesY;
+Grid.SpotTable = SpotTable;
+Grid.TotalHeight = TotalHeight;
+Grid.TotalWidth = TotalWidth;
+Grid.GridXLocations = GridXLocations;
+Grid.GridYLocations = GridYLocations;
+Grid.YLocations = YLocations;
+Grid.XLocations = XLocations;
+Grid.LeftOrRight = LeftOrRight;
+Grid.TopOrBottom = TopOrBottom;

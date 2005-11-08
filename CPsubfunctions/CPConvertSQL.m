4e07ee4fb60b3e84b3742dcb67284cb8345d144e
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-11-08 21:49:21 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2502 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPConvertSQL.m
+++ CPsubfunctions/CPConvertSQL.m
@@ -15,7 +15,7 @@ SubMeasurementFieldnames = fieldnames(Measurements)';
 for RemainingSubMeasurementFieldnames = SubMeasurementFieldnames,
     %%%SubFieldname is the first fieldname in SubMeasurementFieldnames.
     SubFieldname = RemainingSubMeasurementFieldnames{1};
-    
+
     substruct = handles.Measurements.(SubFieldname);
     substructfields = fieldnames(substruct)';
     for ssfc = substructfields,
@@ -28,22 +28,22 @@ for RemainingSubMeasurementFieldnames = SubMeasurementFieldnames,
         if strfind(ssf, 'PathnameOrig'),
             continue;
         end
-                       
+
         if strfind(ssf, 'Text'),
             if (strfind(ssf, 'Text') + 3) == length(ssf),
                 continue;
             end
         end
-        
+
         if strfind(ssf, 'ModuleError'),
-                continue;
-            end
-        
+            continue;
+        end
+
         if strfind(ssf, 'TimeElapsed'),
-                continue;
-            end
-        
-                       
+            continue;
+        end
+
+
         if isfield(substruct, [ssf 'Features']),
             names = handles.Measurements.(SubFieldname).([ssf 'Features']);
         elseif isfield(substruct, [ssf 'Text']),
@@ -55,16 +55,16 @@ for RemainingSubMeasurementFieldnames = SubMeasurementFieldnames,
 
         vals = handles.Measurements.(SubFieldname).(ssf);
         vals;
-        
-        if (~ ischar(vals{1}))   
+
+        if (~ ischar(vals{1}))
             if (size(vals{1},2) ~= length(names)), % make change here vals{1},2
                 if ~isempty(vals{1})
-                error([SubFieldname ' ' ssf ' does not have right number of names ']);
+                    error([SubFieldname ' ' ssf ' does not have right number of names ']);
                 end
             end
         end
-        
-        
+
+
         if (size(vals{1},1) == 1),
             for n = 1:length(names),
                 per_image_names{end+1} = cleanup([SubFieldname '_' ssf '_' names{n}]);
@@ -84,7 +84,7 @@ fmain = fopen(fullfile(OutDir, [basename '.SQL']), 'W');
 fprintf(fmain, 'USE %s;\n', DBname);
 
 %begin writing the sql script for creating tables
-%first create look up table for colmun names 
+%first create look up table for colmun names
 fprintf (fmain, 'CREATE TABLE IF NOT EXISTS FIELDNAMES (TABLENAME CHAR(100), COLUMNNUMBER CHAR(25), ORGFIELDNAME VARCHAR(250));\n');
 
 %create table perimage
@@ -94,10 +94,10 @@ fprintf(fmain, 'CREATE TABLE IF NOT EXISTS %sPerImage (col1 INTEGER PRIMARY KEY'
 p=1;% col1 alread added
 for i = per_image_names,
     p=p+1;
-    if strfind(i{1}, 'Filename') 
+    if strfind(i{1}, 'Filename')
         %fprintf(fmain, ', %s CHAR(128)', i{1});
         fprintf(fmain, ', %s CHAR(128)', ['col',num2str(p)]);
-    elseif  strfind(i{1}, 'Path'),  
+    elseif  strfind(i{1}, 'Path'),
         %fprintf(fmain, ', %s CHAR(128)', i{1});
         fprintf(fmain, ', %s CHAR(128)', ['col',num2str(p)]);
     else
@@ -115,8 +115,8 @@ end
 
 for h=per_object_names,
     p=p+1;
-fprintf(fmain, ', %s FLOAT', ['col',num2str(p)]);
-%fprintf(fmain, ', %s FLOAT', ['Std_',h{1}]);
+    fprintf(fmain, ', %s FLOAT', ['col',num2str(p)]);
+    %fprintf(fmain, ', %s FLOAT', ['Std_',h{1}]);
 end
 
 
@@ -159,31 +159,31 @@ for k=per_image_names;
     fprintf(fcol, '\t%s', ['col', num2str(p)] );
     fprintf(fcol, '\t%s', k{1} );
     fprintf(fcol, '\n');
-    
+
 end
-  
+
 for l=per_object_names,
     p=p+1;
     fprintf(fcol, '%sPerImage',TablePrefix);
     fprintf(fcol, '\t%s', ['col', num2str(p)]);
     fprintf(fcol, '\t%s', ['Mean_', l{1}]);
-    fprintf(fcol, '\n'); 
-   
+    fprintf(fcol, '\n');
+
 end
- for m=per_object_names,
-     p=p+1;
+for m=per_object_names,
+    p=p+1;
     fprintf(fcol, '%sPerImage',TablePrefix);
     fprintf(fcol, '\t%s', ['col', num2str(p)]);
     fprintf(fcol, '\t%s', ['Stdev_', m{1}]);
-    fprintf(fcol, '\n'); 
- end
- 
- 
- 
- 
- p=2;% reset for perobject table
- 
- %perobject table's colnames
+    fprintf(fcol, '\n');
+end
+
+
+
+
+p=2;% reset for perobject table
+
+%perobject table's colnames
 fprintf(fcol,'%sPerObject',TablePrefix);
 fprintf(fcol, '\t%s', 'col1');
 fprintf(fcol, '\t%s','ImageNumber');
@@ -193,27 +193,24 @@ fprintf(fcol,'%sPerObject',TablePrefix);
 fprintf(fcol, '\t%s', 'col2');
 fprintf(fcol, '\t%s','ObjectNumber');
 fprintf(fcol, '\n');
- 
- 
- for n=per_object_names,
-     p=p+1;
+
+
+for n=per_object_names,
+    p=p+1;
     fprintf(fcol, '%sPerObject',TablePrefix);
     fprintf(fcol, '\t%s', ['col', num2str(p)]);
     fprintf(fcol, '\t%s', n{1} );
-    fprintf(fcol, '\n'); 
- end
- %end for colname file
- 
- 
-perobjectvals = [];
+    fprintf(fcol, '\n');
+end
+%end for colname file
 
+
+perobjectvals = [];
 
 for img_idx = FirstSet:LastSet,
     perobjectvals_mean=[];
     fprintf(fimage, '%d', img_idx);
-    objectbaserow = size(perobjectvals, 1);
     objectbasecol = 2;
-        objectbaserow_mean=size(perobjectvals_mean,1);
     numobj = 0;
 
     for RemainingSubMeasurementFieldnames = SubMeasurementFieldnames,
@@ -230,13 +227,13 @@ for img_idx = FirstSet:LastSet,
             if strfind(ssf, 'PathnameOrig'),
                 continue;
             end
-           if strfind(ssf, 'ModuleError'),
+            if strfind(ssf, 'ModuleError'),
                 continue;
             end
-           if strfind(ssf, 'TimeElapsed'),
+            if strfind(ssf, 'TimeElapsed'),
                 continue;
-            end 
-            
+            end
+
             if strfind(ssf, 'Text'),
                 if (strfind(ssf, 'Text') + 3) == length(ssf),
                     continue;
@@ -248,8 +245,6 @@ for img_idx = FirstSet:LastSet,
 
             vals = handles.Measurements.(SubFieldname).(ssf){img_idx};
 
-           
-                
             if (size(vals,1) == 1),
                 if ischar(vals),
                     fprintf(fimage, '\t%s', vals);
@@ -263,54 +258,49 @@ for img_idx = FirstSet:LastSet,
                     else, %vals{cellindex} is not char
                         fprintf(fimage, '\t%g', cell2mat(vals));
                     end
-                %elseif isempty(vals)
-                 %   fprintf(fimage, '\t%g',vals);
+                    %elseif isempty(vals)
+                    %   fprintf(fimage, '\t%g',vals);
                 else %vals is number
-                    
+
                     fprintf(fimage, '\t%g', vals);
                 end
             else
+                objectbaserow = size(perobjectvals,1);
+                objectbaserow_mean=size(perobjectvals_mean,1);
                 if (~ isa(vals, 'numeric')),
                     error('Non-numeric data not currently supported in per-object SQL data');
                 end
-                  
+
                 numcols = size(vals,2);
                 numobj = size(vals,1);
-                   
+
                 perobjectvals((objectbaserow+1):(objectbaserow+numobj), (objectbasecol+1):(objectbasecol+numcols)) = vals;
                 perobjectvals_mean((objectbaserow_mean+1):(objectbaserow_mean+numobj), (objectbasecol-2+1):(objectbasecol-2+numcols)) = vals;
-                
+
                 objectbasecol = objectbasecol + numcols;
+                if numobj > 0,
+                    perobjectvals((objectbaserow+1):end, 1) = img_idx;
+                    perobjectvals((objectbaserow+1):end, 2) = 1:numobj;
+                end
             end
         end
-         
-        
-        if numobj > 0,
-                   
-            perobjectvals((objectbaserow+1):end, 1) = img_idx;
-            perobjectvals((objectbaserow+1):end, 2) = 1:numobj;
-        end
-        
-        
     end
-    %print mean, stdev for all measurements per image
     
+    %print mean, stdev for all measurements per image
     fprintf(fimage,'\t');
     formatstr = ['%g' repmat('\t%g',1,size(perobjectvals_mean, 2)-1)];
-    fprintf(fimage,formatstr,(nanmean(perobjectvals_mean))); % ignore NaN
+    fprintf(fimage,formatstr,(CPnanmean(perobjectvals_mean))); % ignore NaN
     fprintf(fimage,'\t');
     fprintf(fimage,formatstr,nanstd(perobjectvals_mean));%ignore NaN
     fprintf(fimage, '\n');
-  
-    
+
 end
- 
-   
+
 formatstr = ['%g' repmat('\t%g',1,size(perobjectvals, 2)-1) '\n'];
 %if vals{1} is empty skip writting into object file
-  if ~iscell(vals) ||( iscell(vals) && (~isempty(vals{1}))  )
+if ~iscell(vals) ||( iscell(vals) && (~isempty(vals{1}))  )
     fprintf(fobject, formatstr, perobjectvals');
-  end
+end
 
 fclose(fimage);
 fclose(fobject);

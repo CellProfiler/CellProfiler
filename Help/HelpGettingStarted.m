9ba4d160fc65078bd28a52d02554e7e5e3c8cad4
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-11-16 20:18:54 +0000

    Getting Started with CellProfiler
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2633 1fc53939-2000-0410-845c-e8453a809027

---
+++ Help/HelpGettingStarted.m
@@ -0,0 +1,13 @@
+function HelpGettingStarted
+helpdlg(help('HelpGettingStarted'))
+
+% Choose image analysis modules to add to your analysis routine by  
+% clicking '+'.  Typically, the first module which must be run is a  
+% LoadImages module, where you specify the identity of the images  
+% that you want to analyze. Modules are added to the end of the  
+% pipeline, but their order can be adjusted by selecting module(s) and  
+% using the Move up '^' and Move down 'v' buttons. The '-' button  
+% will delete selected module(s) from the pipeline. Clicking a module  
+% in the pipeline will reveal its settings in the space to the right of  
+% the selected module. Properly formatted image analysis modules for  
+% CellProfiler are Matlab m-files that end with .m.

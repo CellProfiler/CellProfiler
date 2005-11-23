f6aa024ef3db72696c5455deb1cd581d9d4c6e4d
xuefang_ma <xuefang_ma@1fc53939-2000-0410-845c-e8453a809027>
2005-11-23 22:46:16 +0000

    update
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@2746 1fc53939-2000-0410-845c-e8453a809027

--- DataTools/ExportDataToFiles.m
+++ DataTools/ExportDataToFiles.m
@@ -2,9 +2,9 @@ function ExportDataToFiles (handles)
 
 ButtonSelected =questdlg('What format do you want to export to?', 'Select Export Type', 'Excel', 'SQL', 'Cancel', 'Excel')
 if strcmp(ButtonSelected, 'Excel'),
-    ExportExcel(handles);
+    CPExportExcel(handles);
 elseif strcmp(ButtonSelected, 'SQL'),
-    ExportSQL(handles);
+    CPExportSQL(handles);
 else
     return;
 end 

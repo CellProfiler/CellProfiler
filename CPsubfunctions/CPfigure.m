fc397f987cab08a6d78a73f91df2f7f78e26f40d
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-12-12 20:34:47 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3100 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPfigure.m
+++ CPsubfunctions/CPfigure.m
@@ -31,7 +31,7 @@ drawnow
 % Matlab to pause and carry out any pending figure window- related
 % commands (like zooming, or pressing timer pause or cancel buttons or
 % pressing a help button.)  If the drawnow command is not used
-% immediately prior to the figure(ThisAlgFigureNumber) line, then
+% immediately prior to the figure(ThisModuleFigureNumber) line, then
 % immediately after the figure line executes, the other commands that
 % have been waiting are executed in the other windows.  Then, when
 % Matlab returns to this module and goes to the subplot line, the
@@ -41,7 +41,12 @@ drawnow
 userData.Application = 'CellProfiler';
 userData.ImageFlag = 0;
 if nargin>0 && isfield(varargin{1},'Pipeline')
-    userData.MyHandles=varargin{1};
+    FixedHandles = varargin{1};
+    FixedHandles = rmfield(FixedHandles,'Pipeline');
+    if isfield(FixedHandles,'Measurements')
+        FixedHandles = rmfield(FixedHandles,'Measurements');
+    end
+    userData.MyHandles = FixedHandles;
     FigHandle=figure(varargin{3:end});
     if nargin>=2
         %%% This is for the typical usage:

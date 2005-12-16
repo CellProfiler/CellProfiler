04ded14e41c5f94d08c27b98d93c8ec3acc304f6
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-12-16 16:26:25 +0000

    creates table for the manual that has short descriptions of all modules
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3156 1fc53939-2000-0410-845c-e8453a809027

---
+++ ManualTableWizard.m
@@ -0,0 +1,40 @@
+function ManualTableWizard
+
+fid = fopen('ManualTableWizardText.xls','w');
+
+Modulefilelist = dir('Modules/*.m');
+for i=1:length(Modulefilelist)
+    name=Modulefilelist(i).name;
+    name=name(1:end-2);
+    fprintf(fid,[name,'\t']);
+    body = char(strread(help(Modulefilelist(i).name),'%s','delimiter','','whitespace',''));
+    flag = 0;
+    for j = 1:size(body,1)
+        if strcmp(body(j,1:75),'  *************************************************************************')
+            flag = 0;
+            fprintf(fid,'\n');
+        end
+        if flag
+            fixedtext = fixthistext2(body(j,:));
+            fprintf(fid,fixedtext(3:end));
+        end
+        if strcmp(body(j,1:20),'  SHORT DESCRIPTION:')
+            flag = 1;
+        end
+    end
+end
+
+fclose(fid);
+
+function fixedtext = fixthistext2(text)
+fixedtext = strrep(text,'''','''''');
+fixedtext = strrep(fixedtext,'\','\\');
+fixedtext = strrep(fixedtext,'%','%%');
+while 1
+    if strcmp(fixedtext(end),' ')
+        fixedtext = fixedtext(1:end-1);
+    else
+        fixedtext = [fixedtext,' '];
+        break;
+    end
+end

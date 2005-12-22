61eb1d38346f67f20fd2656ae03798f935ac205d
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2005-12-22 14:04:14 +0000

    ManualTableWizard now seperates module types
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3185 1fc53939-2000-0410-845c-e8453a809027

--- ManualTableWizard.m
+++ ManualTableWizard.m
@@ -3,11 +3,121 @@ function ManualTableWizard
 fid = fopen('ManualTableWizardText.xls','w');
 
 Modulefilelist = dir('Modules/*.m');
+FileProcessingFiles ={};
+PreProcessingFiles={};
+ObjectProcessingFiles={};
+MeasurementFiles={};
+OtherFiles={};
 for i=1:length(Modulefilelist)
-    name=Modulefilelist(i).name;
-    name=name(1:end-2);
+    name = Modulefilelist(i).name;
+    if file_in_category(Modulefilelist(i).name, 'File Processing')
+        FileProcessingFiles(length(FileProcessingFiles)+1)=cellstr(name);
+    elseif file_in_category(Modulefilelist(i).name, 'Image Processing')
+        PreProcessingFiles(length(PreProcessingFiles)+1)=cellstr(name);
+    elseif file_in_category(Modulefilelist(i).name, 'Object Processing')
+        ObjectProcessingFiles(length(ObjectProcessingFiles)+1)=cellstr(name);
+    elseif file_in_category(Modulefilelist(i).name, 'Measurement')
+        MeasurementFiles(length(MeasurementFiles)+1)=cellstr(name);
+    else
+        OtherFiles(length(OtherFiles)+1)=cellstr(name);
+    end
+end
+
+fprintf(fid,'MODULES\n');
+fprintf(fid,'File Processing Files\n');
+for i=1:length(FileProcessingFiles)
+    name = FileProcessingFiles{i};
+    name = name(1:end-2);
     fprintf(fid,[name,'\t']);
-    body = char(strread(help(Modulefilelist(i).name),'%s','delimiter','','whitespace',''));
+    body = char(strread(help(FileProcessingFiles{i}),'%s','delimiter','','whitespace',''));
+    flag = 0;
+    for j = 1:size(body,1)
+        if strcmp(body(j,1:75),'  *************************************************************************')
+            flag = 0;
+            fprintf(fid,'\n');
+        end
+        if flag
+            fixedtext = fixthistext2(body(j,:));
+            fprintf(fid,fixedtext(3:end));
+        end
+        if strcmp(body(j,1:20),'  SHORT DESCRIPTION:')
+            flag = 1;
+        end
+    end
+end
+
+fprintf(fid,'\nImage Processing Files\n');
+for i=1:length(PreProcessingFiles)
+    name = PreProcessingFiles{i};
+    name = name(1:end-2);
+    fprintf(fid,[name,'\t']);
+    body = char(strread(help(PreProcessingFiles{i}),'%s','delimiter','','whitespace',''));
+    flag = 0;
+    for j = 1:size(body,1)
+        if strcmp(body(j,1:75),'  *************************************************************************')
+            flag = 0;
+            fprintf(fid,'\n');
+        end
+        if flag
+            fixedtext = fixthistext2(body(j,:));
+            fprintf(fid,fixedtext(3:end));
+        end
+        if strcmp(body(j,1:20),'  SHORT DESCRIPTION:')
+            flag = 1;
+        end
+    end
+end
+
+fprintf(fid,'\nObject Processing Files\n');
+for i=1:length(ObjectProcessingFiles)
+    name = ObjectProcessingFiles{i};
+    name = name(1:end-2);
+    fprintf(fid,[name,'\t']);
+    body = char(strread(help(ObjectProcessingFiles{i}),'%s','delimiter','','whitespace',''));
+    flag = 0;
+    for j = 1:size(body,1)
+        if strcmp(body(j,1:75),'  *************************************************************************')
+            flag = 0;
+            fprintf(fid,'\n');
+        end
+        if flag
+            fixedtext = fixthistext2(body(j,:));
+            fprintf(fid,fixedtext(3:end));
+        end
+        if strcmp(body(j,1:20),'  SHORT DESCRIPTION:')
+            flag = 1;
+        end
+    end
+end
+
+fprintf(fid,'\nMeasurement Files\n');
+for i=1:length(MeasurementFiles)
+    name = MeasurementFiles{i};
+    name = name(1:end-2);
+    fprintf(fid,[name,'\t']);
+    body = char(strread(help(MeasurementFiles{i}),'%s','delimiter','','whitespace',''));
+    flag = 0;
+    for j = 1:size(body,1)
+        if strcmp(body(j,1:75),'  *************************************************************************')
+            flag = 0;
+            fprintf(fid,'\n');
+        end
+        if flag
+            fixedtext = fixthistext2(body(j,:));
+            fprintf(fid,fixedtext(3:end));
+        end
+        if strcmp(body(j,1:20),'  SHORT DESCRIPTION:')
+            flag = 1;
+        end
+    end
+end
+
+fprintf(fid,'\nOther Files\n');
+for i=1:length(OtherFiles)
+    name = OtherFiles{i};
+    name = name(1:end-2);
+    fprintf(fid,[name,'\t']);
+    body = char(strread(help(OtherFiles{i}),'%s','delimiter','','whitespace',''));
     flag = 0;
     for j = 1:size(body,1)
         if strcmp(body(j,1:75),'  *************************************************************************')
@@ -86,3 +196,7 @@ while 1
         break;
     end
 end
+
+function c = file_in_category(filename, category)
+h = help(filename);
+c = strfind(h, ['Category: ' category]);

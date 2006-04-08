8d79b3c25df094c999592ae749df9b0ecc9ec7a0
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2006-04-08 07:22:49 +0000

    Add std. dev & median selections for Export Data module
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3473 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPnanmedian.m
@@ -0,0 +1,68 @@
+function m = CPnanmedian(x,dim)
+%NANMEAN Mean value, ignoring NaNs.
+%   M = NANMEAN(X) returns the sample mean of X, treating NaNs as missing
+%   values.  For vector input, M is the mean value of the non-NaN elements
+%   in X.  For matrix input, M is a row vector containing the mean value of
+%   non-NaN elements in each column.  For N-D arrays, NANMEAN operates
+%   along the first non-singleton dimension.
+%
+%   NANMEAN(X,DIM) takes the mean along dimension DIM of X.
+%
+%   See also MEAN, NANMEDIAN, NANSTD, NANVAR, NANMIN, NANMAX, NANSUM.
+
+%   Copyright 1993-2004 The MathWorks, Inc.
+%   $Revision: 2.13.4.3 $  $Date: 2004/07/28 04:38:41 $
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%   Susan Ma
+%   Wyman Li
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 2802 $
+
+m = nanmedian(x,dim);
+
+
+
+
+
+% below is the piece of code, originally used to find mean, but now has
+% been modified to find median.
+
+
+
+%{
+% Find NaNs and set them to zero
+nans = isnan(x);
+x(nans) = 0;
+
+if nargin == 1 % let sum deal with figuring out which dimension to use
+    % Count up non-NaNs.
+    n = sum(~nans);
+    n(n==0) = NaN; % prevent divideByZero warnings
+    % Sum up non-NaNs, and divide by the number of non-NaNs.
+    m = sum(x) ./ n;
+else
+    % Count up non-NaNs.
+    n = sum(~nans,dim);
+    n(n==0) = NaN; % prevent divideByZero warnings
+    % Sum up non-NaNs, and divide by the number of non-NaNs.
+    m = sum(x,dim) ./ n;
+end
+
+%}

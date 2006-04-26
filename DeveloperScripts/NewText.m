3aaf435d83b1984d4d1910fd93992f5939f268e3
mrl <mrl@1fc53939-2000-0410-845c-e8453a809027>
2006-04-26 13:01:17 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3520 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/NewText.m
+++ DeveloperScripts/NewText.m
@@ -1,1 +1,18 @@
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
 %
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%   Susan Ma
+%
+% Website: http://www.cellprofiler.org

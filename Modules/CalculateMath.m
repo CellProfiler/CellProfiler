5c3ac910d67abb623bdd25f7dd3a2bb34f17e93b
peter_swire <peter_swire@1fc53939-2000-0410-845c-e8453a809027>
2006-05-10 15:42:47 +0000

    removed lint
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3579 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/CalculateMath.m
@@ -0,0 +1,184 @@
+function handles = CalculateMath(handles)
+
+% Help for the Calculate Ratios module:
+% Category: Measurement
+%
+% SHORT DESCRIPTION:
+% Calculates the ratio between any measurements already measured (e.g.
+% Intensity of green staining in cytoplasm/Area of cells)
+% *************************************************************************
+%
+% This module can take any measurements produced by previous modules and
+% calculate a ratio. Resulting ratios can also be used to calculate other
+% ratios and be used in Classify Objects.
+%
+% This module currently works on an object-by-object basis (it calculates
+% the ratio for each object) but can also calculate ratios for measurements
+% made for entire images (but only for measurements produced by the
+% Correlation module).
+%
+% Feature Number:
+% The feature number specifies which features from the Measure module(s)
+% will be used for the ratio. See each Measure module's help for the
+% numbered list of the features measured by that module.
+%
+% See also all Measure modules.
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 1843 $
+
+%%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%%
+drawnow
+
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+%textVAR01 = Which object would you like to use for the numerator (The option IMAGE currently only works with Correlation measurements)?
+%choiceVAR01 = Image
+%infotypeVAR01 = objectgroup
+%inputtypeVAR01 = popupmenu
+NumObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+
+%textVAR02 = Which category of measurements would you like to use?
+%choiceVAR02 = AreaShape
+%choiceVAR02 = Correlation
+%choiceVAR02 = Intensity
+%choiceVAR02 = Neighbors
+%choiceVAR02 = Texture
+%inputtypeVAR02 = popupmenu custom
+NumMeasure = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+
+%textVAR03 = Which feature do you want to use? (Enter the feature number - see help for details)
+%defaultVAR03 = 1
+NumFeatureNumber = str2double(handles.Settings.VariableValues{CurrentModuleNum,3});
+
+%textVAR04 = For INTENSITY or TEXTURE features, which image's measurements would you like to use?
+%infotypeVAR04 = imagegroup
+%inputtypeVAR04 = popupmenu
+NumImage = char(handles.Settings.VariableValues{CurrentModuleNum,4});
+
+%textVAR05 = Which object would you like to use for the denominator (The option IMAGE currently only works with Correlation measurements)?
+%choiceVAR05 = Image
+%infotypeVAR05 = objectgroup
+%inputtypeVAR05 = popupmenu
+DenomObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,5});
+
+%textVAR06 = Which category of measurements would you like to use?
+%choiceVAR06 = AreaShape
+%choiceVAR06 = Correlation
+%choiceVAR06 = Intensity
+%choiceVAR06 = Neighbors
+%choiceVAR06 = Texture
+%inputtypeVAR06 = popupmenu custom
+DenomMeasure = char(handles.Settings.VariableValues{CurrentModuleNum,6});
+
+%textVAR07 = Which feature do you want to use? (Enter the feature number - see help for details)
+%defaultVAR07 = 1
+DenomFeatureNumber = str2double(handles.Settings.VariableValues{CurrentModuleNum,7});
+
+%textVAR08 = For INTENSITY or TEXTURE features, which image's measurements would you like to use?
+%infotypeVAR08 = imagegroup
+%inputtypeVAR08 = popupmenu
+DenomImage = char(handles.Settings.VariableValues{CurrentModuleNum,8});
+
+%textVAR09 = Do you want the log (base 10) of the ratio?
+%choiceVAR09 = No
+%choiceVAR09 = Yes
+%inputtypeVAR09 = popupmenu
+LogChoice = char(handles.Settings.VariableValues{CurrentModuleNum,9});
+
+%textVAR10 = Raise to what power?
+%defaultVAR10 = 1
+Power = str2double(handles.Settings.VariableValues{CurrentModuleNum,10});
+
+%textVAR11 = Operation?
+%choiceVAR11 = Multiply
+%choiceVAR11 = Divide
+%choiceVAR11 = Add
+%choiceVAR11 = Subtract
+%inputtypeVAR11 = popupmenu
+Operation = char(handles.Settings.VariableValues(CurrentModuleNum, 11));
+
+%%%VariableRevisionNumber = 4
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% MAKE MEASUREMENTS & SAVE TO HANDLES STRUCTURE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+SetBeingAnalyzed = handles.Current.SetBeingAnalyzed;
+
+if strcmp(NumMeasure,'Intensity') || strcmp(NumMeasure,'Texture')
+    NumMeasure = [NumMeasure, '_', NumImage];
+end
+
+if strcmp(DenomMeasure,'Intensity') || strcmp(DenomMeasure,'Texture')
+    DenomMeasure = [DenomMeasure, '_', DenomImage];
+end
+
+% Get measurements
+NumeratorMeasurements = handles.Measurements.(NumObjectName).(NumMeasure){SetBeingAnalyzed};
+NumeratorMeasurements = NumeratorMeasurements(:,NumFeatureNumber);
+DenominatorMeasurements = handles.Measurements.(DenomObjectName).(DenomMeasure){SetBeingAnalyzed};
+DenominatorMeasurements = DenominatorMeasurements(:,DenomFeatureNumber);
+
+if length(NumeratorMeasurements) ~= length(DenominatorMeasurements)
+    error(['Image processing was canceled in the ', ModuleName, ' module because the specified object names ',NumObjectName,' and ',DenomObjectName,' do not have the same object count.']);
+end
+
+
+if( strcmpi(Operation, 'Multiply') )
+    NewFieldName = [NumObjectName,'_',NumMeasure(1),'_',num2str(NumFeatureNumber),'_Multiply_',DenomObjectName,'_',DenomMeasure(1),'_',num2str(DenomFeatureNumber)];
+    FinalMeasurements = NumeratorMeasurements.*DenominatorMeasurements;
+elseif( strcmpi(Operation, 'Divide') )
+    NewFieldName = [NumObjectName,'_',NumMeasure(1),'_',num2str(NumFeatureNumber),'_Divide_',DenomObjectName,'_',DenomMeasure(1),'_',num2str(DenomFeatureNumber)];
+    FinalMeasurements = NumeratorMeasurements./DenominatorMeasurements;
+elseif( strcmpi(Operation, 'Add') )
+    NewFieldName = [NumObjectName,'_',NumMeasure(1),'_',num2str(NumFeatureNumber),'_Add_',DenomObjectName,'_',DenomMeasure(1),'_',num2str(DenomFeatureNumber)];
+    FinalMeasurements = NumeratorMeasurements + DenominatorMeasurements;
+elseif( strcmpi(Operation, 'Subtract') )
+    NewFieldName = [NumObjectName,'_',NumMeasure(1),'_',num2str(NumFeatureNumber),'_Subtract_',DenomObjectName,'_',DenomMeasure(1),'_',num2str(DenomFeatureNumber)];
+    FinalMeasurements = NumeratorMeasurements - DenominatorMeasurements;
+end
+    
+if strcmp(LogChoice,'Yes')
+    FinalMeasurements = log10(FinalMeasurements);
+end
+
+if ~isnan(Power)
+    FinalMeasurements = FinalMeasurements .^ Power;
+end
+
+handles = CPaddmeasurements(handles,NumObjectName,'Ratio',NewFieldName,FinalMeasurements);
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% The figure window display is unnecessary for this module, so it is
+%%% closed during the starting image cycle.
+if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+    ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+    if any(findobj == ThisModuleFigureNumber)
+        close(ThisModuleFigureNumber)
+    end
+end

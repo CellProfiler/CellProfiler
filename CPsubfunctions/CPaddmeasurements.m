4d7700f80ab6c43eaa7d50319bc959d9f8947830
reipince <reipince@1fc53939-2000-0410-845c-e8453a809027>
2006-06-28 12:58:50 +0000

    now works when objects are not identified in first cycle
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3718 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPaddmeasurements.m
+++ CPsubfunctions/CPaddmeasurements.m
@@ -23,7 +23,8 @@ function handles = CPaddmeasurements(handles,Object,Measure,Feature,Data)
 FeaturesField = [Measure,'Features'];
 
 if isfield(handles.Measurements.(Object),FeaturesField)
-    if handles.Current.SetBeingAnalyzed == 1
+    OldColumn = strmatch(Feature,handles.Measurements.(Object).(FeaturesField));
+    if handles.Current.SetBeingAnalyzed == 1 || isempty(OldColumn)
         CheckForRedundant = strmatch(Feature,handles.Measurements.(Object).(FeaturesField));
         if length(CheckForRedundant) > 1
             error('Image processing was canceled because you are attempting to create the same measurements, please remove redundant module.');
@@ -32,7 +33,6 @@ if isfield(handles.Measurements.(Object),FeaturesField)
         handles.Measurements.(Object).(FeaturesField)(NewColumn) = {Feature};
         handles.Measurements.(Object).(Measure){handles.Current.SetBeingAnalyzed}(:,NewColumn) = Data;
     else
-        OldColumn = strmatch(Feature,handles.Measurements.(Object).(FeaturesField));
         if length(OldColumn) > 1
             error('Image processing was canceled because you are attempting to create the same measurements, please remove redundant module.');
         elseif isempty(OldColumn)

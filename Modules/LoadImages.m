a840624bebb0f2060c7fd19f0ba02eef7f31a7fd
junliu <junliu@1fc53939-2000-0410-845c-e8453a809027>
2006-06-30 18:47:32 +0000

    updated (Jun): fixed unexpected text displays
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3748 1fc53939-2000-0410-845c-e8453a809027

--- Modules/LoadImages.m
+++ Modules/LoadImages.m
@@ -619,13 +619,13 @@ if any(findobj == ThisModuleFigureNumber);
     end
     for n = 1:length(ImageName)
         %%% Activates the appropriate figure window.
-        CPfigure(handles,'Text',ThisModuleFigureNumber);
+        currentfig = CPfigure(handles,'Text',ThisModuleFigureNumber);
         if iscell(ImageName)
             TextString = [ImageName{n},': ',FileNames{n}];
         else
             TextString = [ImageName,': ',FileNames];
         end
-        uicontrol('style','text','units','normalized','fontsize',handles.Preferences.FontSize,'HorizontalAlignment','left','string',TextString,'position',[.05 .85-(n-1)*.15 .95 .1],'BackgroundColor',[.7 .7 .9])
+        uicontrol(currentfig,'style','text','units','normalized','fontsize',handles.Preferences.FontSize,'HorizontalAlignment','left','string',TextString,'position',[.05 .85-(n-1)*.15 .95 .1],'BackgroundColor',[.7 .7 .9])
     end
 end
 

db38c77867051ab35c3824f3a5cdc9ef7652cdd9
reipince <reipince@1fc53939-2000-0410-845c-e8453a809027>
2006-07-11 20:04:53 +0000

    added help for regular expressions
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3811 1fc53939-2000-0410-845c-e8453a809027

--- Modules/LoadImages.m
+++ Modules/LoadImages.m
@@ -47,10 +47,71 @@ function handles = LoadImages(handles)
 % How do you want to load these files?
 % Text is used to load images (or movies) that have a particular piece of
 % text in the name. You have the option of matching text exactly, or using
-% regular expressions to match text. For example, typing image[12]dapi in
-% the box asking for text in common and typing R in the Exact/Regular
-% expression box will select any file containing the digit 1 or 2
-% immediately in between the text 'image' and 'dapi'.
+% regular expressions to match text.
+%
+% When regular expressions is selected, patterns are specified using
+% combinations of metacharacters and literal characters. There are a few
+% classes of metacharacters, partially listed below. More extensive
+% explanation can be found at:
+%   http://www.mathworks.com/access/helpdesk/help/techdoc/matlab_prog/f0-42
+%   649.html
+%  
+% The following metacharacters match exactly one character from its
+% respective set of characters:
+%  
+%   Metacharacter   Meaning
+%  ---------------  --------------------------------
+%              .    Any character
+%             []    Any character contained within the brackets
+%            [^]    Any character not contained within the brackets
+%             \w    A word character [a-z_A-Z0-9]
+%             \W    Not a word character [^a-z_A-Z0-9]
+%             \d    A digit [0-9]
+%             \D    Not a digit [^0-9]
+%             \s    Whitespace [ \t\r\n\f\v]
+%             \S    Not whitespace [^ \t\r\n\f\v]
+%  
+% The following metacharacters are used to logically group subexpressions
+% or to specify context for a position in the match. These metacharacters
+% do not match any characters in the string:
+%
+%   Metacharacter   Meaning
+%  ---------------  --------------------------------
+%            ()     Group subexpression
+%             |     Match subexpression before or after the |
+%             ^     Match expression at the start of string
+%             $     Match expression at the end of string
+%            \<     Match expression at the start of a word
+%            \>     Match expression at the end of a word
+%  
+% The following metacharacters specify the number of times the previous
+% metacharacter or grouped subexpression may be matched:
+%     
+%   Metacharacter   Meaning
+%  ---------------  --------------------------------
+%             *     Match zero or more occurrences
+%             +     Match one or more occurrences
+%             ?     Match zero or one occurrence
+%          {n,m}    Match between n and m occurrences
+%  
+% Characters that are not special metacharacters are all treated literally
+% in a match. To match a character that is a special metacharacter, escape
+% that character with a '\'. For example '.' matches any character, so to
+% match a '.' specifically, use '\.' in your pattern.
+%
+% Examples:
+% 
+%     * [trm]ail matches 'tail' or 'rail' or 'mail'
+%     * [0-9] matches any digit between 0 to 9
+%     * [^Q-S] matches any character other than 'Q' or 'R' or 'S'
+%     * [[]A-Z] matches any upper case alphabet along with square brackets
+%     * [ag-i-9] matches characters 'a' or 'g' or 'h' or 'i' or '-' or '9'
+%     * [a-p]* matches '' or 'a' or 'aab' or 'p' etc.
+%     * [a-p]+ matches  'a' or 'abc' or 'p' etc.
+%     * [^0-9] matches any string that is not a number
+%     * ^[0-9]*$ matches any string that is a natural number or ''
+%     * ^-[0-9]+$|^\+?[0-9]+$ matches any integer
+%
 % Order is used when images (or movies) are present in a repeating order,
 % like DAPI, FITC, Red, DAPI, FITC, Red, and so on, where images are
 % selected based on how many images are in each group and what position
@@ -222,7 +283,7 @@ ImageOrMovie = char(handles.Settings.VariableValues{CurrentModuleNum,11});
 FileFormat = char(handles.Settings.VariableValues{CurrentModuleNum,12});
 %inputtypeVAR12 = popupmenu
 
-%textVAR13 = Analyze all subfolders within the selected folder (Y or N)?
+%textVAR13 = Analyze all subfolders within the selected folder?
 %choiceVAR13 = No
 %choiceVAR13 = Yes
 AnalyzeSubDir = char(handles.Settings.VariableValues{CurrentModuleNum,13});

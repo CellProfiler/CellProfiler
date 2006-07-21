acda9522aa5b891ef36a6a17f29dcb3684c3a876
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2006-07-21 23:24:46 +0000

    temporarily hard-coded DIB reader to be 12 bits
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@3915 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPimread.m
+++ CPsubfunctions/CPimread.m
@@ -36,13 +36,14 @@ elseif nargin == 2,
             error(['The file ', char(CurrentFileName), ' could not be opened. CellProfiler attempted to open it in DIB file format.']);
         end
         A = fread(fid, 52, 'uchar');
-        
+IamHere = 1        
         Width = toDec2(A(5:8));
         Height = toDec2(A(9:12));
 %        BitDepth = toDec2(A(15:16));
 BitDepth = 12;
         Channels = toDec2(A(13:14));
-        
+ IamHere = 6       
+       
         LoadedImage = zeros(Height,Width,Channels);
         for c=1:Channels,
             [Data, Count] = fread(fid, Width * Height, 'uint16', 0, 'l');
@@ -53,6 +54,7 @@ BitDepth = 12;
             LoadedImage(:,:,c) = reshape(Data, [Width Height])' / (2^BitDepth - 1);
         end
         fclose(fid);
+IamHere = 7       
     elseif strcmp('.MAT',upper(ext))
         load(CurrentFileName);
         if exist('Image')

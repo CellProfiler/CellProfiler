277639f1db00cf21b0fa978e71c1d8561c362348
reipince <reipince@1fc53939-2000-0410-845c-e8453a809027>
2006-08-03 18:22:25 +0000

    subfunction useful to make users select output files (made to remove redundant code from AddData and ClearData data tools)
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4064 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPselectoutputfiles.m
@@ -0,0 +1,27 @@
+function [Pathname SelectedFiles] = CPselectoutputfiles(handles)
+
+%%% Ask the user to choose the directory from which to extract measurements.
+if exist(handles.Current.DefaultOutputDirectory, 'dir')
+    Pathname = uigetdir(handles.Current.DefaultOutputDirectory,'Choose the folder that contains the output file(s) to use');
+else
+    Pathname = uigetdir(pwd,'Choose the folder that contains the output file(s) to use');
+end
+
+if Pathname ~= 0
+    %%% Get all files with .mat extension in the chosen directory that contains a 'OUT' in the filename
+    AllFiles = dir(Pathname);                                                        % Get all file names in the chosen directory
+    AllFiles = {AllFiles.name};                                                      % Cell array with file names
+    SelectedFiles = AllFiles(~cellfun('isempty',strfind(AllFiles,'.mat')));          % Keep files that has a .mat extension
+    SelectedFiles = SelectedFiles(~cellfun('isempty',strfind(SelectedFiles,'OUT'))); % Keep files with an 'OUT' in the name
+
+    %%% Let the user select the files
+    [selection,ok] = listdlg('liststring',SelectedFiles,'name','Select output files',...
+        'PromptString','Choose CellProfiler output files to use. Use Ctrl+Click or Shift+Click to choose multiple files.','listsize',[300 500]);
+    if ~ok
+        SelectedFiles = 0;
+    else
+        SelectedFiles = SelectedFiles(selection);
+    end
+else
+    SelectedFiles = 0;
+end

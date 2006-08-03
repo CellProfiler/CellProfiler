fe6a5457b124b8fef534521be6ffbd7e05136991
reipince <reipince@1fc53939-2000-0410-845c-e8453a809027>
2006-08-03 18:30:12 +0000

    added boilerplate
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4073 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPselectoutputfiles.m
+++ CPsubfunctions/CPselectoutputfiles.m
@@ -1,5 +1,25 @@
 function [Pathname SelectedFiles] = CPselectoutputfiles(handles)
 
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 4073 $
+
 %%% Ask the user to choose the directory from which to extract measurements.
 if exist(handles.Current.DefaultOutputDirectory, 'dir')
     Pathname = uigetdir(handles.Current.DefaultOutputDirectory,'Choose the folder that contains the output file(s) to use');

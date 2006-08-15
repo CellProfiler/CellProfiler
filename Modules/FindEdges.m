b0c8991451e58eaf03718b4575601099d22023f1
reipince <reipince@1fc53939-2000-0410-845c-e8453a809027>
2006-08-15 14:30:24 +0000

    added new method to find edges, updated help
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4183 1fc53939-2000-0410-845c-e8453a809027

--- Modules/FindEdges.m
+++ Modules/FindEdges.m
@@ -4,49 +4,73 @@ function handles = FindEdges(handles)
 % Category: Image Processing
 %
 % SHORT DESCRIPTION:
-% The FindEdges module employs serveral methods to identify any edges in an
-% image.  Once the edges are found, a resulting binary image will be produced
-% where only the edges are shown in white while everything else will be black. 
+% Employs several methods to identify edges in an image. Once the edges are
+% found, a grayscale or binary image showing them will be produced.
+% *************************************************************************
+%
+% This module will find the edges of objects in an image. You can choose
+% amongst several methods of finding the edges. All of them, except the
+% Ratio method, will return a binary image. The Ratio method can return a
+% binary image or a grayscale image depending on your choice.
 %
 % Settings:
-%   Method- There are several different methods that can be used to identify
-% edges.  In this module, the user is able to picke between Roberts, Sobel,
-% Prewitt, Log, and Canny.  These methods all implement different
-% techniques and algorithms which are suited towards different images.
-%       Sobel Method - finds edges using the Sobel approximation to the
-%                      derivative. It returns edges at those points where 
-%                      the gradient of which I is maximum. 
-%       Prewitt Method - finds edges using the Prewitt approximation to the
-%                      derivative. It returns edges at those points where
-%                      the gradient of I is maximum. 
-%       Roberts Method - finds edges using the Roberts approximation to the
-%                      derivative. It returns edges at those points where
-%                      the gradient of I is maximum. 
-%       Canny - The Canny method finds edges by looking for local maxima of
-%                      of the gradient of I. The gradient is calculated
-%                      using the derivative of a Gaussian filter. The
-%                      method uses two thresholds, to detect strong and
-%                      weak edges, and includes the weak edges in the
-%                      output only if they are connected to strong edges.
-%                      This method is therefore less likely than the others
-%                      to be fooled by noise, and more likely to detect
-%                      true weak edges.
+%
+% Method: There are several different methods that can be used to identify
+% edges. The user is able to pick between Roberts, Sobel, Prewitt, Log,
+% Canny, and Ipince. These methods all implement different techniques and
+% algorithms which are suited towards different images.
+%   Sobel Method - finds edges using the Sobel approximation to the
+%                  derivative. It returns edges at those points where the
+%                  gradient of the image is maximum.
+%   Prewitt Method - finds edges using the Prewitt approximation to the
+%                  derivative. It returns edges at those points where the
+%                  gradient of the image is maximum.
+%   Roberts Method - finds edges using the Roberts approximation to the
+%                  derivative. It returns edges at those points where the
+%                  gradient of the image is maximum.
+%   LoG Method - This method first applies a Laplacian of Gaussian filter
+%                to the image and then finds zero crossings.
+%   Canny Method - The Canny method finds edges by looking for local maxima
+%                  of the gradient of I. The gradient is calculated using
+%                  the derivative of a Gaussian filter. The method uses two
+%                  thresholds, to detect strong and weak edges, and
+%                  includes the weak edges in the output only if they are
+%                  connected to strong edges. This method is therefore less
+%                  likely than the others to be fooled by noise, and more
+%                  likely to detect true weak edges.
+%   Ratio Method - This method first applies two smoothing filters to the
+%                  image (sum of squares and square of sums), and then
+%                  takes the ratio of the two resulting images to determine
+%                  the edges. The filter size is then very important in
+%                  this method. The larger the filter size, the larger the
+%                  edges will be. The recommended size is 8 pixels, or
+%                  roughly half the width of the objects you wish to edge.
 % To find the best method for an image, you can compare the results of each
 % by eye.
 %
-%   Threshold - Enter the desired threshold or have CellProfiler choose one
-% automatically.  A different process is used to determine the threshold for
+% Threshold: Enter the desired threshold or have CellProfiler choose one
+% automatically. A different process is used to determine the threshold for
 % each method.
-%   Edge Thinning - This setting only applies for the Roberts method.  
-%   Direction - This setting applies only for Sobel and Prewitt methods.  It
-% gives you the option of identifying either only horizontal or vertical
-% edges or both.     
-%   Threshold Factor- The Threshold factor specified by the user will be
+% 
+% Edge Thinning: This setting only applies to the Sobel, Prewitt, and
+% Roberts methods. If thinning is selected, edges found will be thinned out
+% into a line (if possible).
+% 
+% Direction: This setting applies only for the Sobel and Prewitt methods.
+% It gives you the option of identifying either only horizontal or vertical
+% edges or both.
+%
+% Threshold Factor: The Threshold factor specified by the user will be
 % multiplied with the threshold used for edge detection.
-%   Binary or GrayScale- This option allows the option of having the
-% resulting image to have varying degrees of Gray (Grayscale) or of the
-% resulting image being strictly black and white (Binary).
-% *************************************************************************
+%
+% Size of smoothing filter: This setting applies only for the Ratio method.
+% A square of NxN will be used for the filter, where N is the size you
+% specify here. See method description above for further information.
+%
+% Binary or Grayscale: This setting applies only for the Ratio method. It
+% allows the option of having the resulting image to have varying degrees
+% of Gray (Grayscale) or of the resulting image being strictly black and
+% white (Binary).
 
 % CellProfiler is distributed under the GNU General Public License.
 % See the accompanying file LICENSE for details.
@@ -86,16 +110,17 @@ drawnow
 ImageName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 
 %textVAR02 = What do you want to call image?
-%defaultVAR02 = EdgedImaged
+%defaultVAR02 = EdgedImage
 %infotypeVAR02 = imagegroup indep
 OutputName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
 
 %textVAR03 = What method would you like to use?
-%choiceVAR03 = Roberts
 %choiceVAR03 = Sobel
 %choiceVAR03 = Prewitt
-%choiceVAR03 = Log
+%choiceVAR03 = Roberts
+%choiceVAR03 = LoG
 %choiceVAR03 = Canny
+%choiceVAR05 = Ratio
 %inputtypeVAR03 = popupmenu
 Method = char(handles.Settings.VariableValues{CurrentModuleNum,3});
 
@@ -103,20 +128,20 @@ Method = char(handles.Settings.VariableValues{CurrentModuleNum,3});
 %defaultVAR04 = /
 Threshold = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,4}));
 
-%textVAR05 = For the Roberts method, do you want edge thinning?
+%textVAR05 = For the Sobel, Prewitt, and Roberts methods, do you want edge thinning?
 %choiceVAR05 = Thinning
 %choiceVAR05 = No Thinning
 %inputtypeVAR05 = popupmenu
 Thinning = char(handles.Settings.VariableValues{CurrentModuleNum,5});
 
-%textVAR06 = For the Sobel and Prewitt methods, which direction?
+%textVAR06 = For the Sobel and Prewitt methods, which direction do you want to use?
 %choiceVAR06 = Both
 %choiceVAR06 = Horizontal
 %choiceVAR06 = Vertical
 %inputtypeVAR06 = popupmenu
 Direction = char(handles.Settings.VariableValues{CurrentModuleNum,6});
 
-%textVAR07 = For the log and Canny methods, what is the value of sigma? Use '/' for defaults.
+%textVAR07 = For the LoG and Canny methods, what is the value of sigma? Use '/' for defaults.
 %defaultVAR07 = /
 Sigma = str2double(handles.Settings.VariableValues{CurrentModuleNum,7});
 
@@ -124,23 +149,41 @@ Sigma = str2double(handles.Settings.VariableValues{CurrentModuleNum,7});
 %defaultVAR08 = /
 CannyLowThreshold = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,8}));
 
-
 %textVAR09 = What is the Threshold Correction Factor? The Threshold can be automatically found, then multiplied by this factor.
 %defaultVAR09 = 1
 ThresholdCorrectionFactor = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,9}));
 
-%textVAR10 = Do you want the output image to be binary (black/white) or grayscale?
-%choiceVAR10 = Grayscale
-%choiceVAR10 = Binary
-%inputtypeVAR10 = popupmenu
-BinaryOrGray = char(handles.Settings.VariableValues{CurrentModuleNum,10});
+%textVAR10 = For the Ratio method, specify the filter size (in pixels) that you would like to use. (see help for details)
+%defaultVAR10 = 8
+SizeOfSmoothingFilter = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,10}));
 
-%%%VariableRevisionNumber = 1
+%textVAR11 = For the Ratio method, do you want the output image to be binary (black/white) or grayscale?
+%choiceVAR11 = Grayscale
+%choiceVAR11 = Binary
+%inputtypeVAR11 = popupmenu
+BinaryOrGray = char(handles.Settings.VariableValues{CurrentModuleNum,11});
+
+%%%VariableRevisionNumber = 2
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% PRELIMINARY CALCULATIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
+
+%%% Variable check
+if isnan(Threshold)
+    Threshold = [];
+end
+
+if isnan(ThresholdCorrectionFactor)
+    ThresholdCorrectionFactor = 1;
+end
+
+if strcmpi(Thinning, 'thinning')
+    Thinning = 'thinning';
+elseif strcmpi(Thinning, 'no thinning')
+    Thinning = 'nothinning';
+end
 
 %%% Reads (opens) the image you want to analyze and assigns it to a variable,
 %%% "OrigImage".
@@ -154,23 +197,7 @@ drawnow
 
 EdgedImage = OrigImage;
 
-if isnan(Threshold)
-    Threshold = [];
-end
-
-if isnan(ThresholdCorrectionFactor)
-    ThresholdCorrectionFactor = 1;
-end
-
-Direction = lower(Direction);
-Method = lower(Method);
-
 if strcmpi(Method, 'roberts')
-    if strcmpi(Thinning, 'thinning')
-       Thinning = 'thinning';
-    elseif strcmpi(Thinning, 'no thinning')
-       Thinning = 'nothinning';
-    end
     
     [EdgedImage, AutoThresh] = edge(OrigImage, Method, Threshold, Thinning);
     
@@ -180,6 +207,7 @@ if strcmpi(Method, 'roberts')
         end
         EdgedImage = edge(OrigImage, Method, AutoThresh * ThresholdCorrectionFactor, Thinning);
     end
+
 elseif strcmpi(Method, 'canny')
     if isnan(Sigma) || Sigma <= 0
         Sigma = 1; %canny's default is 1
@@ -198,26 +226,24 @@ elseif strcmpi(Method, 'canny')
         end
         EdgedImage = edge(OrigImage, Method, AutoThresh * ThresholdCorrectionFactor, Sigma);
     end
+
 elseif  strcmpi(Method, 'log')
     if isnan(Sigma) || Sigma <= 0
         Sigma = 2; %log's default is two
         CPwarndlg(['The Sigma value you have entered in ', ModuleName, ' may be out of bounds. We set the value to 2']);
     end
 
-    if isnan(CannyLowThreshold)
-        CannyLowThreshold = [];
-    end
-    
-    [EdgedImage, AutoThresh] = edge(OrigImage,Method, [CannyLowThreshold Threshold], Sigma);
+    [EdgedImage, AutoThresh] = edge(OrigImage,Method, Threshold, Sigma);
     
     if ThresholdCorrectionFactor ~= 1
-         if (ThresholdCorrectionFactor * AutoThresh > 1) || (ThresholdCorrectionFactor * AutoThresh < 0)
+        if (ThresholdCorrectionFactor * AutoThresh > 1) || (ThresholdCorrectionFactor * AutoThresh < 0)
             CPwarndlg(['The Threshold Correction Factor you have entered in ', ModuleName, ' may be out of bounds.']);
         end
         EdgedImage = edge(OrigImage, Method, AutoThresh * ThresholdCorrectionFactor, Sigma);
     end
+
 elseif strcmpi(Method, 'sobel') || strcmpi(Method, 'prewitt')
-    [EdgedImage, AutoThresh] = edge(OrigImage, Method, Threshold, Direction);
+    [EdgedImage, AutoThresh] = edge(OrigImage, Method, Threshold, Direction, Thinning);
     
     if ThresholdCorrectionFactor ~= 1
         if (ThresholdCorrectionFactor * AutoThresh > 1) || (ThresholdCorrectionFactor * AutoThresh < 0)
@@ -225,11 +251,37 @@ elseif strcmpi(Method, 'sobel') || strcmpi(Method, 'prewitt')
         end
         EdgedImage = edge(OrigImage, Method, AutoThresh * ThresholdCorrectionFactor, Direction);
     end
+
+elseif strcmpi(Method,'ratio')
+    if isnan(SizeOfSmoothingFilter)
+        error(['Image processing was canceled in the ' ModuleName ' module because the size of smoothing filter you specified was invalid.']);
+    else
+        SizeOfSmoothingFilter = min(30,max(1,floor(SizeOfSmoothingFilter)));
+    end
+    Sq = CPsmooth(OrigImage,'Q',SizeOfSmoothingFilter,0);
+    Mn = CPsmooth(OrigImage,'S',SizeOfSmoothingFilter,0);
+    EdgedImage = Mn./Sq;
+    %%% The ratio image has really weird numbers, put it in the 0-1 range:
+    [handles, EdgedImage] = CPrescale(handles,EdgedImage,'S',[]);
+    if strcmp(BinaryOrGray,'Binary')
+        if isempty(Threshold)
+            AutoThresh = graythresh(EdgedImage);
+            if ThresholdCorrectionFactor ~= 1
+                if (ThresholdCorrectionFactor * AutoThresh > 1) || (ThresholdCorrectionFactor * AutoThresh < 0)
+                    CPwarndlg(['The Threshold Correction Factor you have entered in ', ModuleName, ' may be out of bounds.']);
+                end
+                Threshold = ThresholdCorrectionFactor * AutoThresh;
+            else
+                Threshold = AutoThresh;
+            end
+        end
+        EdgedImage = im2bw(EdgedImage, Threshold);
+    end
+%     if strcmp(Thinning,'thinning')
+%         EdgedImage = imerode(EdgedImage,strel('disk',3));
+%     end
 end
 
-if ~strcmp(BinaryOrGray,'Binary')
-    EdgedImage = double(EdgedImage);
-end
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% DISPLAY RESULTS %%%
@@ -251,7 +303,7 @@ if any(findobj == ThisModuleFigureNumber)
     %%%  image.
     subplot(2,1,2);
     CPimagesc(EdgedImage,handles);
-    title('Edged Image');
+    title(OutputName);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

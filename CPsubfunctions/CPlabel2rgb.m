52ddef6482f4bf7ca5b29a27be3a2f07b86c8846
vicky_lay <vicky_lay@1fc53939-2000-0410-845c-e8453a809027>
2006-08-18 17:54:43 +0000

    reverted
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4200 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPlabel2rgb.m
+++ CPsubfunctions/CPlabel2rgb.m
@@ -27,29 +27,12 @@ function [im, handles]=CPlabel2rgb(handles, image)
 
 %%% Note that the label2rgb function doesn't work when there are no objects
 %%% in the label matrix image, so there is an "if".
-numregion = double(max(image(:)));
 if sum(sum(image)) >= 1
-    if nargin == 2
-        cmap = eval([handles.Preferences.LabelColorMap '(255)']);
-        try
-            if numregion>length(handles.newcmap)
-                newregions = numregions-length(handles.newcmap);
-                newindex = round(rand(1,newregions)*255);
-                index = [index newindex];
-                handles.newcmap = cmap(index,:,:);
-            end
-        catch
-            S = rand('state');
-            rand('state', 0);
-            index = round(rand(1,numregion)*255);
-            handles.Pipeline.TrackObjects.Colormap = cmap(index,:,:);
-            rand('state', S);
-        end
-        im = label2rgb(image, handles.Pipeline.TrackObjects.Colormap, 'k', 'noshuffle');
-    else
-        cmap = eval([handles.Preferences.LabelColorMap '(max(2,max(image(:))))']);
-        im = label2rgb(image, cmap, 'k', 'shuffle');
-    end
+    cmap = eval([handles.Preferences.LabelColorMap '(max(2,max(image(:))))']);
+    im = label2rgb(image, cmap, 'k', 'shuffle');
 else
     im=image;
 end
+
+
+

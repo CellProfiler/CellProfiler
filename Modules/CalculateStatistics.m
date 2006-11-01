b65b84625fca070d5222a2a3ecfc60e57a415554
junliu <junliu@1fc53939-2000-0410-845c-e8453a809027>
2006-11-01 21:04:53 +0000

    updated CalcStatistics based on the EC50 version so now ther eis only one. -anne
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4404 1fc53939-2000-0410-845c-e8453a809027

--- Modules/CalculateStatistics.m
+++ Modules/CalculateStatistics.m
@@ -4,8 +4,8 @@ function handles = CalculateStatistics(handles)
 % Category: Measurement
 %
 % SHORT DESCRIPTION:
-% Calculates measures of assay quality (V and Z' factors) for all
-% measured features made from images.
+% Calculates measures of assay quality (V and Z' factors) and dose response
+% data (EC50) for all measured features made from images.
 % *************************************************************************
 %
 % The V and Z' factors are statistical measures of assay quality and are
@@ -15,7 +15,9 @@ function handles = CalculateStatistics(handles)
 % placing this module at the end of a pipeline allows you to choose which
 % measured features are most powerful for distinguishing positive and
 % negative control samples, or for accurately quantifying the assay's
-% response to dose.
+% response to dose. Both Z' and V factors will be calculated for all
+% measured values (Intensity, AreaShape, Texture, etc.). These measurements
+% can be exported as the "Experiment" set of data.
 %
 % For both Z' and V factors, the highest possible value (best assay
 % quality) = 1 and they can range into negative values (for assays where
@@ -62,7 +64,7 @@ function handles = CalculateStatistics(handles)
 % .00000001
 % .000000003
 % .000000001
-% (Note that in this examples, the Z' and V factors will be meaningless because
+% (Note that in this example, the Z' and V factors will be meaningless because
 % there is only one sample at the each dose, so the standard deviation of
 % measured features at each dose will be zero).
 %
@@ -86,6 +88,8 @@ function handles = CalculateStatistics(handles)
 %
 % Code for the calculation of Z' and V factors was kindly donated by Ilya
 % Ravkin: http://www.ravkin.net
+%
+% This module currently contains code copyrighted by Carlos Evangelista.
 
 % CellProfiler is distributed under the GNU General Public License.
 % See the accompanying file LICENSE for details.
@@ -119,19 +123,34 @@ drawnow
 
 [CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
 
-%textVAR01 = What did you call the grouping values?
+%textVAR01 = What did you call the grouping values you loaded for each image cycle? See help for details.
 %infotypeVAR01 = datagroup
-%inputtypeVAR01 = popupmenu
 DataName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+%inputtypeVAR01 = popupmenu
 
-%textVAR02 = In order to run this module, you must use the Load Text module to load a grouping value for each cycle. This is either a marking of whether each cycle is a positive or negative control (for Z factor) or it is concentrations (doses) for each curve (required for meaningful V factors). Both Z and V factors will be calculated for all measured values (Intensity, AreaShape, Texture, etc.). These measurements can be exported as the "Experiment" set of data.
+%textVAR02 = Would you like to log-transform the grouping values before attempting to fit a sigmoid curve?
+%choiceVAR02 = Yes
+%choiceVAR02 = No
+LogOrLinear = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+%inputtypeVAR02 = popupmenu
 
-%%%VariableRevisionNumber = 2
+%textVAR03 = If you want to save the plotted dose response data for each feature as an interactive figure in the default output folder, enter the filename here (.fig extension will be automatically added). Note: the figures do not stay open during processing because it tends to cause memory issues when so many windows are open. Note: This option is not compatible with running the pipeline on a cluster of computers.
+%defaultVAR03 = Do not save
+%infotypeVAR03 = imagegroup indep
+FigureName = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+
+%%%VariableRevisionNumber = 3
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% PRELIMINARY CALCULATIONS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
+
+%%% Checks whether the user has the Image Processing Toolbox.
+LicenseStats = license('test','statistics_toolbox');
+if LicenseStats ~= 1
+    CPwarndlg('It appears that you do not have a license for the Statistics Toolbox of Matlab.  You will be able to calculate V and Z'' factors, but not EC50 values. Typing ''ver'' or ''license'' at the Matlab command line may provide more information about your current license situation.');
+end
 
 if handles.Current.SetBeingAnalyzed == handles.Current.NumberOfImageSets
 
@@ -202,7 +221,18 @@ if handles.Current.SetBeingAnalyzed == handles.Current.NumberOfImageSets
                                 CPwarndlg('There was an error in the Calculate Statistics module involving the number of text elements loaded for it.  CellProfiler will proceed but this module will be skipped.');
                                 return;
                             else
-                                [v, z] = VZfactors(GroupingValues,Ymatrix);
+                                [v, z, OrderedUniqueDoses, OrderedAverageValues] = VZfactors(GroupingValues,Ymatrix);
+                                if LicenseStats == 1
+                                    if ~strcmpi(FigureName,'Do not save')
+                                        PartialFigureName = fullfile(handles.Current.DefaultOutputDirectory,FigureName);
+                                    else PartialFigureName = FigureName;
+                                    end
+                                    ec50stats = CPec50(OrderedUniqueDoses',OrderedAverageValues,LogOrLinear,PartialFigureName,ModuleName,DataName);
+                                    ec = ec50stats(:,3);
+                                    if strcmpi(LogOrLinear,'Yes')
+                                        ec = exp(ec);
+                                    end
+                                end
                             end
 
                             measurefield = [ObjectName,'Statistics'];
@@ -210,28 +240,21 @@ if handles.Current.SetBeingAnalyzed == handles.Current.NumberOfImageSets
                             if isfield(handles.Measurements,'Experiment')
                                 if isfield(handles.Measurements.Experiment,measurefield)
                                     OldEnd = length(handles.Measurements.Experiment.(featuresfield));
-                                    for a = 1:length(z)
-                                        handles.Measurements.Experiment.(measurefield){1}(1,OldEnd+a) = z(a);
-                                        handles.Measurements.Experiment.(measurefield){1}(1,OldEnd+length(z)+a) = v(a);
-                                        handles.Measurements.Experiment.(featuresfield){OldEnd+a} = ['Zfactor_',MeasureName,'_',MeasureFeatures{a}];
-                                        handles.Measurements.Experiment.(featuresfield){OldEnd+length(z)+a} = ['Vfactor_',MeasureName,'_',MeasureFeatures{a}];
-                                    end
-                                else
-                                    for a = 1:length(z)
-                                        handles.Measurements.Experiment.(measurefield){1}(1,a) = z(a);
-                                        handles.Measurements.Experiment.(measurefield){1}(1,length(z)+a) = v(a);
-                                        handles.Measurements.Experiment.(featuresfield){a} = ['Zfactor_',MeasureName,'_',MeasureFeatures{a}];
-                                        handles.Measurements.Experiment.(featuresfield){length(z)+a} = ['Vfactor_',MeasureName,'_',MeasureFeatures{a}];
-                                    end
+                                else OldEnd = 0;
                                 end
-                            else
-                                for a = 1:length(z)
-                                    handles.Measurements.Experiment.(measurefield){1}(1,a) = z(a);
-                                    handles.Measurements.Experiment.(measurefield){1}(1,length(z)+a) = v(a);
-                                    handles.Measurements.Experiment.(featuresfield){a} = ['Zfactor_',MeasureName,'_',MeasureFeatures{a}];
-                                    handles.Measurements.Experiment.(featuresfield){length(z)+a} = ['Vfactor_',MeasureName,'_',MeasureFeatures{a}];
+                            else OldEnd = 0;
+                            end
+                            for a = 1:length(z)
+                                handles.Measurements.Experiment.(measurefield){1}(1,OldEnd+a) = z(a);
+                                handles.Measurements.Experiment.(measurefield){1}(1,OldEnd+length(z)+a) = v(a);
+                                handles.Measurements.Experiment.(featuresfield){OldEnd+a} = ['Zfactor_',MeasureName,'_',MeasureFeatures{a}];
+                                handles.Measurements.Experiment.(featuresfield){OldEnd+length(z)+a} = ['Vfactor_',MeasureName,'_',MeasureFeatures{a}];
+                                if LicenseStats == 1
+                                    handles.Measurements.Experiment.(measurefield){1}(1,OldEnd+2*length(z)+a) = ec(a);
+                                    handles.Measurements.Experiment.(featuresfield){OldEnd+2*length(z)+a} = ['EC50_',MeasureName,'_',MeasureFeatures{a}];
                                 end
                             end
+
                         end
                     end
                 end
@@ -253,15 +276,18 @@ if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
     end
 end
 
-%%%%%%%%%%%%%%%%%%%%
-%%% SUBFUNCTIONS %%%
-%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% V AND Z SUBFUNCTIONS %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 drawnow
 
 % Code for the calculation of Z' and V factors was kindly donated by Ilya
 % Ravkin: http://www.ravkin.net
 
-function [v, z] = VZfactors(xcol, ymatr)
+function [v, z, OrderedUniqueDoses, OrderedAverageValues] = VZfactors(xcol, ymatr)
 % xcol is (Nobservations,1) column vector of grouping values
 % (in terms of dose curve it may be Dose).
 % ymatr is (Nobservations, Nmeasures) matrix, where rows correspond to observations
@@ -278,8 +304,9 @@ vstd = mean(stds);
 v = 1 - 6 .* (vstd ./ range);
 zstd = stds(1, :) + stds(length(xs), :);
 z = 1 - 3 .* (zstd ./ range);
-
-
+OrderedUniqueDoses = xs;
+OrderedAverageValues = avers;
+ 
 function [xs, avers, stds] = LocShrinkMeanStd(xcol, ymatr)
 ncols = size(ymatr,2);
 [labels, labnum, xs] = LocVectorLabels(xcol);
@@ -316,3 +343,95 @@ for i = 1 : n
     labels(inds(i)) = labnum;
 end
 uniqsortvals = uniqsortvals(1 : labnum);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% EC50 SUBFUNCTIONS %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function results=CPec50(conc,responses,LogOrLinear,PartialFigureName,ModuleName,ConcName)
+% EC50 Function to fit a dose-response data to a 4 parameter dose-response
+%   curve.
+% 
+% Requirements: nlinfit function in the Statistics Toolbox
+%           and accompanying m.files: calc_init_params.m and sigmoid.m
+% Inputs: 1. a 1 dimensional array of drug concentrations
+%         2. the corresponding m x n array of responses 
+% Algorithm: generate a set of initial coefficients including the Hill
+%               coefficient
+%            fit the data to the 4 parameter dose-response curve using
+%               nonlinear least squares
+% Output: a matrix of the 4 parameters
+%         results[m,1]=min
+%         results[m,2]=max
+%         results[m,3]=ec50
+%         results[m,4]=Hill coefficient
+%
+% Copyright 2004 Carlos Evangelista 
+% send comments to CCEvangelista@aol.com
+% Version 1.0    01/07/2004
+
+%%% If we are using a log-domain set of doses, we have a better chance of
+%%% fitting a sigmoid to the curve if the concentrations are
+%%% log-transformed.
+if strcmpi(LogOrLinear,'Yes')
+    conc = log(conc);
+end
+
+[m,n]=size(responses);
+results=zeros(n,4);
+for i=1:n
+    response=responses(:,i);
+    initial_params=calc_init_params(conc,response);
+    % OPTIONS = statset('display','iter');
+    %%% Turns off MATLAB-level warnings but saves the previous warning state.
+    PreviousWarningState = warning('off', 'all');
+    [coeffs,r,J]=nlinfit(conc,response,'CPsigmoid',initial_params);
+    if ~strcmpi(PartialFigureName,'Do not save')
+        %%% This produces the figure with the interactive graph.
+        if strcmpi(LogOrLinear,'Yes')
+            XaxisLabel = ['log(',ConcName,')'];
+        else XaxisLabel = [ConcName,''];
+        end
+        YaxisLabel = ['Feature #',num2str(i)];
+        FigureHandle = CPnlintool(conc,response,'CPsigmoid',initial_params,.05,XaxisLabel,YaxisLabel);        
+        try saveas(FigureHandle,[PartialFigureName,num2str(i),'.fig'],'fig');
+            try close(FigureHandle)
+            end
+        catch
+            errordlg(['Image processing was NOT canceled in the ', ModuleName, ' module, but the figure could not be saved to the hard drive for some reason. Check your settings.  The error is: ', lasterr])
+        end
+    end
+    %%% Turns MATLAB-level warnings back on.
+    warning(PreviousWarningState);
+    for j=1:4
+        results(i,j)=coeffs(j);
+    end
+end
+
+
+
+function init_params = calc_init_params(x,y)
+% This generates the min, max, x value at the mid-y value, and Hill
+% coefficient. These values are used for calculating dose responses using
+% nlinfit.
+
+%%% Parameters 1&2
+init_params(1) = min(y);
+init_params(2) = max(y);
+%%% Parameter 3
+% OLD:  parms(3)=(min(x)+max(x))/2;
+YvalueAt50thPercentile = (min(y)+max(y))/2;
+PairedValues = [y,x];
+DistanceToCentralYValue = abs(PairedValues(:,1) - YvalueAt50thPercentile);
+LocationOfNearest = find(DistanceToCentralYValue == min(DistanceToCentralYValue));
+XvalueAt50thPercentile = PairedValues(LocationOfNearest(1),2);
+init_params(3) = XvalueAt50thPercentile;
+%%% Parameter 4
+sizey=size(y);
+sizex=size(x);
+if (y(1)-y(sizey))./(x(2)-x(sizex))>0
+    init_params(4)=(y(1)-y(sizey))./(x(2)-x(sizex));
+else
+    init_params(4)=1;
+end

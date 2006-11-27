9f3e12b68ee28d80f5ef2e4947541b20d54ef521
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2006-11-27 20:09:28 +0000

    Jason's changes plus Anne's slight fixes.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4454 1fc53939-2000-0410-845c-e8453a809027

--- Modules/Align.m
+++ Modules/Align.m
@@ -112,13 +112,16 @@ AlignMethod = char(handles.Settings.VariableValues{CurrentModuleNum,8});
 drawnow
 
 %%% Reads the images.
-Image1 = CPretrieveimage(handles,Image1Name,ModuleName,'MustBeGray','CheckScale');
-
-Image2 = CPretrieveimage(handles,Image2Name,ModuleName,'MustBeGray','CheckScale');
-
+%Image1 = CPretrieveimage(handles,Image1Name,ModuleName,'MustBeGray','CheckScale');
+Image1 = CPretrieveimage(handles,Image1Name,ModuleName,'DontCheckColor','CheckScale');
+[M1 N1 P1] = size(Image1);
+%Image2 = CPretrieveimage(handles,Image2Name,ModuleName,'MustBeGray','CheckScale');
+Image2 = CPretrieveimage(handles,Image2Name,ModuleName,'DontCheckColor','CheckScale');
+[M2 N2 P2] = size(Image2);
 %%% Same for Image 3.
 if ~strcmpi(Image3Name,'Do not use')
-    Image3 = CPretrieveimage(handles,Image3Name,ModuleName,'MustBeGray','CheckScale');
+    Image3 = CPretrieveimage(handles,Image3Name,ModuleName,'DontCheckColor','CheckScale');
+    [M3 N3 P3] = size(Image3);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -128,31 +131,143 @@ drawnow
 
 %%% Aligns three input images.
 if ~strcmpi(Image3Name,'Do not use')
+    % Check to make sure all images are the same size
+    % if not take the minimum size of them and warn user
+    M = [M1 M2 M3];
+    N = [N1 N2 N3];
+    P = [P1 P2 P3];    
+    if any(diff(M))||any(diff(N))||any(diff(P))
+        Mmin=min(M);
+        Nmin=min(N);
+        Pmin=min(P);
+        if isempty(findobj('Tag',['Msgbox_' ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': 3 Images not all same size']))
+            CPwarndlg(['The images loaded into ' ModuleName ' which is number ' num2str(CurrentModuleNum) ' are not all the same size. The images will be cropped to the minimum dimension of (' num2str(Mmin) ', ' num2str(Nmin) ', ' num2str(Pmin) ').'],[ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': 3 Images not all same size'],'replace');
+        end        
+        Image1=Image1(1:Mmin,1:Nmin,1:Pmin);
+        Image2=Image2(1:Mmin,1:Nmin,1:Pmin);
+        Image3=Image3(1:Mmin,1:Nmin,1:Pmin);        
+    end
     %%% Aligns 1 and 2 (see subfunctions at the end of the module).
-    [sx, sy] = autoalign(Image1, Image2, AlignMethod);
-    Temp1 = subim(Image1, sx, sy);
-    Temp2 = subim(Image2, -sx, -sy);
-    %%% Assumes 3 is stuck to 2.
-    Temp3 = subim(Image3, -sx, -sy);
-    %%% Aligns 2 and 3.
-    [sx2, sy2] = autoalign(Temp2, Temp3, AlignMethod);
-    Results = ['(1 vs 2: X ', num2str(sx), ', Y ', num2str(sy), ...
-        ') (2 vs 3: X ', num2str(sx2), ', Y ', num2str(sy2),')'];
-    if strcmp(AdjustImage,'Yes') == 1
-        AlignedImage2 = subim(Temp2, sx2, sy2);
-        AlignedImage3 = subim(Temp3, -sx2, -sy2);
-        %%% 1 was already aligned with 2.
-        AlignedImage1 = subim(Temp1, sx2, sy2);
+    [sx, sy] = autoalign(sum(Image1,3)/sum(max(max(Image1))), sum(Image2,3)/sum(max(max(Image2))), AlignMethod);
+    if (P1 > 1)||(P2 > 1)||(P3 > 1)
+        %% Color Images
+        if isempty(findobj('Tag',['Msgbox_' ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': working on color images']))
+            CPwarndlg(['The images loaded into ' ModuleName ' which is number ' num2str(CurrentModuleNum) ' are color. The images will be converted to Gray for alignment and the alignment will be applied to the color images.'],[ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': working on color images'],'replace');
+        end
+        AlignR1 = subim(Image1(:,:,1), sx, sy);
+        AlignG1 = subim(Image1(:,:,2), sx, sy);
+        AlignB1 = subim(Image1(:,:,3), sx, sy);
+        Temp1 = zeros([size(AlignR1) 3]);
+        Temp1(:,:,1) = AlignR1;
+        Temp1(:,:,2) = AlignG1;
+        Temp1(:,:,3) = AlignB1;
+        AlignR2 = subim(Image2(:,:,1), -sx, -sy);
+        AlignG2 = subim(Image2(:,:,2), -sx, -sy);
+        AlignB2 = subim(Image2(:,:,3), -sx, -sy);
+        Temp2 = zeros([size(AlignR2) 3]);
+        Temp2(:,:,1) = AlignR2;
+        Temp2(:,:,2) = AlignG2;
+        Temp2(:,:,3) = AlignB2;
+        %%% Assumes 3 is stuck to 2.
+        AlignR3 = subim(Image3(:,:,1), -sx, -sy);
+        AlignG3 = subim(Image3(:,:,2), -sx, -sy);
+        AlignB3 = subim(Image3(:,:,3), -sx, -sy);
+        Temp3 = zeros([size(AlignR3) 3]);
+        Temp3(:,:,1) = AlignR3;
+        Temp3(:,:,2) = AlignG3;
+        Temp3(:,:,3) = AlignB3;
+        %%% Aligns 2 and 3.
+%        [sx2, sy2] = autoalign(Temp2, Temp3, AlignMethod);
+        [sx2, sy2] = autoalign(sum(Temp2,3)/sum(max(max(Temp2))), sum(Temp3,3)/sum(max(max(Temp3))), AlignMethod);
+        Results = ['(1 vs 2: X ', num2str(sx), ', Y ', num2str(sy), ...
+            ') (2 vs 3: X ', num2str(sx2), ', Y ', num2str(sy2),')'];
+        if strcmp(AdjustImage,'Yes') == 1
+           %AlignedImage2 = subim(Temp2, sx2, sy2);
+           AlignR23 = subim(Temp2(:,:,1), sx2, sy2);
+           AlignG23 = subim(Temp2(:,:,2), sx2, sy2);
+           AlignB23 = subim(Temp2(:,:,3), sx2, sy2);
+           AlignedImage2 = zeros([size(AlignR23) 3]);
+           AlignedImage2(:,:,1) = AlignR23;
+           AlignedImage2(:,:,2) = AlignG23;
+           AlignedImage2(:,:,3) = AlignB23;    
+            %AlignedImage3 = subim(Temp3, -sx2, -sy2);
+           AlignR32 = subim(Temp3(:,:,1), -sx2, -sy2);
+           AlignG32 = subim(Temp3(:,:,2), -sx2, -sy2);
+           AlignB32 = subim(Temp3(:,:,3), -sx2, -sy2);
+           AlignedImage3 = zeros([size(AlignR32) 3]);
+           AlignedImage3(:,:,1) = AlignR32;
+           AlignedImage3(:,:,2) = AlignG32;
+           AlignedImage3(:,:,3) = AlignB32; 
+           %%% 1 was already aligned with 2.
+           %AlignedImage1 = subim(Temp1, sx2, sy2);
+           AlignR12 = subim(Temp1(:,:,1), sx2, sy2);
+           AlignG12 = subim(Temp1(:,:,2), sx2, sy2);
+           AlignB12 = subim(Temp1(:,:,3), sx2, sy2);
+           AlignedImage1 = zeros([size(AlignR12) 3]);
+           AlignedImage1(:,:,1) = AlignR12;
+           AlignedImage1(:,:,2) = AlignG12;
+           AlignedImage1(:,:,3) = AlignB12; 
+        end
+
+    else
+        %% Gray Images
+        Temp1 = subim(Image1, sx, sy);
+        Temp2 = subim(Image2, -sx, -sy);
+        %%% Assumes 3 is stuck to 2.
+        Temp3 = subim(Image3, -sx, -sy);
+        %%% Aligns 2 and 3.
+        [sx2, sy2] = autoalign(Temp2, Temp3, AlignMethod);
+        Results = ['(1 vs 2: X ', num2str(sx), ', Y ', num2str(sy), ...
+            ') (2 vs 3: X ', num2str(sx2), ', Y ', num2str(sy2),')'];
+        if strcmp(AdjustImage,'Yes') == 1
+            AlignedImage2 = subim(Temp2, sx2, sy2);
+            AlignedImage3 = subim(Temp3, -sx2, -sy2);
+            %%% 1 was already aligned with 2.
+            AlignedImage1 = subim(Temp1, sx2, sy2);
+        end
     end
 else %%% Aligns two input images.
-    [sx, sy] = autoalign(Image1, Image2, AlignMethod);
+    M = [M1 M2];
+    N = [N1 N2];
+    P = [P1 P2];    
+    if any(diff(M))||any(diff(N))||any(diff(P))
+        Mmin=min(M);
+        Nmin=min(N);
+        Pmin=min(P);       
+        if isempty(findobj('Tag',['Msgbox_' ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': 2 Images not all same size']))
+            CPwarndlg(['The images loaded into ' ModuleName ' which is number ' num2str(CurrentModuleNum) ' are not all the same size. The images will be cropped to the minimum dimension of (' num2str(Mmin) ', ' num2str(Nmin) ', ' num2str(Pmin) ').'],[ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': 2 Images not all same size'],'replace');
+        end
+        Image1=Image1(1:Mmin,1:Nmin,1:Pmin);
+        Image2=Image2(1:Mmin,1:Nmin,1:Pmin);
+    end
+    [sx, sy] = autoalign(sum(Image1,3)/sum(max(max(Image1))), sum(Image2,3)/sum(max(max(Image2))), AlignMethod);
     Results = ['(1 vs 2: X ', num2str(sx), ', Y ', num2str(sy),')'];
     if strcmp(AdjustImage,'Yes') == 1
-        AlignedImage1 = subim(Image1, sx, sy);
-        AlignedImage2 = subim(Image2, -sx, -sy);
+       if (P1 > 1)||(P2 > 1)
+            if isempty(findobj('Tag',['Msgbox_' ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': working on color images']))
+                CPwarndlg(['The images loaded into ' ModuleName ' which is number ' num2str(CurrentModuleNum) ' are color. The images will be converted to Gray for alignment and the alignment will be applied to the color images.'],[ModuleName ', ModuleNumber ' num2str(CurrentModuleNum) ': working on color images'],'replace');
+            end
+             % Apply subim to each individual color element
+             AlignR1 = subim(Image1(:,:,1), sx, sy);
+             AlignG1 = subim(Image1(:,:,2), sx, sy);
+             AlignB1 = subim(Image1(:,:,3), sx, sy);             
+             AlignedImage1 = zeros([size(AlignR1) 3]);
+             AlignedImage1(:,:,1) = AlignR1;
+             AlignedImage1(:,:,2) = AlignG1; 
+             AlignedImage1(:,:,3) = AlignB1;
+             AlignR2 = subim(Image2(:,:,1), -sx, -sy);
+             AlignG2 = subim(Image2(:,:,2), -sx, -sy);
+             AlignB2 = subim(Image2(:,:,3), -sx, -sy);             
+             AlignedImage2 = zeros([size(AlignR2) 3]);
+             AlignedImage2(:,:,1) = AlignR2;
+             AlignedImage2(:,:,2) = AlignG2; 
+             AlignedImage2(:,:,3) = AlignB2;
+       else    
+            AlignedImage1 = subim(Image1, sx, sy);
+            AlignedImage2 = subim(Image2, -sx, -sy);
+       end
     end
 end
-
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% DISPLAY RESULTS %%%
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -164,20 +279,24 @@ if any(findobj == ThisModuleFigureNumber)
     if strcmp(AdjustImage,'Yes')
         %%% For three input images.
         if (~strcmpi(Image3Name,'Do not use') && all(size(Image1) == size(Image2)) && all(size(Image1) == size(Image3))),
-            OriginalRGB(:,:,1) = Image3;
-            OriginalRGB(:,:,2) = Image2;
-            OriginalRGB(:,:,3) = Image1;
-            AlignedRGB(:,:,1) = AlignedImage3;
-            AlignedRGB(:,:,2) = AlignedImage2;
-            AlignedRGB(:,:,3) = AlignedImage1;
+            OriginalRGB(:,:,1) = sum(Image3,3)/sum(max(max(Image3)));
+            OriginalRGB(:,:,2) = sum(Image2,3)/sum(max(max(Image2)));
+            OriginalRGB(:,:,3) = sum(Image1,3)/sum(max(max(Image1)));
+            AlignedRGB(:,:,1) = sum(AlignedImage3,3)/sum(max(max(AlignedImage3)));
+            AlignedRGB(:,:,2) = sum(AlignedImage2,3)/sum(max(max(AlignedImage2)));
+            AlignedRGB(:,:,3) = sum(AlignedImage1,3)/sum(max(max(AlignedImage1)));
         %%% For two input images.
         elseif all(size(Image1) == size(Image2)),
-            OriginalRGB(:,:,1) = zeros(size(Image1));
-            OriginalRGB(:,:,2) = Image2;
-            OriginalRGB(:,:,3) = Image1;
-            AlignedRGB(:,:,1) = zeros(size(AlignedImage1));
-            AlignedRGB(:,:,2) = AlignedImage2;
-            AlignedRGB(:,:,3) = AlignedImage1;
+            %%% Note that the size is recalculated in case images were
+            %%% cropped to be the same size.
+            [M1 N1 P1] = size(Image1);
+            OriginalRGB(:,:,1) = zeros(M1,N1);
+            OriginalRGB(:,:,2) = sum(Image2,3)/sum(max(max(Image2)));
+            OriginalRGB(:,:,3) = sum(Image1,3)/sum(max(max(Image1)));
+            [aM1, aN1, aP1] = size(AlignedImage1);
+            AlignedRGB(:,:,1) = zeros(aM1,aN1);
+            AlignedRGB(:,:,2) = sum(AlignedImage2,3)/sum(max(max(Image2)));
+            AlignedRGB(:,:,3) = sum(AlignedImage1,3)/sum(max(max(Image1)));
         else
             OriginalRGB = Image1;
             AlignedRGB = AlignedImage1;

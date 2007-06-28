ecbe0b929e6853bcedd5962fa35198337f2c85a9
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-06-28 04:21:58 +0000

    added module for smoothing with bilateral filter
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4705 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/SmoothKeepingEdges.m
@@ -0,0 +1,273 @@
+function handles = SmoothKeepingEdges(handles)
+% Help for the Smooth Keeping Edges module:
+% Category: Image Processing
+%
+% SHORT DESCRIPTION: Smooths a grayscale image while preserving edges.
+% Uses the Bilateral Filter, as implemented by Jiawen Chen.
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%   Peter Swire
+%   Rodrigo Ipince
+%   Vicky Lay
+%   Jun Liu
+%   Chris Gang
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 1718 $
+
+%%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%%
+drawnow
+
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+%textVAR01 = What did you call the image to be smoothed?
+%infotypeVAR01 = imagegroup
+ImageName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+%inputtypeVAR01 = popupmenu
+
+%textVAR2 = What do you want to call the smoothed image?
+%defaultVAR2 = SmoothedImage
+%infotypeVAR2 = imagegroup indep
+SmoothedImageName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+
+%textVAR03 = Smoothing options:
+
+%textVAR04 = What spatial filter radius should be used, in pixels? (The approximate size of preserved objects is good)?
+%defaultVAR04 = 16.0
+SpatialRadius = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,4}));
+
+%textVAR05 = What intensity-based raiud should be used, in intensity units? (Half the intensity step that indicates an edge is good.  Set to 0.0 to calculate from the image.)?
+%defaultVAR05 = 0.1
+IntensityRadius = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,5}));
+
+%%%VariableRevisionNumber = 1
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Reads (opens) the image to be analyzed and assigns it to a variable,
+%%% "OrigImage".
+OrigImage = CPretrieveimage(handles,ImageName,ModuleName,'MustBeGray','CheckScale');
+
+if (IntensityRadius == 0.0),
+    % use MAD of gradients to estimate scale, use half that estimate
+    % XXX - adjust such that it returns 1.0 for worm images.
+    IntensityRadius = ImageMAD(OrigImage) / 2.0;
+end
+    
+%%%%%%%%%%%%%%%%%%%%%
+%%% IMAGE ANALYSIS%%%
+%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+SmoothedImage = bilateralFilter(OrigImage, OrigImage, SpatialRadius, IntensityRadius, SpatialRadius / 2.0, IntensityRadius / 2.0);
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+
+if any(findobj == ThisModuleFigureNumber)
+    %%% Activates the appropriate figure window.
+    CPfigure(handles,'Image',ThisModuleFigureNumber);
+    if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+        CPresizefigure(OrigImage,'TwoByOne',ThisModuleFigureNumber)
+    end
+    %%% A subplot of the figure window is set to display the original image.
+    subplot(2,1,1);
+    CPimagesc(OrigImage,handles);
+    title(['Input Image, cycle # ',num2str(handles.Current.SetBeingAnalyzed)]);
+    %%% A subplot of the figure window is set to display the Grayscale
+    %%% Image.
+    subplot(2,1,2);
+    CPimagesc(SmoothedImage,handles);
+    title('Smoothed Image');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% SAVE DATA TO HANDLES STRUCTURE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+fieldname = SmoothedImageName;
+handles.Pipeline.(fieldname) = SmoothedImage;
+
+
+function MAD = ImageMAD(Image)
+    GradientImage = Image(:,1:end-1)-Image(:,2:end);
+    MAD = median(abs(GradientImage(:) - median(GradientImage(:))));
+
+
+% Code below is (C) Jiawen Chen, MIT CSAIL, and distributed under the
+% MIT License.
+%
+% output = bilateralFilter( data, edge, sigmaSpatial, sigmaRange, ...
+%                          samplingSpatial, samplingRange )
+%
+% Bilateral and Cross-Bilateral Filter
+%
+% Bilaterally filters the image 'data' using the edges in the image 'edge'.
+% If 'data' == 'edge', then it the normal bilateral filter.
+% Else, then it is the "cross" or "joint" bilateral filter.
+%
+% Note that for the cross bilateral filter, data does not need to be
+% defined everywhere.  Undefined values can be set to 'NaN'.  However, edge
+% *does* need to be defined everywhere.
+%
+% data and edge should be of the same size and greyscale.
+% (i.e. they should be ( height x width x 1 matrices ))
+%
+% data is the only required argument
+%
+% By default:
+% edge = data
+% sigmaSpatial = samplingSpatial = min( width, height ) / 16;
+% sigmaRange = samplingRange = ( max( edge( : ) ) - min( edge( : ) ) ) / 10
+% 
+%
+function output = bilateralFilter( data, edge, sigmaSpatial, sigmaRange, samplingSpatial, samplingRange )
+
+if ~exist( 'edge', 'var' ),
+    edge = data;
+end
+
+inputHeight = size( data, 1 );
+inputWidth = size( data, 2 );
+
+if ~exist( 'sigmaSpatial', 'var' ),
+    sigmaSpatial = min( inputWidth, inputHeight ) / 16;
+end
+
+edgeMin = min( edge( : ) );
+edgeMax = max( edge( : ) );
+edgeDelta = edgeMax - edgeMin;
+
+if ~exist( 'sigmaRange', 'var' ),
+    sigmaRange = 0.1 * edgeDelta;
+end
+
+if ~exist( 'samplingSpatial', 'var' ),
+    samplingSpatial = sigmaSpatial;
+end
+
+if ~exist( 'samplingRange', 'var' ),
+    samplingRange = sigmaRange;
+end
+
+if size( data ) ~= size( edge ),
+    error( 'data and edge must be of the same size' );
+end
+
+% parameters
+derivedSigmaSpatial = sigmaSpatial / samplingSpatial;
+derivedSigmaRange = sigmaRange / samplingRange;
+
+paddingXY = floor( 2 * derivedSigmaSpatial ) + 1;
+paddingZ = floor( 2 * derivedSigmaRange ) + 1;
+
+% allocate 3D grid
+downsampledWidth = floor( ( inputWidth - 1 ) / samplingSpatial ) + 1 + 2 * paddingXY;
+downsampledHeight = floor( ( inputHeight - 1 ) / samplingSpatial ) + 1 + 2 * paddingXY;
+downsampledDepth = floor( edgeDelta / samplingRange ) + 1 + 2 * paddingZ;
+
+gridData = zeros( downsampledHeight, downsampledWidth, downsampledDepth );
+gridData2 = gridData;
+gridWeights = zeros( downsampledHeight, downsampledWidth, downsampledDepth );
+gridWeights2 = gridWeights;
+
+% compute downsampled indices
+[ jj, ii ] = meshgrid( 0 : inputWidth - 1, 0 : inputHeight - 1 );
+
+% ii =
+% 0 0 0 0 0
+% 1 1 1 1 1
+% 2 2 2 2 2
+
+% jj =
+% 0 1 2 3 4
+% 0 1 2 3 4
+% 0 1 2 3 4
+
+% so when iterating over ii( k ), jj( k )
+% get: ( 0, 0 ), ( 1, 0 ), ( 2, 0 ), ... (down columns first)
+
+di = round( ii / samplingSpatial ) + paddingXY + 1;
+dj = round( jj / samplingSpatial ) + paddingXY + 1;
+dz = round( ( edge - edgeMin ) / samplingRange ) + paddingZ + 1;
+
+% perform scatter (there's probably a faster way than this)
+% normally would do downsampledWeights( di, dj, dk ) = 1, but we have to
+% perform a summation to do box downsampling
+
+for k = 1 : numel( dz ),
+       
+    dataZ = data( k ); % traverses the image column wise, same as di( k )
+    if ~isnan( dataZ  ),
+        
+        dik = di( k );
+        djk = dj( k );
+        dzk = dz( k );
+
+        gridData( dik, djk, dzk ) = gridData( dik, djk, dzk ) + dataZ;
+        gridWeights( dik, djk, dzk ) = gridWeights( dik, djk, dzk ) + 1;
+        
+    end
+end
+
+% make gaussian kernel
+kernelWidth = 2 * derivedSigmaSpatial + 1;
+kernelHeight = kernelWidth;
+kernelDepth = 2 * derivedSigmaRange + 1;
+
+halfKernelWidth = floor( kernelWidth / 2 );
+halfKernelHeight = floor( kernelHeight / 2 );
+halfKernelDepth = floor( kernelDepth / 2 );
+
+[gridX, gridY, gridZ] = meshgrid( 0 : kernelWidth - 1, 0 : kernelHeight - 1, 0 : kernelDepth - 1 );
+gridX = gridX - halfKernelWidth;
+gridY = gridY - halfKernelHeight;
+gridZ = gridZ - halfKernelDepth;
+gridRSquared = ( gridX .* gridX + gridY .* gridY ) / ( derivedSigmaSpatial * derivedSigmaSpatial ) + ( gridZ .* gridZ ) / ( derivedSigmaRange * derivedSigmaRange );
+kernel = exp( -0.5 * gridRSquared );
+
+% convolve
+blurredGridData = convn( gridData, kernel, 'same' );
+blurredGridWeights = convn( gridWeights, kernel, 'same' );
+
+% divide
+blurredGridWeights( blurredGridWeights == 0 ) = -2; % avoid divide by 0, won't read there anyway
+normalizedBlurredGrid = blurredGridData ./ blurredGridWeights;
+normalizedBlurredGrid( blurredGridWeights < -1 ) = 0; % put 0s where it's undefined
+blurredGridWeights( blurredGridWeights < -1 ) = 0; % put zeros back
+
+% upsample
+[ jj, ii ] = meshgrid( 0 : inputWidth - 1, 0 : inputHeight - 1 ); % meshgrid does x, then y, so output arguments need to be reversed
+% no rounding
+di = ( ii / samplingSpatial ) + paddingXY + 1;
+dj = ( jj / samplingSpatial ) + paddingXY + 1;
+dz = ( edge - edgeMin ) / samplingRange + paddingZ + 1;
+
+% interpn takes rows, then cols, etc
+% i.e. size(v,1), then size(v,2), ...
+output = interpn( normalizedBlurredGrid, di, dj, dz );

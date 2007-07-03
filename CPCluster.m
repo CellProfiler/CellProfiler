90e21710e87d5d8dfa1a07e5096dcaab41fb8afc
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-07-03 16:21:10 +0000

    better debugging
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4713 1fc53939-2000-0410-845c-e8453a809027

--- CPCluster.m
+++ CPCluster.m
@@ -3,8 +3,19 @@ function CPCluster(batchfile,clusterfile)
 %%% Must list all CellProfiler modules here
 %#function Align ApplyThreshold Average CalculateMath CalculateRatios CalculateStatistics ClassifyObjects ClassifyObjectsByTwoMeasurements ColorToGray Combine ConvertToImage CorrectIllumination_Apply CorrectIllumination_Calculate CreateBatchFiles CreateWebPage Crop DefineGrid DisplayDataOnImage DisplayGridInfo DisplayHistogram DisplayImageHistogram DisplayMeasurement DistinguishPixelLabels Exclude ExpandOrShrink ExportToDatabase ExportToExcel FilterByObjectMeasurement FindEdges Flip GrayToColor IdentifyObjectsInGrid IdentifyPrimAutomatic IdentifyPrimManual IdentifySecondary IdentifyTertiarySubregion InvertIntensity LoadImages LoadSingleImage LoadText MaskImage MeasureCorrelation MeasureImageAreaOccupied MeasureImageGranularity MeasureImageIntensity MeasureImageSaturationBlur MeasureObjectAreaShape MeasureObjectIntensity MeasureObjectNeighbors MeasureTexture Morph OverlayOutlines PlaceAdjacent Relate RenameOrRenumberFiles RescaleIntensity Resize Restart Rotate SaveImages SendEmail Smooth SpeedUpCellProfiler SplitOrSpliceMovie Subtract SubtractBackground Tile CPaddmeasurements CPaverageimages CPblkproc CPcd CPclearborder CPcompilesubfunction CPcontrolhistogram CPconvertsql CPdilatebinaryobjects CPerrordlg CPfigure CPgetfeature CPhelpdlg CPhistbins CPimagesc CPimagetool CPimread CPinputdlg CPlabel2rgb CPlistdlg CPlogo CPmakegrid CPmsgbox CPnanmean CPnanmedian CPnanstd CPnlintool CPplotmeasurement CPquestdlg CPrelateobjects CPrescale CPresizefigure CPretrieveimage CPretrievemediafilenames CPrgsmartdilate CPselectmodules CPselectoutputfiles CPsigmoid CPsmooth CPtextdisplaybox CPtextpipe CPthresh_tool CPthreshold CPwaitbar CPwarndlg CPwhichmodule CPwritemeasurements
 
-load(batchfile);
-load(clusterfile);
+try
+    load(batchfile);
+catch
+    reportBatchError(['Batch Error: Loading batch file (' batchfile ')']);
+end
+
+try
+    load(clusterfile);
+catch
+    reportBatchError(['Batch Error: Loading cluster file (' clusterfile ')']);
+end
+
+
 tic
 handles.Current.BatchInfo.Start = cluster.StartImage;
 handles.Current.BatchInfo.End = cluster.EndImage;
@@ -19,13 +30,28 @@ for BatchSetBeingAnalyzed = cluster.StartImage:cluster.EndImage,
         try
             handles = feval(ModuleName,handles);
         catch
-            handles.BatchError = [ModuleName ' ' lasterr];
-            disp(['Batch Error: ' ModuleName ' ' lasterr]);
-            rethrow(lasterror);
-            quit;
+            reportBatchError(['Batch Error: ' ModuleName]);
         end
     end
 end
 cd(cluster.OutputFolder);
 handles.Pipeline = [];
 save(sprintf('%s%d_to_%d_OUT',cluster.BatchFilePrefix,cluster.StartImage,cluster.EndImage),'handles');
+
+function reportBatchError(errorstring)
+errorinfo = lasterror;
+if isfield(errorinfo, 'stack'),
+    try
+        stackinfo = errorinfo.stack(1,1);
+        ExtraInfo = [' (file: ', stackinfo.file, ' function: ', stackinfo.name, ' line: ', num2str(stackinfo.line), ')'];
+    catch
+        %%% The line stackinfo = errorinfo.stack(1,1); will fail if the
+        %%% errorinfo.stack is empty, which sometimes happens during
+        %%% debugging, I think. So we catch it here.
+        ExtraInfo = '';
+    end
+end
+disp([errorstring ': ' lasterr]);
+disp(ExtraInfo);
+rethrow(errorinfo);
+quit;

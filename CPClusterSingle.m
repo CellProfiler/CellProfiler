d48349c7d305477ab1549df1d1f193e188a556bf
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-07-16 18:58:53 +0000

    improved batch runner
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4723 1fc53939-2000-0410-845c-e8453a809027

--- CPClusterSingle.m
+++ CPClusterSingle.m
@@ -1,7 +1,10 @@
 function CPClusterSingle(batchfile,BatchSetBeingAnalyzed,OutputFolder,BatchFilePrefix)
 
+tic
+
 %%% Must list all CellProfiler modules here
 %#function CPwritemeasurements CPlogo CPconvertsql CPmsgbox CPretrievemediafilenames CPrelateobjects CPselectmodules CPaddmeasurements CPtextdisplaybox CPthresh_tool CPselectoutputfiles CPnanmean CPfigure CPrescale CPinputdlg CPimagesc CPclearborder CPlabel2rgb CPimagetool CPwarndlg CPhistbins CPcontrolhistogram CPtextpipe CPdilatebinaryobjects CPsigmoid CPimread CPwhichmodule CPthreshold CPmakegrid CPlistdlg CPcompilesubfunction CPsmooth CPresizefigure CPgetfeature CPwaitbar CPrgsmartdilate CPretrieveimage CPnanstd CPerrordlg CPaverageimages CPcd CPblkproc CPnanmedian CPplotmeasurement CPhelpdlg CPquestdlg CPnlintool Tile SaveImages SplitOrSpliceMovie Morph CalculateStatistics SpeedUpCellProfiler FilterByObjectMeasurement PlaceAdjacent LoadImages ConvertToImage ApplyThreshold MeasureTexture GrayToColor Align MeasureObjectAreaShape MeasureObjectNeighbors CreateBatchFiles MeasureImageGranularity ExportToDatabase DistinguishPixelLabels ClassifyObjects Crop InvertIntensity RescaleIntensity ExpandOrShrink IdentifyTertiarySubregion CorrectIllumination_Apply CalculateRatios CreateWebPage Restart SendEmail CorrectIllumination_New Combine ExportToExcel LoadText MeasureCorrelation ClassifyObjectsByTwoMeasurements DisplayImageHistogram CalculateMath DefineGrid Smooth MaskImage Relate Subtract CorrectIllumination_Calculate RenameOrRenumberFiles Resize IdentifySecondary OverlayOutlines MeasureObjectIntensity DisplayHistogram ColorToGray Average IdentifyObjectsInGrid SmoothKeepingEdges LoadSingleImage IdentifyPrimManual DisplayMeasurement MeasureImageIntensity SubtractBackground Flip MeasureImageSaturationBlur IdentifyPrimAutomatic Exclude DisplayDataOnImage Rotate FindEdges DisplayGridInfo MeasureImageAreaOccupied strel
+
 
 
 try
@@ -27,14 +30,11 @@ if strcmp(BatchSetBeingAnalyzed, 'all'),
     quit;
 end
 
-disp('past loop');
-
-foo = strel('disk', 7, 0)
+toc
 
 % arguments come in as strings, convert to integer
 BatchSetBeingAnalyzed = str2num(BatchSetBeingAnalyzed);
 
-tic
 handles.Current.BatchInfo.Start = BatchSetBeingAnalyzed;
 handles.Current.BatchInfo.End = BatchSetBeingAnalyzed;
 
@@ -60,11 +60,7 @@ for SlotNumber = 1:handles.Current.NumberOfModules,
     end
     disp('done');
     toc
-    if SlotNumber == BatchSetBeingAnalyzed,
-        quit;
-    end
 end
-toc
 
 % handles.Pipeline = [];
 % save(sprintf('%s%d_OUT',BatchFilePrefix,BatchSetBeingAnalyzed),'handles');
@@ -88,5 +84,4 @@ if isfield(errorinfo, 'stack'),
 end
 disp([errorstring ': ' lasterr]);
 disp(ExtraInfo);
-rethrow(errorinfo);
 quit;

01cb1ca4c0c566175262896081bbf6149dd760bc
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-07-17 17:52:44 +0000

    added batching back to CPClusterSingle
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4726 1fc53939-2000-0410-845c-e8453a809027

--- CPClusterSingle.m
+++ CPClusterSingle.m
@@ -1,70 +1,80 @@
-function CPClusterSingle(batchfile,BatchSetBeingAnalyzed,OutputFolder,BatchFilePrefix)
+function CPClusterSingle(batchfile,StartingSet,EndingSet,OutputFolder,BatchFilePrefix)
+
+%%% Must list all CellProfiler modules here
+%#function CPwritemeasurements CPlogo CPconvertsql CPmsgbox CPretrievemediafilenames CPrelateobjects CPselectmodules CPaddmeasurements CPtextdisplaybox CPthresh_tool CPselectoutputfiles CPnanmean CPfigure CPrescale CPinputdlg CPimagesc CPclearborder CPlabel2rgb CPimagetool CPwarndlg CPhistbins CPcontrolhistogram CPtextpipe CPdilatebinaryobjects CPsigmoid CPimread CPwhichmodule CPthreshold CPmakegrid CPlistdlg CPcompilesubfunction CPsmooth CPresizefigure CPgetfeature CPwaitbar CPrgsmartdilate CPretrieveimage CPnanstd CPerrordlg CPaverageimages CPcd CPblkproc CPnanmedian CPplotmeasurement CPhelpdlg CPquestdlg CPnlintool Tile SaveImages SplitOrSpliceMovie Morph CalculateStatistics SpeedUpCellProfiler FilterByObjectMeasurement PlaceAdjacent LoadImages ConvertToImage ApplyThreshold MeasureTexture GrayToColor Align MeasureObjectAreaShape MeasureObjectNeighbors CreateBatchFiles MeasureImageGranularity ExportToDatabase DistinguishPixelLabels ClassifyObjects Crop InvertIntensity RescaleIntensity ExpandOrShrink IdentifyTertiarySubregion CorrectIllumination_Apply CalculateRatios CreateWebPage Restart SendEmail CorrectIllumination_New Combine ExportToExcel LoadText MeasureCorrelation ClassifyObjectsByTwoMeasurements DisplayImageHistogram CalculateMath DefineGrid Smooth MaskImage Relate Subtract CorrectIllumination_Calculate RenameOrRenumberFiles Resize IdentifySecondary OverlayOutlines MeasureObjectIntensity DisplayHistogram ColorToGray Average IdentifyObjectsInGrid SmoothKeepingEdges LoadSingleImage IdentifyPrimManual DisplayMeasurement MeasureImageIntensity SubtractBackground Flip MeasureImageSaturationBlur IdentifyPrimAutomatic Exclude DisplayDataOnImage Rotate FindEdges DisplayGridInfo MeasureImageAreaOccupied
 
 tic
 
-%%% Must list all CellProfiler modules here
-%#function CPwritemeasurements CPlogo CPconvertsql CPmsgbox CPretrievemediafilenames CPrelateobjects CPselectmodules CPaddmeasurements CPtextdisplaybox CPthresh_tool CPselectoutputfiles CPnanmean CPfigure CPrescale CPinputdlg CPimagesc CPclearborder CPlabel2rgb CPimagetool CPwarndlg CPhistbins CPcontrolhistogram CPtextpipe CPdilatebinaryobjects CPsigmoid CPimread CPwhichmodule CPthreshold CPmakegrid CPlistdlg CPcompilesubfunction CPsmooth CPresizefigure CPgetfeature CPwaitbar CPrgsmartdilate CPretrieveimage CPnanstd CPerrordlg CPaverageimages CPcd CPblkproc CPnanmedian CPplotmeasurement CPhelpdlg CPquestdlg CPnlintool Tile SaveImages SplitOrSpliceMovie Morph CalculateStatistics SpeedUpCellProfiler FilterByObjectMeasurement PlaceAdjacent LoadImages ConvertToImage ApplyThreshold MeasureTexture GrayToColor Align MeasureObjectAreaShape MeasureObjectNeighbors CreateBatchFiles MeasureImageGranularity ExportToDatabase DistinguishPixelLabels ClassifyObjects Crop InvertIntensity RescaleIntensity ExpandOrShrink IdentifyTertiarySubregion CorrectIllumination_Apply CalculateRatios CreateWebPage Restart SendEmail CorrectIllumination_New Combine ExportToExcel LoadText MeasureCorrelation ClassifyObjectsByTwoMeasurements DisplayImageHistogram CalculateMath DefineGrid Smooth MaskImage Relate Subtract CorrectIllumination_Calculate RenameOrRenumberFiles Resize IdentifySecondary OverlayOutlines MeasureObjectIntensity DisplayHistogram ColorToGray Average IdentifyObjectsInGrid SmoothKeepingEdges LoadSingleImage IdentifyPrimManual DisplayMeasurement MeasureImageIntensity SubtractBackground Flip MeasureImageSaturationBlur IdentifyPrimAutomatic Exclude DisplayDataOnImage Rotate FindEdges DisplayGridInfo MeasureImageAreaOccupied strel
-
-
-
 try
-    state = warning('off', 'all');
+    state = warning('off', 'all'); % necessary to get around pipelines that complain about missing functions.
     load(batchfile);
     warning(state);
 catch
     reportBatchError(['Batch Error: Loading batch file (' batchfile ')']);
 end
 
-% If we get the argument 'all', print out the imageset numbers for
-% each set that still needs to run.
-if strcmp(BatchSetBeingAnalyzed, 'all'),
+% If we get the argument 'all', use EndingSet as a step size and print
+% out the imageset numbers for each set that still needs to run.
+if strcmp(StartingSet, 'all'),
     if exist(OutputFolder) ~= 7, % directory
         error(sprintf('Output folder %s does not exist.', OutputFolder));
         quit;
     end
-    for imageset = 2:handles.Current.NumberOfImageSets,
-        if ~ exist(sprintf('%s/%s%d_DONE.mat',OutputFolder,BatchFilePrefix,imageset)),
-            disp(imageset);
+    StepSize = str2num(EndingSet);
+    Starts = 2:StepSize:handles.Current.NumberOfImageSets;
+    Ends = Starts + StepSize - 1;
+    Ends(end) = handles.Current.NumberOfImageSets;
+    for imagesets = [Starts ; Ends],
+        if ~ exist(sprintf('%s/%s%d_to_%d_DONE',OutputFolder,BatchFilePrefix,imageset(1),imageset(2))),
+            disp(sptrintf('%d %d', imageset(1), imageset(2));
         end
     end
     quit;
 end
 
-toc
-
 % arguments come in as strings, convert to integer
-BatchSetBeingAnalyzed = str2num(BatchSetBeingAnalyzed);
+StartingSet = str2num(StartingSet);
+EndingSet = str2num(EndingSet);
 
-handles.Current.BatchInfo.Start = BatchSetBeingAnalyzed;
-handles.Current.BatchInfo.End = BatchSetBeingAnalyzed;
+handles.Current.BatchInfo.Start = StartingSet;
+handles.Current.BatchInfo.End = EndingSet;
 
-disp(sprintf('Analyzing set %d.', BatchSetBeingAnalyzed));
-handles.Current.SetBeingAnalyzed = BatchSetBeingAnalyzed;
+for BatchSetBeingAnalyzed = StartingSet:EndingSet,
+    disp(sprintf('Analyzing set %d.', BatchSetBeingAnalyzed));
+    handles.Current.SetBeingAnalyzed = BatchSetBeingAnalyzed;
 
-disp('Pipeline:')
-for SlotNumber = 1:handles.Current.NumberOfModules,
-    ModuleNumberAsString = sprintf('%02d', SlotNumber);
-    ModuleName = char(handles.Settings.ModuleNames(SlotNumber));
-    disp(sprintf('     module %d - %s', SlotNumber, ModuleName));
-end
-
-for SlotNumber = 1:handles.Current.NumberOfModules,
-    ModuleNumberAsString = sprintf('%02d', SlotNumber);
-    ModuleName = char(handles.Settings.ModuleNames(SlotNumber));
-    disp(sprintf('executing module %d - %s', SlotNumber, ModuleName));
-    handles.Current.CurrentModuleNumber = ModuleNumberAsString;
-    try
-        handles = feval(ModuleName,handles);
-    catch
-        reportBatchError(['Batch Error: ' ModuleName]);
+    if (BatchSetBeingAnalyzed == StartingSet),
+        disp('Pipeline:')
+        for SlotNumber = 1:handles.Current.NumberOfModules,
+            ModuleNumberAsString = sprintf('%02d', SlotNumber);
+            ModuleName = char(handles.Settings.ModuleNames(SlotNumber));
+            disp(sprintf('     module %d - %s', SlotNumber, ModuleName));
+        end
     end
-    disp('done');
-    toc
+
+    for SlotNumber = 1:handles.Current.NumberOfModules,
+        t_start = toc;
+        ModuleNumberAsString = sprintf('%02d', SlotNumber);
+        ModuleName = char(handles.Settings.ModuleNames(SlotNumber));
+        disp(sprintf('  executing module %d - %s', SlotNumber, ModuleName));
+        handles.Current.CurrentModuleNumber = ModuleNumberAsString;
+        try
+            handles = feval(ModuleName,handles);
+        catch
+            reportBatchError(['Batch Error: ' ModuleName]);
+        end
+        t_end = toc;
+        disp(sprintf('    %f seconds', t_end - t_start));
+     end
 end
+
+t_tot = toc;
+disp(sprintf('All sets analyzed in %f seconds (%f per image set)', t_tot, t_tot / (EndingSet - StartingSet + 1))
+
 
 % handles.Pipeline = [];
 % save(sprintf('%s%d_OUT',BatchFilePrefix,BatchSetBeingAnalyzed),'handles');
-OutputFileName = sprintf('%s/%s%d_DONE.mat',OutputFolder,BatchFilePrefix,BatchSetBeingAnalyzed);
+OutputFileName = sprintf('%s/%s%d_to_%d_DONE.mat',OutputFolder,BatchFilePrefix,StartingSet,EndingSet);
 save(OutputFileName,'BatchSetBeingAnalyzed');
 disp(sprintf('Created %s',OutputFileName));
 quit;

3096742ccce09fbf5affab44ad5c349c8258dec1
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-08-14 18:58:38 +0000

    handle cases of no overlapping children or parents
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4766 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPrelateobjects.m
+++ CPsubfunctions/CPrelateobjects.m
@@ -82,25 +82,32 @@ BothForegroundMask = (ChildLabelMatrix > 0) & (ParentLabelMatrix > 0);
 %%% child/parent overlap counts.
 ParentChildLabelHistogram = full(sparse(ParentLabelMatrix(BothForegroundMask), ChildLabelMatrix(BothForegroundMask), 1, NumberOfParents, NumberOfSubobjects));
 
-%%% For each child, we must choose a single parent.  We will choose
-%%% this by maximum overlap, which in this case is maximum value in
-%%% the child's column in the histogram.  sort() will give us the
-%%% necessary parent (row) index as its second return argument.
-[OverlapCounts, ParentIndexes] = sort(ParentChildLabelHistogram);
-
-% Get the parent list.
-ParentList = ParentIndexes(end, :);
-
-% handle the case of a zero overlap -> no parent
-ParentList(OverlapCounts(end, :) == 0) = 0;
-
-% transpose to a column vector
-ParentList = ParentList';
-
-%%% Now we need the number of children for each parent.  We can get
-%%% this as a histogram, again.  Must only use children that actually
-%%% have a parent.
-ChildCounts = full(sparse(ParentList(ParentList > 0), 1, 1, NumberOfParents, 1));
+%%% Make sure there are overlapping parents and children
+if any(ParentChildLabelHistogram(:)),
+    %%% For each child, we must choose a single parent.  We will choose
+    %%% this by maximum overlap, which in this case is maximum value in
+    %%% the child's column in the histogram.  sort() will give us the
+    %%% necessary parent (row) index as its second return argument.
+    [OverlapCounts, ParentIndexes] = sort(ParentChildLabelHistogram);
+    
+    % Get the parent list.
+    ParentList = ParentIndexes(end, :);
+    
+    % handle the case of a zero overlap -> no parent
+    ParentList(OverlapCounts(end, :) == 0) = 0;
+    
+    % transpose to a column vector
+    ParentList = ParentList';
+    
+    %%% Now we need the number of children for each parent.  We can get
+    %%% this as a histogram, again.  Must only use children that actually
+    %%% have a parent.
+    ChildCounts = full(sparse(ParentList(ParentList > 0), 1, 1, NumberOfParents, 1));
+else
+    %%% No overlapping parents or children.
+    ChildCounts = zeros(1, NumberOfParents);
+    ParentList = zeros(1, NumberOfSubobjects);
+end
 
 %%% Add the new measurements to the handles
 handles = CPaddmeasurements(handles,ChildName,'Parent',ParentName,ParentList);

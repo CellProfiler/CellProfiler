bfefbef595cc710a51bf4cfd59524ac16c427e3e
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-09-20 19:07:24 +0000

    plot a density map
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4841 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPdensity_plot.m
@@ -0,0 +1,26 @@
+function figurenumber = CPdensity_plot(xvals, yvals, figurenumber)
+
+if nargin == 3,
+    CPfigure(figurenumber);
+else
+    figurenumber = CPfigure;
+end
+   
+minx = min(xvals);
+maxx = max(xvals);
+xedges = linspace(minx, maxx, 100);
+xvals = (xvals - minx) / (maxx - minx);
+
+miny = min(yvals);
+maxy = max(yvals);
+yedges = linspace(miny, maxy, 100);
+yvals = (yvals - miny) / (maxy - miny);
+
+counts = full(sparse(round(99 * yvals + 1), round(99 * xvals + 1), 1, 100, 100));
+counts = log(counts + 1);
+ax = newplot;
+surf(xedges, yedges, counts, counts, 'EdgeColor','none', 'FaceColor','interp');
+view(ax,2);
+colormap(ax,flipud(gray(256)))
+grid(ax,'off');
+drawnow

a1524063f4e12842db6252a93c9d30deb281eadf
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2007-12-14 17:43:27 +0000

    better error dialog
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4950 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPmsgbox.m
+++ CPsubfunctions/CPmsgbox.m
@@ -6,27 +6,69 @@ drawnow;
 
 %% This allows message boxes to be closed with 'Windows -> Close All'
 userData.Application = 'CellProfiler';
+tempstring = varargin{1};
+for i=1:length(tempstring),
+    % add a newline to each line of the input
+    tempstring{i} = [tempstring{i} 10];
+end
+userData.WarningText = [tempstring{:}];
 set(h,'UserData',userData);
 
 try
     % Find the ui elements (not the most portable way to do this, hence the "try"
     children = get(h, 'Children');
-    ax = children(1);
-    okbutton = children(2);
-    oldtext = get(ax, 'Children');
+    ax = children(2);
+    okbutton = children(3);
+    txt = get(ax, 'Children');
     
     % Compute the rescaling
     oldfontsize = get(ax, 'FontSize');
     newfontsize = get(0, 'defaultuicontrolfontsize');
     ratio = (newfontsize+1) / oldfontsize;
 
+
+    % move the ok button to the left to make room for the copy-to-clipboard button
+    set(ax, 'Units', 'points');
+    okPos = get(okbutton, 'Position');
+    axPos = get(ax, 'Position');
+    % 3.5 is 1 + 2.5, 2.5 is the width of the Copy button below
+    okPos(1) = (axPos(3) - 3.5 * okPos(3)) / 2;
+    set(okbutton, 'Position', okPos);
+
     % change fonts, and switch everything to normalized layout
     set(okbutton, 'FontSize', newfontsize, 'units', 'normalized');
     set(ax, 'units', 'normalized', 'FontSize', newfontsize);
+    set(txt, 'units', 'normalized', 'Fontsize', newfontsize);
+
+
 
     % resize the msgbox
     oldPos = get(h, 'Position');
     set(h, 'Position', [oldPos(1:2), oldPos(3:4)*ratio], 'Resize', 'on', 'Units', 'characters');
+
+    Font.FontUnits=get(okbutton, 'FontUnits');
+    Font.FontSize=get(okbutton,'FontSize');
+    Font.FontName=get(okbutton,'FontName');
+    Font.FontWeight=get(okbutton,'FontWeight');
+
+    okPos = get(okbutton, 'Position');
+    copypos = okPos;
+    % set the position next to the OK button
+    copypos(1) = copypos(1) + copypos(3);
+    % make it 2.5 times wider
+    copypos(3) = copypos(3) * 2.5;
+
+    CopyHandle = uicontrol(h                                   , ...
+        Font                                                   , ...
+        'Style'              ,'pushbutton'                     , ...
+        'Units'              ,'normalized'                     , ...
+        'Position'           , copypos                         , ...
+        'CallBack'           ,'ud = get(gcbf, ''UserData''); clipboard(''copy'', ud.WarningText);'                    , ...
+        'KeyPressFcn'        ,@doKeyPress                       , ...
+        'String'             ,'Copy To Clipboard'               , ...
+        'HorizontalAlignment','center'                          , ...
+        'Tag'                ,'CopyButton'                        ...
+        );
 
 
     % why are both of these necessary? (I hate matlab gui)

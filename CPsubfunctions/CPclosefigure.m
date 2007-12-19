569a5d1c4afeac3a4942a4f4420cf483eee2aa35
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2007-12-19 15:41:32 +0000

    New function to close an unused window in numerous modules
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@4986 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPclosefigure.m
@@ -0,0 +1,37 @@
+function CPclosefigure(handles,CurrentModule)
+%
+% This function closes a window for the current set being analyzed.
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Authors:
+%   Anne E. Carpenter
+%   Thouis Ray Jones
+%   In Han Kang
+%   Ola Friman
+%   Steve Lowe
+%   Joo Han Chang
+%   Colin Clarke
+%   Mike Lamprecht
+%   Peter Swire
+%   Rodrigo Ipince
+%   Vicky Lay
+%   Jun Liu
+%   Chris Gang
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 4922 $
+
+%%% The figure window display is unnecessary for the calling module, so it is
+%%% closed during the starting image cycle.
+if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+    ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+    if any(findobj == ThisModuleFigureNumber)
+        close(ThisModuleFigureNumber)
+    end
+end

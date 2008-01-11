8dc742d17703b9879b5ca767883353a46e8e25b0
ljosa <ljosa@1fc53939-2000-0410-845c-e8453a809027>
2008-01-11 20:16:44 +0000

    New modules for the Northwestern project.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5016 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/IdentifyPrimLoG.m
@@ -0,0 +1,106 @@
+function handles = IdentifyPrimLog(handles)
+
+% Help for the Identify Primary LoG module:
+% Category: Object Processing
+%
+% SHORT DESCRIPTION:
+% Identifies objects given only an image as input.
+% *************************************************************************
+%
+% This module identifies primary objects (e.g. nuclei) in grayscale images
+% that show bright objects on a dark background.
+%
+% $Revision: 5009 $
+
+%%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%%
+drawnow
+
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+%textVAR01 = What did you call the images you want to process?
+%infotypeVAR01 = imagegroup
+ImageName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+%inputtypeVAR01 = popupmenu
+
+%textVAR02 = What do you want to call the objects identified by this module?
+%defaultVAR02 = Nuclei
+%infotypeVAR02 = objectgroup indep
+ObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+
+%textVAR03 = Typical diameter of objects, in pixel units:
+%defaultVAR03 = 10
+Radius = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+
+%textVAR04 = Score threshold for match
+%defaultVAR04 = 1e-3
+Threshold = char(handles.Settings.VariableValues{CurrentModuleNum,4});
+
+%%%VariableRevisionNumber = 1
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% PRELIMINARY ERROR CHECKING & FILE HANDLING %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+OrigImage = double(CPretrieveimage(handles,ImageName,ModuleName,'MustBeGray','CheckScale'));
+Radius = str2double(Radius);
+Threshold = str2double(Threshold);
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% IMAGE ANALYSIS %%%
+%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+im = double(OrigImage) - double(min(OrigImage(:)));
+im = im / max(im(:));
+
+ac = lapofgau(1 - im, Radius);
+ac(find(ac < Threshold)) = Threshold;
+ac = ac - Threshold;
+ac2 = ac;
+indices = find(imregionalmax(ac2));
+maxima = sortrows([indices ac2(indices)], -2);
+
+bw = false(size(im));
+bw(maxima(:,1)) = true;
+FinalLabelMatrixImage = bwlabel(bw);
+
+% The dilated mask is used only for visualization.
+dilated = imdilate(bw, strel('disk', 2));
+vislabel = bwlabel(dilated);
+r = im;
+g = im;
+b = im;
+r(dilated) = 1;
+g(dilated) = 0;
+b(dilated) = 0;
+visRGB = cat(3, r, g, b);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+if any(findobj == ThisModuleFigureNumber)
+  CPfigure(handles,'Image',ThisModuleFigureNumber);
+  CPimagesc(visRGB, handles);
+  title([ObjectName, ' cycle # ',num2str(handles.Current.SetBeingAnalyzed)]);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% SAVE DATA TO HANDLES STRUCTURE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+prefixes = {'Segmented', 'SmallRemovedSegmented'};
+for i=1:length(prefixes)
+  prefix = prefixes{i};
+  fieldname = [prefix, ObjectName];
+  handles.Pipeline.(fieldname) = FinalLabelMatrixImage;
+
+  handles = CPsaveObjectCount(handles, fieldname, FinalLabelMatrixImage);
+  handles = CPsaveObjectLocations(handles, fieldname, FinalLabelMatrixImage);
+end
--- Modules/UnifyObjects.m
+++ Modules/UnifyObjects.m
@@ -4,8 +4,12 @@ function handles = UnifyObjects(handles)
 % Category: Object Processing
 %
 % SHORT DESCRIPTION:
+% 
 % Relabels objects so that objects within a specified distance of each
-% other have the same label.
+% other have the same label.  Optionally, only merge two objects if
+% the straight line connecting their centroids has a relatively
+% uniform intensity in a specified image.
+%
 % *************************************************************************
 %
 % If the distance threshold is zero, only objects that are touching
@@ -17,11 +21,8 @@ function handles = UnifyObjects(handles)
 %
 % $Revision$
 
-%%%%%%%%%%%%%%%%%
-%%% VARIABLES %%%
-%%%%%%%%%%%%%%%%%
+%%% VARIABLES
 drawnow
-
 [CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
 
 %textVAR01 = What did you call the objects you want to filter?
@@ -33,41 +34,88 @@ ObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 %defaultVAR02 = UnifiedObjects
 %infotypeVAR02 = objectgroup indep
 RelabeledObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
-
 %textVAR03 = Distance within which objects should be unified
 %defaultVAR03 = 0
 DistanceThreshold = str2num(char(handles.Settings.VariableValues{CurrentModuleNum,3})); %#ok Ignore MLint
 
+%textVAR04 = Grayscale image the intensities of which are used to determine whether to merge (optional, see help)
+%infotypeVAR04 = imagegroup
+%defaultVAR04 = None
+GrayscaleImageName = char(handles.Settings.VariableValues{CurrentModuleNum,4});
+%inputtypeVAR04 = popupmenu custom
+
 %%%VariableRevisionNumber = 1
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% PRELIMINARY CALCULATIONS & FILE HANDLING %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Repeat for each of the three images by calling a subfunction that
+% does the actual work.
+handles = doItForObjectName(handles, 'Segmented', ObjectName, RelabeledObjectName, DistanceThreshold, GrayscaleImageName);
+handles = doItForObjectName(handles, 'SmallRemovedSegmented', ObjectName, RelabeledObjectName, DistanceThreshold, GrayscaleImageName);
+
+function handles = doItForObjectName(handles, prefix, ObjectName, RelabeledObjectName, DistanceThreshold, GrayscaleImageName)
+drawnow
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+Orig = CPretrieveimage(handles, [prefix, ObjectName], ModuleName);
+
+%%% IMAGE ANALYSIS
 drawnow
 
-Orig = CPretrieveimage(handles,['Segmented', ObjectName],ModuleName);
-
-
-%%%%%%%%%%%%%%%%%%%%%%
-%%% IMAGE ANALYSIS %%%
-%%%%%%%%%%%%%%%%%%%%%%
-drawnow
-
-dilated = imdilate(Orig > 0, strel('disk', DistanceThreshold));
-merged = bwlabel(dilated);
-merged(Orig == 0) = 0;
-Relabeled = merged;
-
-
-%%%%%%%%%%%%%%%%%%%%%%%
-%%% DISPLAY RESULTS %%%
-%%%%%%%%%%%%%%%%%%%%%%%
+if strcmp(GrayscaleImageName, 'None')
+  dilated = imdilate(Orig > 0, strel('disk', DistanceThreshold));
+  merged = bwlabel(dilated);
+  merged(Orig == 0) = 0;
+  Relabeled = merged;
+else
+  % Within each non-contiguous object of Relabeled, consider all pairs
+  % of components.  Compute a profile of intensities (from
+  % GrayscaleImage) along the line connecting the components' centroids.
+  % If this profile looks like the components belong to separate
+  % objects, break the object back up. (XXX)
+  GrayscaleImage = double(CPretrieveimage(handles, GrayscaleImageName, ModuleName, 'MustBeGray', 'CheckScale'));
+  Relabeled = Orig;
+  props = regionprops(Orig, {'Centroid'});
+  n = length(props);
+  for a=1:n
+    xa = props(a).Centroid(1);
+    ya = props(a).Centroid(2);
+    for b=1:n
+      xb = props(b).Centroid(1);
+      yb = props(b).Centroid(2);
+      d = sqrt((xa - xb)^2 + (ya - yb)^2);
+      if d <= DistanceThreshold
+	coords = brlinexya(xa, ya, xb, yb);
+	xp = coords(:,1);
+	yp = coords(:,2);
+	profile = zeros(size(coords,1),1);
+	for p = 1:length(profile)
+	  profile(p) = GrayscaleImage(yp(p), xp(p));
+	end
+	if min(profile) > min(profile(1), profile(end))*0.9
+	  Relabeled(Relabeled == b) = a;
+	end
+      end
+    end
+  end
+end
+    
+%%% DISPLAY RESULTS
 drawnow
 
 ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
 if any(findobj == ThisModuleFigureNumber)
     OrigRGB = CPlabel2rgb(handles,Orig);
-    RelabeledRGB = CPlabel2rgb(handles,Relabeled);
+
+    vislabel = Relabeled;
+    props = regionprops(Relabeled, {'ConvexImage', 'BoundingBox'});
+    for k=1:length(props)
+      ci = props(k).ConvexImage;
+      bb = props(k).BoundingBox;
+      mask = false(size(Orig));
+      mask(bb(2)+0.5:bb(2)+bb(4)-0.5, bb(1)+0.5:bb(1)+bb(3)-0.5) = ci;
+      mask(imerode(mask, strel('disk', 1))) = 0;
+      vislabel(mask) = k;
+    end
+    RelabeledRGB = CPlabel2rgb(handles, vislabel);
 
     CPfigure(handles,'Image',ThisModuleFigureNumber);
     if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
@@ -81,13 +129,11 @@ if any(findobj == ThisModuleFigureNumber)
     title(RelabeledObjectName);
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% SAVE DATA TO HANDLES STRUCTURE %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% SAVE DATA TO HANDLES STRUCTURE
 drawnow
 
 %%% Saves the final segmented label matrix image to the handles structure.
-fieldname = ['Segmented',RelabeledObjectName];
+fieldname = [prefix, RelabeledObjectName];
 handles.Pipeline.(fieldname) = Relabeled;
 
 if ~isfield(handles.Measurements,RelabeledObjectName)

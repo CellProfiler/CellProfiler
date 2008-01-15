f65ed2d3a45624f2401c378d067c35e11e3bf663
ljosa <ljosa@1fc53939-2000-0410-845c-e8453a809027>
2008-01-15 17:38:03 +0000

    Bug fix: Added missing lapofgau function.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5017 1fc53939-2000-0410-845c-e8453a809027

--- Modules/IdentifyPrimLoG.m
+++ Modules/IdentifyPrimLoG.m
@@ -104,3 +104,14 @@ for i=1:length(prefixes)
   handles = CPsaveObjectCount(handles, fieldname, FinalLabelMatrixImage);
   handles = CPsaveObjectLocations(handles, fieldname, FinalLabelMatrixImage);
 end
+
+function f = lapofgau(im, s);
+% im: image matrix (2 dimensional)
+% s: filter width
+% f: filter output.
+% Author: Baris Sumengen - sumengen@ece.ucsb.edu
+
+sigma = (s-1)/3;
+op = fspecial('log',s,sigma); 
+op = op - sum(op(:))/prod(size(op)); % make the op to sum to zero
+f = filter2(op,im);

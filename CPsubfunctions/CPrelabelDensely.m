b33fc85dd955314b9bf630bb7963d4825ed27917
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-02-01 15:53:57 +0000

    Minor: suppress output
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5060 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPrelabelDensely.m
+++ CPsubfunctions/CPrelabelDensely.m
@@ -2,8 +2,8 @@
 % consecutively.
 function labels = CPrelabelDensely(labels)
 props = regionprops(labels, {'Area', 'PixelIdxList'});
-present = find([props.Area] > 0)
-missing = find([props.Area] == 0)
+present = find([props.Area] > 0);
+missing = find([props.Area] == 0);
 for i=1:length(missing)
   object_to_relabel = present(length(present) - i + 1);
   if object_to_relabel > missing(i)

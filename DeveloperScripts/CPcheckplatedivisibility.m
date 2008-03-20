f73474b3c3f7e2fc6357d76950a354fbd0294f37
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2008-03-20 21:29:26 +0000

    update help
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5130 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/CPcheckplatedivisibility.m
+++ DeveloperScripts/CPcheckplatedivisibility.m
@@ -1,4 +1,4 @@
-function CPcheckplatedivisibility(handles.Current.NumberOfImageSets,NumberOfCyclesPerPlate);
+function CPcheckplatedivisibility(NumberOfImageSets,NumberOfCyclesPerPlate);
 
 % CellProfiler is distributed under the GNU General Public License.
 % See the accompanying file LICENSE for details.
@@ -16,7 +16,7 @@ function CPcheckplatedivisibility(handles.Current.NumberOfImageSets,NumberOfCycl
 %%% evenly divisible by the number of image cycles per plate. Issues a
 %%% warning if not (a warning that does not cancel processing).
 
-Divisibility = rem(handles.Current.NumberOfImageSets,NumberOfCyclesPerPlate);
+Divisibility = rem(NumberOfImageSets,NumberOfCyclesPerPlate);
 if  Divisibility~=0
     CPwarndlg(['Given your specifications for numbers of rows, columns, and image cycles per well, the number of image cycles you have chosen works out to ', num2str(handles.Current.NumberOfImageSets/NumberOfCyclesPerPlate),' plates, which is not an integer. You may want to check that your settings and/or image files are correct.']);
 end

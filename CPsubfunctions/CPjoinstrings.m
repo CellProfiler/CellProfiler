33e937fdb65dd3fc452476396409fcbc8201ed01
ljosa <ljosa@1fc53939-2000-0410-845c-e8453a809027>
2008-04-25 14:59:32 +0000

    New function CPjoinstrings for use in calles to CPaddmeasurements.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5173 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPjoinstrings.m
@@ -0,0 +1,35 @@
+% CPJOINSTRINGS Convert arguments to strings and join together with underscore.
+%    The function takes a variable number of argument.  Each argument can be
+%    a string (i.e., a character array) or a number.
+%
+%    Examples:
+%       s = CPjoinstrings() returns the empty string.
+%       s = CPjoinstrings('foo') returns 'foo'.
+%       s = CPjoinstrings('foo', 42) returns 'foo_42'.
+%       s = CPjoinstrings('foo', 23, 'bar') returns 'foo_23_bar'.
+function string = CPjoinstrings(varargin)
+%
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 5139 $
+string = '';
+for i = 1:nargin
+  if i > 1
+    string = [string, '_'];
+  end
+  arg = varargin{i};
+  if ischar(arg)
+    fragment = arg;
+  else
+    fragment = num2str(arg);
+  end
+  string = [string, fragment];
+end

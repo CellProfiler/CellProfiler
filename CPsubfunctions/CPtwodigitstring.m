d625d1deaab6cfd2b2ad109e30888fb657f823a3
carpenter <carpenter@1fc53939-2000-0410-845c-e8453a809027>
2008-05-01 18:52:42 +0000

    Updated to new CPaddmeasurements strategy
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5233 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPtwodigitstring.m
@@ -0,0 +1,7 @@
+function twodigit = CPtwodigitstring(val)
+%TwoDigitString is a function like num2str(int) but it returns a two digit
+%representation of a string for our purposes.
+if ((val > 99) || (val < 0)),
+    error(['TwoDigitString: Can''t convert ' num2str(val) ' to a 2 digit number']);
+end
+twodigit = sprintf('%02d', val);

1a550ea29af123ba498a11a91ff59d70fd3a0649
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-06-13 21:08:07 +0000

    Adds some checks, and uses CPjoinstrings
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5345 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/CPtruncatefeaturename.m
+++ DeveloperScripts/CPtruncatefeaturename.m
@@ -1,11 +1,21 @@
-TruncatedName = CPtruncatefeaturename(FeatureName,StrLen,dlmtr);
+function TruncatedName = CPtruncatefeaturename(FeatureName,MinStrLen,dlmtr)
 
-if ~exist(dlmtr,'var')
+if nargin < 3
     dlmtr = '_';
 end
 
+if isempty(FeatureName)
+    error('FeatureName cannot be an empty string')
+end
+
+TruncatedName = '';
 FeatureNameSubstrings = textscan(FeatureName,'%s','delimiter',dlmtr);
 for idxStr = 1:length(FeatureNameSubstrings{1})
     Str = FeatureNameSubstrings{1}{idxStr};
-    TruncatedName{idxStr} = Str(1:min(length(Str),MinStrLen));
+    TruncatedName = CPjoinstrings(TruncatedName,Str(1:min(length(Str),MinStrLen)));
+end
+
+%% Just in case, after all the truncation
+if length(TruncatedName) > 63
+    TruncatedName = TruncatedName(1:63);
 end

c322df0bbb7f1f5d73768e1f420cd2b6f1a6bbe8
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-06-17 14:12:43 +0000

    Major: Changed help re: saving, Changed to FeatureNbr to match other modules, added a check for all NaNs in denominator, added error message, implemented CPtruncatefeaturename, and changed saving procedure if both objects have the same name
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5358 1fc53939-2000-0410-845c-e8453a809027

--- Modules/CalculateRatios.m
+++ Modules/CalculateRatios.m
@@ -22,11 +22,12 @@ function handles = CalculateRatios(handles)
 % zero will also be changed to the average of the rest of the data, because
 % you cannot take the log of zero.
 %
-% The ratios will be stored along with the numerator object's data. If the
-% numerator is an object, data will be under the name Ratio. If the
-% numerator is an image, data will be under the name SingleRatio or
-% MultipleRatio depending on whether the denominator is another image or an
-% object, respectively.
+% Saving:
+% The ratio measurements are stored as 'Ratio_...'. If both measures are 
+% image-based, then a single ratio (per cycle) will be stored as 'Image' data.  
+% If one measure is object-based and one image-based, then the ratios will
+% be stored associated with the object, one ratio per object.  If both are 
+% objects, then the ratios are stored with both objects.
 %
 % Feature Number:
 % The feature number specifies which features from the Measure module(s)
@@ -54,7 +55,7 @@ drawnow
 
 [CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
 
-%textVAR01 = What do you want to call the ratio calculated by this module?
+%textVAR01 = What do you want to call the ratio calculated by this module?  The prefix 'Ratio_' will be applied to your entry, or simply leave as 'Automatic' and a sensible name will be generated
 %defaultVAR01 = Automatic
 RatioName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 
@@ -76,7 +77,7 @@ Category{1} = char(handles.Settings.VariableValues{CurrentModuleNum,3});
 
 %textVAR04 = Which feature do you want to use? (Enter the feature number - see help for details)
 %defaultVAR04 = 1
-FeatureNumber{1} = str2double(handles.Settings.VariableValues{CurrentModuleNum,4});
+FeatureNbr{1} = str2double(handles.Settings.VariableValues{CurrentModuleNum,4});
 
 %textVAR05 = For INTENSITY or TEXTURE features, which image's measurements would you like to use?
 %infotypeVAR05 = imagegroup
@@ -104,7 +105,7 @@ Category{2} = char(handles.Settings.VariableValues{CurrentModuleNum,8});
 
 %textVAR09 = Which feature do you want to use? (Enter the feature number - see help for details)
 %defaultVAR09 = 1
-FeatureNumber{2} = str2double(handles.Settings.VariableValues{CurrentModuleNum,9});
+FeatureNbr{2} = str2double(handles.Settings.VariableValues{CurrentModuleNum,9});
 
 %textVAR10 = For INTENSITY or TEXTURE features, which image's measurements would you like to use?
 %infotypeVAR10 = imagegroup
@@ -129,46 +130,46 @@ LogChoice = char(handles.Settings.VariableValues{CurrentModuleNum,12});
 drawnow
 SetBeingAnalyzed = handles.Current.SetBeingAnalyzed;
 
-%% Check FeatureNumber
+%% Check FeatureNbr
 for NumerDenom = 1:2
-    if isempty(FeatureNumber{NumerDenom}) || isnan(FeatureNumber{NumerDenom})
+    if isempty(FeatureNbr{NumerDenom}) || isnan(FeatureNbr{NumerDenom})
         error(['Image processing was canceled in the ', ModuleName, ' module because your entry for feature number is not valid.']);
     end
 end
 
 %% Get the correct fieldname where measurements are located
-OrigCategory = Category;
 for NumerDenom=1:2
-    FeatureName{NumerDenom} = CPgetfeaturenamesfromnumbers(handles,ObjectName{NumerDenom},...
-        Category{NumerDenom},FeatureNumber{NumerDenom},Image{NumerDenom},TextureScale{NumerDenom});
-    % end
 
     %% Get measurements
     try
-        %% NOTE: Numerator is NumerDenomMeasurements{1} and 
-        %% NOTE: Denominator is NumerDenomMeasurements{2} 
+        FeatureName{NumerDenom} = CPgetfeaturenamesfromnumbers(handles,ObjectName{NumerDenom},...
+            Category{NumerDenom},FeatureNbr{NumerDenom},Image{NumerDenom},TextureScale{NumerDenom});
+        
+        %% NOTE:    Numerator data will be NumerDenomMeasurements{1} and 
+        %%          Denominator data will be NumerDenomMeasurements{2} 
         NumerDenomMeasurements{NumerDenom} = ...
             handles.Measurements.(ObjectName{NumerDenom}).(FeatureName{NumerDenom}){SetBeingAnalyzed};
     catch
         error(['Image processing was canceled in the ', ModuleName, ...
-            ' module because an error ocurred when retrieving the numerator data.' ...
-            ' Either the category of measurement you chose, ', FeatureName{NumerDenom},...
-            ', was not available for ', ObjectName{NumerDenom},', or the feature number, ', ...
-            num2str(FeatureNumber{NumerDenom}), ', exceeded the amount of measurements.']);
+            ' module (#' num2str(CurrentModuleNum) ...
+            ') because an error ocurred when retrieving the data.  '...
+            'Likely the category of measurement you chose, ',...
+            Category{NumerDenom}, ', was not available for ', ...
+            ObjectName{NumerDenom},' with feature number ' num2str(FeatureNbr{NumerDenom}) ...
+            ', possibly specific to image ''' Image{NumerDenom} ''' and/or ' ...
+            'Texture Scale = ' num2str(TextureScale{NumerDenom}) '.']);
     end
 end
 % Check size of data, and make them the same size
 if length(NumerDenomMeasurements{1}) ~= length(NumerDenomMeasurements{2})
-    try
-        if strcmp(ObjectName{1},'Image')
-            NumerDenomMeasurements{1} = NumerDenomMeasurements{1}*ones(size(NumerDenomMeasurements{2}));
-        elseif strcmp(ObjectName{2},'Image')
-            NumerDenomMeasurements{2} = NumerDenomMeasurements{2}*ones(size(NumerDenomMeasurements{2}));
-        else
-            error('');
-        end
-    catch
-        error(['Image processing was canceled in the ', ModuleName, ' module because the specified object names ',ObjectName{1},' and ',ObjectName{2},' do not have the same amount of measurements.']);
+    if strcmp(ObjectName{1},'Image')
+        NumerDenomMeasurements{1} = NumerDenomMeasurements{1}*ones(size(NumerDenomMeasurements{2}));
+    elseif strcmp(ObjectName{2},'Image')
+        NumerDenomMeasurements{2} = NumerDenomMeasurements{2}*ones(size(NumerDenomMeasurements{1}));
+    else
+        error(['Image processing was canceled in the ', ModuleName, ...
+            ' module because the specified object names ''',ObjectName{1},...
+            ''' and ''',ObjectName{2},''' do not have the same number of measurements.']);
     end
 end
 
@@ -177,7 +178,14 @@ end
 %% of the remaining values.
 NumerDenomMeasurements{2}(NumerDenomMeasurements{2}==0) = NaN;
 NumerDenomMeasurements{2}(isnan(NumerDenomMeasurements{2})) = CPnanmean(NumerDenomMeasurements{2});
-FinalMeasurements = NumerDenomMeasurements{1}./NumerDenomMeasurements{2};
+if ~all(isnan(NumerDenomMeasurements{2}))
+    FinalMeasurements = NumerDenomMeasurements{1}./NumerDenomMeasurements{2};
+else
+    FinalMeasurements = NaN;
+    CPwarndlg(['A ratio of ' NumerDenomMeasurements{1} ' and ' NumerDenomMeasurements{2} ...
+        ' within ' ModuleName ' on cycle '  str2double(SetBeingAnalyzed) ...
+        ' resulted in all NaNs.  You may want to check your settings.'])
+end
 if strcmp(LogChoice,'Yes')
     %%% We cannot take the log of zero, so replace zeros with the mean of the remaining values.
     %FinalMeasurements(FinalMeasurements==0)=CPnanmean(FinalMeasurements);
@@ -191,40 +199,30 @@ FinalMeasurements(isnan(FinalMeasurements))=0;
 
 if ~isvarname(RatioName)
     RatioName = 'Automatic';
-    CPwarndlg(['The ratio name you entered was invalid, and has been replaced with ',RatioName,'.']);
-elseif strcmpi(RatioName, 'Automatic')
-
-    %% TODO
-%     %% Since Matlab's max name length is 63, we need to truncate the fieldname
-%     MinStrLen = 5;
-%     FirstFeatureNameSubstrings = textscan(FirstFeatureName,'%s','delimiter','_');
-%     for idxStr = 1:length(FirstFeatureNameSubstrings{1})
-%         Str = FirstFeatureNameSubstrings{1}{idxStr};
-%         FirstTruncatedName{idxStr} = Str(1:min(length(Str),MinStrLen));
-%     end
-%     SecondFeatureNameSubstrings = textscan(SecondFeatureName,'%s','delimiter','_');
-%     for idxStr = 1:length(SecondFeatureNameSubstrings{1})
-%         Str = SecondFeatureNameSubstrings{1}{idxStr};
-%         SecondTruncatedName{idxStr} = Str(1:min(length(Str),MinStrLen));
-%     end
-%     NewFirstFeatureName = CPjoinstrings(FirstTruncatedName{:});
-%     NewSecondFeatureName = CPjoinstrings(SecondTruncatedName{:});
-
-
-    RatioName = CPjoinstrings('Ratio',ObjectName{1},FeatureName{1},'DividedBy',...
-                                ObjectName{2},FeatureName{2});
+    CPwarndlg(['The ratio name you entered was invalid, and has been replaced '...
+        'with an automatically generated name based on your inputs (equivalent to ''Automatic'' setting).']);
+end
+if strcmpi(RatioName, 'Automatic')
+    RatioName = CPjoinstrings('Ratio',ObjectName{1},FeatureName{1},...
+                          'DividedBy',ObjectName{2},FeatureName{2});
+    RatioName = CPtruncatefeaturename(RatioName,5);
 else
     RatioName = CPjoinstrings('Ratio', RatioName);
 end
 
-if strcmp(ObjectName{1}, 'Image') and strcmp(ObjectName{2}, 'Image'),
-    handles = CPaddmeasurements(handles,'Image',CPjoinstrings('Ratio', RatioName),FinalMeasurements);
+%% Save, depending on type of measurement (ObjectName)
+%% Note that Image measurements are scalars, while Objects are potentially vectors
+if strcmp(ObjectName{1}, 'Image') && strcmp(ObjectName{2}, 'Image'),
+    handles = CPaddmeasurements(handles,'Image',RatioName,FinalMeasurements);
 else
-    if ~ strcmp(ObjectName{1}, 'Image'),
+    if ~strcmp(ObjectName{1}, 'Image'),
         handles = CPaddmeasurements(handles,ObjectName{1},RatioName,FinalMeasurements);
     end
-    if ~ strcmp(ObjectName{2}, 'Image'),
-        handles = CPaddmeasurements(handles,ObjectName{2},RatioName,FinalMeasurements);
+    %% Add to second object (if different from the first)
+    if ~strcmp(ObjectName{1}, ObjectName{2})
+        if ~strcmp(ObjectName{2}, 'Image'),
+            handles = CPaddmeasurements(handles,ObjectName{2},RatioName,FinalMeasurements);
+        end
     end
 end
 
@@ -248,12 +246,12 @@ if any(findobj == ThisModuleFigureNumber)
     if SetBeingAnalyzed == handles.Current.StartingImageSet
 
         % Text for Name of measurement
-        if strcmp(OrigCategory{1},'Intensity') || strcmp(OrigCategory{1},'Texture')
+        if strcmp(Category{1},'Intensity') || strcmp(Category{1},'Texture')
             DisplayName1 = [ObjectName{1} ' ' FeatureName{1} ' in ' Image{1}];
         else
             DisplayName1 = [ObjectName{1} ' ' FeatureName{1}];
         end
-        if strcmp(OrigCategory{2},'Intensity') || strcmp(OrigCategory{2},'Texture')
+        if strcmp(Category{2},'Intensity') || strcmp(Category{2},'Texture')
             DisplayName2 = [ObjectName{2} ' ' FeatureName{2} ' in ' Image{2}];
         else
             DisplayName2 = [ObjectName{2} ' ' FeatureName{2}];

184fd86a4ddef8bcb2fb2ca3f0606fa8c63722c3
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-06-19 17:39:33 +0000

    Updated for new CPaddmeasurements scheme, and new Gelfand movies
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5377 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/ObjectVelocities.m
+++ DeveloperScripts/ObjectVelocities.m
@@ -11,13 +11,15 @@ MIN_TOTAL_DIST = 5;
 % load /Volumes/imaging_analysis/2007_07_02Microtube_Motor_Screens_VladimirGelfand/2007_10_11MoviesForAnne/Converted/F07/output_2007_10_26/DefaultOUT__15.mat
 % load /Users/dlogan/Data/2007_10_11MoviesForAnne/Converted/F05/output_2007_11_06/DefaultOUT.mat
 % load /Users/dlogan/Data/2007_10_11MoviesForAnne/Converted/F07/output_2007_11_06/DefaultOUT.mat
-load('/Volumes/imaging_analysis-2/2007_07_02Microtube_Motor_Screens_VladimirGelfand/3_11_08_B05/2008_03_28_output/DefaultOUT__1.mat','handles')
+% load('/Volumes/imaging_analysis-2/2007_07_02Microtube_Motor_Screens_VladimirGelfand/3_11_08_B05/2008_03_28_output/DefaultOUT__1.mat','handles')
+load('/Volumes/IMAGING_ANALYSIS/2007_07_02Microtube_Motor_Screens_VladimirGelfand/2008_06_02_wild_type/TIF files wildtype from 31108/D02_31108/2008_06_05_output/DefaultOUT__13.mat')
 
-Objects = handles.Pipeline.TrackObjects.Labels;
-Locations = handles.Pipeline.TrackObjects.Locations;
+Objects = handles.Pipeline.TrackObjects.FilteredSpeckles.Labels;
+Locations = handles.Pipeline.TrackObjects.FilteredSpeckles.Locations;
 
 %% find max Label across frames
 HighestObjNum = max(cellfun(@(x)max(x(:)), Objects));
+% HighestObjNum = max(Objects(:));
 
 U = NaN .* ones(HighestObjNum,length(Objects)-1);
 V = NaN .* ones(HighestObjNum,length(Objects)-1);
@@ -35,7 +37,7 @@ for iObjNum = 1:HighestObjNum
     for iFrm = 1:STEP:length(Objects)
         
         thisFrameLocation = Locations{iFrm}(Objects{iFrm} == iObjNum,:);
-        
+%         thisFrameLocation = Locations(Objects(iFrm) == iObjNum,:);
         %% If object does not exist in one frame, then skip it
         if isempty(thisFrameLocation)
             break

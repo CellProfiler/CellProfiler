610a5c35f68509205d1698da313a34d5b6cb3cbb
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-06-25 21:20:23 +0000

    Minor edit
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5419 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPgetfeaturenamesfromnumbers.m
+++ CPsubfunctions/CPgetfeaturenamesfromnumbers.m
@@ -1,26 +1,26 @@
 function FeatureName = CPgetfeaturenamesfromnumbers(handles,ObjectName,Category,FeatureNumber,Image,TextureScale)
-%% Get FeatureNames from FeatureNumbers
-%%
-%% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber)
-%% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber, Image)
-%% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber, Image, TextureScale)
-%%
-%%  where   handles is the handles structure
-%%          ObjectNamem & Category are character strings
-%%          FeatureNumber, TextureScale are positive integers
-%%          Image can be can be a string (Image) or Numeric (Texture)
-%%
-%% where we are finding FeatureName from
-%%  handles.Measurements.ObjectName.Category_FeatureName (for Non-image/non-texture) OR
-%%  handles.Measurements.ObjectName.Category_FeatureName_Image (for Image) OR
-%%  handles.Measurements.ObjectName.Category_FeatureName_Image_TextureScale (for Texture)
+% Get FeatureNames from FeatureNumbers
+%
+% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber)
+% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber, Image)
+% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumber, Image, TextureScale)
+%
+%  where   handles is the handles structure
+%          ObjectNamem & Category are character strings
+%          FeatureNumber, TextureScale are positive integers
+%          Image can be can be a string (Image) or Numeric (Texture)
+%
+% where we are finding FeatureName from
+%  handles.Measurements.ObjectName.Category_FeatureName (for Non-image/non-texture) OR
+%  handles.Measurements.ObjectName.Category_FeatureName_Image (for Image) OR
+%  handles.Measurements.ObjectName.Category_FeatureName_Image_TextureScale (for Texture)
 
-%% Note!!  We are relying on the fact that the features should have been 
-%% added in the order matching that of each Module's Help text!
+% Note!!  We are relying on the fact that the features should have been 
+% added in the order matching that of each Module's Help text!
 
 error(nargchk(4, 6, nargin, 'string'))
 
-%% Parse inputs
+% Parse inputs
 if ~exist('Image','var'), Image = ''; end
 if ~exist('TextureScale','var'), TextureScale = ''; end
 if strcmp(Category,'Intensity')
@@ -34,12 +34,12 @@ AllFieldNames = fieldnames(handles.Measurements.(ObjectName));
 CurrentMeasure = [CPjoinstrings(Category,'.*',Image,num2str(TextureScale)),'$'];
 FieldnumsCategoryCell = regexp(AllFieldNames,CurrentMeasure);
 FieldnumsCategory = find(~cellfun('isempty',FieldnumsCategoryCell));
-%% Could do error checking here, since the next line is where this subfn usually errors
-%% (if it can't find a FeatureName), but we ought to use 'try/catch' in the 
-%% calling function, for better error handling
+% Could do error checking here, since the next line is where this subfn usually errors
+% (if it can't find a FeatureName), but we ought to use 'try/catch' in the 
+% calling function, for better error handling
 FeatureName = AllFieldNames(FieldnumsCategory(FeatureNumber));
 
-%% CHECK: There should be one Measurement that fulfills the above criteria
+% CHECK: There should be one Measurement that fulfills the above criteria
 if length(FeatureName) < 1
     error('No Measurements found.  Please check that the measurement exists in the pipeline above this module.')
 elseif length(FeatureName) > 1

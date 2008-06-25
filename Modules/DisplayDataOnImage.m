648d091e3a3b5da79e1dcb24a3ad64875e42fe4d
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-06-25 22:40:20 +0000

    Updated with most recent Measurements, included CPgetfeaturenamesfromnumbers, and incorporated CPimcapture
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5421 1fc53939-2000-0410-845c-e8453a809027

--- Modules/DisplayDataOnImage.m
+++ Modules/DisplayDataOnImage.m
@@ -45,15 +45,18 @@ drawnow
 ObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 
 %textVAR02 = Which category of measurements would you like to use?
+%choiceVAR02 = AreaOccupied
 %choiceVAR02 = AreaShape
 %choiceVAR02 = Children
-%choiceVAR02 = Correlation
 %choiceVAR02 = Intensity
 %choiceVAR02 = Neighbors
 %choiceVAR02 = Ratio
 %choiceVAR02 = Texture
+%choiceVAR02 = ImageQuality
+%choiceVAR02 = Granularity
+%choiceVAR02 = RadialDistribution
 %inputtypeVAR02 = popupmenu custom
-Measure = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+MeasureChoice = char(handles.Settings.VariableValues{CurrentModuleNum,2});
 
 %textVAR03 = Which feature do you want to use? (Enter the feature number - see help for details)
 %defaultVAR03 = 1
@@ -63,20 +66,38 @@ if isempty(FeatureNo) || isnan(FeatureNo)
     error(['Image processing was canceled in the ', ModuleName, ' module because your entry for the Feature Number is invalid.']);
 end
 
-%textVAR04 = For INTENSITY or TEXTURE features, which image was used to make the measurements?
+%textVAR04 = For AREASHAPE, INTENSITY or TEXTURE features, which image was used to make the measurements?
 %infotypeVAR04 = imagegroup
 %inputtypeVAR04 = popupmenu
 Image = char(handles.Settings.VariableValues{CurrentModuleNum,4});
 
-%textVAR05 = Which image do you want to display the data on?
-%infotypeVAR05 = imagegroup
-%inputtypeVAR05 = popupmenu
-DisplayImage = char(handles.Settings.VariableValues{CurrentModuleNum,5});
+%textVAR05 = For TEXTURE or RADIALDISTRIBUTION features, what previously measured texture scale (TEXTURE) or previously used number of bins (RADIALDISTRIBUTION) do you want to use?
+%defaultVAR05 = 1
+UserSpecifiedNumber = char(handles.Settings.VariableValues{CurrentModuleNum,5});
 
-%textVAR06 = What do you want to call the generated image with data?
-%defaultVAR06 = OrigDataDisp
-%infotypeVAR06 = imagegroup indep
-DataImage = char(handles.Settings.VariableValues{CurrentModuleNum,6});
+%textVAR06 = Which image do you want to display the data on?
+%infotypeVAR06 = imagegroup
+%inputtypeVAR06 = popupmenu
+DisplayImage = char(handles.Settings.VariableValues{CurrentModuleNum,6});
+
+%textVAR07 = What do you want to call the generated image with data?
+%defaultVAR07 = OrigDataDisp
+%infotypeVAR07 = imagegroup indep
+DataImage = char(handles.Settings.VariableValues{CurrentModuleNum,7});
+
+%textVAR08 = What resolution do you want the generated image captured at (units of DPI)?
+%choiceVAR08 = 96
+%choiceVAR08 = 150
+%choiceVAR08 = 300
+%inputtypeVAR08 = popupmenu custom
+DPIToSave = char(handles.Settings.VariableValues{CurrentModuleNum,8});
+
+%textVAR09 = What elements from the figure do you want to save?
+%choiceVAR09 = Figure
+%choiceVAR09 = Axes
+%choiceVAR09 = Image
+%inputtypeVAR09 = popupmenu
+SavedImageContents = lower(char(handles.Settings.VariableValues{CurrentModuleNum,9}));
 
 %%%VariableRevisionNumber = 1
 
@@ -90,42 +111,17 @@ SetBeingAnalyzed = handles.Current.SetBeingAnalyzed;
 
 %%% Get the correct fieldname where measurements are located
 CellFlg = 0;
-switch Measure
-    case 'AreaShape'
-        if strcmp(ObjectName,'Image')
-            Measure = '^AreaOccupied_.*Features$';
-            Fields = fieldnames(handles.Measurements.Image);
-            TextComp = regexp(Fields,Measure);
-            A = cellfun('isempty',TextComp);
-            try
-                Measure = Fields{find(A==0)+1};
-            catch
-                error(['Image processing was canceled in the ', ModuleName, ' module because the category of measurement you chose, ', Measure, ', was not available for ', ObjectName]);
-            end
-            CellFlg = 1;
-        end
-    case 'Intensity'
-        Measure = ['Intensity_' Image];
-    case 'Texture'
-        Measure = ['Texture_[0-9]*[_]?' Image '$'];
-        Fields = fieldnames(handles.Measurements.(ObjectName));
-        TextComp = regexp(Fields,Measure);
-        A = cellfun('isempty',TextComp);
-        try
-            Measure = Fields{A==0};
-        catch
-            error(['Image processing was canceled in the ', ModuleName, ' module because the category of measurement you chose, ', Measure, ', was not available for ', ObjectName]);
-        end
-    case 'Ratio'
-        Measure = '.*Ratio$';
-        Fields = fieldnames(handles.Measurements.(ObjectName));
-        TextComp = regexp(Fields,Measure);
-        A = cellfun('isempty',TextComp);
-        try
-            Measure = Fields{A==0};
-        catch
-            error(['Image processing was canceled in the ', ModuleName, ' module because the category of measurement you chose, ', Measure, ', was not available for ', ObjectName]);
-        end
+try
+    switch lower(MeasureChoice)
+        case {'areaoccupied','intensity','granularity','imagequality','radialdistribution'}
+            FeatureName = CPgetfeaturenamesfromnumbers(handles, ObjectName, MeasureChoice, FeatureNo, Image);
+        case {'areashape','neighbors','ratio'}
+            FeatureName = CPgetfeaturenamesfromnumbers(handles, ObjectName, MeasureChoice, FeatureNo);
+        case {'texture','radialdistribution'}
+            FeatureName = CPgetfeaturenamesfromnumbers(handles, ObjectName, MeasureChoice, FeatureNo, Image, UserSpecifiedNumber);
+    end
+catch
+    error(['Image processing was canceled in the ', ModuleName, ' module because the category of measurement you chose, ', MeasureChoice, ', was not available for ', ObjectName]);
 end
 
 %%% Reads the image.
@@ -138,7 +134,7 @@ drawnow
 
 ErrorFlag = 0;
 try
-    tmp = handles.Measurements.(ObjectName).(Measure){SetBeingAnalyzed};
+    ListOfMeasurements = handles.Measurements.(ObjectName).(FeatureName){SetBeingAnalyzed};
 catch
     ErrorFlag = 1;
 end
@@ -150,16 +146,17 @@ if ErrorFlag
     title('No objects identified.');
     CPwarndlg(['No objects were identified. This could mean that the measurements you have specified in the ',ModuleName,' are not being processed. Please verify that the Measure module precedes this module.']); 
 else
-    ListOfMeasurements = tmp(:,FeatureNo);
     if CellFlg
-        ListOfMeasurements = ListOfMeasurements{1};
+        if iscell(ListOfMeasurements),
+            ListOfMeasurements = ListOfMeasurements{1};
+        end
     end
     StringListOfMeasurements = cellstr(num2str(ListOfMeasurements));
 
     %%% Extracts the XY locations. This is temporarily hard-coded
     if ~strcmp(ObjectName,'Image')
-        Xlocations = handles.Measurements.(ObjectName).Location{SetBeingAnalyzed}(:,1);
-        Ylocations = handles.Measurements.(ObjectName).Location{SetBeingAnalyzed}(:,2);
+        Xlocations = handles.Measurements.(ObjectName).Location_Center_X{SetBeingAnalyzed};
+        Ylocations = handles.Measurements.(ObjectName).Location_Center_Y{SetBeingAnalyzed};
     else
         Xlocations = size(OrigImage,2)/2;
         Ylocations = size(OrigImage,1)/2;
@@ -171,13 +168,31 @@ else
     drawnow
     
     ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+    
     %%% Activates the appropriate figure window.
     DataHandle = CPfigure(handles,'Image',ThisModuleFigureNumber);
-    
+
+    %%% Modules won't create a figure window if the user has requested not
+    %%% to. But in this case, we need a figure window in order to get a 
+    %%% screenshot.
+    %%% Here, if the user doesn't want the figure displayed, we make it
+    %%% invisible. The image capture will still work anyway.
+    userdoesntwantwindow = ~handles.Preferences.DisplayWindows(str2num(handles.Current.CurrentModuleNumber));
+    if userdoesntwantwindow,
+        set(DataHandle,'visible','off');
+    end
     CPimagesc(OrigImage,handles);
     colormap(gray);
-    FeatureDisp = handles.Measurements.(ObjectName).([Measure,'Features']){FeatureNo};
-    Title = [ObjectName,', ',FeatureDisp,' on ',Image];
+    
+    % Now extract the FeatureName itself
+    % (1) Strip Category prefix + slash
+    justtheFeatureName = FeatureName(length([MeasureChoice,'_'])+1:end);
+    % (2) Find position of next slash (if there is one)
+    slash_index = regexp(justtheFeatureName,'_');
+    % (3) FeatureName is the string prior to the slash
+    if ~isempty(slash_index), slash_index = slash_index(1); justtheFeatureName = justtheFeatureName(1:slash_index-1); end
+
+    Title = [ObjectName,', ',justtheFeatureName,' on ',Image];
     Title = strrep(Title,'_','\_');
     title(Title);
 
@@ -190,7 +205,24 @@ else
     Info.ListOfMeasurements = ListOfMeasurements;
     Info.TextHandles = TextHandles;
     set(DataHandle,'UserData',Info);
+
+    dpi = str2double(DPIToSave);
+    if isnan(dpi),
+        error(['Image processing was canceled in the ', ModuleName, ' module because the value entered for the DPI (',DPIToSave,')was not numeric']);
+    end
+    switch SavedImageContents 
+        case 'image',   opt = 'img';
+        case 'axes',    opt = 'imgAx';
+        case 'figure',  opt = 'all';
+    end
+    drawnow;    % Need this here for the imcapture to work correctly
+    handles.Pipeline.(DataImage)= imcapture(DataHandle,opt,dpi);
+
+    % if the user didn't want to display the window in the first place,
+    % destroy this one so we don't have an invisible window hanging
+    % around
+    if userdoesntwantwindow && ishandle(DataHandle),
+        close(DataHandle);
+    end
 end
 
-OneFrame = getframe(DataHandle);
-handles.Pipeline.(DataImage)=OneFrame.cdata;

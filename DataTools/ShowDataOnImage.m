4f59634c4bac1a3e374b434d7966d5ebb73179c2
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-07-18 17:01:30 +0000

    Updated with new Measurements. Checked on example fly PIPE.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5468 1fc53939-2000-0410-845c-e8453a809027

--- DataTools/ShowDataOnImage.m
+++ DataTools/ShowDataOnImage.m
@@ -62,11 +62,14 @@ if RawFileName == 0,return,end
 
 load(fullfile(RawPathname,RawFileName));
 
+% Try to convert features
+handles = CP_convert_old_measurements(handles);
+
 %%% Call the function CPgetfeature(), which opens a series of list dialogs and
 %%% lets the user choose a feature. The feature can be identified via 'ObjectTypename',
 %%% 'FeatureType' and 'FeatureNo'.
 try
-    [ObjectTypename,FeatureType,FeatureNo] = CPgetfeature(handles,1,{'Features' 'Description'});
+    [ObjectTypename,FeatureType] = CPgetfeature(handles,1);
 catch
     ErrorMessage = lasterr;
     CPerrordlg(['An error occurred in the ShowDataOnImage Data Tool. ' ErrorMessage(30:end)]);
@@ -85,8 +88,11 @@ if SampleNumber > length(handles.Measurements.(ObjectTypename).(FeatureType))
     return
 end
 
-%%% Looks up the corresponding image file name.
-PotentialImageNames = handles.Measurements.Image.FileNamesText;
+%%% Looks up the corresponding image file name
+PipelineFields = fieldnames(handles.Pipeline);
+idx = strmatch('FileList',PipelineFields);
+PotentialImageNames = {}; for i = 1:length(idx), PotentialImageNames = cat(1,PotentialImageNames,handles.Pipeline.(PipelineFields{idx(i)}){SampleNumber}); end
+
 %%% Error detection.
 if isempty(PotentialImageNames)
     PromptMessage = 'CellProfiler was not able to look up the image file names used to create these measurements to help you choose the correct image on which to display the results. You may continue, but you are on your own to choose the correct image file.';
@@ -123,11 +129,12 @@ else
         try delete(h), end
         ImageFileName = {FileName};
     else
-        ImageFileName = handles.Measurements.Image.FileNames{SampleNumber}(Selection);
+        ImageFileName = handles.Pipeline.(PipelineFields{idx(Selection)}){SampleNumber};
         PromptMessage = ['Browse to find the image called ', ImageFileName,'.'];
-        Pathname=char(handles.Measurements.Image.PathNames{SampleNumber}(Selection));
-        FileName=char(ImageFileName);
-        if  ~exist(fullfile(Pathname,char(ImageFileName)),'file') %path and file does not exist there.
+        prefix = 'FileList';
+        Pathname = handles.Pipeline.(['Pathname',PipelineFields{idx(Selection)}(strfind(PipelineFields{idx(Selection)},prefix)+length(prefix):end)]);
+        FileName = char(ImageFileName);
+        if  ~exist(fullfile(Pathname,FileName),'file') %path and file does not exist there.
             %%% Prompts the user with the image file name.
             h = CPmsgbox(PromptMessage);
 
@@ -159,37 +166,32 @@ end
 TextFlag = 0;
 if iscell(tmp)    
     if FeatureType == 'Tracking'
-        StringListOfMeasurements = tmp(:,FeatureNo);
+        StringListOfMeasurements = tmp;
     else
         StringListOfMeasurements = handles.Measurements.(ObjectTypename).(FeatureType){SampleNumber};
     end    
     ListOfMeasurements = StringListOfMeasurements;
     TextFlag = 1;
 else
-    ListOfMeasurements = tmp(:,FeatureNo);
+    ListOfMeasurements = tmp;
     StringListOfMeasurements = cellstr(num2str(ListOfMeasurements));
 end
 
 %%% Extracts the XY locations. This is temporarily hard-coded
-Xlocations = handles.Measurements.(ObjectTypename).Location{SampleNumber}(:,1);
-Ylocations = handles.Measurements.(ObjectTypename).Location{SampleNumber}(:,2);
+Xlocations = handles.Measurements.(ObjectTypename).Location_Center_X{SampleNumber};
+Ylocations = handles.Measurements.(ObjectTypename).Location_Center_Y{SampleNumber};
 
 %%% Create window
-ImageFileName = strrep(ImageFileName,'_','\_');
+ImageDisp = strrep(ImageFileName,'_','\_');
 FigureHandle = CPfigure(handles,'image');
 CPimagesc(ImageToDisplay,handles);
-if TextFlag
-    FeatureDisp = handles.Measurements.(ObjectTypename).([FeatureType,'Description']){FeatureNo};
-else
-    FeatureDisp = handles.Measurements.(ObjectTypename).([FeatureType,'Features']){FeatureNo};
-end
-ImageDisp = ImageFileName{1};
+FeatureDisp = strrep(FeatureType,'_','\_');
+
 title([ObjectTypename,', ',FeatureDisp,' on ',ImageDisp])
 
 %%% Overlays the values in the proper location in the image.
 TextHandles = text(Xlocations , Ylocations , StringListOfMeasurements,...
     'HorizontalAlignment','center', 'color', [1 0 0],'fontsize',handles.Preferences.FontSize);
-
 
 %%% Create structure and save it to the UserData property of the window
 Info = get(FigureHandle,'UserData');
@@ -202,16 +204,11 @@ set(FigureHandle,'UserData',Info);
 StdUnit = 'point';
 StdColor = get(0,'DefaultUIcontrolBackgroundColor');
 PointsPerPixel = 72/get(0,'ScreenPixelsPerInch');
-VersionCheck = version;
-if strcmp(computer,'MAC') && str2num(VersionCheck(1:3)) < 7.1 %#ok Ignore MLint
-    DisplayButtonCallback1 = 'CPmsgbox(''A bug in MATLAB is preventing this function from working on the Mac platform. Service Request #1-RR6M1''), drawnow';
-else
-    DisplayButtonCallback1 = 'CurrentTextHandles = getfield(get(gcbf,''Userdata''),''TextHandles''); try, propedit(CurrentTextHandles,''v6''); catch, CPmsgbox(''A bug in MATLAB is preventing this function from working. Service Request #1-RR6M1''), end; drawnow';
-end
+
 uicontrol('Parent',FigureHandle, ...
     'Unit',StdUnit, ...
     'BackgroundColor',StdColor, ...
-    'CallBack',DisplayButtonCallback1, ...
+    'CallBack',@DisplayButtonCallback1, ...
     'Position',PointsPerPixel*[2 2 90 22], ...
     'Units','Normalized',...
     'String','Text Properties', ...
@@ -219,22 +216,20 @@ uicontrol('Parent',FigureHandle, ...
     'FontSize',handles.Preferences.FontSize);
 
 if ~TextFlag
-    DisplayButtonCallback2 = 'NumberOfDecimals = inputdlg(''Enter the number of decimal places to display'',''Enter the number of decimal places'',1,{''0''}); CurrentTextHandles = getfield(get(gcbf,''Userdata''),''TextHandles''); NumberValues = getfield(get(gcbf,''Userdata''),''ListOfMeasurements''); if(isempty(NumberOfDecimals)) return; end; Command = [''%.'',num2str(NumberOfDecimals{1}),''f'']; NewNumberValues = num2str(NumberValues,Command); CellNumberValues = cellstr(NewNumberValues); PropName(1) = {''string''}; set(CurrentTextHandles,PropName, CellNumberValues); drawnow';
     uicontrol('Parent',FigureHandle, ...
         'Unit',StdUnit, ...
         'BackgroundColor',StdColor, ...
-        'CallBack',DisplayButtonCallback2, ...
+        'CallBack',@DisplayButtonCallback2, ...
         'Position',PointsPerPixel*[100 2 135 22], ...
         'Units','Normalized',...
         'String','Significant digits', ...
         'Style','pushbutton', ...
         'FontSize',handles.Preferences.FontSize);
 
-    DisplayButtonCallback3 = 'CurrentTextHandles = getfield(get(gcbf,''Userdata''),''TextHandles''); ListOfMeasurements = getfield(get(gcbf,''Userdata''),''ListOfMeasurements''); StringListOfMeasurements = cellstr(num2str(ListOfMeasurements)); PropName(1) = {''string''}; set(CurrentTextHandles,PropName, StringListOfMeasurements);drawnow';
     uicontrol('Parent',FigureHandle, ...
         'Unit',StdUnit, ...
         'BackgroundColor',StdColor, ...
-        'CallBack',DisplayButtonCallback3, ...
+        'CallBack',@DisplayButtonCallback3, ...
         'Position',PointsPerPixel*[240 2 135 22], ...
         'Units','Normalized',...
         'String','Restore labels', ...
@@ -242,24 +237,80 @@ if ~TextFlag
         'FontSize',handles.Preferences.FontSize);
 end
 
-DisplayButtonCallback4 = 'CurrentTextHandles = getfield(get(gcbf,''Userdata''),''TextHandles''); set(CurrentTextHandles, ''visible'', ''off''); drawnow';
 uicontrol('Parent',FigureHandle, ...
     'Unit',StdUnit, ...
     'BackgroundColor',StdColor, ...
-    'CallBack',DisplayButtonCallback4, ...
+    'CallBack',@DisplayButtonCallback4, ...
     'Position',PointsPerPixel*[380 2 85 22], ...
     'Units','Normalized',...
     'String','Hide labels', ...
-    'Style','pushbutton', ...
+    'Style','togglebutton', ...
     'FontSize',handles.Preferences.FontSize);
 
-DisplayButtonCallback5 = 'CurrentTextHandles = getfield(get(gcbf,''Userdata''),''TextHandles''); set(CurrentTextHandles, ''visible'', ''on''); drawnow';
-uicontrol('Parent',FigureHandle, ...
-    'Unit',StdUnit, ...
-    'BackgroundColor',StdColor, ...
-    'CallBack',DisplayButtonCallback5, ...
-    'Position',PointsPerPixel*[470 2 85 22], ...
-    'Units','Normalized',...
-    'String','Show labels', ...
-    'Style','pushbutton', ...
-    'FontSize',handles.Preferences.FontSize);
+%%%%%%%%%%%%%%%%%%%%
+%%% SUBFUNCTIONS %%%
+%%%%%%%%%%%%%%%%%%%%
+%%
+%%% SUBFUNCTION - DisplayButtonCallback1
+function DisplayButtonCallback1(hObject,eventdata)
+
+VersionCheck = version;
+
+if strcmp(computer,'MAC') && str2num(VersionCheck(1:3)) < 7.1 %#ok Ignore MLint
+    CPmsgbox('A bug in MATLAB is preventing this function from working on the Mac platform. Service Request #1-RR6M1');
+    drawnow;
+else
+    info = get(gcbf,'Userdata');
+    CurrentTextHandles = info.TextHandles; 
+    try
+        propedit(CurrentTextHandles); 
+    catch
+        CPmsgbox('A bug in MATLAB is preventing this function from working. Service Request #1-RR6M1');
+    end
+    drawnow;
+end
+
+%%
+%%% SUBFUNCTION - DisplayButtonCallback2
+function DisplayButtonCallback2(hObject,eventdata)
+
+NumberOfDecimals = inputdlg('Enter the number of decimal places to display','Enter the number of decimal places',1,{'0'});
+info = get(gcbf,'Userdata');
+CurrentTextHandles = info.TextHandles;
+NumberValues = info.ListOfMeasurements; 
+if(isempty(NumberOfDecimals)) 
+    return; 
+end
+Command = ['%.',num2str(NumberOfDecimals{1}),'f']; 
+NewNumberValues = num2str(NumberValues,Command); 
+CellNumberValues = cellstr(NewNumberValues); 
+PropName(1) = {'string'}; 
+set(CurrentTextHandles,PropName, CellNumberValues); 
+drawnow;
+
+%%
+%%% SUBFUNCTION - DisplayButtonCallback3
+function DisplayButtonCallback3(hObject,eventdata)
+
+info = get(gcbf,'Userdata');
+CurrentTextHandles = info.TextHandles; 
+ListOfMeasurements = info.ListOfMeasurements; 
+StringListOfMeasurements = cellstr(num2str(ListOfMeasurements)); 
+PropName(1) = {'string'}; 
+set(CurrentTextHandles,PropName, StringListOfMeasurements);
+drawnow;
+    
+%%
+%%% SUBFUNCTION - DisplayButtonCallback5
+function DisplayButtonCallback4(hObject,eventdata)
+
+info = get(gcbf,'Userdata');
+CurrentTextHandles = info.TextHandles; 
+if get(hObject,'value'),
+    set(CurrentTextHandles, 'visible', 'off'); 
+    set(hObject,'string','Show labels');
+else
+    set(CurrentTextHandles, 'visible', 'on'); 
+    set(hObject,'string','Hide labels');
+end
+drawnow;

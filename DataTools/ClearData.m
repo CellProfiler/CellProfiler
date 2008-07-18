2b34efadec7b187f273b7c6cd8bfbb13f73c439e
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2008-07-18 15:22:29 +0000

    fixed ClearData, improved CPgetfeature prompts
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5464 1fc53939-2000-0410-845c-e8453a809027

--- DataTools/ClearData.m
+++ DataTools/ClearData.m
@@ -39,38 +39,27 @@ end
 
 %%% Load the first specified CellProfiler output file so we can choose the
 %%% feature to be removed.
+%%% Load the specified CellProfiler output file
 try
-    clear handles;
-    load(fullfile(Pathname, SelectedFiles{1}));
+    temp = load(fullfile(Pathname, SelectedFiles{1}));
+    handles = CP_convert_old_measurements(temp.handles);
 catch
-    CPerrordlg([SelectedFiles{1},' is not a CellProfiler or MATLAB file (it does not have the extension .mat)'])
+    CPerrordlg(['Unable to load file ''', fullfile(Pathname, SelectedFiles{1}), ''' (possibly not a CellProfiler output file).'])
     return
 end
 
-%%% Quick check if it seems to be a CellProfiler file or not
-if ~exist('handles','var')
-    CPerrordlg([SelectedFiles{1} ,' is not a CellProfiler output file.'])
-    return
-end
-
-%%% Let the user select which feature to delete
-Suffix = {'Features','Text','Description'};
-try
-    [ObjectTypename,FeatureType,FeatureNbr,SuffixNbr] = CPgetfeature(handles,0,Suffix);
-catch
-    ErrorMessage = lasterr;
-    CPerrordlg(['An error occurred in the ClearData data tool. ' ErrorMessage(30:end)]);
-    return
-end
+[MeasureObject,Measurefieldname] = CPgetfeature(handles,false);
 
 %%% If Cancel button pressed
-if isempty(ObjectTypename),return,end
+if isempty(MeasureObject),return,end
 
 %%% Ask the user if he really wants to clear the selected feature
-Confirmation = CPquestdlg('Are you sure you want to delete the selected feature?','Confirmation','Yes','Cancel','Cancel');
+Confirmation = CPquestdlg(['Are you sure you want to delete the selected feature? (', MeasureObject, ' ', Measurefieldname, ')'],'Confirmation','Yes','Cancel','Cancel');
 if strcmp(Confirmation,'Cancel')
     return
 end
+
+
 
 %%% Loop over the selected files and remove the selected feature
 %%% An cell array is used to indicated any errors in the processing
@@ -79,56 +68,20 @@ for FileNbr = 1:length(SelectedFiles)
 
     %%% Load the specified CellProfiler output file
     try
-        clear handles
-        load(fullfile(Pathname, SelectedFiles{FileNbr}));
+        temp = load(fullfile(Pathname, SelectedFiles{FileNbr}));
+        handles = CP_convert_old_measurements(temp.handles);
     catch
-        errors{FileNbr} = [SelectedFiles{FileNbr},' is not a CellProfiler or MATLAB file (it does not have the extension .mat)'];
-        continue
+        errors{FileNbr} = [fullfile(Pathname, SelectedFiles{FileNbr}),' is not a CellProfiler or MATLAB file (it does not have the extension .mat)'];
+        return
     end
 
-    %%% Quick check if it seems to be a CellProfiler file or not
-    if ~exist('handles','var')
-        errors{FileNbr} = [SelectedFiles{FileNbr},' is not a CellProfiler output file'];
-        continue
-    end
-
-    %%% Get the cell array of data
-    data = handles.Measurements.(ObjectTypename).(FeatureType);
-
-    %%% If there is only one feature for this feature type, we should remove the entire field
-    %%% from the handles structure
-    if size(data{1},2) == 1 || strcmp(Suffix{SuffixNbr},'Description')
-
-        % Remove the data field and the associated field with suffix 'Text'/'Features'
-        handles.Measurements.(ObjectTypename) = rmfield(handles.Measurements.(ObjectTypename),FeatureType);
-        handles.Measurements.(ObjectTypename) = rmfield(handles.Measurements.(ObjectTypename),[FeatureType,Suffix{SuffixNbr}]);
-
-        %%% If this was the last measurement in the ObjectTypename (e.g., Nuclei, Cells, Cytoplasm)
-        %%% remove the ObjectTypename too
-        if isempty(fieldnames(handles.Measurements.(ObjectTypename)))
-            handles.Measurements = rmfield(handles.Measurements,ObjectTypename);
-        end
-        %%% Otherwise we need to loop over the image sets and remove the column indicated by
-        %%% 'FeatureNbr'
-    else
-        %%% Loop over the image sets and remove the specified feature
-        for ImageSetNbr = 1:length(data)
-            data{ImageSetNbr} = cat(2,data{ImageSetNbr}(:,1:FeatureNbr-1),data{ImageSetNbr}(:,FeatureNbr+1:end));
-        end
-        handles.Measurements.(ObjectTypename).(FeatureType) = data;
-
-        %%% Remove the feature from the associated description field with
-        %%% suffix 'Features', 'Text', or 'Description'
-        text = handles.Measurements.(ObjectTypename).([FeatureType,Suffix{SuffixNbr}]);
-        text = cat(2,text(1:FeatureNbr-1),text(FeatureNbr+1:end));
-        handles.Measurements.(ObjectTypename).([FeatureType,Suffix{SuffixNbr}]) = text;
-    end
+    handles.Measurements.(MeasureObject) = rmfield(handles.Measurements.(MeasureObject), Measurefieldname);
 
     %%% Save the updated CellProfiler output file
     try
-        save(fullfile(Pathname, SelectedFiles{FileNbr}),'handles')
+        save(fullfile(Pathname, SelectedFiles{FileNbr}),'handles');
     catch
-        errors{FileNbr} = ['Could not save updated ',SelectedFiles{FileNbr},' file.'];
+        errors{FileNbr} = ['Could not save updated ',fullfile(Pathname, SelectedFiles{FileNbr}),' file.'];
         continue
     end
 end
@@ -136,10 +89,10 @@ end
 %%% Finished, display success or warning windows if we failed for some data set
 error_index = find(~cellfun('isempty',errors));
 if isempty(error_index)
-    CPmsgbox('Data successfully deleted.')
+    CPmsgbox('Data successfully deleted.');
 else
     %%% Show a warning dialog box for each error
     for k = 1:length(error_index)
-        CPwarndlg(errors{error_index(k)},'Clear Data failure')
+        CPwarndlg(errors{error_index(k)},'Clear Data failure');
     end
 end

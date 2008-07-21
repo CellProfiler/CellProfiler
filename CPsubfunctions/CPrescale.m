13abfc4ce4370dbe61bb039de6f7f446071eb232
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-07-21 21:07:13 +0000

    Minor fix
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5494 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPrescale.m
+++ CPsubfunctions/CPrescale.m
@@ -43,10 +43,8 @@ elseif strncmpi(RescaleOption,'E',1) == 1
     LowestPixelOrig = MethodSpecificArguments{1};
     HighestPixelOrig = MethodSpecificArguments{2};
     LowestPixelRescale = MethodSpecificArguments{3};
-    LowestPixelRescaleBoundingValue = MethodSpecificArguments{4};
-    HighestPixelRescale = MethodSpecificArguments{5};
-    HighestPixelRescaleBoundingValue = MethodSpecificArguments{6};
-    ImageName = MethodSpecificArguments{7};
+    HighestPixelRescale = MethodSpecificArguments{4};
+    ImageName = MethodSpecificArguments{5};
     if (strcmp(upper(LowestPixelOrig), 'AA') & strcmp(upper(HighestPixelOrig), 'AA')) == 1
         if handles.Current.SetBeingAnalyzed == 1
             try
@@ -108,18 +106,13 @@ elseif strncmpi(RescaleOption,'E',1) == 1
         LowestPixelOrig = str2double(LowestPixelOrig);
         HighestPixelOrig = str2double(HighestPixelOrig);
     end
-    %%% Rescales the Image.
-    % (1) Scale and shift the original image to produce the rescaled image.
+    % Scale and shift the original image to produce the rescaled image.
     % Here, we find the linear transformation that maps the user-specified
     %   old bounding values to their new bounding values
     hi = HighestPixelOrig; HI = HighestPixelRescale;
     lo = LowestPixelOrig; LO = LowestPixelRescale;
     X = inv([lo 1; hi 1])*[LO; HI]; m = X(1); b = X(2); 
     OutputImage = InputImage*m + b;
-    %(2) Pixels in the original image higher/lower than the user-input 
-    %   highest/lowest bound are set to the desired value.
-    OutputImage(OutputImage < LowestPixelRescale) =    LowestPixelRescaleBoundingValue;
-    OutputImage(OutputImage > HighestPixelRescale) =   HighestPixelRescaleBoundingValue;
 elseif strncmpi(RescaleOption,'C',1) == 1
     OutputImage = uint8(InputImage*255);
 else error(['For the rescaling option, you must enter N, S, M, G, E, or C for the method by which to rescale the image. Your entry was ', RescaleOption])

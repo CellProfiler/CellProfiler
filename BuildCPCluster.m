7ef51a4fd13671bd7d906d220bfa0f5c3ece8989
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-07-21 18:26:14 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5491 1fc53939-2000-0410-845c-e8453a809027

--- BuildCPCluster.m
+++ BuildCPCluster.m
@@ -1,8 +1,48 @@
+function BuildCPCluster
+
+% Add the current directory to the path
+disp('Adding directories to path...');
 addpath(pwd);
-addpath(fullfile(pwd,'ImageTools'));
-addpath(fullfile(pwd,'DataTools'));
-addpath(fullfile(pwd,'CPsubfunctions'));
-addpath(fullfile(pwd,'Modules'));
-addpath(fullfile(pwd,'Help'));
-mcc -v -R -nodisplay -m CPCluster.m
+
+% Check to see if neccesary directories are present and if they are, add
+% them to the path
+try
+    addpath(fullfile(pwd,'ImageTools'));
+catch
+    error('The ImageTools directory is not present in the current directory. Please check to see if it exists and try again.');
+end
+try
+    addpath(fullfile(pwd,'DataTools'));
+catch
+    error('The DataTools directory is not present in the current directory. Please check to see if it exists and try again.');
+end
+try
+    addpath(fullfile(pwd,'CPsubfunctions'));
+catch
+    error('The CPsubfunctions directory is not present in the current directory. Please check to see if it exists and try again.');
+end
+try
+    addpath(fullfile(pwd,'Modules'));
+catch
+    error('The Modules directory is not present in the current directory. Please check to see if it exists and try again.');
+end
+try
+    addpath(fullfile(pwd,'Help'));
+catch
+    error('The Help directory is not present in the current directory. Please check to see if it exists and try again.');
+end
+
+% Attempt to build CPCluster.m
+if exist('CPCluster.m','file'),
+    disp('Building CPCluster.m....');
+    mcc -v -R -nodisplay -m CPCluster.m
+    disp('Finished building');
+else
+    error('CPCluster.m is not present in the current directory. Please check to see if it exists and try again.');
+end
+
+% Change the permissions
+!chmod -R 775 *'
+
+% Exit
 quit

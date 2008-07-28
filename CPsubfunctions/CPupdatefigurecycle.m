401f73be0d38f65e198c45d21ebc241aedd9556b
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-07-28 18:19:34 +0000

    Added error catching if no '#' is found, and added Help line
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5513 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPupdatefigurecycle.m
+++ CPsubfunctions/CPupdatefigurecycle.m
@@ -1,4 +1,5 @@
 function CPupdatefigurecycle(SetBeingAnalyzed,FigureNumber)
+% CPupdatefigurecycle Assigns proper cycle # to figure title
 
 % CellProfiler is distributed under the GNU General Public License.
 % See the accompanying file LICENSE for details.
@@ -14,6 +15,9 @@ function CPupdatefigurecycle(SetBeingAnalyzed,FigureNumber)
 
 OldText = get(FigureNumber,'name');
 NumberSignIndex = find(OldText=='#');
+if isempty(NumberSignIndex)
+   error(['CPupdatefigurecycle could not locate a number sign (#) in the name field of Figure #' num2str(FigureNumber)]) 
+end
 OldTextUpToNumberSign = OldText(1:NumberSignIndex(1));
 NewNum = SetBeingAnalyzed;
 set(FigureNumber,'name',[OldTextUpToNumberSign,num2str(NewNum)]);

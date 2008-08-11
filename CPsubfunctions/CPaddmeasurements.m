10f5a2ec5c266bc9379cd7b3675db381d9e7a347
thouis <thouis@1fc53939-2000-0410-845c-e8453a809027>
2008-08-11 19:55:52 +0000

    allow overwriting of image set 1 in batch processing mode
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5567 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPaddmeasurements.m
+++ CPsubfunctions/CPaddmeasurements.m
@@ -29,12 +29,14 @@ FirstSet = (ImageSetNumber == 1);
 OldMeasurement = ...
     isfield(handles.Measurements, ObjectName) && ...
     isfield(handles.Measurements.(ObjectName), FeatureName);
+BatchProcessing = isfield(handles.Current, 'BatchInfo');
 
 if (length(FeatureName) > namelengthmax),
     error(['The feature name ''', FeatureName, ''' is too long.']);
 end
 
-if (FirstSet && OldMeasurement),
+%%% Don't allow overwriting of measurements, *except* in batch processing.
+if (FirstSet && OldMeasurement && ~BatchProcessing),
     error(['Image processing was canceled because you are attempting to recreate the same measurements, please remove redundant module (#', handles.Current.CurrentModuleNumber, ').']);
 end
 

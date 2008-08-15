b75852fcb1662da49fff93fe54ec0966c137c61a
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-08-15 19:51:14 +0000

    Handles all Category cases now.  Changed TextureScale to the more general 'SizeScale'
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5605 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPgetfeaturenamesfromnumbers.m
+++ CPsubfunctions/CPgetfeaturenamesfromnumbers.m
@@ -1,20 +1,20 @@
-function FeatureName = CPgetfeaturenamesfromnumbers(handles,ObjectName,Category,FeatureNumberOrName,Image,TextureScale)
+function FeatureName = CPgetfeaturenamesfromnumbers(handles,ObjectName,Category,FeatureNumberOrName,Image,SizeScale)
 % Get FeatureNames from FeatureNumbers or validate if FeatureName
 %
 % handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumberOrName)
 % handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumberOrName, Image)
-% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumberOrName, Image, TextureScale)
+% handles = CPgetfeaturenamesfromnumbers(handles, ObjectName, Category, FeatureNumberOrName, Image, SizeScale)
 %
 %  where   handles is the handles structure
 %          ObjectName & Category are character strings
 %          FeatureNumberOrName is a positive integer or a feature name
-%          TextureScale is a positive integers
+%          SizeScale is a positive integer
 %          Image can be can be a string (Image) or Numeric (Texture)
 %
 % where we are finding FeatureName from
 %  handles.Measurements.ObjectName.Category_FeatureName (for Non-image/non-texture) OR
 %  handles.Measurements.ObjectName.Category_FeatureName_Image (for Image) OR
-%  handles.Measurements.ObjectName.Category_FeatureName_Image_TextureScale (for Texture)
+%  handles.Measurements.ObjectName.Category_FeatureName_Image_SizeScale (for Texture)
 
 % Note!!  We are relying on the fact that the features should have been 
 % added in the order matching that of each Module's Help text!
@@ -25,17 +25,24 @@ if ~ isnan(str2double(FeatureNumberOrName))
 end
 % Parse inputs
 if ~exist('Image','var'), Image = ''; end
-if ~exist('TextureScale','var'), TextureScale = ''; end
-if strcmp(Category,'Intensity')
-    TextureScale = '';
-elseif ~strcmp(Category,'Texture')
-    TextureScale = ''; 
+if ~exist('SizeScale','var'), SizeScale = ''; end
+if strcmp(Category,'AreaShape') || strcmp(Category,'Ratio')
+    SizeScale = '';
     Image = '';
+elseif strcmp(Category,'Intensity') || strcmp(Category,'Granularity') || ...
+        strcmp(Category,'Children') || strcmp(Category,'Parent')
+    SizeScale = '';
+elseif strcmp(Category,'Texture') || strcmp(Category,'RadialDistribution')
+    % Nothing to do.  These should have all arguments specified
+elseif strcmp(Category,'Neighbors')
+    Image = '';
+else 
+    error('Measurement category could not be found.')
 end
 
 AllFieldNames = fieldnames(handles.Measurements.(ObjectName));
 if isnumeric(FeatureNumberOrName)
-    CurrentMeasure = [CPjoinstrings(Category,'.*',Image,num2str(TextureScale)),'$'];
+    CurrentMeasure = [CPjoinstrings(Category,'.*',Image,num2str(SizeScale)),'$'];
     FieldnumsCategoryCell = regexp(AllFieldNames,CurrentMeasure);
     FieldnumsCategory = find(~cellfun('isempty',FieldnumsCategoryCell));
     % Could do error checking here, since the next line is where this subfn usually errors
@@ -46,8 +53,8 @@ else
     CurrentMeasure = CPjoinstrings(Category,'_',FeatureNumberOrName);
     if ~ isempty(Image)
     	CurrentMeasure = CPjoinstrings(CurrentMeasure,'_',Image);
-    	if ~ isempty(TextureScale)
-            CurrentMeasure = CPjoinstrings(CurrentMeasure,'_',num2str(TextureScale));
+    	if ~ isempty(SizeScale)
+            CurrentMeasure = CPjoinstrings(CurrentMeasure,'_',num2str(SizeScale));
     	end
     end
     Matches=strcmp(AllFieldNames,CurrentMeasure);

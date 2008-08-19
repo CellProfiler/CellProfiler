d888551859898378cc9f80dd6cee0330aac539fa
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-08-19 20:28:05 +0000

    Added explicit includes.  More specific file cleanup. Sets permissions at end.  Added help
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5631 1fc53939-2000-0410-845c-e8453a809027

--- BuildCellProfiler.m
+++ BuildCellProfiler.m
@@ -1,26 +1,30 @@
 function BuildCellProfiler
 % BuildCellProfiler Build an exectuable copy of CellProfiler
 %
-%   Before running this function:
-%       (1) copy a set of CellProfiler files and folders 
-%       to a new directory.  Include CellProfiler.m and the folders 
-%       Modules, DataTools, ImageTools, CPsubfunctions, and Help.  
-%       (2) Set the current directory to this new directory.
+%   Before running this function, set the current directory to 
+%   your root (or trunk) CellProfiler directory, which includes 
+%   CellProfiler.m and the Modules, CPsubfunctions, etc folders.
 %
-%       A new folder called CompiledCellProfiler will be created beneath 
-%       the current folder with the new compiled files.
+%   A new folder called CompiledCellProfiler will be created at the 
+%   same level as the CP root folder.  This placement is to avoid duplicate
+%   function names if you run CP from the root directory after building.
 %
-%       Read the readme.txt file there, and set your path variables either
-%       manually, or use the run_CellProfiler.sh script at a terminal prompt.
-%       See the created readme.txt file for more details.
+%   Read the readme.txt file created, and set your path variables either
+%   manually, try the CellProfiler*.command scripts, or use the 
+%   run_CellProfiler.sh script at a terminal prompt.
+%   See readme.txt file for more details.
+%
+%   Try this command at the terminal prompt:
+%   /Applications/MATLAB_R2008a/bin/matlab -nojvm -r "cd ~/trunk/CellProfiler, BuildCellProfiler; quit"
 
 %% Check that the basic files and folders are in the right place
-assert(exist('./CellProfiler.m','file') == 2)
-assert(exist('./Modules','dir') == 7)
-assert(exist('./DataTools','dir') == 7)
-assert(exist('./ImageTools','dir') == 7)
-assert(exist('./CPsubfunctions','dir') == 7)
-assert(exist('./Help','dir') == 7)
+err_txt = 'BuildCellProfiler needs to be in the trunk CellProfiler directory';
+assert(exist('./CellProfiler.m','file') == 2, err_txt)
+assert(exist('./Modules','dir') == 7, err_txt)
+assert(exist('./DataTools','dir') == 7, err_txt)
+assert(exist('./ImageTools','dir') == 7, err_txt)
+assert(exist('./CPsubfunctions','dir') == 7, err_txt)
+assert(exist('./Help','dir') == 7, err_txt)
 
 CompileWizard
 
@@ -30,13 +34,25 @@ movefile( 'CellProfiler.m', 'Old_CellProfiler.m');
 movefile( 'CompileWizard_CellProfiler.m', 'CellProfiler.m');
 
 %% Compile
-mcc -m CellProfiler -a './CPsubfunctions/CPsplash.jpg'
+%%  -I including folders manually, since they don't get added otherwise
+%%  -C generate separarte CTF archive
+%%  -a Needed to add non-matlab .jpg file
+mcc -m -C CellProfiler -I Modules -I ./DataTools -I ./ImageTools ...
+    -I ./CPsubfunctions -I ./Help -a './CPsubfunctions/CPsplash.jpg'
 
 %% Move files and cleanup
-mkdir('.', 'CompiledCellProfiler')
-mkdir('./CompiledCellProfiler', 'Modules')
-movefile('./Modules/*.txt', './CompiledCellProfiler/Modules')
-movefile('CellProfiler*.*','./CompiledCellProfiler/')
-movefile('readme.txt','./CompiledCellProfiler/')
-movefile('run_CellProfiler.sh','./CompiledCellProfiler/')
+mkdir('..', 'CompiledCellProfiler')
+mkdir('../CompiledCellProfiler', 'Modules')
+movefile('./Modules/*.txt', '../CompiledCellProfiler/Modules')
+movefile('readme.txt','../CompiledCellProfiler/')
 movefile( 'Old_CellProfiler.m', 'CellProfiler.m');
+movefile('mccExcludedFiles.log','../CompiledCellProfiler/')
+
+movefile('*CellProfiler*.*','../CompiledCellProfiler/')
+
+%% Copy some useful scripts and files
+copyfile('../CompiledCellProfiler/CellProfilerManual.pdf','.')
+copyfile('../CompiledCellProfiler/CellProfiler*.command','.')
+
+%% Set Permissions
+unix('chmod 775 ../CompiledCellProfiler/CellProfiler*.command');

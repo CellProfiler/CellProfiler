de886ca0b5f1444d7cf9a8b8e0df7f0844a4ce02
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-09-03 21:21:19 +0000

    Added help for SaveCurrentCellProfilerCode
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5735 1fc53939-2000-0410-845c-e8453a809027

---
+++ Help/HelpSaveCurrentCellProfilerCode.m
@@ -0,0 +1,15 @@
+function HelpSaveCurrentCellProfilerCode
+
+% File > Save Current CellProfiler code.
+%
+% This is only to be used in CellProfiler Developer's version.
+% It allows you to save all the Modules, DataTools, ImageTools and 
+% CPsubfunctions at the current revision as a ZIP file. This file is then
+% placed in the default output directory.
+
+helpdlg(help('HelpSaveCurrentCellProfilerCode'))
+
+% We have one line of actual code in these files so that the help is
+% visible. We are not using CPhelpdlg because using helpdlg instead allows
+% the help to be accessed from the command line of MATLAB. The one line of
+% code in each help file (helpdlg) is never run from inside CP anyway.

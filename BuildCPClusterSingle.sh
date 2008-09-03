250934145991c9251df344628aa757cafc9b2410
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-09-03 20:14:39 +0000

    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5732 1fc53939-2000-0410-845c-e8453a809027

--- BuildCPClusterSingle.sh
+++
@@ -1,6 +0,0 @@
-matlab -r BuildCPClusterSingle
-cp CPClusterSingle /imaging/analysis/People/Ray/CPClusterSingle
-cp CPClusterSingle.ctf /imaging/analysis/People/Ray/CPClusterSingle
-cd /imaging/analysis/People/Ray/CPClusterSingle
-rm -rf CPClusterSingle_mcr unpacking.txt
-bsub -q priority -K -o unpacking.txt ./CPClusterSingle.sh fail

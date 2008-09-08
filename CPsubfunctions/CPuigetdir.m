336274b9dc3c23dd8887b0ed440fff9a0845be8c
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-09-08 01:39:25 +0000

    This CP function is present only because Matlab's uigetdir does not display the expected text in thte title bar. This adds a dialog box which displays the text which belongs in the title bar.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5755 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPuigetdir.m
@@ -0,0 +1,17 @@
+function Dirname = CPuigetdir(start_path,txt)
+% This CP function is present only because Matlab's uigetdir 
+%% does not display the expected text in thte title bar.
+%% This adds a dialog box which displays the text which belongs 
+%% in the title bar.
+%% See documentation for uigetdir for more information.
+
+if ismac
+    [ScreenWidth,ScreenHeight] = CPscreensize;
+    h = CPmsgbox(txt);
+    old_pos = get(h,'position');
+    set(h,'Position',[0 ScreenHeight,old_pos(3:4)])
+end
+
+Dirname = uigetdir(start_path,txt);
+
+close(h)

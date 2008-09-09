b51578702a15e5089de488f951b24edb4a481c56
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2008-09-09 16:41:33 +0000

    Added module, EditObjectsManually.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5765 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/EditObjectsManually.m
@@ -0,0 +1,276 @@
+function handles = EditObjectsManually(handles)
+
+% Help for the Edit Objects Manually module:
+% Category: Object Processing
+%
+% SHORT DESCRIPTION:
+% User interface for removing objects manually from an image
+% *************************************************************************
+%
+% This module allows you to remove objects through a user interface. The
+% module displays three images: the objects as originally segmented,
+% the objects that have not been removed and the objects that have been
+% removed.
+% If you click on an object in the "not removed" image, it moves to the
+% "removed" image and will be removed. If you click on an object in the
+% "removed" image, it moves to the "not removed" image and will not be
+% removed. If you click on an object in the original image, it will
+% toggle its "removed" state.
+%
+% The pipeline pauses once per processed image when it reaches this module.
+% You have to press the continue button to accept the selected objects
+% and continue the pipeline.
+%
+% See also FilterByObjectMeasurement, Exclude, OverlayOutlines, ConvertToImage.
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 5759 $
+
+%%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%%
+drawnow
+
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+%textVAR01 = What did you call the objects you want to filter?
+%infotypeVAR01 = objectgroup
+%inputtypeVAR01 = popupmenu
+ObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+
+%textVAR02 = What do you want to call the remaining objects?
+%defaultVAR02 = FilteredObjects
+%infotypeVAR02 = objectgroup indep
+RemainingObjectName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+
+%textVAR03 = What do you want to call the outlines of the remaining objects (optional)?
+%defaultVAR03 = Do not use
+%infotypeVAR03 = outlinegroup indep
+SaveOutlines = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+%%%VariableRevisionNumber = 1
+
+%%%%%%%%%%%%%%%%%%
+%%% SETUP      %%%
+%%%%%%%%%%%%%%%%%%
+
+%%% Operate on this image
+SegmentedObjectImage = CPretrieveimage(handles,['Segmented', ObjectName],ModuleName);
+%%% The following is only relevant for objects identified using
+%%% Identify Primary modules, not Identify Secondary modules.
+fieldname = ['UneditedSegmented',ObjectName];
+if isfield(handles.Pipeline, fieldname)
+    UneditedSegmentedObjectImage = CPretrieveimage(handles,fieldname,ModuleName);
+else
+    UneditedSegmentedObjectImage = [];
+end
+
+fieldname = ['SmallRemovedSegmented',ObjectName];
+if isfield(handles.Pipeline, fieldname)
+    SmallRemovedSegmentedObjectImage = CPretrieveimage(handles,fieldname,ModuleName);
+else
+    SmallRemovedSegmentedObjectImage = [];
+end
+
+%%% Set up the three display images
+img = CPlabel2rgb(handles,SegmentedObjectImage);
+my_data = struct(...
+	'SegmentedObjectImage', SegmentedObjectImage,...
+    'Filter',AllLabels(SegmentedObjectImage),...
+    'IncludedSegmentedObjectImage',SegmentedObjectImage,...
+    'ExcludedSegmentedObjectImage',zeros(size(SegmentedObjectImage,1),size(SegmentedObjectImage,2)),...
+    'handles',handles);
+
+%%%%%%%%%%%%%%%%%%
+%%% DISPLAY UI %%%
+%%%%%%%%%%%%%%%%%%
+drawnow;
+
+ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+if any(findobj == ThisModuleFigureNumber)
+    %%% Activates the appropriate figure window.
+    CPfigure(handles,'Image',ThisModuleFigureNumber);
+    if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+        CPresizefigure(img,'TwoByTwo',ThisModuleFigureNumber);
+    end
+    %%% A subplot of the figure window is set to display the original image.
+    subplot(2,2,1);
+    my_data.hOriginal=CPimagesc(img,handles);
+    set(my_data.hOriginal,'ButtonDownFcn',{@ImageClickCallback,'toggle'});
+    title(['Previously identified ', ObjectName,', cycle # ',num2str(handles.Current.SetBeingAnalyzed)]);
+    %%%
+    %%% The continue button exits the modal state
+    %%%
+    hContinue=uicontrol('Position',[30 30 100 20],'String','Continue',...
+                        'Callback','set(gcbo,''UserData'',0);uiresume(gcbf)',...
+                        'UserData',struct('operation','none'),...
+                        'DeleteFcn','uiresume(gcbf)'...
+                    );
+    my_data.hContinue = hContinue;
+    uibuttons = [...
+    uicontrol('Position',[30,120,100,20],...
+              'String','Keep all',...
+              'Callback',{@CommandCallback, hContinue,'keepall'}),...
+    uicontrol('Position',[30,90,100,20],...
+              'String','Remove all',...
+              'Callback',{@CommandCallback, hContinue,'removeall'}),...
+    uicontrol('Position',[30,60,100,20],...
+              'String','Reverse selection',...
+              'Callback',{@CommandCallback, hContinue,'toggleall'})];
+    set(my_data.hOriginal,'UserData',my_data);
+    my_data = Update('keepall',[1,1],my_data,handles);
+    set(gcf,'CloseRequestFcn','uiresume(gcbf);closereq');
+    while (ishandle(hContinue) && isstruct(get(hContinue,'UserData')))
+        x = get(hContinue,'UserData');
+        if ~ strcmp(x.operation,'none')
+            my_data = Update(x.operation,x.pos,my_data,handles);
+        end
+        drawnow;
+        uiwait(gcf);
+    end;
+    set(hContinue,'Visible','off');
+    set(uibuttons,'Visible','off');
+    set(my_data.hOriginal,'ButtonDownFcn',@CPimagetool);
+    set(my_data.hIncluded,'ButtonDownFcn',@CPImagetool);
+    set(my_data.hExcluded,'ButtonDownFcn',@CPImagetool);
+    
+    %%% Keep points in optional image whose labels are in the filter array
+    if ~ isempty(UneditedSegmentedObjectImage)
+        NewUneditedSegmentedObjectImage = UneditedSegmentedObjectImage;
+        NewUneditedSegmentedObjectImage(~ ismember(NewUneditedSegmentedObjectImage,my_data.Filter))=0;
+    end
+    
+    if ~ isempty(SmallRemovedSegmentedObjectImage)
+        NewSmallRemovedSegmentedObjectImage = SmallRemovedSegmentedObjectImage;
+        NewSmallRemovedSegmentedObjectImage(~ ismember(SmallRemovedSegmentedObjectImage,my_data.Filter))=0;
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%
+    %%% Add object      %%%
+    %%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%% Saves the final segmented label matrix image to the handles structure.
+    fieldname = ['Segmented',RemainingObjectName];
+    handles.Pipeline.(fieldname) = my_data.IncludedSegmentedObjectImage;
+
+    %%% Saves images to the handles structure so they can be saved to the hard
+    %%% drive, if the user requested.
+    if ~strcmp(SaveOutlines,'Do not use')
+        handles.Pipeline.(SaveOutlines) = CPlabelperim(my_data.IncludedSegmentedObjectImage,8);
+    end
+    %%% The following is only relevant for objects identified using
+    %%% Identify Primary modules, not Identify Secondary modules.
+    if ~isempty(UneditedSegmentedObjectImage)
+        %%% Saves the segmented image, not edited for objects along the edges or
+        %%% for size, to the handles structure.
+        fieldname = ['UneditedSegmented',RemainingObjectName];
+        handles.Pipeline.(fieldname) = NewUneditedSegmentedObjectImage;
+    end
+    if ~ isempty(SmallRemovedSegmentedObjectImage)
+        %%% Saves the segmented image, only edited for small objects, to the
+        %%% handles structure.
+        fieldname = ['SmallRemovedSegmented',RemainingObjectName];
+        handles.Pipeline.(fieldname) = NewSmallRemovedSegmentedObjectImage;
+    end
+   
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% ImageClickCallback - handle a click on one of the images
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ImageClickCallback(hObject,eventdata,operation) %#ok<INUSL>
+    pos=get(get(gcbo,'Parent'),'CurrentPoint');
+    pos=floor(pos(1,1:2));
+    hData = get(hObject,'UserData');
+    if ishandle(hData.hContinue)
+        x=struct('operation',operation,'pos',pos);
+        set(hData.hContinue,'UserData',x);
+    end
+    uiresume(gcf);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% CommandCallback - handle a command button
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function CommandCallback(hObject, eventdata, hContinue, operation) %#ok<INUSL>
+    set(hContinue,'UserData',struct('operation',operation,'pos',[]));
+    uiresume(gcf);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Update - update the GUI and data for a image click
+%%%    operation - 'toggle','keep' or 'remove'
+%%%    pos - position of click in image coords
+%%%    hData - my_data structure above
+%%%    handles - main handles for CP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function hDataOut=Update(operation, pos, hData, handles)
+
+    if ~isempty(pos)
+        label = hData.SegmentedObjectImage(pos(2),pos(1));
+        if label == 0
+            operation='none';
+        end
+    end
+    if strcmp(operation,'toggle')
+        if ~ isempty(find(hData.Filter==label,1))
+            operation='remove';
+        else
+            operation='keep';
+        end
+    end
+    if strcmp(operation,'keep')
+        if isempty(find(hData.Filter==label, 1))
+            hData.Filter(length(hData.Filter)+1) = label;
+        end
+    elseif strcmp(operation,'remove')
+        hData.Filter=hData.Filter(hData.Filter~=label);
+    elseif strcmp(operation,'keepall')
+        hData.Filter=AllLabels(hData.SegmentedObjectImage);
+    elseif strcmp(operation,'removeall')
+        hData.Filter = [];
+    elseif strcmp(operation,'toggleall')
+        NewLabels=AllLabels(hData.SegmentedObjectImage);
+        hData.Filter=NewLabels(~ ismember(NewLabels,hData.Filter));
+    elseif ~strcmp(operation,'none')
+        error(['Unsupported operation: ',operation]);
+    end
+    hData.IncludedSegmentedObjectImage = hData.SegmentedObjectImage;
+    hData.ExcludedSegmentedObjectImage = hData.SegmentedObjectImage;
+    hData.IncludedSegmentedObjectImage(~ismember(hData.IncludedSegmentedObjectImage,hData.Filter))=0;
+    hData.ExcludedSegmentedObjectImage(ismember(hData.ExcludedSegmentedObjectImage,hData.Filter))=0;
+    subplot(2,2,2,'replace');
+    if max(hData.IncludedSegmentedObjectImage(:)) == 0
+        img = zeros(size(hData.IncludedSegmentedObjectImage,1),...
+                    size(hData.IncludedSegmentedObjectImage,2),3);
+    else
+        img = CPlabel2rgb(handles, hData.IncludedSegmentedObjectImage);
+    end
+    hData.hIncluded=CPimagesc(img, handles);
+    title('Objects to keep');
+    set(hData.hIncluded,'ButtonDownFcn',{@ImageClickCallback,'remove'});
+    subplot(2,2,4,'replace');
+    if max(hData.ExcludedSegmentedObjectImage(:)) == 0
+        img = zeros(size(hData.ExcludedSegmentedObjectImage,1),...
+                    size(hData.ExcludedSegmentedObjectImage,2),3);
+    else
+        img = CPlabel2rgb(handles, hData.ExcludedSegmentedObjectImage);
+    end
+    hData.hExcluded=CPimagesc(img, handles);
+    title('Objects to remove');
+    set(hData.hExcluded,'ButtonDownFcn',{@ImageClickCallback,'keep'});
+    set(hData.hOriginal,'UserData',hData);
+    set(hData.hIncluded,'UserData',hData);
+    set(hData.hExcluded,'UserData',hData);
+    hDataOut = hData;
+end
+
+function Filter=AllLabels(SegmentedObjectImage)
+    Filter=unique(SegmentedObjectImage(SegmentedObjectImage ~= 0));
+end

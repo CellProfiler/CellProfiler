27b3182991bb5f0627d9f794f6dfd717d5b736a2
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2008-09-10 15:53:44 +0000

    New subfunction to grab or parse out the current svn revision number
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@5782 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPsvnversionnumber.m
@@ -0,0 +1,35 @@
+function svn_ver_char = CPsvnversionnumber
+%% Find the current svn version number
+%% First, try using the 'svn info' command, but if svn is not
+%% installed, or if deployed, loop all functions and parse out
+str_to_find = 'Revision: ';
+
+try
+    if ~isdeployed
+        if ~ispc
+            [status,info] = unix('svn info');
+        else
+            [status,info] = dos('svn info');
+        end
+
+        if status == 0 %% if successful
+            %% Parse out svn Revision Number
+            pos = findstr(info,str_to_find);
+            if length(pos) == 1
+                first = pos+length(str_to_find);
+                %% The '+4' gets an extra space for vers < 9999, and should
+                %% be good 'til 99,999!
+                last = first+4; 
+                svn_ver_char = info(first:last);
+                return
+            end
+        end
+    end
+catch
+    svn_ver_char = CPsvnloopfunctions;
+    return
+end
+
+%% If you've gotten here without returning
+%% then just do the loop
+svn_ver_char = CPsvnloopfunctions;

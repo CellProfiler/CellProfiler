861b82dff14991f7a61e41db72abceab82916b5b
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2008-10-30 18:27:08 +0000

    LoadImageDirectory will combine all images in a directory to create an image.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6008 1fc53939-2000-0410-845c-e8453a809027

---
+++ Modules/LoadImageDirectory.m
@@ -0,0 +1,226 @@
+function handles = LoadImageDirectory(handles)
+
+% Help for the LoadImageDirectory module:
+% Category: File Processing
+%
+% SHORT DESCRIPTION:
+% Makes a projection either by averaging or taking the maximum pixel value
+% at each pixel position of a number of images organized by directory
+%
+% *************************************************************************
+%
+% This module combines a set of images by averaging or by taking the maximum
+% pixel intensity at each pixel position. When this module is used to 
+% average a Z-stack (3-D image stack), this process is known as making 
+% a projection.
+%
+% Settings:
+%
+% * Enter the pathname to the folders containing the images:
+%   This is the base directory that contains each of the directories
+%   to be combined into a projection. Relative paths are from the image
+%   folder: for instance "." operates on the directories in the image folder.
+%
+% * Enter the text that the folders have in common:
+%   Enter some portion of the directory name that's common to all
+%   directories for this type of image. For instance, "DNA-A01" and
+%   "DNA-A02" can be selected by entering "DNA". Enter "*" to take all
+%   directories.
+%
+% * Analyze all subfolders within each folder:
+%   If yes, the module will read image files in subfolders of each
+%   folder it looks at. If no, it will only read image files out of
+%   the named folder.
+% 
+% * How do you want to load these files:
+%   Exact match - if the text below matches some part of the text in the
+%                 image file name, the file will be accepted.
+%   Regular expressions - use a regular expression to match the image
+%                         file name (see LoadImages for details).
+%
+% * What kind of projection would you like to make?:
+%   If you choose Average, the average pixel intensity at each pixel
+%   position will be used to created the final image.  If you choose
+%   Maximum, the maximum pixel value at each pixel position will be used to
+%   created the final image.
+% * What do you want to call the projected image?:
+%   Give a name to the resulting image, which could be used in subsequent
+%   modules.
+%
+% Measurements:
+% Image / DirectoryName - the name of the directory from which we took
+%                         the image files for this image
+%       / PathName      - the path to that directory
+%
+% See also LoadImages
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 5025 $
+
+%%%%%%%%%%%%%%%%%
+%%% VARIABLES %%%
+%%%%%%%%%%%%%%%%%
+drawnow
+
+[CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
+
+%textVAR01 = Enter the pathname to the folders containing images:
+%defaultVAR01 = .
+Pathname = char(handles.Settings.VariableValues{CurrentModuleNum,1});
+
+%textVAR02 = Enter the text that the folders have in common:
+%defaultVAR02 = *
+DirTextToFind = char(handles.Settings.VariableValues{CurrentModuleNum,2});
+if strcmp(DirTextToFind,'*')
+    DirTextToFind = '';
+end
+
+%textVAR03 = Analyze all subfolders within each folder?
+%choiceVAR03 = No
+%choiceVAR03 = Yes
+AnalyzeSubDir = char(handles.Settings.VariableValues{CurrentModuleNum,3});
+%inputtypeVAR03 = popupmenu
+
+%textVAR04 = How do you want to load these files?
+%choiceVAR04 = Exact match
+%choiceVAR04 = Regular expressions
+ExactOrRegExp = char(handles.Settings.VariableValues{CurrentModuleNum,4});
+%inputtypeVAR04 = popupmenu
+
+%textVAR05 = Enter the text that one type of file has in common:
+%defaultVAR05 = *
+FileTextToFind = char(handles.Settings.VariableValues{CurrentModuleNum,5});
+if strcmp(FileTextToFind,'*')
+    FileTextToFind = '';
+end;
+
+%textVAR06 = What kind of projection would you like to make?
+%choiceVAR06 = Average
+%choiceVAR06 = Maximum
+ProjectionType = char(handles.Settings.VariableValues{CurrentModuleNum,6});
+%inputtypeVAR06 = popupmenu
+
+%textVAR07 = What do you want to call the projected image?
+%defaultVAR07 = ProjectedBlue
+%infotypeVAR07 = imagegroup indep
+ImageName = char(handles.Settings.VariableValues{CurrentModuleNum,7});
+
+%%%VariableRevisionNumber = 1
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% IMAGE ANALYSIS %%%
+%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+dir_fieldname = ['DirectoryList', ImageName];
+path_fieldname = ['Pathname', ImageName];
+if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+    %%%
+    %%% First time through - collect all directory names
+    %%%
+    
+    %%% Get the pathname and check that it exists
+    if strncmp(Pathname,'.',1)
+        if length(Pathname) == 1
+            Pathname = handles.Current.DefaultImageDirectory;
+        else
+	    % If the pathname start with '.', interpret it relative to
+            % the default image dir.
+            Pathname = fullfile(handles.Current.DefaultImageDirectory,Pathname(2:end));
+        end
+    end
+    if ~exist(Pathname,'dir')
+        error(['Image processing was canceled in the ', ModuleName, ' module because the directory "',Pathname,'" does not exist. Be sure that no spaces or unusual characters exist in your typed entry and that the pathname of the directory begins with /.'])
+    end
+    DirectoryList = CPdir(Pathname);
+    DirMatchesName = arrayfun(@(x) ~isempty(strfind(x.name,DirTextToFind)), DirectoryList);
+    IsADir = [DirectoryList.isdir];
+    DirectoryList = DirectoryList(DirMatchesName(:) & IsADir(:));
+    DirectoryNames = arrayfun(@(x) {x.name},DirectoryList);
+    handles.Pipeline.(dir_fieldname) = DirectoryNames;
+    handles.Pipeline.(path_fieldname) = Pathname;
+    NumberOfImageSets = length(DirectoryNames);
+    if NumberOfImageSets == 0
+        error(['Image processing was canceled in the ', ModuleName, ' module because no directories were found']);
+    end
+    %%% Checks whether another load images module has already recorded a
+    %%% number of cycles.  If it has, it will not be set at the default
+    %%% of 1.  Then, it checks whether the number already stored as the
+    %%% number of cycles is equal to the number of cycles that this
+    %%% module has found.  If not, an error message is generated. Note:
+    %%% this will not catch the case where the number of cycles
+    %%% detected by this module is more than 1 and another module has
+    %%% detected only one cycle, since there is no way to tell whether
+    %%% the 1 stored in handles.Current.NumberOfImageSets is the default value or a
+    %%% value determined by another image-loading module.
+    if handles.Current.NumberOfImageSets ~= 1;
+        if handles.Current.NumberOfImageSets ~= NumberOfImageSets
+            error(['Image processing was canceled in the ', ModuleName, ' module because the number of image cycles loaded (', num2str(NumberOfImageSets),') does not equal the number of image cycles loaded by another image-loading module (', num2str(handles.Current.NumberOfImageSets), '). Please check the settings.'])
+        end
+    end
+    handles.Current.NumberOfImageSets = NumberOfImageSets;
+end
+%%%
+%%% Handle the list of files
+%%%
+DirPaths = handles.Pipeline.(dir_fieldname);
+DirPath = fullfile(handles.Pipeline.(path_fieldname),...
+                   DirPaths{handles.Current.SetBeingAnalyzed});
+FileList = CPretrievemediafilenames(DirPath,FileTextToFind,AnalyzeSubDir(1), ExactOrRegExp,'Image');
+if isempty(FileList)
+    error(['Image processing was canceled in the ', ModuleName, ' module because no files were found in the ', DirPath, ' directory.']);
+end
+for i=1:length(FileList)
+    Image = CPimread(fullfile(DirPath,FileList{i}));
+    if i == 1
+        ProjectionImage = zeros(size(Image));
+    end
+    if strcmp(ProjectionType,'Average')
+        ProjectionImage = ProjectionImage + Image;
+    else
+        ProjectionImage = max(ProjectionImage, Image);
+    end
+end
+if strcmp(ProjectionType,'Average')
+    ProjectionImage = ProjectionImage / length(FileList);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% DISPLAY RESULTS %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+if any(findobj == ThisModuleFigureNumber)
+    %%% Activates the appropriate figure window.
+    CPfigure(handles,'Image',ThisModuleFigureNumber);
+    if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+        CPresizefigure(ProjectionImage,'OneByOne',ThisModuleFigureNumber)
+    end
+    hAx = subplot(1,1,1,'Parent',ThisModuleFigureNumber);
+    CPimagesc(ProjectionImage,handles,hAx);
+    title(hAx,['Projected Image # ', num2str(handles.Current.SetBeingAnalyzed)]);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% SAVE DATA TO HANDLES STRUCTURE %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+drawnow
+
+%%% Saves the averaged image to the handles structure so it can be used by
+%%% subsequent modules.
+handles.Pipeline.(ImageName) = ProjectionImage;
+handles = CPaddmeasurements(handles, 'Image', ...
+				['DirectoryName_', ImageName], DirPath);
+handles = CPaddmeasurements(handles, 'Image', ...
+				['PathName_', ImageName], ...
+				handles.Pipeline.(path_fieldname));

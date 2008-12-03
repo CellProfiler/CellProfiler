e99b6a3a5a1f0e195f81d132c3bfc1ace69886d9
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-12-03 23:22:31 +0000

    Bugfix: Runs only if handles.Current.SetBeingAnalyzed == 1
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6106 1fc53939-2000-0410-845c-e8453a809027

--- Modules/ConfirmIfAllFilesPresent.m
+++ Modules/ConfirmIfAllFilesPresent.m
@@ -124,193 +124,195 @@ OutputPathname = char(handles.Settings.VariableValues{CurrentModuleNum,11});
 
 drawnow;
 
-% Get the pathname and check that it exists
-if strncmp(InputPathname,'.',1)
-    if length(InputPathname) == 1
-        InputPathname = handles.Current.DefaultImageDirectory;
-    else
-    % If the pathname starts with '.', interpret it relative to
-    % the default image dir.
-        InputPathname = fullfile(handles.Current.DefaultImageDirectory,strrep(strrep(InputPathname(2:end),'/',filesep),'\',filesep));
-    end
-end
-if ~exist(InputPathname,'dir')
-    error(['Image processing was canceled in the ', ModuleName, ' module because the directory "',InputPathname,'" does not exist. Be sure that no spaces or unusual characters exist in your typed entry and that the pathname of the directory begins with /.'])
-end
-
-% Exclude options if user specifies
-ImageName = ImageName(~strcmp('Do not use',TextToFind));
-TextToFind = TextToFind(~strcmp('Do not use',TextToFind));
-
-% Extract the file names
-for n = 1:length(ImageName)
-    FileList = CPretrievemediafilenames(InputPathname,char(TextToFind(n)),AnalyzeSubDir(1), 'Regular','Image');
-
-    % Remove thumbnails
-    if ~isempty(FileList) && ~strcmp(ThumbText,'Do not use'),
-        thumb_idx = regexp(FileList,ThumbText);
-        if ~isempty(thumb_idx), FileList = FileList(cellfun(@isempty,thumb_idx)); end
-    end
-    
-    % Checks whether any files are left.
-    if isempty(FileList)
-        error(['Image processing was canceled in the ', ModuleName, ' module because there are no image files with the text "', TextToFind{n}, '" in the chosen directory (or subdirectories, if you requested them to be analyzed as well).'])
-    end
-    
-    [IndivPathnames,IndivFileNames{n}] = cellfun(@fileparts,FileList,'UniformOutput',false);
-    [AllPathnames{n},ignore,idx{n}] = unique(IndivPathnames);
-end
-
-% First, check if directories in which channel images were located are identical. If
-% not, keep track of the different ones for each channel (e.g., if a
-% channel image is found in only some directories and not in others)
-uniquePaths = unique(cat(2,AllPathnames{:}));
-MismatchedDirectories = cellfun(@setdiff,repmat({uniquePaths},[1 length(AllPathnames)]),AllPathnames,'UniformOutput',false);
-
-% Second, check if the images in each directory/subdirectory match up by
-% channel
-MismatchedFilenames = cell(1,length(ImageName));
-FileNamesForEachChannel = cell(length(idx),length(uniquePaths));
-for m = 1:length(uniquePaths)
-    FileNamesForChannelN = cell(1,length(idx));
-    for n = 1:length(idx)
-        % FileNamesForEachChannel{channel}{subdirectory}: Cell array of strings
-        FileNamesForEachChannel{n}{m} = IndivFileNames{n}(idx{n} == m);
-        
-        % Find the position of the channel text in the filenames for
-        % each subdirectory
-        TextToFindIdx = unique(cell2mat(regexp(FileNamesForEachChannel{n}{m},TextToFind{n})));
-        % If the position is the same for all...
-        if isscalar(TextToFindIdx)
-            %... drop the filename text after the channel text and use the 
-            % remainder for comparision
-            % ASSUMPTION: Files from same system share common prefix during
-            % the same run
-            FileNamesForChannelN{n} = strvcat(FileNamesForEachChannel{n}{m});
-            FileNamesForChannelN{n} = FileNamesForChannelN{n}(:,1:TextToFindIdx-1);
+if handles.Current.SetBeingAnalyzed == 1
+    % Get the pathname and check that it exists
+    if strncmp(InputPathname,'.',1)
+        if length(InputPathname) == 1
+            InputPathname = handles.Current.DefaultImageDirectory;
         else
-            %... otherwise, error
-            error(['The specified text for ',ImageName{n},' is not located at a consistent position within the filenames in directory ',uniquePaths{m}]);
+        % If the pathname starts with '.', interpret it relative to
+        % the default image dir.
+            InputPathname = fullfile(handles.Current.DefaultImageDirectory,strrep(strrep(InputPathname(2:end),'/',filesep),'\',filesep));
         end
     end
-    % Compare the filename strings pair-wise through the channel
-    % combinations and pull out differences
-    combChannels = nchoosek(1:length(idx),2);
-    for n = 1:size(combChannels,1)
-        chan1 = combChannels(n,1); chan2 = combChannels(n,2);
-        [ignore,ia,ib] = setxor(FileNamesForChannelN{[chan1 chan2]},'rows');
-        if ~isempty(ia)
-            i = find(idx{chan1} == m);
-            if all(cellfun(@isempty,IndivPathnames(i(ia)))),
-                separator = repmat(' ',[length(ia) 1]);
+    if ~exist(InputPathname,'dir')
+        error(['Image processing was canceled in the ', ModuleName, ' module because the directory "',InputPathname,'" does not exist. Be sure that no spaces or unusual characters exist in your typed entry and that the pathname of the directory begins with /.'])
+    end
+
+    % Exclude options if user specifies
+    ImageName = ImageName(~strcmp('Do not use',TextToFind));
+    TextToFind = TextToFind(~strcmp('Do not use',TextToFind));
+
+    % Extract the file names
+    for n = 1:length(ImageName)
+        FileList = CPretrievemediafilenames(InputPathname,char(TextToFind(n)),AnalyzeSubDir(1), 'Regular','Image');
+
+        % Remove thumbnails
+        if ~isempty(FileList) && ~strcmp(ThumbText,'Do not use'),
+            thumb_idx = regexp(FileList,ThumbText);
+            if ~isempty(thumb_idx), FileList = FileList(cellfun(@isempty,thumb_idx)); end
+        end
+
+        % Checks whether any files are left.
+        if isempty(FileList)
+            error(['Image processing was canceled in the ', ModuleName, ' module because there are no image files with the text "', TextToFind{n}, '" in the chosen directory (or subdirectories, if you requested them to be analyzed as well).'])
+        end
+
+        [IndivPathnames,IndivFileNames{n}] = cellfun(@fileparts,FileList,'UniformOutput',false);
+        [AllPathnames{n},ignore,idx{n}] = unique(IndivPathnames);
+    end
+
+    % First, check if directories in which channel images were located are identical. If
+    % not, keep track of the different ones for each channel (e.g., if a
+    % channel image is found in only some directories and not in others)
+    uniquePaths = unique(cat(2,AllPathnames{:}));
+    MismatchedDirectories = cellfun(@setdiff,repmat({uniquePaths},[1 length(AllPathnames)]),AllPathnames,'UniformOutput',false);
+
+    % Second, check if the images in each directory/subdirectory match up by
+    % channel
+    MismatchedFilenames = cell(1,length(ImageName));
+    FileNamesForEachChannel = cell(length(idx),length(uniquePaths));
+    for m = 1:length(uniquePaths)
+        FileNamesForChannelN = cell(1,length(idx));
+        for n = 1:length(idx)
+            % FileNamesForEachChannel{channel}{subdirectory}: Cell array of strings
+            FileNamesForEachChannel{n}{m} = IndivFileNames{n}(idx{n} == m);
+
+            % Find the position of the channel text in the filenames for
+            % each subdirectory
+            TextToFindIdx = unique(cell2mat(regexp(FileNamesForEachChannel{n}{m},TextToFind{n})));
+            % If the position is the same for all...
+            if isscalar(TextToFindIdx)
+                %... drop the filename text after the channel text and use the 
+                % remainder for comparision
+                % ASSUMPTION: Files from same system share common prefix during
+                % the same run
+                FileNamesForChannelN{n} = strvcat(FileNamesForEachChannel{n}{m});
+                FileNamesForChannelN{n} = FileNamesForChannelN{n}(:,1:TextToFindIdx-1);
             else
-                separator = repmat(filesep,[length(ia) 1]);
+                %... otherwise, error
+                error(['The specified text for ',ImageName{n},' is not located at a consistent position within the filenames in directory ',uniquePaths{m}]);
             end
-            MismatchedFilenames{chan1} = [MismatchedFilenames{chan1}; cellstr(strcat(char(IndivPathnames(i(ia))'),separator,char(IndivFileNames{chan1}(i(ia))')))];
         end
-        if ~isempty(ib)
-            i = find(idx{chan2} == m);
-            if all(cellfun(@isempty,IndivPathnames(i(ib)))),
-                separator = repmat(' ',[length(ib) 1]);
+        % Compare the filename strings pair-wise through the channel
+        % combinations and pull out differences
+        combChannels = nchoosek(1:length(idx),2);
+        for n = 1:size(combChannels,1)
+            chan1 = combChannels(n,1); chan2 = combChannels(n,2);
+            [ignore,ia,ib] = setxor(FileNamesForChannelN{[chan1 chan2]},'rows');
+            if ~isempty(ia)
+                i = find(idx{chan1} == m);
+                if all(cellfun(@isempty,IndivPathnames(i(ia)))),
+                    separator = repmat(' ',[length(ia) 1]);
+                else
+                    separator = repmat(filesep,[length(ia) 1]);
+                end
+                MismatchedFilenames{chan1} = [MismatchedFilenames{chan1}; cellstr(strcat(char(IndivPathnames(i(ia))'),separator,char(IndivFileNames{chan1}(i(ia))')))];
+            end
+            if ~isempty(ib)
+                i = find(idx{chan2} == m);
+                if all(cellfun(@isempty,IndivPathnames(i(ib)))),
+                    separator = repmat(' ',[length(ib) 1]);
+                else
+                    separator = repmat(filesep,[length(ib) 1]);
+                end
+                MismatchedFilenames{chan2} = [MismatchedFilenames{chan2}; cellstr(strcat(char(IndivPathnames(i(ib))'),separator,char(IndivFileNames{chan2}(i(ib))')))];
+            end
+        end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%
+    %%% DISPLAY RESULTS %%%
+    %%%%%%%%%%%%%%%%%%%%%%%
+
+    % List mismatched directories
+    TextString{1} = 'Mismatched directories found:';
+    if all(cellfun(@isempty,MismatchedDirectories))
+        TextString{2} = '  None';
+    else
+        for n = 1:length(MismatchedDirectories)
+            for m = 1:size(MismatchedDirectories{n},1),
+                TextString{end+1} = ['  ',MismatchedDirectories{n}{m,:}];
+            end
+        end
+    end
+
+    TextString{end+1} = '';
+
+    % List mismatched filenames
+    TextString{end+1} = 'Mismatched filenames found:';
+    if cellfun(@isempty,MismatchedFilenames)
+        TextString{end+1} = '  None';
+    else
+        for n = 1:length(MismatchedFilenames)
+            if ~isempty(MismatchedFilenames{n}),
+                [pathstr,fn] = cellfun(@fileparts,MismatchedFilenames{n},'UniformOutput',false);
+                uniquepathstr = unique(pathstr);
+                if all(cellfun(@isempty,uniquepathstr)),  % Empty unique path: All files in root directory
+                    for m = 1:length(fn),
+                        TextString{end+1} = ['   ',fn{m}];
+                    end
+                else                        % Non-empty unique path: Some files in sub-directories
+                    for m = 1:length(uniquepathstr),
+                        if ~isempty(uniquepathstr{m}), % At least one file in sub-directory
+                            TextString{end+1} = [' Subdirectory:',uniquepathstr{m}];
+                        end
+                        i = find(~cellfun(@isempty,regexp(pathstr,uniquepathstr{m})));
+                        for p = 1:length(i),
+                            TextString{end+1} = ['   ',fn{i(p)}];
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    drawnow;
+
+    ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
+
+    if any(findobj == ThisModuleFigureNumber);
+        if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
+            CPresizefigure('','NarrowText',ThisModuleFigureNumber)
+        end    
+        % Display list
+        currentfig = CPfigure(handles,'Text',ThisModuleFigureNumber);
+        for n = 1:length(TextString),
+            uicontrol(currentfig,'style','text','units','normalized','fontsize',handles.Preferences.FontSize,'HorizontalAlignment','left','string',TextString{n},'position',[.05 .9-(n-1)*.04 .95 .04],'BackgroundColor',[.7 .7 .9]);
+        end
+    end
+
+    % Output file if desired
+    if strncmpi(SaveOutputFile,'y',1),
+        if strncmp(OutputPathname,'&',1)
+            % If the pathname is '&', set it to the default output dir.
+            if length(OutputPathname) == 1
+                OutputPathname = handles.Current.DefaultOutputDirectory;
+                OutputFilename = [ModuleName,'_output'];
+                OutputExtension = '.txt';
             else
-                separator = repmat(filesep,[length(ib) 1]);
+            % If the pathname starts with '&', interpret it relative to
+            % the default output dir.
+                [OutputExtendedPathname,OutputFilename,OutputExtension] = fileparts(OutputPathname(2:end));
+                OutputPathname = fullfile(handles.Current.DefaultOutputDirectory,OutputExtendedPathname,'');
             end
-            MismatchedFilenames{chan2} = [MismatchedFilenames{chan2}; cellstr(strcat(char(IndivPathnames(i(ib))'),separator,char(IndivFileNames{chan2}(i(ib))')))];
-        end
-    end
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%
-%%% DISPLAY RESULTS %%%
-%%%%%%%%%%%%%%%%%%%%%%%
-
-% List mismatched directories
-TextString{1} = 'Mismatched directories found:';
-if all(cellfun(@isempty,MismatchedDirectories))
-    TextString{2} = '  None';
-else
-    for n = 1:length(MismatchedDirectories)
-        for m = 1:size(MismatchedDirectories{n},1),
-            TextString{end+1} = ['  ',MismatchedDirectories{n}{m,:}];
-        end
-    end
-end
-
-TextString{end+1} = '';
-
-% List mismatched filenames
-TextString{end+1} = 'Mismatched filenames found:';
-if cellfun(@isempty,MismatchedFilenames)
-    TextString{end+1} = '  None';
-else
-    for n = 1:length(MismatchedFilenames)
-        if ~isempty(MismatchedFilenames{n}),
-            [pathstr,fn] = cellfun(@fileparts,MismatchedFilenames{n},'UniformOutput',false);
-            uniquepathstr = unique(pathstr);
-            if all(cellfun(@isempty,uniquepathstr)),  % Empty unique path: All files in root directory
-                for m = 1:length(fn),
-                    TextString{end+1} = ['   ',fn{m}];
-                end
-            else                        % Non-empty unique path: Some files in sub-directories
-                for m = 1:length(uniquepathstr),
-                    if ~isempty(uniquepathstr{m}), % At least one file in sub-directory
-                        TextString{end+1} = [' Subdirectory:',uniquepathstr{m}];
-                    end
-                    i = find(~cellfun(@isempty,regexp(pathstr,uniquepathstr{m})));
-                    for p = 1:length(i),
-                        TextString{end+1} = ['   ',fn{i(p)}];
-                    end
-                end
-            end
-        end
-    end
-end
-
-drawnow;
-
-ThisModuleFigureNumber = handles.Current.(['FigureNumberForModule',CurrentModule]);
-
-if any(findobj == ThisModuleFigureNumber);
-    if handles.Current.SetBeingAnalyzed == handles.Current.StartingImageSet
-        CPresizefigure('','NarrowText',ThisModuleFigureNumber)
-    end    
-    % Display list
-    currentfig = CPfigure(handles,'Text',ThisModuleFigureNumber);
-    for n = 1:length(TextString),
-        uicontrol(currentfig,'style','text','units','normalized','fontsize',handles.Preferences.FontSize,'HorizontalAlignment','left','string',TextString{n},'position',[.05 .9-(n-1)*.04 .95 .04],'BackgroundColor',[.7 .7 .9]);
-    end
-end
-
-% Output file if desired
-if strncmpi(SaveOutputFile,'y',1),
-    if strncmp(OutputPathname,'&',1)
-        % If the pathname is '&', set it to the default output dir.
-        if length(OutputPathname) == 1
-            OutputPathname = handles.Current.DefaultOutputDirectory;
-            OutputFilename = [ModuleName,'_output'];
-            OutputExtension = '.txt';
         else
-        % If the pathname starts with '&', interpret it relative to
-        % the default output dir.
-            [OutputExtendedPathname,OutputFilename,OutputExtension] = fileparts(OutputPathname(2:end));
-            OutputPathname = fullfile(handles.Current.DefaultOutputDirectory,OutputExtendedPathname,'');
+            [OutputPathname,OutputFilename,OutputExtension] = fileparts(OutputPathname);
         end
-    else
-        [OutputPathname,OutputFilename,OutputExtension] = fileparts(OutputPathname);
-    end
-    if ~exist(OutputPathname,'dir')
-        error(['Image processing was canceled in the ', ModuleName, ' module because the directory "',OutputPathname,'" does not exist. Be sure that no spaces or unusual characters exist in your typed entry and that the pathname of the directory begins with /.'])
-    end
+        if ~exist(OutputPathname,'dir')
+            error(['Image processing was canceled in the ', ModuleName, ' module because the directory "',OutputPathname,'" does not exist. Be sure that no spaces or unusual characters exist in your typed entry and that the pathname of the directory begins with /.'])
+        end
 
-    fid = fopen(fullfile(OutputPathname,[OutputFilename OutputExtension]),'at+');
-    if fid > 0,
-        fprintf(fid,'%s\n',['Output of ',ModuleName]);
-        fprintf(fid,'%s\n','%%%%%%%%%%%%%%%%%%%%%%%%');
-        for i = 1:length(TextString)
-            fprintf(fid,'%s\n',TextString{i});
+        fid = fopen(fullfile(OutputPathname,[OutputFilename OutputExtension]),'at+');
+        if fid > 0,
+            fprintf(fid,'%s\n',['Output of ',ModuleName]);
+            fprintf(fid,'%s\n','%%%%%%%%%%%%%%%%%%%%%%%%');
+            for i = 1:length(TextString)
+                fprintf(fid,'%s\n',TextString{i});
+            end
+            fclose(fid);
+        else
+            error([ModuleName,': Failed to open the output file for writing']);
         end
-        fclose(fid);
-    else
-        error([ModuleName,': Failed to open the output file for writing']);
     end
 end

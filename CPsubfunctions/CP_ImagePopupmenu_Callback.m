ec49490e79812bc9a0b665c02443f5db9bea5438
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2008-12-03 15:20:55 +0000

    Bugfix: Assign title to proper axes
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6104 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CP_ImagePopupmenu_Callback.m
+++ CPsubfunctions/CP_ImagePopupmenu_Callback.m
@@ -1,21 +1,21 @@
 function CP_ImagePopupmenu_Callback(hObject,eventdata)
 
-%% Callback used to show multiple figures.  
-%% A popup menu controls the display
-%% 
-%% UserData is expected to be a struct with 'img' and 'title' fields,
-%% one for each image entry.  
-%%
-%% **NOTE** Make sure that the uicontrol 'String' entries correspond to the 
-%% 'UserData' entries
-%%
-%% See IdentifySecondary.m for an example of usage.
+% Callback used to show multiple figures.  
+% A popup menu controls the display
+% 
+% UserData is expected to be a struct with 'img' and 'title' fields,
+% one for each image entry.  
+%
+% **NOTE** Make sure that the uicontrol 'String' entries correspond to the 
+% 'UserData' entries
+%
+% See IdentifySecondary.m for an example of usage.
 
 % $Revision$
 
-UserData = get(hObject,'UserData'); 
+UserData = get(hObject,'userdata'); 
 h_image = findobj(gcbf,'type','image');
-val = get(hObject,'Value');
+val = get(hObject,'value');
 
 set(h_image,'cdata',UserData(val).img);
-title(UserData(val).title)
+title(get(h_image(1),'parent'),UserData(val).title)

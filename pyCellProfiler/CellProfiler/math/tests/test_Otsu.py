355f309c6e9461d4e294ab59881ceb683a21b57e
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2008-12-23 19:04:48 +0000

    renaming modules as lowercase
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6640 1fc53939-2000-0410-845c-e8453a809027

---
+++ pyCellProfiler/CellProfiler/math/tests/test_Otsu.py
@@ -0,0 +1,23 @@
+"""Test Otsu module
+
+"""
+import unittest
+import numpy
+import numpy.random
+from CellProfiler.Math.Otsu import Otsu
+
+class testOtsu(unittest.TestCase):
+    def test_01_TwoValues(self):
+        x = Otsu([.2,.8])
+        self.assertTrue(x >= .2)
+        self.assertTrue(x <= .8)
+    
+    def test_02_TwoDistributions(self):
+        numpy.random.seed(0)
+        x0 = numpy.random.uniform(.1,.4,size=1000)
+        x1 = numpy.random.uniform(.6,1.0,size=1000)
+        x = numpy.append(x0,x1)
+        numpy.random.shuffle(x)
+        threshold = Otsu(x)
+        self.assertTrue(threshold >= .4)
+        self.assertTrue(threshold <= .6)

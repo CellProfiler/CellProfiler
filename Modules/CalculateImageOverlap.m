5d8d808f7a04eb4ad5efddd2db8cfc169547dead
klmadden <klmadden@1fc53939-2000-0410-845c-e8453a809027>
2008-12-23 20:22:07 +0000

    Changes to 'CalculateImageOverlap.m for Wormbinary comparisons
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6647 1fc53939-2000-0410-845c-e8453a809027

--- Modules/CalculateImageOverlap.m
+++ Modules/CalculateImageOverlap.m
@@ -56,12 +56,6 @@ GroundTruthName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 TestImgName = char(handles.Settings.VariableValues{CurrentModuleNum,2});
 
 
-%textVAR03 = What do you want to call the output calculated by this module? The prefix 'Overlap' will be used if you do not specify a name.
-%defaultVAR03 = Automatic
-OutputFeatureName = char(handles.Settings.VariableValues(CurrentModuleNum,3));
-%%%VariableRevisionNumber = 6
-%%%VariableRevisionNumber = 6
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% MAKE MEASUREMENTS & SAVE TO HANDLES STRUCTURE %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -69,9 +63,9 @@ drawnow
 
 SetBeingAnalyzed = handles.Current.SetBeingAnalyzed;
 
-GroundTruth = CPretrieveimage(handles,GroundTruthName,ModuleName, 'MustBeBinary', 'DontCheckScale');
+GroundTruth = CPretrieveimage(handles,GroundTruthName,ModuleName, 'DontCheckColor', 'DontCheckScale');
 
-TestImg = CPretrieveimage(handles,TestImgName,ModuleName, 'MustBeBinary', 'DontCheckScale');
+TestImg = CPretrieveimage(handles,TestImgName,ModuleName, 'DontCheckColor', 'DontCheckScale');
 
 %%Threshold Images
 TruePosImg = GroundTruth>0;
@@ -81,6 +75,7 @@ TestPosImg = TestImg>0;
 TrueNegImg = GroundTruth<1;
 
 %% Subtract images to find the overlap
+%add a try catch in case the images are different sizes  -KateTODO
 FalsePosImg = imsubtract(TestImg,GroundTruth);
 
 FalseNegImg = imsubtract(GroundTruth,TestImg);
@@ -101,9 +96,15 @@ FalseNegPixels = sum(FalseNegImg(:));
 
 %% Find the Ffactor
 
+
+
 PixelsLabeledinClass = TruePosPixels + FalsePosPixels;
 
 PixelsBelongingtoClass = TruePosPixels + FalseNegPixels;
+
+FalsePosRate = FalsePosPixels./(FalsePosPixels + TrueNegPixels);
+
+FalseNegRate = FalseNegPixels./(PixelsBelongingtoClass);
 
 Precision = TruePosPixels./PixelsLabeledinClass;
 
@@ -111,18 +112,12 @@ Recall = TruePosPixels./PixelsBelongingtoClass;
 
 Ffactor = (2.*(Precision.*Recall))./(Precision+Recall);
 
-%% fill out field name
-if isempty(OutputFeatureName) || strcmp(OutputFeatureName,'Automatic') == 1
-    FullFeatureName = CPjoinstrings('Overlay',GroundTruthName, TestImgName);
-
-    %% Since Matlab's max name length is 63, we need to truncate the fieldname
-    TruncFeatureName = CPtruncatefeaturename(FullFeatureName);
-else
-    TruncFeatureName = CPjoinstrings('Overlay',OutputFeatureName);
-end
-
 %% Save
-handles = CPaddmeasurements(handles,'Image',TruncFeatureName,Ffactor);
+handles = CPaddmeasurements(handles,'Image','Ffactor',Ffactor);
+handles = CPaddmeasurements(handles,'Image','Precision',Precision);
+handles = CPaddmeasurements(handles,'Image','Recall',Recall);
+handles = CPaddmeasurements(handles,'Image','FalsePosRate',FalsePosRate);
+handles = CPaddmeasurements(handles,'Image','FalseNegRate',FalseNegRate);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -164,13 +159,20 @@ if any(findobj == ThisModuleFigureNumber)
                 str{1} =    ['Precision: ' num2str(Precision,'%4.2f')];
                 str{2} =    ['Recall: ' num2str(Recall, '%4.2f')];
                 str{3} =    ['F-factor: ',num2str(Ffactor,'%4.2f')];
+                str{4} =    ['False Positive Rate: ',num2str(FalsePosRate,'%4.2f')];
+                str{5} =    ['False Negative Rate:',num2str(FalseNegRate,'%4.2f')];
+                
 
                 for i = 1:length(str),
                     h = uicontrol(ThisModuleFigureNumber,'Style','Text','Units','Normalized','Position',[pos(1) pos(2)-0.04*i pos(3:4)],...
                         'BackgroundColor',bgcolor,'HorizontalAlignment','Left','String',str{i},'FontSize',handles.Preferences.FontSize);
                 end
 end
+end
 
+           
+        
+   
            
         
    

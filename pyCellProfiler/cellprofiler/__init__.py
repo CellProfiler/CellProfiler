22d635f785ba5f1fd28cef12da3eed3076d91690
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2008-12-23 19:38:08 +0000

    now start to fix breakage from renaming
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6646 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/cellprofiler/__init__.py
+++ pyCellProfiler/cellprofiler/__init__.py
@@ -3,10 +3,3 @@
    $Revision$
 """
 
-__all__=['CellProfilerGUI']
-
-# aliases for new capitalizations
-import Module as cpmodule
-import Image as cpimage
-import Variable as variable
-import Preferences as preferences

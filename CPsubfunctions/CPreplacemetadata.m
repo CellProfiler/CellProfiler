7179db5c1cbc32e5766f5fcf51945dc1940e613d
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2009-02-06 21:48:21 +0000

    Works for >1 token substitution
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6850 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPreplacemetadata.m
+++ CPsubfunctions/CPreplacemetadata.m
@@ -3,10 +3,10 @@ function str = CPreplacemetadata(handles,str)
 %% Substitute Metadata tokens if found
 token = regexp(str, '\(\?[<](?<token>.+?)[>]\)','tokens');
 token = [token{:}];
-assert(numel(token) == 0 || numel(token) == 1, ...
-    ['The number of regular expression tokens found was not 0 or 1.  '...
-    'Please adjust your token settings, i.e. (?<token>)'])
-if isfield(handles.Measurements.Image,['Metadata_' token{1}]);
-    replace_string = handles.Measurements.Image.(['Metadata_' token{1}]);
-    str = regexprep(str, '\(\?[<](?<token>.+?)[>]\)', replace_string);
+
+for i = 1:numel(token)
+    if isfield(handles.Measurements.Image,['Metadata_' token{i}]);
+        replace_string = handles.Measurements.Image.(['Metadata_' token{i}]);
+        str = regexprep(str, ['\(\?[<](' token{i} ')[>]\)'], replace_string);
+    end
 end

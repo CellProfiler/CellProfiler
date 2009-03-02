d29d222d45852356470e8ef7f01508c48bbf9ca8
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-03-02 17:05:08 +0000

    Make sure that selected directories lie under the Default Image directory
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@6999 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPretrievemediafilenames.m
+++ CPsubfunctions/CPretrievemediafilenames.m
@@ -13,7 +13,6 @@ function [handles,FileNames] = CPretrievemediafilenames(handles, Pathname, TextT
 % $Revision$
 
 if strncmpi(recurse,'S',1) && ~isfield(handles.Pipeline,'PathNameSubFolders')
-
     idx = 1;
     More = 'Yes';
     while strcmp(More,'Yes')
@@ -22,7 +21,18 @@ if strncmpi(recurse,'S',1) && ~isfield(handles.Pipeline,'PathNameSubFolders')
             error('Processing was stopped because the user chose Cancel');
         end
         Directories{idx} = SubDirectory;
-        idx = idx + 1;
+        
+        % Make sure the selected directories all lie under the Default Image path
+        % Here, characters are treated like numbers in order to find where the 
+        % difference lies between the DefaultImage path and the selected
+        % path.
+        str = strvcat(Pathname,Directories{idx});
+        offset = find(str(1,:)-str(2,:),1,'first');
+        if offset < length(Pathname),
+            CPwarndlg('All selected director(ies) must be located under the Default image folder. Please choose a different directory, or Cancel and then change the Default Image directory appropriately and try again.','Error in directory selection','replace');
+        else
+          idx = idx + 1;
+        end
         More = CPquestdlg('Do you want to choose another directory?');
     end
     if strcmp(More,'Cancel')
@@ -37,9 +47,8 @@ if strncmpi(recurse,'S',1) && ~isfield(handles.Pipeline,'PathNameSubFolders')
     [handles.Pipeline.PathNameSubFolders,Directories] = deal(SelectedDirectoryTree(:)');
     
 elseif strncmpi(recurse,'Y',1) && ~isfield(handles.Pipeline,'PathNameSubFolders')
-
-    %% CPselectdirectories is still too slow for a lot of subfolders on
-    %% bcb_image
+    % CPselectdirectories is still too slow for a lot of subfolders on
+    % bcb_image
     Directories = CPgetdirectorytree(Pathname);
     %         Directories= CPselectdirectories(Directories);
     handles.Pipeline.PathNameSubFolders = Directories;
@@ -54,21 +63,21 @@ end
 FileNames = cell(0);
 Count = 1;
 for i=1:length(Directories)
-    %%% Lists all the contents of that path into a structure which includes the
-    %%% name of each object as well as whether the object is a file or
-    %%% directory.
+    % Lists all the contents of that path into a structure which includes the
+    % name of each object as well as whether the object is a file or
+    % directory.
     FilesAndDirsStructure = CPdir(Directories{i});
-    %%% Puts the names of each object into a list.
+    % Puts the names of each object into a list.
     FileAndDirNames = sortrows({FilesAndDirsStructure.name}');
-    %%% Puts the logical value of whether each object is a directory into a list.
+    % Puts the logical value of whether each object is a directory into a list.
     LogicalIsDirectory = [FilesAndDirsStructure.isdir];
-    %%% Eliminates directories from the list of file names.
+    % Eliminates directories from the list of file names.
     FileNamesNoDir = FileAndDirNames(~LogicalIsDirectory);
     if isempty(FileNamesNoDir) == 1
         continue;
     else
-        %%% Makes a logical array that marks with a "1" all file names that start
-        %%% with a period (hidden files):
+        % Makes a logical array that marks with a "1" all file names that start
+        % with a period (hidden files):
         DiscardLogical1 = strncmp(FileNamesNoDir,'.',1);
 
         if strncmpi(ImageOrMovie,'I',1)
@@ -94,32 +103,32 @@ for i=1:length(Directories)
             end
         end
 
-        %%% Combines all of the DiscardLogical arrays into one.
+        % Combines all of the DiscardLogical arrays into one.
         DiscardLogical = DiscardLogical1 | DiscardsByExtension;
-        %%% Eliminates filenames to be discarded.
+        % Eliminates filenames to be discarded.
         if isempty(DiscardLogical)
             NotYetTextMatchedFileNames = FileNamesNoDir;
         else NotYetTextMatchedFileNames = FileNamesNoDir(~DiscardLogical);
         end
 
-        %%% Loops through the names in the Directory listing, looking for the text
-        %%% of interest.  Creates the array Match which contains the numbers of the
-        %%% file names that match.
+        % Loops through the names in the Directory listing, looking for the text
+        % of interest.  Creates the array Match which contains the numbers of the
+        % file names that match.
         if strcmp(Directories{i},Pathname)
-            ExtraPath=[];
+            ExtraPath = [];
         else
-            ExtraPath=Directories{i};
-            ExtraPath=ExtraPath(length(Pathname)+1:end);
+            ExtraPath = Directories{i};
+            ExtraPath = ExtraPath(length(Pathname)+1:end);
         end
         for j=1:length(NotYetTextMatchedFileNames)
             FileName = char(NotYetTextMatchedFileNames(j));
             ShouldAddFile = false;
             if ~isempty(TextToFind)
                 if strncmpi(ExactOrRegExp,'E',1)
-                    %%% This used to be findstr, but that produced bad results
-                    %%% if the user entered a long string as the text to find,
-                    %%% and there is a filename with part, but not all, of the
-                    %%% name in common with that long string.
+                    % This used to be findstr, but that produced bad results
+                    % if the user entered a long string as the text to find,
+                    % and there is a filename with part, but not all, of the
+                    % name in common with that long string.
                     if ~isempty(strfind(lower(FileName), lower(TextToFind)))||isempty(TextToFind)
                         ShouldAddFile = true;
                     end

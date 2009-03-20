5c82c41d4f994b43d864a8b5cd2220a6e87e6647
anne <anne@1fc53939-2000-0410-845c-e8453a809027>
2009-03-20 20:06:42 +0000

    Updated settings for PyCP
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7122 1fc53939-2000-0410-845c-e8453a809027

--- Modules/RenameOrRenumberFiles.m
+++ Modules/RenameOrRenumberFiles.m
@@ -7,21 +7,22 @@ function handles = RenameOrRenumberFiles(handles)
 % Renames or renumbers files on the hard drive.
 % *************************************************************************
 %
-% File renaming utility that deletes or adds text anywhere within image
-% file names. It is especially useful as a file renumbering utility that
-% converts numbers within image file names to solve improper ordering of
-% files on Unix/Mac OSX systems.  
+% This file renaming utility adjusts text within image file names. 
+% Be very careful with this module because its purpose is to rename (=
+% overwrite) files!! You will have the opportunity to confirm the name
+% change for the first cycle only. The folder containing the files must not
+% contain subfolders or the subfolders and their contents will also be
+% renamed. It is worth doing a practice run with copies of images first.
 %
-% The settings ask the user to indicate how many characters to retain at
-% the beginning and end of each filename. These are the characters that
-% will remain unchanged. The user may choose to add text or numbers between
+%
+% Settings:
+%
+% * How many characters to retain at the beginning and end of each
+% filename? These are the characters that will remain unaltered and note
+% that all other characters in between will be removed.
+% * The user may choose to add text or numbers between
 % the characters that are to be retained.
 %
-% Be very careful since you will be renaming (= overwriting) your files!!
-% You will have the opportunity to confirm the name change for the first
-% cycle only. The folder containing the files must not contain subfolders
-% or the subfolders and their contents will also be renamed. It is worth
-% doing a practice run with copies of images first.
 %
 % Examples:
 % 
@@ -31,6 +32,18 @@ function handles = RenameOrRenumberFiles(handles)
 % DrosDAPI_100.tif  -> DrosDAPI_100.tif
 % (to accomplish this, retain 4 characters at the end, retain 9 characters
 % at the beginning, and use 3 numerical digits between).
+%
+% Renumbering is especially useful when numbers within image filenames do
+% not have a minimum number of digits and thus appear out of order when
+% listed in some Unix/Mac OSX systems. For example, on some systems, files
+% would appear like this and be measured out of expected sequence by
+% CellProfiler:
+% DrosDAPI_1.tif
+% DrosDAPI_10.tif
+% DrosDAPI_2.tif
+% DrosDAPI_3.tif
+% DrosDAPI_4.tif
+% ...
 %
 % Rename:
 % 1DrosophilaDAPI_1.tif    -> 1DrosDP_1.tif
@@ -60,11 +73,15 @@ drawnow
 
 [CurrentModule, CurrentModuleNum, ModuleName] = CPwhichmodule(handles);
 
+% 
+%
 % Klmadden 2009_03_20 I think this warning should be moved to a header
 % position in the variable settings:
-% This module allows you to rename (=overwrite) your files. Please See the help for this module for other warnings.
-% And the first variable should say say:
+% This module allows you to rename (overwrite) your files. Please see the help for this module for warnings.
+% So then the first variable should just say:
 % What did you call the images you want to rename/renumber?
+% 
+% See additional PyCP settings below...
 
 
 %textVAR01 = What did you call the images you want to rename/renumber? Be very careful since you will be renaming (= overwriting) your files!! See the help for this module for other warnings.
@@ -72,19 +89,39 @@ drawnow
 ImageName = char(handles.Settings.VariableValues{CurrentModuleNum,1});
 %inputtypeVAR01 = popupmenu
 
-%textVAR02 = How many characters at the beginning of the file name do you want to retain?
+%textVAR02 = How many characters at the beginning of the file name do you want to retain unaltered?
 %defaultVAR02 = 6
 NumberCharactersPrefix = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,2}));
 
-%textVAR03 = How many characters at the end do you want to retain, including file extension?
+%textVAR03 = How many characters at the end do you want to retain unaltered, including the .file extension if present?
 %defaultVAR03 = 8
 NumberCharactersSuffix = str2double(char(handles.Settings.VariableValues{CurrentModuleNum,3}));
 
-%textVAR04 = Enter any text you want to place between those two portions of filename. Type  "Do not use" to leave as is.
+% NEW PyCP setting: What do you want to do with the remaining characters (those not retained above)?
+% Options: Delete them all, or Extract the numerical characters
+%
+% If 'extract numerical characters', setting #5 below should appear.
+
+% NEW PyCP: Should we have the following be "Do you want to add text
+% between the beginning and ending portions of filename?" and if yes then
+% ask what text?  And, this variable (4) should be placed after (5) - we
+% want to address the numerical characters first since we just asked about
+% them in the New setting above. I think this means the code should be
+% rearranged a bit because I bet right now the text is automatically placed first and the
+% numbers second (which is the most useful scenario, I think). So, as I've
+% rewritten the settings, rearranging 4 and 5, it appears that numbers will
+% come first and text second - should we
+% also offer the user the choice of putting text first and numbers second?
+%
+%textVAR04 = Enter any text you want to place between the beginning and ending portions of the filename. Type  "Do not use" to leave as is.
 %defaultVAR04 = Do not use
 TextToAdd = char(handles.Settings.VariableValues{CurrentModuleNum,4});
 
-%textVAR05 = How many numerical digits would you like to use between those two portions of filename, for renumbering purposes? Type "Do not use" to leave as is.
+%New PyCP: The text "Type Do not use to leave as is." should no longer be
+%necessary (because #5 only appears if 'extract numerical chars" was
+%chosen).
+%
+%textVAR05 = How many numerical digits would you like to use between the beginning and ending portions of the filename, for renumbering purposes? Type "Do not use" to leave as is.
 %defaultVAR05 = Do not use
 NumberDigits = char(handles.Settings.VariableValues{CurrentModuleNum,5});
 

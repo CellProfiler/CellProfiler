12f7ff1155c2d627e784191551c3b348b054bc48
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-03-25 18:50:20 +0000

    Added 'Do not use' to MeasureRadialDist
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7157 1fc53939-2000-0410-845c-e8453a809027

--- CPsubfunctions/CPimportPreviousModuleSettings.m
+++ CPsubfunctions/CPimportPreviousModuleSettings.m
@@ -219,7 +219,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Standardization of non-used parameter text placeholders
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-idx = ismember(cellstr(lower(char(Settings.VariableValues{ModuleNum-Skipped,:}))),lower({'NO FILE LOADED','Leave this blank','Leave this black','Do not load','Do not save','/'}));
+idx = ismember(cellstr(lower(char(Settings.VariableValues{ModuleNum-Skipped,:}))),lower({'NO FILE LOADED','Leave this blank','Leave this black','None','Do not load','Do not save','/'}));
 if any(idx),
     [Settings.VariableValues{ModuleNum-Skipped,idx}] = deal('Do not use');
     NeedsPlaceholderUpdateMsg = true;

c76cf544d1a3b10ecfbec86aaf00acdce6cd14b3
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-03-28 20:15:36 +0000

    New: Add images to handles.Pipeline
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7185 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPaddimages.m
@@ -0,0 +1,31 @@
+function handles = CPaddimages(handles, varargin)
+% Add images to the handles.Pipeline structure.
+% Location will be "handles.Pipeline.ImageName".
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 5722 $
+
+% Parse out varargin
+if mod(length(varargin),2) ~= 0 || ...
+   ~all(cellfun(@ischar,varargin(1:2:end)) & cellfun(@isnumeric,varargin(2:2:end)))
+    error('The argument list must be of the form: ''ImageName1'', ImageData1, etc');
+else
+    ImageName = varargin(1:2:end);
+    ImageData = varargin(2:2:end);
+end
+
+CPvalidfieldname(ImageName);
+
+% Checks have passed, add the data
+for i = 1:length(ImageName)
+    handles.Pipeline.(ImageName{i}) = ImageData{i};
+end

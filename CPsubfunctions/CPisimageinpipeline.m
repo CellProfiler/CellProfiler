4bf09eabe3d8bb52de06d48b916eab4e14426dab
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-03-29 10:22:33 +0000

    New: Checks if image is in handles.Pipeline or not
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7186 1fc53939-2000-0410-845c-e8453a809027

---
+++ CPsubfunctions/CPisimageinpipeline.m
@@ -0,0 +1,17 @@
+function ispresent = CPisimageinpipeline(handles, fieldname)
+% Add images to the handles.Pipeline structure.
+% Location will be "handles.Pipeline.ImageName".
+
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision: 5722 $
+
+ispresent = isfield(handles.Pipeline,fieldname);

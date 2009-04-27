c1ee29b0f4b4ef3b8c7aac24e4e941b295979c49
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-04-27 14:03:34 +0000

    Minor: Closes msgbox when batch completely submitted
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7402 1fc53939-2000-0410-845c-e8453a809027

--- DataTools/SubmitBatch.m
+++ DataTools/SubmitBatch.m
@@ -221,7 +221,7 @@ uipushbutton(SBh,'Submit',[posx+125 10 75 uiheight],'[foo,fig] = gcbo;set(fig,''
 uiwait()
 if get(SBh,'Userdata') == 1,     % The user pressed the Submit button
     CPmsgbox(['PLEASE WAIT while the webserver submits the batches for processing.  ' ...
-        'A window should appear with your batch number, however this may take a minute or so...']);
+        'A window should appear with your batch number, however this may take a minute or so...'],mfilename);
     unix_dir = get(DataDirCtl,'String');
     local_path = get(LocalPathCtl, 'String');
     unix_path = get(UnixPathCtl, 'String');
@@ -237,5 +237,5 @@ if get(SBh,'Userdata') == 1,     % The user pressed the Submit button
     SubmitInfo.CPCluster = get(CPClusterCtl,'String');
     SubmitInfo.WriteData = get(WriteDataCtl,'Value');
 end
-delete(SBh)
+delete([SBh(:); findobj(allchild(0),'name',mfilename)]);
 end

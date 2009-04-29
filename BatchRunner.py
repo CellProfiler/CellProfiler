ff85e0ce4d3b9cc0c7b7eebfecfaed4f681ba82e
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2009-04-29 13:21:07 +0000

    Updates to script (via leek)
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7418 1fc53939-2000-0410-845c-e8453a809027

--- BatchRunner.py
+++ BatchRunner.py
@@ -1,7 +1,7 @@
-#!/broad/tools/apps/Python-2.5.2/bin/python
+#!/broad/tools/Linux/x86_64/pkgs/python_2.6.1/bin/python
 from sys import argv,exit,path
 from subprocess import Popen, PIPE, STDOUT
-from scipy.io.mio import loadmat
+from scipy.io.matlab import loadmat
 from os.path import exists
 
 if len(argv) != 6:
@@ -18,7 +18,7 @@ CPCluster=path[0]
 
 # Load Batch_data and figure out the sets that need running
 batch_info = loadmat("%(datadir)s/Batch_data.mat"%(locals()))
-num_sets = batch_info['handles'].Current.NumberOfImageSets
+num_sets = batch_info['handles'][0,0].Current[0,0].NumberOfImageSets[0,0]
 
 # Loop over batches, check status file, print out commands for those that need it
 for start in range(1, num_sets + 1, batch_size):
@@ -28,7 +28,6 @@ for start in range(1, num_sets + 1, batch_size):
     status_file_name = "%(datadir)s/status/Batch_%(start)d_to_%(end)d_DONE.mat"%(locals())
     if not exists(status_file_name):
         print "bsub -q %(queue)s -o %(datadir)s/txt_output/%(start)s_to_%(end)s.txt %(CPCluster)s/CPCluster.py %(datadir)s/Batch_data.mat %(start)s %(end)s %(datadir)s/status Batch_ %(write_data)s %(timeout)d"%(locals())
-
 
 
 #  for i in `; do

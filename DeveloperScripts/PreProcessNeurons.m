f48c1c4832a90055481884eed607aa55d3b3f488
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2009-07-22 15:09:29 +0000

    Added LogTransform option
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7787 1fc53939-2000-0410-845c-e8453a809027

--- DeveloperScripts/PreProcessNeurons.m
+++ DeveloperScripts/PreProcessNeurons.m
@@ -4,6 +4,8 @@
 
 clear
 
+doLogTransform = 1;
+
 %%% CHOOSE ONE SECTION BELOW %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%% JEN PAN images
@@ -35,11 +37,30 @@ clear
 
 %%%% JEN PAN images analyzed 2009-05-11 : LOCAL
 strel_size = 4;
+
+% PathRoot = '/Users/dlogan/Projects/2008_04_15_Lithium_Neurons_JenPan/images/';
+% PathName = '080716/plate 2';
+% OutputPathRoot = '/Users/dlogan/Projects/2008_04_15_Lithium_Neurons_JenPan/images/';
+% OutputPathName = [PathName '_pre_processed_with_tophat_minus_bottomhat_size4'];
+% if doLogTransform
+%     OutputPathName = [OutputPathName '_logTransformed'];
+% end
+% search_string = '.*_[B-G][0-9][0-9].*.TIF'; %% Rows A and H are empty
+% 
+% 
+% PathRoot = '/Volumes/Image08/hcs/';
+% PathName = 'JenPan/ADSAPlate0423-1-4x/2008-05-09/8069';
+% OutputPathRoot = '/Volumes/imaging_analysis/2008_04_15_Lithium_Neurons_JenPan/image08_ADSAPlate0423-1-4x_8069/';
+% OutputPathName = PathName;
+% search_string = '.*_s[0-9]_w[0-9][a-zA-Z0-9].*.tif';
+
+
 PathRoot = '/Users/dlogan/Projects/2008_04_15_Lithium_Neurons_JenPan/images/';
 PathName = '080716/plate 2';
 OutputPathRoot = '/Users/dlogan/Projects/2008_04_15_Lithium_Neurons_JenPan/images/';
 OutputPathName = [PathName '_pre_processed_with_tophat_minus_bottomhat_size4'];
 search_string = '.*_[B-G][0-9][0-9].*.TIF'; %% Rows A and H are empty
+
 %%%% 
 
 
@@ -69,6 +90,16 @@ end
 for i = length(Good):-1:1
     disp(i)
     I = imread(fullfile([PathRoot PathName],Good{i}));
+    
+    %% Log transform
+    if doLogTransform
+        if max(I(:)) > 0,
+            ImageNoZeros = max(I, min(I(I > 0)));
+            I = log2(double(ImageNoZeros));
+        else
+            I = zeros(size(I));
+        end
+    end
 
     J = imsubtract(imadd(I,imtophat(I,SE)), imbothat(I,SE));
     colormap(gray)

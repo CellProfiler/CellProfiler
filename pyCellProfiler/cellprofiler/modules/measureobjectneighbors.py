IMG-159 - fixed problem with masking with rectangle & color image, other color image handling. Fixed bug in MeasureObjectNeighbors - was adding a matrix and not an Image object to image set.
git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@7798 1fc53939-2000-0410-845c-e8453a809027

pyCellProfiler/cellprofiler/modules/measureobjectneighbors.py
@@ -17,6 +17,7 @@ import scipy.ndimage as scind
 import matplotlib.cm
 
 import cellprofiler.cpmodule as cpm
+import cellprofiler.cpimage as cpi
 import cellprofiler.measurements as cpmeas
 import cellprofiler.settings as cps
 import cellprofiler.preferences as cpprefs
@@ -322,7 +323,8 @@ class MeasureObjectNeighbors(cpm.CPModule):
             img[:,:,0][~ object_mask] = 0
             img[:,:,1][~ object_mask] = 0
             img[:,:,2][~ object_mask] = 0
-            image_set.add(self.count_image_name.value, img)
+            count_image = cpi.Image(img, masking_objects = objects)
+            image_set.add(self.count_image_name.value, count_image)
         else:
             neighbor_cm = matplotlib.cm.get_cmap(cpprefs.get_default_colormap())
         if self.wants_percent_touching_image.value:
@@ -332,8 +334,9 @@ class MeasureObjectNeighbors(cpm.CPModule):
             img[:,:,0][~ object_mask] = 0
             img[:,:,1][~ object_mask] = 0
             img[:,:,2][~ object_mask] = 0
+            touching_image = cpi.Image(img, masking_objects = objects)
             image_set.add(self.touching_image_name.value,
-                          img)
+                          touching_image)
         else:
             percent_touching_cm = matplotlib.cm.get_cmap(cpprefs.get_default_colormap())
         if not workspace.frame is None:

pyCellProfiler/cellprofiler/modules/tests/test_measureobjectneighbors.py
@@ -364,7 +364,7 @@ class TestMeasureObjectNeighbors(unittest.TestCase):
         module.count_image_name.value = 'my_image'
         module.count_colormap.value = 'jet'
         module.run(workspace)
-        image = workspace.image_set.get_image('my_image')
+        image = workspace.image_set.get_image('my_image').pixel_data
         self.assertEqual(tuple(image.shape), (10,10,3))
         # Everything off of the images should be black
         self.assertTrue(np.all(image[labels[labels==0],:]==0))
@@ -386,7 +386,7 @@ class TestMeasureObjectNeighbors(unittest.TestCase):
         module.touching_image_name.value = 'my_image'
         module.touching_colormap.value = 'jet'
         module.run(workspace)
-        image = workspace.image_set.get_image('my_image')
+        image = workspace.image_set.get_image('my_image').pixel_data
         self.assertEqual(tuple(image.shape), (10,10,3))
         # Everything off of the images should be black
         self.assertTrue(np.all(image[labels[labels==0],:]==0))
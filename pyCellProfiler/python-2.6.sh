833e4a339992f223e7aec830f40733587d43b592
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2009-09-24 18:33:54 +0000

    Use hardcoded paths to some directories
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8019 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/python-2.6.sh
+++ pyCellProfiler/python-2.6.sh
@@ -3,7 +3,7 @@ export DK_ROOT=/broad/tools/dotkit
 source /broad/tools/dotkit/ksh/.dk_init
 use -q Python-2.6
 use -q matlab
-export MPLCONFIGDIR=./.matplotlib
-export PYTHONPATH=./site-packages:.:/broad/tools/Linux/x86_64/pkgs/matplotlib_0.98.5.2/lib/python2.6/site-packages:$PYTHONPATH
+export MPLCONFIGDIR=/imaging/analysis/CPCluster/CellProfiler-2.0/.matplotlib
+export PYTHONPATH=/imaging/analysis/CPCluster/CellProfiler-2.0/site-packages:.:/broad/tools/Linux/x86_64/pkgs/matplotlib_0.98.5.2/lib/python2.6/site-packages:$PYTHONPATH
 python "$@"
 

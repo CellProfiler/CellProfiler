c9aed509bc15a3d7df95657f5e2f4f906781840c
klmadden <klmadden@1fc53939-2000-0410-845c-e8453a809027>
2009-10-02 19:25:57 +0000

    help for InvertForPrinting
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8095 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/cellprofiler/modules/invertforprinting.py
+++ pyCellProfiler/cellprofiler/modules/invertforprinting.py
@@ -1,16 +1,25 @@
-'''invertforprinting.py - Invert images for printing
+'''InvertForPrinting - Invert fluorescent images into brightfield-looking images for printing.
+<hr>
+This module turns a single or multi-channel immunofluorescent-stained image
+into an image that resembles a brightfield image stained with similarly-
+colored stains, which generally prints better.
+    
+You have the option of combining up to three grayscale images (representing
+the red, green and blue channels of a color image) or of operating on
+a single color image. The module can produce either three grayscale
+images or one color image on output.
 
-CellProfiler is distributed under the GNU General Public License.
-See the accompanying file LICENSE for details.
-
-Developed by the Broad Institute
-Copyright 2003-2009
-
-Please see the AUTHORS file for credits.
-
-Website: http://www.cellprofiler.org
+If you want to invert the grayscale intensities of an image, use <b>ImageMath</b>
 '''
-
+#CellProfiler is distributed under the GNU General Public License.
+#See the accompanying file LICENSE for details.
+#
+#Developed by the Broad Institute
+#Copyright 2003-2009
+#
+#Please see the AUTHORS file for credits.
+#
+#Website: http://www.cellprofiler.org
 __version__="$Revision$"
 
 import numpy as np
@@ -23,20 +32,7 @@ CC_GRAYSCALE = "Grayscale"
 CC_COLOR = "Color"
 CC_ALL = [CC_COLOR, CC_GRAYSCALE]
 class InvertForPrinting(cpm.CPModule):
-    '''Short description:
-    Inverts Fluorescent-looking images into brightfield-looking images.
-    
-    **********************************************************************
-    
-    This module turns a single or multi-channel immunofluorescent-stained image
-    into an image that resembles a brightfield image stained with similarly-
-    colored stains, which generally prints better.
-    
-    You have the option of combining up to three grayscale images (representing
-    the red, green and blue channels of a color image) or of operating on
-    a single color image. The module can produce either three grayscale
-    images or one color image on output.
-    '''
+   
     category = 'Image Processing'
     variable_revision_number = 1
     

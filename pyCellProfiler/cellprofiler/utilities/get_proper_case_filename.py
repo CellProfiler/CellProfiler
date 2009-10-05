eb0f6f5cab04664eb41f2c0e49522ff2da8c400c
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2009-10-05 17:27:09 +0000

    IMG-288 temporary fix to disable get_proper_case_filename
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8112 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/cellprofiler/utilities/get_proper_case_filename.py
+++ pyCellProfiler/cellprofiler/utilities/get_proper_case_filename.py
@@ -16,7 +16,7 @@ __version__="$Revision$"
 import sys
 import os
 
-if sys.platform.startswith("win"):
+if sys.platform.startswith("win") or False:
     import _get_proper_case_filename
     def get_proper_case_filename(path):
         result = _get_proper_case_filename.get_proper_case_filename(unicode(path))

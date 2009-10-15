622d25c5aa5a4fede78d06ba719e0d7aed100e0d
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2009-10-15 10:23:22 +0000

    Minor: add boilerplate with svn revision
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8201 1fc53939-2000-0410-845c-e8453a809027

--- Modules/MeasureNeurons.m
+++ Modules/MeasureNeurons.m
@@ -8,6 +8,18 @@ function handles = MeasureNeurons(handles,varargin)
 %
 %
 
+% CellProfiler is distributed under the GNU General Public License.
+% See the accompanying file LICENSE for details.
+%
+% Developed by the Whitehead Institute for Biomedical Research.
+% Copyright 2003,2004,2005.
+%
+% Please see the AUTHORS file for credits.
+%
+% Website: http://www.cellprofiler.org
+%
+% $Revision$
+
 %%%%%%%%%%%%%%%%%
 %%% VARIABLES %%%
 %%%%%%%%%%%%%%%%%

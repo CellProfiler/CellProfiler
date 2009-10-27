82ef70d96528fa03c69a996098b43623e1af1675
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2009-10-27 13:23:25 +0000

    Tests for formatreader.py
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8323 1fc53939-2000-0410-845c-e8453a809027

---
+++ pyCellProfiler/bioformats/tests/test_formatreader.py
@@ -0,0 +1,95 @@
+'''test_formatreader.py - test the Bioformats format reader wrapper
+
+CellProfiler is distributed under the GNU General Public License.
+See the accompanying file LICENSE for details.
+
+Developed by the Broad Institute
+Copyright 2003-2009
+
+Please see the AUTHORS file for credits.
+
+Website: http://www.cellprofiler.org
+'''
+
+__version__="$Revision$"
+
+import numpy as np
+import os
+import unittest
+
+import bioformats.formatreader as F
+import cellprofiler.utilities.jutil as J
+from cellprofiler.modules.tests import example_images_directory
+
+env = F.get_env()
+
+class TestFormatReader(unittest.TestCase):
+    def test_01_01_make_format_tools_class(self):
+        FormatTools = F.make_format_tools_class(env)
+        self.assertEqual(FormatTools.CAN_GROUP, 1)
+        self.assertEqual(FormatTools.CANNOT_GROUP, 2)
+        self.assertEqual(FormatTools.DOUBLE, 7)
+        self.assertEqual(FormatTools.FLOAT, 6)
+        self.assertEqual(FormatTools.INT16, 2)
+        self.assertEqual(FormatTools.INT8, 0)
+        self.assertEqual(FormatTools.MUST_GROUP, 0)
+        self.assertEqual(FormatTools.UINT16, 3)
+        self.assertEqual(FormatTools.UINT32, 5)
+        self.assertEqual(FormatTools.UINT8, 1)
+
+    def test_02_01_make_image_reader(self):
+        path = os.path.join(example_images_directory(), 'ExampleSBSImages',
+                            'Channel1-01-A-01.tif')
+        ImageReader = F.make_image_reader_class(env)
+        FormatTools = F.make_format_tools_class(env)
+        reader = ImageReader()
+        reader.setId(path)
+        self.assertEqual(reader.getDimensionOrder(), "XYCZT")
+        metadata = J.jdictionary_to_string_dictionary(env, reader.getMetadata())
+        self.assertEqual(int(metadata["ImageWidth"]), reader.getSizeX())
+        self.assertEqual(int(metadata["ImageLength"]), reader.getSizeY())
+        self.assertEqual(reader.getImageCount(), 1)
+        self.assertEqual(reader.getSizeC(), 1)
+        self.assertEqual(reader.getSizeT(), 1)
+        self.assertEqual(reader.getSizeZ(), 1)
+        self.assertEqual(reader.getPixelType(), FormatTools.UINT8)
+        self.assertEqual(reader.getRGBChannelCount(), 1)
+        
+    def test_03_01_read_tif(self):
+        path = os.path.join(example_images_directory(), 'ExampleSBSImages',
+                            'Channel1-01-A-01.tif')
+        ImageReader = F.make_image_reader_class(env)
+        FormatTools = F.make_format_tools_class(env)
+        reader = ImageReader()
+        reader.setId(path)
+        data = reader.openBytes(0)
+        data.shape = (reader.getSizeY(), reader.getSizeX())
+        #
+        # Data as read by cellprofiler.modules.loadimages.load_using_PIL
+        #
+        expected_0_10_0_10 = np.array(
+            [[ 0,  7,  7,  6,  5,  8,  4,  2,  1,  2],
+             [ 0,  8,  8,  7,  6, 10,  4,  2,  2,  2],
+             [ 0,  9,  9,  7,  8,  8,  2,  1,  3,  2],
+             [ 0, 10,  9,  8, 10,  6,  2,  2,  3,  2],
+             [ 0, 10, 10, 10,  9,  4,  2,  2,  2,  2],
+             [ 0,  9,  9, 10,  8,  3,  2,  4,  2,  2],
+             [ 0,  9,  9, 10,  8,  2,  2,  4,  3,  2],
+             [ 0,  9,  8,  9,  7,  4,  2,  2,  2,  2],
+             [ 0, 10, 11,  9,  9,  4,  2,  2,  2,  2],
+             [ 0, 12, 13, 12,  9,  4,  2,  2,  2,  2]], dtype=np.uint8)
+        expected_n10_n10 = np.array(
+            [[2, 1, 1, 1, 2, 2, 1, 2, 1, 2],
+             [1, 2, 2, 2, 2, 1, 1, 1, 2, 1],
+             [1, 1, 1, 2, 1, 2, 2, 2, 2, 1],
+             [2, 2, 2, 2, 3, 2, 2, 2, 2, 1],
+             [1, 2, 2, 1, 1, 1, 1, 1, 2, 2],
+             [2, 1, 2, 2, 2, 1, 1, 2, 2, 2],
+             [2, 2, 3, 2, 2, 1, 2, 2, 2, 1],
+             [3, 3, 1, 2, 2, 2, 2, 3, 2, 2],
+             [3, 2, 2, 2, 2, 2, 2, 2, 3, 3],
+             [5, 2, 3, 3, 2, 2, 2, 3, 2, 2]], dtype=np.uint8)
+        self.assertTrue(np.all(expected_0_10_0_10 == data[:10,:10]))
+        self.assertTrue(np.all(expected_n10_n10 == data[-10:,-10:]))
+       
+        

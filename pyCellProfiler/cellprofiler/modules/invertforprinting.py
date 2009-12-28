9879b55278eddd6bbefe22333215e4121af14cb5
anne <anne@1fc53939-2000-0410-845c-e8453a809027>
2009-12-28 20:41:12 +0000

    Updated settings text and help
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8885 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/cellprofiler/modules/invertforprinting.py
+++ pyCellProfiler/cellprofiler/modules/invertforprinting.py
@@ -1,13 +1,11 @@
-'''InvertForPrinting - Invert fluorescent images into brightfield-looking images for printing.
+'''Invert For Printing inverts fluorescent images into brightfield-looking images for printing
 <hr>
 This module turns a single or multi-channel immunofluorescent-stained image
-into an image that resembles a brightfield image stained with similarly-
-colored stains, which generally prints better.
-    
-You have the option of combining up to three grayscale images (representing
-the red, green and blue channels of a color image) or of operating on
-a single color image. The module can produce either three grayscale
-images or one color image on output.
+into an image that resembles a brightfield image stained with similarly-colored stains, which generally prints better.
+
+You have the option of operating on up to three grayscale images (representing
+the red, green and blue channels of a color image) or on an image that is already a color image. The module can produce either three grayscale
+images or one color image as output.
 
 If you want to invert the grayscale intensities of an image, use <b>ImageMath</b>
 '''

669666fe83ba3700fd56a86b11d2e670ea42394a
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2010-01-07 21:05:41 +0000

    Logging for John H
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@8995 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/python-2.6.sh
+++ pyCellProfiler/python-2.6.sh
@@ -10,5 +10,14 @@ export MPLCONFIGDIR=/imaging/analysis/CPCluster/CellProfiler-2.0/.matplotlib
 fi
 export PYTHONPATH=/imaging/analysis/CPCluster/CellProfiler-2.0/site-packages:.:/broad/tools/Linux/x86_64/pkgs/matplotlib_0.98.5.2/lib/python2.6/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server
+
+# Do some logging.
+which python > /local/scratch/DELETEME.${LSB_JOBID}
+echo "$@" >> /local/scratch/DELETEME.${LSB_JOBID}
+env | sort >> /local/scratch/DELETEME.${LSB_JOBID}
+python -c "import sys; print sys.path" | sed 's,\[,,g' | sed 's,\],,g' | tr ',' '\n' | sort  >> /local/scratch/DELETEME.${LSB_JOBID}
+cat /local/scratch/DELETEME.${LSB_JOBID} | mail -s "[PYTHON-ERROR-QUEST] Job: ${LSB_JOBID} on Host: `hostname -s`" jbh@broadinstitute.org
+rm /local/scratch/DELETEME.${LSB_JOBID}
+
 python "$@"
 

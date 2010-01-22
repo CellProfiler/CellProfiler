fix blur calculation
git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@9143 1fc53939-2000-0410-845c-e8453a809027

pyCellProfiler/cellprofiler/cpmath/radial_power_spectrum.py
@@ -8,13 +8,13 @@ def rps(img):
     radii2 = (np.arange(img.shape[0]).reshape((img.shape[0], 1)) ** 2) + (np.arange(img.shape[1])** 2)
     radii2 = np.minimum(radii2, np.flipud(radii2))
     radii2 = np.minimum(radii2, np.fliplr(radii2))
-    halfwidth = min(img.shape[0], img.shape[1]) / 4.0 # truncate early to avoid edge effects
+    maxwidth = min(img.shape[0], img.shape[1]) / 8.0 # truncate early to avoid edge effects
     if img.ptp() > 0:
         img = img / np.median(abs(img - img.mean())) # intensity invariant
     mag = abs(fft2(img - np.mean(img)))
     power = abs(fft2(img - np.mean(img)))**2
     radii = np.floor(np.sqrt(radii2)).astype(np.int) + 1
-    labels = np.arange(2, np.floor(halfwidth)).astype(np.int).tolist() # skip DC component
+    labels = np.arange(2, np.floor(maxwidth)).astype(np.int).tolist() # skip DC component
     if len(labels) > 0:
         magsum = nd_sum(mag, radii, labels)
         powersum = nd_sum(power, radii, labels)
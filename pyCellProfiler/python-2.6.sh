6eab72bf0805890bef234465797ac3fd0a9f486c
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2010-01-22 13:35:53 +0000

    Removed logging code, added code to append last checked-out CP
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@9136 1fc53939-2000-0410-845c-e8453a809027

--- pyCellProfiler/python-2.6.sh
+++ pyCellProfiler/python-2.6.sh
@@ -3,21 +3,14 @@ export DK_ROOT=/broad/tools/dotkit
 source /broad/tools/dotkit/ksh/.dk_init
 use -q Python-2.6
 use -q Java-1.6
-IMAGEWEB=imageweb
-if test $USER = $IMAGEWEB
-then
 export MPLCONFIGDIR=/imaging/analysis/CPCluster/CellProfiler-2.0/.matplotlib
-fi
 export PYTHONPATH=/imaging/analysis/CPCluster/CellProfiler-2.0/site-packages:.:/broad/tools/Linux/x86_64/pkgs/matplotlib_0.98.5.2/lib/python2.6/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server
-
-# Do some logging.
-which python > /local/scratch/DELETEME.${LSB_JOBID}
-echo "$@" >> /local/scratch/DELETEME.${LSB_JOBID}
-env | sort >> /local/scratch/DELETEME.${LSB_JOBID}
-python -c "import sys; print sys.path" | sed 's,\[,,g' | sed 's,\],,g' | tr ',' '\n' | sort  >> /local/scratch/DELETEME.${LSB_JOBID}
-cat /local/scratch/DELETEME.${LSB_JOBID} | mail -s "[PYTHON-ERROR-QUEST] Job: ${LSB_JOBID} on Host: `hostname -s`" jbh@broadinstitute.org
-rm /local/scratch/DELETEME.${LSB_JOBID}
-
+#
+# This code lists the CellProfiler checkout directory for
+# the latest checkout and adds that to the end of the Python path
+#
+export LAST_CHECKOUT=`echo "import os;cpdir='/imaging/analysis/CPCluster/CellProfiler-2.0';print os.path.join(cpdir,str(max(*[int(x) for x in os.listdir(cpdir) if x.isdigit()])))" | python`
+export PYTHONPATH=$PYTHONPATH:$LAST_CHECKOUT
 python "$@"
 

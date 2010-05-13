b8891111aec7b095ed429c8af4cf04c72912e263
mbray <mbray@1fc53939-2000-0410-845c-e8453a809027>
2010-05-13 13:44:35 +0000

    Validate that All:First is using an image provided by LoadImages/LoadData
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@9957 1fc53939-2000-0410-845c-e8453a809027

--- cellprofiler/modules/correctilluminationcalculate.py
+++ cellprofiler/modules/correctilluminationcalculate.py
@@ -511,6 +511,12 @@ class CorrectIlluminationCalculate(cpm.CPModule):
         return output_image
     
     def validate_module(self, pipeline):
+        '''Produce error if 'All:First' is selected and input image is not provided by the file image provider.'''
+        if not pipeline.is_image_from_file(self.image_name.value) and self.each_or_all == EA_ALL_FIRST:
+            raise cps.ValidationError(
+                    "All: First cycle requires that the input image be provided by LoadImages or LoadData.",
+                    self.each_or_all)
+        
         '''Modify the image provider attributes based on other setttings'''
         d = self.illumination_image_name.provided_attributes
         if self.each_or_all == EA_ALL_ACROSS:

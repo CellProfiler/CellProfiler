7c6309a674d33669edc9e200f34f44ade549fe22
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-05-25 18:55:51 +0000

    DisplayPlatMap now displays the proper image_set_number when in datatool mode.
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10005 1fc53939-2000-0410-845c-e8453a809027

--- cellprofiler/modules/displayplatemap.py
+++ cellprofiler/modules/displayplatemap.py
@@ -192,7 +192,7 @@ class DisplayPlatemap(cpm.CPModule):
                          title='Display platemap #%d'%(self.module_num), 
                          subplots=(1,1))
             if self.title.value != '':
-                title = '%s (cycle %s)'%(self.title.value, workspace.image_set.number+1)
+                title = '%s (cycle %s)'%(self.title.value, workspace.measurements.image_set_number)
             else:
                 title = '%s(%s)'%(self.agg_method, self.plot_measurement.value)
             figure.subplot_platemap(0, 0, pm_dict, self.plate_type,

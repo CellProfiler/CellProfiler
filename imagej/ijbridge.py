7814229caf2e280c5c6b2deb726a2c1d927a2fa0
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-08-12 15:19:53 +0000

    progress on ijbridge
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10399 1fc53939-2000-0410-845c-e8453a809027

--- imagej/ijbridge.py
+++ imagej/ijbridge.py
@@ -3,13 +3,19 @@ ijbridge provides a high-level interface to ImageJ so in-process and inter-
 process execution of ImageJ can be abstracted and branched cleanly.
 '''
 import sys
+import numpy as np
+import os, tempfile
+import Image as PILImage
+from subprocess import Popen, PIPE, STDOUT
+import shlex
+
 
 class ij_bridge(object):
    '''This class provides a high-level interface for running ImageJ from
    CellProfiler. It is intended to abstract away whether IJ is being run within
    the same process or in a separate process.
    '''   
-   def inject_image(self, image, name=None):
+   def inject_image(self, pixel_data, name=None):
       '''inject an image into ImageJ for processing'''
       raise NotImplementedError
 
@@ -91,69 +97,103 @@ class in_proc_ij_bridge(ij_bridge):
       '''show the ImageJ user interface'''
       ijmacros.show_imagej()
       
-      
+
+
 class inter_proc_ij_bridge(ij_bridge):
    '''Interface for running ImageJ in a separate process from CellProfiler.
    '''
-   def inject_image(self, image, name=None):
+   def __init__(self):
+      os.environ['CLASSPATH'] = '/Users/afraser/CellProfiler/:'+os.environ['CLASSPATH']
+      self.ijproc = Popen(shlex.split('java Adam'), stdin=PIPE, stdout=PIPE,
+                          stderr=None)
+   
+##   def __del__(self):
+##      os.kill(self.ijproc.pid)
+   
+   def inject_image(self, pixel_data, name=None):
       '''inject an image into ImageJ for processing'''
-      raise NotImplementedError
+      tmpdir = tempfile.mkdtemp()
+      filename = os.path.join(tmpdir, 'cp_image')
+
+##      print 'INJECT_IMAGE: writing image to',filename
+      
+      if pixel_data.ndim == 3 and pixel_data.shape[2] == 4:
+         mode = 'RGBA'
+      elif pixel_data.ndim == 3:
+         mode = 'RGB'
+      else:
+         mode = 'L'
+      pil = PILImage.fromarray(pixel_data, mode)
+      pil.save(filename, 'png')
+      
+      filename = '/Users/afraser/cpa_example/images/AS_09125_050116000001_A01f00d0.png'
+      
+      print communicate(self.ijproc, 'inject %s'%(filename))
+      
 
    def get_current_image(self):
       '''returns the WindowManager's current image as a numpy float array'''
-      raise NotImplementedError
+      tmpdir = tempfile.mkdtemp()
+      filename = os.path.join(tmpdir, 'ij_image.png')
+      
+      print communicate('get %s'%(filename))
+##      output = 'wrote file /Users/afraser/cpa_example/images/AS_09125_050116000001_A01f00d0.png'
+      if output.startswith('wrote file '):
+         filename = output.strip('wrote file ')
+         pil = PILImage.open(filename, 'r')
+         pil.show()
+##         pixels = np.frombuffer(pil.tostring())
+##         pixels.reshape(
 
    def get_commands(self):
       '''returns a list of the available command strings'''
-      raise NotImplementedError
+      self.ijproc.stdin.write('get_commands\n')
+      self.ijproc.stdin.flush()
+      print self.ijproc.stdout.readlines()
 
    def execute_command(self, command, options=None):
       '''execute the named command within ImageJ'''
-      raise NotImplementedError
+      self.ijproc.stdin.write('command %s\n'%(command))
+      self.ijproc.stdin.flush()
 
    def execute_macro(self, macro_text):
       '''execute a macro in ImageJ
     
       macro_text - the macro program to be run
       '''
-      raise NotImplementedError
+      self.ijproc.stdin.write('macro %s\n'%(macro_text))
+      self.ijproc.stdin.flush()
    
    def show_imagej(self):
       '''show the ImageJ user interface'''
-      raise NotImplementedError
+      self.ijproc.stdin.write('show_imagej\n')
+      self.ijproc.stdin.flush()
+      
+   def quit(self):
+      self.ijproc.stdin.write('quit\n')
+      self.ijproc.stdin.flush()
+      
+      
+def communicate(proc, inp):
+   '''
+   '''
+   proc.stdin.write(inp+'\n')
+   proc.stdin.flush()
+   return proc.stdout.readline()
+      
 
-      
-      
-def get_imageplus_wrapper(imageplus_obj):
-   '''Wrap the imageplus object as a Java class'''
-   class ImagePlus(object):
-      def __init__(self):
-         self.o = imageplus_obj
-      def lockSilently(self): pass
-      def unlock(self): pass
-      def getBitDepth(self): pass
-      def getBytesPerPixel(self): pass
-      def getChannel(self): pass
-      def getChannelProcessor(self): pass
-      def getCurrentSlice(self): pass
-      def getDimensions(self): pass
-      def getFrame(self): pass
-      def getHeight(self): pass
-      def getID(self): pass
-      def getImageStackSize(self): pass
-      def getNChannels(self): pass
-      def getNDimensions(self): pass
-      def getNFrames(self): pass
-      def getNSlices(self): pass
-      def getProcessor(self): pass
-      def getTitle(self): pass
-      def getWidth(self): pass
-      def setPosition(self, channel, slice, frame): pass
-      def setSlice(self, slice): pass
-      def setTitle(self, title): pass
-      def show(self): pass
-      def show_with_message(self, message): pass
-      def hide(self): pass
-      def getWindow(self): pass
-   return ImagePlus()
-      
+if __name__ == '__main__':
+   ipb = inter_proc_ij_bridge()
+
+   pixels = (np.ones((100,100)) * 255.).astype(np.uint8)
+   ipb.inject_image(pixels, 'name ignored')
+
+##   ipb.execute_macro('run("Invert");')
+##
+##   ipb.get_current_image()
+##   
+##   ipb.get_commands()
+##
+##   ipb.quit()
+##   sys.exit()
+   

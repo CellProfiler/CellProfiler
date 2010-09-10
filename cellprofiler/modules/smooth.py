c6d839acf96d4afaee1f48d7ed0ed91fadcac184
dlogan <dlogan@1fc53939-2000-0410-845c-e8453a809027>
2010-09-10 18:47:01 +0000

    Standardize placement of 2 subplots into landscape mode
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10465 1fc53939-2000-0410-845c-e8453a809027

--- cellprofiler/modules/smooth.py
+++ cellprofiler/modules/smooth.py
@@ -176,12 +176,12 @@ class Smooth(cpm.CPModule):
 
     def display(self, workspace):
         figure = workspace.create_or_find_figure(title="Smooth, image cycle #%d"%(
-                workspace.measurements.image_set_number),subplots=(1,2))
+                workspace.measurements.image_set_number),subplots=(2,1))
         figure.subplot_imshow_grayscale(0, 0, 
                                         workspace.display_data.pixel_data,
                                         "Original: %s" % 
                                         self.image_name.value)
-        figure.subplot_imshow_grayscale(0, 1,
+        figure.subplot_imshow_grayscale(1, 0,
                                         workspace.display_data.output_pixels,
                                         "Filtered: %s" %
                                         self.filtered_image_name.value,

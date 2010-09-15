db4703f22abc20b9dde802228a5e27b399c7f56f
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-09-15 17:55:54 +0000

    rabble! rabble! rabble!
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10480 1fc53939-2000-0410-845c-e8453a809027

---
+++ imagej/TCPClient.java
@@ -0,0 +1,230 @@
+import ij.ImagePlus;
+import ij.ImageJ;
+import ij.macro.*;
+import ij.process.*;
+import ij.Menus;
+import ij.IJ;
+import java.io.*;
+import java.util.*;
+import ij.WindowManager;
+import java.lang.StringBuffer;
+import java.lang.String;
+import java.net.*;
+
+/*
+   This is the client program for running ImageJ in a separate process from
+   CellProfiler.
+   
+   Usage: java TCPClient port#
+*/
+
+class TCPClient {
+    
+    String RESP_SUCCESS = "success ";
+
+    private static char[] concat(char[] A, char[] B) {
+        char[] C = new char[A.length + B.length];
+        System.arraycopy(A, 0, C, 0, A.length);
+        System.arraycopy(B, 0, C, A.length, B.length);
+        return C;
+    }    
+    
+    public static final byte[] intToByteArray(int value) {
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)value};
+    }
+
+    public static void main(String argv[]) throws Exception{
+        String output;
+        assert argv.length == 1;
+        System.out.println("Socket to "+argv[0]);
+        Socket clientSocket = new Socket("localhost", Integer.valueOf(argv[0]).intValue());
+        
+        DataOutputStream to_server = new DataOutputStream(clientSocket.getOutputStream());
+        DataInputStream from_server = new DataInputStream(clientSocket.getInputStream());
+
+        InterProcessIJBridge ijb = new InterProcessIJBridge();
+    
+        int result;
+        while(true) {
+            System.out.println("<CLIENT> reading from socket...");
+            int msg_size = from_server.readInt();
+            System.out.println("<CLIENT> message size: "+msg_size);
+    
+            byte[] cmd = new byte[8];
+            result = from_server.read(cmd, 0, 8);
+            String inp = new String(cmd);
+            System.out.println("<CLIENT> command: " + inp);
+            
+            byte[] bites = new byte[msg_size];
+            from_server.readFully(bites, 0, msg_size);
+        
+            System.out.println("<CLIENT> got " + inp);
+            
+            if (inp.startsWith("quit")){
+                to_server.write(intToByteArray(0));
+                to_server.writeBytes("success ");
+                to_server.flush();
+                clientSocket.close();
+                ijb.quit_imagej();
+                break;
+            } 
+            else if (inp.startsWith("inject")){
+                ijb.inject_image(bites);
+                to_server.write(intToByteArray(0));
+                to_server.writeBytes("success ");
+                to_server.flush();
+            }
+            else if (inp.startsWith("getimg")){
+                byte[] pixels = ijb.get_current_image();
+                // data size
+                to_server.write(intToByteArray(pixels.length + 8));
+                // message
+                to_server.writeBytes("success ");
+                // data
+                to_server.write(intToByteArray(ijb.get_current_image_width()));
+                to_server.write(intToByteArray(ijb.get_current_image_height()));
+                to_server.write(pixels);
+                to_server.flush();
+            }
+            else if (inp.startsWith("macro")){ 
+                ijb.execute_macro(new String(bites));
+                to_server.write(intToByteArray(0));
+                to_server.writeBytes("success ");
+                to_server.flush();
+            }
+            else if (inp.startsWith("command")){ 
+                // TODO: need to handle command "options"
+                ijb.execute_command(new String(bites));
+                to_server.write(intToByteArray(0));
+                to_server.writeBytes("success ");
+                to_server.flush();
+            }
+            else if (inp.startsWith("getcmds")){ 
+                Enumeration cmds = ijb.get_commands();
+                
+                String cmd_list = "";
+                while (cmds.hasMoreElements()) {
+                    String ijcmd = (String)cmds.nextElement();
+                    if (cmd_list.length() == 0) {
+                        cmd_list = cmd_list + ijcmd;
+                    } else {
+                        cmd_list = cmd_list + "\n" + ijcmd;
+                    }
+                }
+                // data size
+                to_server.write(intToByteArray(cmd_list.length()));
+                // message
+                to_server.writeBytes("success ");
+                // data
+                to_server.writeBytes(cmd_list);
+                to_server.flush();
+            }
+            else if (inp.startsWith("showij")){ 
+                ijb.show_imagej();
+            }
+        }
+    }
+}
+
+
+class InterProcessIJBridge {
+    public ImageJ ij;
+
+    public InterProcessIJBridge() {
+        ij = new ImageJ();
+    }
+
+    public void inject_image(byte[] data) {
+        System.out.println("<CLIENT> injecting");
+        ByteArrayInputStream bis = new ByteArrayInputStream(data);
+        DataInputStream in = new DataInputStream(bis);
+        try {
+            int w = 0;
+            int h = 0;
+            w = in.readInt();
+            h = in.readInt();
+            byte[] pixels = new byte[w * h];
+            System.out.println(w + " " + h);
+            in.readFully(pixels, 0, w * h);
+            in.close();
+            
+            ByteProcessor bp = new ByteProcessor(w, h, pixels, null);
+            ImagePlus imageplus = new ImagePlus("", bp);
+            imageplus.show();
+        } catch (java.io.IOException e) {
+            System.out.println(e);
+            return;
+        }
+    }
+
+    public int get_current_image_width() throws 
+        FileNotFoundException, IOException {
+        ImagePlus imageplus = WindowManager.getCurrentImage();
+        return imageplus.getWidth();
+    }
+
+    public int get_current_image_height() throws 
+        FileNotFoundException, IOException {
+        ImagePlus imageplus = WindowManager.getCurrentImage();
+        return imageplus.getHeight();
+    }
+
+    public byte[] get_current_image() throws 
+        FileNotFoundException, IOException {
+        // We currently only handle returning a single channel.
+        ImagePlus imageplus = WindowManager.getCurrentImage();
+        ImageProcessor ip = imageplus.getProcessor();
+        // ip.getpixels returns a reference to this image's pixel array. The
+        // array type (byte[], short[], float[] or int[]) depends on image type.
+        return (byte[])ip.getPixels();
+    }
+
+    public void execute_macro(String macro_text) {
+        System.err.println("executing macro "+macro_text);
+        show_imagej();
+        Interpreter interp = new Interpreter();
+        interp.run(macro_text);
+    }
+
+    public void show_imagej() {
+        ij.setVisible(true);
+        ij.toFront();
+    }
+
+    public Enumeration get_commands(){
+        Hashtable hashtable = Menus.getCommands();
+        if (hashtable == null){
+            //
+            // This is a little bogus, but works - trick IJ into initializing
+            //
+            IJ.run("pleaseignorethis");
+            hashtable = Menus.getCommands();
+            if (hashtable == null){
+                return null;
+            }
+        }
+        Enumeration keys = hashtable.keys();
+        return keys;
+    }
+
+    public void execute_command(String command){
+        IJ.run(command);
+    }
+
+    public void execute_command(String command, String options){
+        IJ.run(command, options);        
+    }
+
+    public void quit_imagej(){
+        System.out.println("<CLIENT> exiting");
+        java.lang.System.exit(0);
+        // prompts to save if images are open which we don't want
+        // ij.quit();
+    }
+
+}
+
--- imagej/ijbridge.py
+++ imagej/ijbridge.py
@@ -8,6 +8,13 @@ import os, tempfile
 import Image as PILImage
 from subprocess import Popen, PIPE, STDOUT
 import shlex
+import socket
+import cellprofiler.utilities.jutil as J
+import imagej.macros as ijmacros
+import imagej.windowmanager as ijwm
+import imagej.imageprocessor as ijiproc
+import imagej.imageplus as ijip
+import struct
 
 
 class ij_bridge(object):
@@ -15,7 +22,7 @@ class ij_bridge(object):
    CellProfiler. It is intended to abstract away whether IJ is being run within
    the same process or in a separate process.
    '''   
-   def inject_image(self, pixel_data, name=None):
+   def inject_image(self, pixels, name=None):
       '''inject an image into ImageJ for processing'''
       raise NotImplementedError
 
@@ -43,14 +50,6 @@ class ij_bridge(object):
       raise NotImplementedError
 
 
-
-
-import cellprofiler.utilities.jutil as J
-import imagej.macros as ijmacros
-import imagej.windowmanager as ijwm
-import imagej.imageprocessor as ijiproc
-import imagej.imageplus as ijip
-
 class in_proc_ij_bridge(ij_bridge):
    '''Interface for running ImageJ in a the same process as CellProfiler.
    '''
@@ -62,9 +61,9 @@ class in_proc_ij_bridge(ij_bridge):
 ##      it's scope'''
 ##      J.detach()
       
-   def inject_image(self, pixel_data, name=''):
+   def inject_image(self, pixels, name=''):
       '''inject an image into ImageJ for processing'''
-      ij_processor = ijiproc.make_image_processor(pixel_data * 255.0)
+      ij_processor = ijiproc.make_image_processor(pixels * 255.0)
       image_plus = ijip.make_imageplus_from_processor(name, ij_processor)
       if sys.platform == "darwin":
          ijwm.set_temp_current_image(image_plus)
@@ -75,8 +74,8 @@ class in_proc_ij_bridge(ij_bridge):
       '''returns the WindowManager's current image as a numpy float array'''
       image_plus = ijwm.get_current_image()
       ij_processor = image_plus.getProcessor()
-      pixel_data = ijiproc.get_image(ij_processor) / 255.0
-      return pixel_data
+      pixels = ijiproc.get_image(ij_processor) / 255.0
+      return pixels
    
    def get_commands(self):
       '''returns a list of the available command strings'''
@@ -96,104 +95,190 @@ class in_proc_ij_bridge(ij_bridge):
    def show_imagej(self):
       '''show the ImageJ user interface'''
       ijmacros.show_imagej()
-      
+   
 
+def read_nbytes(socket, nbytes):
+   '''use in place of socket.recv(nbytes)'''
+   data = socket.recv(nbytes)
+   while len(data) < nbytes:
+      data += socket.recv(nbytes - len(data))
+   return data
+      
+def communicate(socket, cmd, data=None):
+   '''Sends a command and optional data (string) to the client socket.
+   Message format: [SIZE (4 bytes) | COMMAND (8 bytes) | DATA (N bytes)]
+   Returns a tuple containing the response message and data.
+   '''
+   assert len(cmd) == 8, ('Commands must contain exactly 8 characters.'
+                         'Command was: "%s"'%(cmd))
+   # Send the message size
+   if data is None: 
+      data = ''
+   msg_size = len(data)
+   print '<SERVER> sending msg: [ %s | %s | <DATA: %s bytes> ]'%(msg_size, cmd, len(data))
+   nbytes = np.array([msg_size], ">i4").tostring()
+   # Send the number of bytes in the data
+   socket.send(nbytes)
+   # Send the 8 byte command 
+   socket.send(cmd)
+   if len(data) > 0:
+      socket.send(data)
+   
+   print '<SERVER> waiting for response...'
+   # Get the response size
+   size = read_nbytes(socket, 4)   
+   size = struct.unpack('>i4', size)[0]
+   print '<SERVER> response size:', repr(size)
+   # Get the response message
+   msg = read_nbytes(socket, 8)
+   print '<SERVER> response to %s:%s'%(cmd, msg)
+   # Get any attached data
+   data = read_nbytes(socket, size)
+   return msg, data
+   
 
 class inter_proc_ij_bridge(ij_bridge):
    '''Interface for running ImageJ in a separate process from CellProfiler.
    '''
+   # We use a limited vocabulary of command names to talk to ImageJ.
+   # All command names must contain exactly 8 characters and must be changed in
+   # TCPClient.py if they are changed here.
+   INJECT       = 'inject  '
+   GET_IMAGE    = 'getimg  '
+   GET_COMMANDS = 'getcmds '
+   COMMAND      = 'command '
+   MACRO        = 'macro   '
+   SHOW_IMAGEJ  = 'showij  '
+   QUIT         = 'quit    '
+   
    def __init__(self):
-      os.environ['CLASSPATH'] = '/Users/afraser/CellProfiler/:'+os.environ['CLASSPATH']
-      self.ijproc = Popen(shlex.split('java Adam'), stdin=PIPE, stdout=PIPE,
-                          stderr=None)
+      os.environ['CLASSPATH'] = '%s:%s'%(os.path.split(__file__)[0], 
+                                         os.environ['CLASSPATH'])
+      self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.server_socket.bind(("", 0))
+      hostaddr, port = self.server_socket.getsockname()
+      self.server_socket.listen(5)
+      print "ImageJ bridge TCPServer waiting for client on port", port
+      self.ijproc = Popen(shlex.split('java -Xmx512m TCPClient %s'%(port)),
+                          stdin=None, stdout=None, stderr=None)
+      self.client_socket, address = self.server_socket.accept()
+      print "ImageJ bridge got a connection from", address
    
-##   def __del__(self):
-##      os.kill(self.ijproc.pid)
-   
-   def inject_image(self, pixel_data, name=None):
+   def inject_image(self, pixels, name=None):
       '''inject an image into ImageJ for processing'''
-      tmpdir = tempfile.mkdtemp()
-      filename = os.path.join(tmpdir, 'cp_image')
-
-##      print 'INJECT_IMAGE: writing image to',filename
-      
-      if pixel_data.ndim == 3 and pixel_data.shape[2] == 4:
-         mode = 'RGBA'
-      elif pixel_data.ndim == 3:
-         mode = 'RGB'
-      else:
-         mode = 'L'
-      pil = PILImage.fromarray(pixel_data, mode)
-      pil.save(filename, 'png')
-      
-      filename = '/Users/afraser/cpa_example/images/AS_09125_050116000001_A01f00d0.png'
-      
-      print communicate(self.ijproc, 'inject %s'%(filename))
-      
+      data = (np.array([pixels.shape[1]], ">i4").tostring() + 
+              np.array([pixels.shape[0]], ">i4").tostring() + 
+              (pixels*255).astype('uint8').tostring())
+      msg, data = communicate(self.client_socket, self.INJECT, data)
+      assert msg.startswith('success')
 
    def get_current_image(self):
       '''returns the WindowManager's current image as a numpy float array'''
-      tmpdir = tempfile.mkdtemp()
-      filename = os.path.join(tmpdir, 'ij_image.png')
-      
-      print communicate('get %s'%(filename))
-##      output = 'wrote file /Users/afraser/cpa_example/images/AS_09125_050116000001_A01f00d0.png'
-      if output.startswith('wrote file '):
-         filename = output.strip('wrote file ')
-         pil = PILImage.open(filename, 'r')
-         pil.show()
-##         pixels = np.frombuffer(pil.tostring())
-##         pixels.reshape(
+      msg, data = communicate(self.client_socket, self.GET_IMAGE)
+      w = struct.unpack('>i4',data[:4])[0]
+      h = struct.unpack('>i4',data[4:8])[0]
+      pixels = data[8:]
+      if msg.startswith('success'):
+         im = PILImage.fromstring('L', (w,h), pixels)
+##         im.show()
+         return pil_to_np(im)
+      else:
+         raise Exception("Get current image failed to return an image")
 
    def get_commands(self):
       '''returns a list of the available command strings'''
-      self.ijproc.stdin.write('get_commands\n')
-      self.ijproc.stdin.flush()
-      print self.ijproc.stdout.readlines()
+      msg, data = communicate(self.client_socket, self.GET_COMMANDS)
+      return data.split('\n')
 
    def execute_command(self, command, options=None):
       '''execute the named command within ImageJ'''
-      self.ijproc.stdin.write('command %s\n'%(command))
-      self.ijproc.stdin.flush()
+      msg, data = communicate(self.client_socket, self.COMMAND, command)
+      assert msg.startswith('success')
 
    def execute_macro(self, macro_text):
       '''execute a macro in ImageJ
-    
       macro_text - the macro program to be run
       '''
-      self.ijproc.stdin.write('macro %s\n'%(macro_text))
-      self.ijproc.stdin.flush()
+      msg, data = communicate(self.client_socket, self.MACRO, macro_text)
+      assert msg.startswith('success')
    
    def show_imagej(self):
       '''show the ImageJ user interface'''
-      self.ijproc.stdin.write('show_imagej\n')
-      self.ijproc.stdin.flush()
+      msg, data = communicate(self.client_socket, self.SHOW_IMAGEJ)
+      assert msg.startswith('success')
       
    def quit(self):
-      self.ijproc.stdin.write('quit\n')
-      self.ijproc.stdin.flush()
-      
-      
-def communicate(proc, inp):
-   '''
-   '''
-   proc.stdin.write(inp+'\n')
-   proc.stdin.flush()
-   return proc.stdout.readline()
+      print '<SERVER> quit'
+      msg, data = communicate(self.client_socket, self.QUIT)
+      self.client_socket.close()
+      self.server_socket.close()
+      assert msg.startswith('success')
+##      os.kill(self.ijproc.pid, 9)
       
 
+def np2pil(imdata):
+   '''Convert np image data to PIL Image'''
+   if len(imdata.shape) == 2:
+      buf = np.dstack([imdata, imdata, imdata])
+   elif len(imdata.shape) == 3:
+      buf = imdata
+      assert imdata.shape[2] >= 3, 'Cannot convert the given numpy array to PIL'
+   if buf.dtype != 'uint8':
+      buf = (buf * 255.0).astype('uint8')
+   im = PILImage.fromstring(mode='RGB', size=(buf.shape[1],buf.shape[0]),
+                            data=buf.tostring())
+   return im
+ 
+def pil_to_np( pilImage ):
+   """
+   load a PIL image and return it as a numpy array of uint8.  For
+   grayscale images, the return array is MxN.  For RGB images, the
+   return value is MxNx3.  For RGBA images the return value is MxNx4
+   """
+   def toarray(im):
+      'return a 1D array of floats'
+      x_str = im.tostring('raw', im.mode)
+      x = np.fromstring(x_str,np.uint8)
+      return x
+   
+   if pilImage.mode[0] == 'P':
+      im = pilImage.convert('RGBA')
+      x = toarray(im)
+      x = x.reshape(-1, 4)
+      if np.all(x[:,0] == x):
+         im = pilImage.convert('L')
+      pilImage = im
+
+      if pilImage.mode[0] in ('1', 'L', 'I', 'F'):
+         x = toarray(pilImage)
+         x.shape = pilImage.size[1], -1
+         return x
+      else:
+         x = toarray(pilImage.convert('RGBA'))
+         x.shape = pilImage.size[1], pilImage.size[0], 4
+         # discard alpha if all 1s
+         if (x[:,:,3] == 255).all():
+            return x[:,:,:3]
+         return x
+
+     
 if __name__ == '__main__':
+   from time import time
    ipb = inter_proc_ij_bridge()
 
-   pixels = (np.ones((100,100)) * 255.).astype(np.uint8)
-   ipb.inject_image(pixels, 'name ignored')
-
-##   ipb.execute_macro('run("Invert");')
-##
-##   ipb.get_current_image()
-##   
-##   ipb.get_commands()
-##
-##   ipb.quit()
-##   sys.exit()
+   pixels = np.random.standard_normal((400,600))
+   pixels[100:300,200:400] = 0
    
+   t0 = time()
+   ipb.inject_image(pixels, 'name ignored')
+   print 'time to inject:',time()-t0
+   ipb.get_current_image()
+   ipb.execute_macro('run("Invert");')
+   ipb.execute_command('Add Noise')
+   t0 = time()
+   ipb.get_current_image()
+   print 'time to get image:',time()-t0
+   cmds = ipb.get_commands()
+   print 'ImageJ commands:', cmds
+   ipb.quit()
+   J.kill_vm()

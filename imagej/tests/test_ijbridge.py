f526960ed68d0f3b853999764ef5bceb9881e793
afraser <afraser@1fc53939-2000-0410-845c-e8453a809027>
2010-09-15 17:57:19 +0000

    test the imagej bridge (incomplete)
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10481 1fc53939-2000-0410-845c-e8453a809027

---
+++ imagej/tests/test_ijbridge.py
@@ -0,0 +1,53 @@
+'''test_ijbridge - test ijbridge.py
+
+'''
+# CellProfiler is distributed under the GNU General Public License.
+# See the accompanying file LICENSE for details.
+#
+# Developed by the Broad Institute
+# Copyright 2003-2010
+# 
+# Please see the AUTHORS file for credits.
+#
+# Website: http://www.cellprofiler.org
+
+__version__ = "$Revision$"
+
+import os
+import unittest
+
+import cellprofiler.utilities.jutil as J
+from imagej.ijbridge import *
+
+
+class TestIJBridge(unittest.TestCase):
+   def test_01_01_load_imageplus(self):
+      ij1 = in_proc_ij_bridge()
+      ij2 = inter_proc_ij_bridge()
+      pixels = np.random.standard_normal((400,600))
+      pixels[100:300,200:400] = 0
+
+      ij1.inject_image(pixels, 'name ignored')
+      ij2.inject_image(pixels, 'name ignored')
+
+      im1 = ij1.get_current_image()
+      im2 = ij2.get_current_image()
+      assert im1 == im2
+      
+      ij1.execute_macro('run("Invert");')
+      ij2.execute_macro('run("Invert");')
+
+      im1 = ij1.get_current_image()
+      im2 = ij2.get_current_image()
+      assert im1 == im2
+
+      ij1.execute_command('Add Noise')
+      ij2.execute_command('Add Noise')
+
+      im1 = ij1.get_current_image()
+      im2 = ij2.get_current_image()
+      assert im1 == im2
+
+      cmds1 = ij1.get_commands()
+      cmds2 = ij2.get_commands()
+      assert cmds1 == cmds2

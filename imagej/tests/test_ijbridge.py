505476577fa38f96662fa6a753d5b83fa635f809
leek <leek@1fc53939-2000-0410-845c-e8453a809027>
2010-09-28 20:33:37 +0000

    Run inter-process tests on Mac only - they stop all unit testing on my PC
    git-svn-id: https://svn.broadinstitute.org/CellProfiler/trunk/CellProfiler@10542 1fc53939-2000-0410-845c-e8453a809027

--- imagej/tests/test_ijbridge.py
+++ imagej/tests/test_ijbridge.py
@@ -88,41 +88,40 @@ class TestIJBridge(unittest.TestCase):
       ijb =  in_proc_ij_bridge.getInstance()
       cmds = ijb.get_commands()
       assert set(cmds).issuperset(set(IJ_CMDS))
-
    #
-   # inter-proc (TCP) tests
+   # inter-proc (TCP) tests - Mac only
    #
+   if sys.platform == 'darwin':
+      def test_02_01_ipc_inject_and_get(self):
+         ijb = inter_proc_ij_bridge.getInstance()
+         ijb.inject_image(PIXELS, 'name ignored')
+         im = ijb.get_current_image()
+         np.testing.assert_array_almost_equal(im, PIXELS)
       
-   def test_02_01_ipc_inject_and_get(self):
-      ijb = inter_proc_ij_bridge.getInstance()
-      ijb.inject_image(PIXELS, 'name ignored')
-      im = ijb.get_current_image()
-      np.testing.assert_array_almost_equal(im, PIXELS)
-
-   def test_02_02_ipc_invert_macro(self):
-      ijb = inter_proc_ij_bridge.getInstance()
-      ijb.inject_image(PIXELS, 'name ignored')
-      ijb.execute_macro('run("Invert");')
-      im = ijb.get_current_image()
-      assert im.min() >= 0.0
-      assert im.max() <= 1.0
-      np.testing.assert_array_almost_equal(im, INVERTED)
-
-   def test_02_03_ipc_command(self):
-      ijb = inter_proc_ij_bridge.getInstance()
-      ijb.inject_image(PIXELS, 'name ignored')
-      ijb.execute_command('Smooth')
-      im = ijb.get_current_image()
-      assert (im != PIXELS).any()
-      assert im.min() >= 0.0
-      assert im.max() <= 1.0
-
-   def test_02_04_ipc_get_commands(self):
-      ijb =  inter_proc_ij_bridge.getInstance()
-      cmds = ijb.get_commands()
-      assert set(cmds) == set(IJ_CMDS)
-
-   def test_02_05_quit_ij(self):
-      ijb = inter_proc_ij_bridge.getInstance()
-      ijb.quit()
-
+      def test_02_02_ipc_invert_macro(self):
+         ijb = inter_proc_ij_bridge.getInstance()
+         ijb.inject_image(PIXELS, 'name ignored')
+         ijb.execute_macro('run("Invert");')
+         im = ijb.get_current_image()
+         assert im.min() >= 0.0
+         assert im.max() <= 1.0
+         np.testing.assert_array_almost_equal(im, INVERTED)
+      
+      def test_02_03_ipc_command(self):
+         ijb = inter_proc_ij_bridge.getInstance()
+         ijb.inject_image(PIXELS, 'name ignored')
+         ijb.execute_command('Smooth')
+         im = ijb.get_current_image()
+         assert (im != PIXELS).any()
+         assert im.min() >= 0.0
+         assert im.max() <= 1.0
+      
+      def test_02_04_ipc_get_commands(self):
+         ijb =  inter_proc_ij_bridge.getInstance()
+         cmds = ijb.get_commands()
+         assert set(cmds).issuperset(set(IJ_CMDS))
+   
+      def test_02_05_quit_ij(self):
+         ijb = inter_proc_ij_bridge.getInstance()
+         ijb.quit()
+      

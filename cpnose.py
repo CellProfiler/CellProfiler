db301b601be3cf7f8f792cca41d40e9e52df79f2
Thouis (Ray) Jones <thouis@gmail.com>
2011-06-09 15:47:05 +0200

    make numpy errors quiet, take all of the args, and add a cheap hack to make gui tests fail quickly

--- cpnose.py
+++ cpnose.py
@@ -3,10 +3,19 @@ import sys
 
 from killjavabridge import KillVMPlugin
 
+import numpy as np
+np.seterr(all='ignore')
+
+if '--noguitests' in sys.argv:
+    sys.argv.remove('--noguitests')
+    import wx
+    for s in dir(wx):
+        del wx.__dict__[s]
+
 if len(sys.argv) == 0:
     args = ['--testmatch=(?:^)test_.*']
 else:
-    args = sys.argv[1:]
+    args = sys.argv
 
 nose.main(argv=args + ['--with-kill-vm', '--exe'],
           addplugins=[KillVMPlugin()])

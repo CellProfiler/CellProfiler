bbca6072092d3f4e3b3f1834fb700484d92ddb09
Thouis (Ray) Jones <thouis@gmail.com>
2011-07-04 13:06:29 +0200

    typo

--- cellprofiler/measurements.py
+++ cellprofiler/measurements.py
@@ -3,7 +3,7 @@
 CellProfiler is distributed under the GNU General Public License.
 See the accompanying file LICENSE for details.
 
-Copyright (c) 2003-2009 Massachusetts nstitute of Technology
+Copyright (c) 2003-2009 Massachusetts Institute of Technology
 Copyright (c) 2009-2011 Broad Institute
 All rights reserved.
 

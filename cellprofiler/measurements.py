baea664fb9bcf019d011323b4bb2abb01940f8a0
Jacob Silterra <jsilter@gmail.com>
2011-07-17 19:11:36 -0400

    Added checking code to deal with empty arrays being stored in IMAGE and EXPERIMENT fields

--- cellprofiler/measurements.py
+++ cellprofiler/measurements.py
@@ -450,7 +450,7 @@ class Measurements(object):
                 self.hdf5_dict[IMAGE, 'ImageNumber', image_set_number] = image_set_number
         
         if object_name == EXPERIMENT or object_name == IMAGE:
-            if not np.isscalar(data) and data is not None:
+            if not np.isscalar(data) and data is not None and len(data) > 0:
                 data = data[0]
             if data is None:
                 data = []
@@ -508,10 +508,17 @@ class Measurements(object):
             if isinstance(v, str):
                 return unicode(str(v)).decode('unicode_escape')
             return v
+        
+        def helper(data):
+            if len(data) > 0:
+                return data[0]
+            else:
+                return np.array([])
+            
         if object_name == EXPERIMENT:
-            return unwrap_string(self.hdf5_dict[EXPERIMENT, feature_name, 0][0])
+            return unwrap_string(helper(self.hdf5_dict[EXPERIMENT, feature_name, 0]))
         if object_name == IMAGE:
-            return unwrap_string(self.hdf5_dict[IMAGE, feature_name, image_set_number][0])
+            return unwrap_string(helper(self.hdf5_dict[IMAGE, feature_name, image_set_number]))
         vals = self.hdf5_dict[object_name, feature_name, image_set_number]
         if vals is None:
             return np.array([])

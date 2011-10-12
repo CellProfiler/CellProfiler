Clumsy fix for scipy bug - standard deviation code in ndimage flattens labels then does array broadcasting to unflattened input.
git-svn-id: https://svn.broadinstitute.org/CellProfiler/branches/release_10953/CellProfiler@11590 1fc53939-2000-0410-845c-e8453a809027

cellprofiler/modules/straightenworms.py
@@ -624,7 +624,7 @@ class StraightenWorms(cpm.CPModule):
             straightened_image = image_set.get_image(image_name).pixel_data
             indices = np.arange(nbins, nbins * (nworms + 1))
             bin_means = nd_mean(straightened_image, mbin, indices)
-            bin_stds = nd_standard_deviation(straightened_image, mbin, indices)
+            bin_stds = nd_standard_deviation(straightened_image.flatten(), mbin.flatten(), indices)
             bin_means.shape = (nworms, nbins)
             bin_stds.shape = (nworms, nbins)
             for b in range(nbins):
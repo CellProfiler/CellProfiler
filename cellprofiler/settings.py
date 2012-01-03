771cf9eead3e9db4c662dee440ade2a8edf5c26e
Lee Kamentsky <leek@broadinstitute.org>
2012-01-03 14:59:31 -0500

    Added filter setting + GUI + tests

--- cellprofiler/settings.py
+++ cellprofiler/settings.py
@@ -1952,6 +1952,231 @@ class Color(Setting):
     def __init(self, text, value="gray", *args, **kwargs):
         super(Color, self).__init(text, value, *args, **kwargs)
         
+class Filter(Setting):
+    '''A filter that can be applied to an object
+    
+    A filter returns a value when applied to an object such as a string
+    which is evaluated as either True (accept it) or False (reject it).
+    
+    The setting value is composed of tokens with a scheme-like syntax:
+    
+    (and (filename contains "_w1_") (extension is "tif"))
+    
+    Each predicate has a symbolic name which is used to find it. The predicate
+    has an evaluation function and a display name. Predicates also have lists
+    of the predicates that they operate on. The leftmost predicate takes two
+    arguments. Other predicates, it is up to the developer. Predicates
+    are called with the object of interest as the first argument and the
+    evaluation value of the predicate to the right as the second argument.
+    
+    For something like "filename contains "foo"", "contains" returns a function
+    that returns true if the first argument is "foo" and "filename" parses
+    the first of its arguments to get the filename and returns the result of 
+    applying the result of "contains" to the filename.
+    
+    There are three special predicates:
+    "and", "or" and "literal".
+    '''
+    class FilterPredicate(object):
+        def __init__(self, symbol, display_name, function, subpredicates):
+            self.symbol = symbol
+            self.display_name = display_name
+            self.function = function
+            self.subpredicates = subpredicates
+        
+        def __call__(self, *args, **kwargs):
+            return self.function(*args, **kwargs)
+    
+    @classmethod
+    def eval_list(cls, x, *args):
+        return [arg[0](x, *arg[1:]) for arg in args]
+    AND_PREDICATE = FilterPredicate(
+        "and", "All", 
+        lambda x, *l: all(Filter.eval_list(x, *l)), list)
+    OR_PREDICATE = FilterPredicate(
+        "or", "Any", 
+        lambda x, *l: any(Filter.eval_list(x, *l)), list)
+    LITERAL_PREDICATE = FilterPredicate("literal", "Custom value", None, [])
+    CONTAINS_PREDICATE = FilterPredicate(
+        "contain", "Contain",
+        lambda x, y: x.find(y) >= 0, [LITERAL_PREDICATE])
+    STARTS_WITH_PREDICATE = FilterPredicate(
+        "startwith", "Start with",
+       lambda x, y: x.startswith(y), [LITERAL_PREDICATE])
+    ENDSWITH_PREDICATE = FilterPredicate(
+        "endwith", "End with",
+        lambda x, y: x.endswith(y), [LITERAL_PREDICATE])
+    CONTAINS_REGEXP_PREDICATE = FilterPredicate(
+        "containregexp", "Contain regular expression", 
+        lambda x,y: re.match(y, x) is not None,
+        [LITERAL_PREDICATE])
+    EQ_PREDICATE = FilterPredicate(
+        "eq", "Exactly match", lambda x,y: x == y, [LITERAL_PREDICATE])
+    
+    class DoesPredicate(FilterPredicate):
+        '''Pass the arguments through (no-op)'''
+        def __init__(self, subpredicates):
+            super(self.__class__, self).__init__(
+                "does", "Does",
+                lambda x, f, *l: f(x, *l), subpredicates)
+            
+    class DoesNotPredicate(FilterPredicate):
+        '''Negate the result of the arguments'''
+        def __init__(self, subpredicates):
+            super(self.__class__, self).__init__(
+                "doesnot", "Does not",
+                lambda x, f, *l: not f(x, *l), subpredicates)
+        
+    def __init__(self, text, predicates, value = "", **kwargs):
+        super(self.__class__, self).__init__(text, value, **kwargs)
+        self.predicates = predicates
+        
+    def evaluate(self, x):
+        '''Evaluate the value passed using the predicates'''
+        tokens = self.parse()
+        return tokens[0](x, *tokens[1:])
+    
+    def parse(self):
+        '''Parse the value into filter predicates, literals and lists
+        
+        Returns the value of the text as a list.
+        '''
+        tokens = []
+        s = self.value_text
+        predicates = self.predicates
+        while len(s) > 0:
+            token, s, predicates = self.parse_token(s, predicates)
+            tokens.append(token)
+        return tokens
+    
+    def default(self):
+        '''A default list of tokens to use if things go horribly wrong
+        
+        We need to be able to generate a default list of tokens if the
+        pipeline has been corrupted and the text can't be parsed.
+        '''
+        tokens = []
+        predicates = self.predicates
+        while len(predicates) > 0:
+            token = predicates[0]
+            if token is self.LITERAL_PREDICATE:
+                tokens.append("")
+                predicates = self.LITERAL_PREDICATE.subpredicates
+            else:
+                tokens.append(token)
+                predicates = token.subpredicates
+        return tokens
+    
+    @classmethod
+    def parse_token(cls, s, predicates):
+        '''Parse a token out of the front of the string
+        
+        Returns the next token in the string, the rest of the string
+        and the acceptable tokens for the rest of the string.
+        '''
+        orig_predicates = predicates
+        if list in predicates:
+            needs_list = True
+            predicates = list(predicates)
+            predicates.remove(list)
+        else:
+            needs_list = False
+        if s[0] == "(":
+            if not needs_list:
+                raise ValueError("List not allowed in current context")
+            s = s[1:]
+            result = []
+            while s[0] != ")":
+                token, s, predicates = cls.parse_token(s, predicates)
+                result.append(token)
+            if len(s) > 1 and s[1] == ' ':
+                return result, s[2:], orig_predicates
+            return result, s[1:], orig_predicates
+        elif needs_list:
+            raise ValueError("List required in current context")
+        if s[0] == "\"":
+            if cls.LITERAL_PREDICATE not in predicates:
+                raise ValueError("Literal not allowed in current context")
+            escape_next = False
+            result = ""
+            for i in range(1, len(s)):
+                if escape_next:
+                    result += s[i]
+                    escape_next = False
+                elif s[i] == "\\":
+                    escape_next = True
+                elif s[i] == "\"":
+                    return result, s[(i+1):], []
+                else:
+                    result += s[i]
+            raise ValueError("Unterminated literal")
+        parts = s.split(" ", 1)
+        if len(parts) == 1:
+            kwd, rest = s, ""
+        else:
+            kwd, rest = parts
+        if kwd == cls.AND_PREDICATE.symbol:
+            match = cls.AND_PREDICATE
+        elif kwd == cls.OR_PREDICATE.symbol:
+            match = cls.OR_PREDICATE
+        else:
+            matches = [x for x in predicates 
+                       if x is not list and x.symbol == kwd]
+            if len(matches) == 0:
+                raise ValueError('The filter predicate, "%s", was not in the list of allowed predicates ("%s")' % 
+                                 (kwd,'","'.join([x.symbol for x in predicates])))
+            match = matches[0]
+        if match.subpredicates is list:
+            predicates = [list] + predicates
+        elif match.subpredicates is not None:
+            predicates = match.subpredicates
+        return match, rest, predicates
+    
+    def build(self, structure):
+        '''Build the textual representation of a filter from its structure
+        
+        structure: the processing structure, represented using a nested list.
+        
+        The top layer of the list corresponds to the tokens in the value
+        string. For instance, a list of [foo, bar, baz] where foo, bar and baz
+        are filter predicates that have symbolic names of "foo", "bar" and "baz"
+        will yield the string, "foo bar baz". The list [foo, bar, "baz"] will
+        treat "baz" as a literal and yield the string, 'foo bar "baz"'.
+        
+        Nesting can be done using nested lists. For instance,
+        
+        [or [eq "Hello"] [eq "World"]]
+        
+        becomes
+        
+        "or (eq "Hello")(eq "World")"
+        
+        The function sets the filter's value using the generated string.
+        '''
+        self.text = self.build_string(structure)
+        
+    @classmethod
+    def build_string(cls, structure):
+        '''Return the text representation of structure
+         
+        This is a helper function for self.build. See self.build's 
+        documentation.
+        '''
+        s = []
+        for element in structure:
+            if isinstance(element, Filter.FilterPredicate):
+                s.append(unicode(element.symbol))
+            elif isinstance(element, basestring):
+                s.append(u'"'+element+u'"')
+            else:
+                s.append(u"("+cls.build_string(element)+")")
+        return u" ".join(s)
+
+    def test_valid(self, pipeline):
+        try:
+            tokens = self.parse()
+        except ValueError:
+            raise ValidationError("Invalid filter expression: %s" % self.value_text, self)
 
 class SettingsGroup(object):
     '''A group of settings that are managed together in the UI.
---
+++ cellprofiler/tests/test_settings.py
@@ -0,0 +1,94 @@
+'''test_settings.py - test the settings classes'''
+# CellProfiler is distributed under the GNU General Public License.
+# See the accompanying file LICENSE for details.
+# 
+# Developed by the Broad Institute
+# Copyright 2003-2011
+# 
+# Please see the AUTHORS file for credits.
+# 
+# Website: http://www.cellprofiler.org
+
+import unittest
+
+import cellprofiler.settings as cps
+
+class TestFilterSetting(unittest.TestCase):
+    def test_01_01_simple(self):
+        filters = [ cps.Filter.FilterPredicate("foo", "Foo", lambda a: a=="x", [])]
+        f = cps.Filter("", filters, "foo")
+        self.assertTrue(f.evaluate("x"))
+        self.assertFalse(f.evaluate("y"))
+        
+    def test_01_02_compound(self):
+        f2 = cps.Filter.FilterPredicate("bar", "Bar", lambda: "y", [])
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a, b: a==b(), [f2])
+        f = cps.Filter("", [f1], "foo bar")
+        self.assertFalse(f.evaluate("x"))
+        self.assertTrue(f.evaluate("y"))
+        
+    def test_01_03_literal(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a, b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1], 'foo "x"')
+        self.assertTrue(f.evaluate("x"))
+        self.assertFalse(f.evaluate("y"))
+        f = cps.Filter("", [f1], 'foo "y"')
+        self.assertTrue(f.evaluate("y"))
+        self.assertFalse(f.evaluate("x"))
+        
+    def test_01_04_escaped_literal(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a, b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1], 'foo "\\\\"')
+        self.assertTrue(f.evaluate("\\"))
+        self.assertFalse(f.evaluate("/"))
+        
+    def test_01_05_literal_with_quote(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a, b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1], 'foo "\\""')
+        self.assertTrue(f.evaluate("\""))
+        self.assertFalse(f.evaluate("/"))
+        
+    def test_01_06_parentheses(self):
+        f1 = cps.Filter.FilterPredicate("eq", "Foo", lambda a, b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f2 = cps.Filter.FilterPredicate("ne", "Bar", lambda a, b: a!=b,
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1,f2], 'and (eq "x") (ne "y")')
+        self.assertTrue(f.evaluate("x"))
+        self.assertFalse(f.evaluate("y"))
+        self.assertFalse(f.evaluate("z"))
+        
+    def test_01_07_or(self):
+        f1 = cps.Filter.FilterPredicate("eq", "Foo", lambda a, b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        
+        f = cps.Filter("", [f1], 'or (eq "x") (eq "y")')
+        self.assertTrue(f.evaluate("x"))
+        self.assertTrue(f.evaluate("y"))
+        self.assertFalse(f.evaluate("z"))
+        
+    def test_02_01_build_one(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a: a=="foo", [])
+        f = cps.Filter("", [f1])
+        f.build([f1])
+        self.assertEqual(f.text, "foo")
+        
+    def test_02_02_build_literal(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a,b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1])
+        f.build([f1, "bar"])
+        self.assertEqual(f.text, 'foo "bar"')
+        
+    def test_02_03_build_nested(self):
+        f1 = cps.Filter.FilterPredicate("foo", "Foo", lambda a,b: a==b, 
+                                        [cps.Filter.LITERAL_PREDICATE])
+        f = cps.Filter("", [f1])
+        f.build([cps.Filter.OR_PREDICATE, [f1, "bar"], [f1, u"baz"]])
+        self.assertEqual(f.text, 'or (foo "bar") (foo "baz")')
+        
+
+        

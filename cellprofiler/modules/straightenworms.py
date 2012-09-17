561ee4157dd16ad7078ee906731ebcf4cbb82b49
Lee Kamentsky <leek@broadinstitute.org>
2012-09-17 16:47:25 -0400

    Issue #400 - training file cache was broken and is now fixed

--- cellprofiler/modules/straightenworms.py
+++ cellprofiler/modules/straightenworms.py
@@ -476,9 +476,9 @@ class StraightenWorms(cpm.CPModule):
     def read_params(self, workspace):
         '''Read the training params or use the cached value'''
         d = self.get_dictionary(workspace.image_set_list)
-        if "training_params" in d:
-            return d["training_params"]
-        params = d["training_params"] = {}
+        if "training_params" not in d:
+            d["training_params"] = {}
+        params = d["training_params"]
         params = read_params(self.training_set_directory,
                              self.training_set_file_name,
                              params)

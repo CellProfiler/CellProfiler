1d6bcfdc87bf695dacb544430f918cba355c7aa1
Lee Kamentsky <leek@broadinstitute.org>
2013-01-23 16:16:32 -0500

    Java code copyrights

--- java/src/main/java/org/cellprofiler/imageset/filter/AbstractURLPredicateBase.java
+++ java/src/main/java/org/cellprofiler/imageset/filter/AbstractURLPredicateBase.java
@@ -1,3 +1,15 @@
+/**
+ * CellProfiler is distributed under the GNU General Public License.
+ * See the accompanying file LICENSE for details.
+ *
+ * Copyright (c) 2003-2009 Massachusetts Institute of Technology
+ * Copyright (c) 2009-2013 Broad Institute
+ * All rights reserved.
+ * 
+ * Please see the AUTHORS file for credits.
+ * 
+ * Website: http://www.cellprofiler.org
+ */
 package org.cellprofiler.imageset.filter;
 
 import java.util.List;

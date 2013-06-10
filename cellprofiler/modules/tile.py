25b5224ce26062cacb4a6008c727b7d10475ad44
Lee Kamentsky <leek@broadinstitute.org>
2013-06-10 14:25:16 -0400

    issue #663 fixed

--- cellprofiler/modules/tile.py
+++ cellprofiler/modules/tile.py
@@ -257,7 +257,6 @@ class Tile(cpm.CPModule):
             
     def post_group(self, workspace, grouping):
         image_set = workspace.image_set
-        assert isinstance(image_set, cpi.ImageSet)
         if self.output_image.value not in image_set.get_names():
             d = self.get_dictionary(workspace.image_set_list)
             image_set.add(self.output_image.value, 

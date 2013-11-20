c98abe8b3839677ae2837769fa624c3d13111a3e
Lee Kamentsky <leek@broadinstitute.org>
2013-11-20 16:47:00 -0500

    issue #903 fixed

--- cellprofiler/settings.py
+++ cellprofiler/settings.py
@@ -1240,6 +1240,7 @@ class ObjectNameProvider(NameProvider):
     def test_valid(self, pipeline):
         if self.value_text in cellprofiler.measurements.disallowed_object_names:
             raise ValidationError("Object names may not be any of %s" % (", ".join(cellprofiler.measurements.disallowed_object_names)), self)
+        super(ObjectNameProvider, self).test_valid(pipeline)
 
 
 class OutlineNameProvider(ImageNameProvider):

88fa8405f34b999a76920592aec46e5a789be45e
Lee Kamentsky <leek@broadinstitute.org>
2014-01-24 09:49:53 -0500

    issue #1027 made tile an aggregation module

--- cellprofiler/modules/tile.py
+++ cellprofiler/modules/tile.py
@@ -267,11 +267,16 @@ class Tile(cpm.CPModule):
             workspace.display_data.image = output_pixels
             
     def post_group(self, workspace, grouping):
-        image_set = workspace.image_set
-        if self.output_image.value not in image_set.get_names():
-            d = self.get_dictionary(workspace.image_set_list)
-            image_set.add(self.output_image.value, 
-                          cpi.Image(d[TILED_IMAGE]))
+        if self.tile_method == T_ACROSS_CYCLES:
+            image_set = workspace.image_set
+            if self.output_image.value not in image_set.get_names():
+                d = self.get_dictionary(workspace.image_set_list)
+                image_set.add(self.output_image.value, 
+                              cpi.Image(d[TILED_IMAGE]))
+    
+    def is_aggregation_module(self):
+        '''Need to run all cycles in same worker if across cycles'''
+        return self.tile_method == T_ACROSS_CYCLES
     
     def display(self, workspace, figure):
         '''Display 

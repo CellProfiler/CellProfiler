f5685aefd77da7a20263e64c81c2f7710d6cc467
Lee Kamentsky <leek@broadinstitute.org>
2014-02-28 13:48:09 -0500

    issue #37 use HDF5Dict directly to speed things up

--- BatchProfiler/GetPipeline.py
+++ BatchProfiler/GetPipeline.py
@@ -17,29 +17,27 @@ Website: http://www.cellprofiler.org
 import cgitb
 cgitb.enable()
 import cgi
-import cellprofiler.preferences
-cellprofiler.preferences.set_headless()
 
 import os, sys
 import RunBatch
-from cellprofiler.measurements import Measurements, EXPERIMENT
-from cellprofiler.pipeline import M_PIPELINE
-try:
-    from cellprofiler.pipeline import M_USER_PIPELINE
-except:
-    M_USER_PIPELINE = "Pipeline_UserPipeline"
-from cellprofiler.modules.createbatchfiles import F_BATCH_DATA_H5
+from cellprofiler.utilities.hdf5_dict import HDF5Dict
+M_USER_PIPELINE = "Pipeline_UserPipeline"
+M_PIPELINE = "Pipeline_Pipeline"
+F_BATCH_DATA_H5 = "Batch_data.h5"
+EXPERIMENT = "Experiment"
 
 form = cgi.FieldStorage()
 batch_id = int(form["batch_id"].value)
 my_batch = RunBatch.LoadBatch(batch_id)
 path = os.path.join(my_batch["data_dir"], F_BATCH_DATA_H5)
-m = Measurements(filename=path, mode="r")
-if m.has_feature(EXPERIMENT, M_USER_PIPELINE):
+h = HDF5Dict(path, mode="r")
+if M_USER_PIPELINE in h.second_level_names(EXPERIMENT):
     feature = M_USER_PIPELINE
 else:
     feature = M_PIPELINE
-pipeline_text = m[EXPERIMENT, feature]
+pipeline_text = h[EXPERIMENT, feature, 0][0].decode("unicode_escape")
+h.close()
+del h
 if isinstance(pipeline_text, unicode):
     pipeline_text = pipeline_text.encode("us-ascii")
 print "Content-Type: text/plain"
@@ -47,4 +45,6 @@ print "Content-Length: %d" % len(pipeline_text)
 print "Content-Disposition: attachment; filename=\"%d.cppipe\"" % batch_id
 print
 sys.stdout.write(pipeline_text)
+sys.stdout.flush()
+os._exit(0)
 

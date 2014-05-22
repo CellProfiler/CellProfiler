09f3930831f39f451deba481f9e1cdcbaf195262
Lee Kamentsky <leek@broadinstitute.org>
2014-05-22 16:06:29 -0400

    One last tweak - don't run the examples tests

--- jenkins/mac/jenkins_build.sh
+++ jenkins/mac/jenkins_build.sh
@@ -57,7 +57,7 @@ svn --trust-server-cert --non-interactive co https://svn.broadinstitute.org/Cell
 export CP_EXAMPLEIMAGES=$(pwd)/ExampleImages
 export CP_TESTIMAGES=$(pwd)/TestImages
 set +e
-arch -i386 python cpnose.py --with-kill-vm --noguitests --with-xunit
+arch -i386 python cpnose.py --with-kill-vm --noguitests --with-xunit -m \(\?\:\^\)test_\(\?\!example\).\*
 set -e
 arch -i386 python setup.py py2app
 

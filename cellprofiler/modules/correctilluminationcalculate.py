50f6ac4b3660057fcce9dcb5c6fa36903102d201
Lee Kamentsky <leek@broadinstitute.org>
2014-06-13 08:56:40 -0400

    issue #1148 clear the image cache after each image set + some code to keep the second in a list of CorrectIlluminationCalculate modules from running the first as part of what it does.

--- cellprofiler/modules/correctilluminationcalculate.py
+++ cellprofiler/modules/correctilluminationcalculate.py
@@ -493,11 +493,22 @@ class CorrectIlluminationCalculate(cpm.CPModule):
                 self.illumination_image_name.value, self)
             d = self.get_dictionary(image_set_list)[OUTPUT_IMAGE] = {}
             if self.each_or_all == EA_ALL_FIRST:
+                #
+                # Find the module that provides the image we need
+                #
+                md = workspace.pipeline.get_provider_dictionary(
+                    self.image_name.group, self)
+                src_module, src_setting = md[self.image_name.value][-1]
+                modules = list(pipeline.modules())
+                idx = modules.index(src_module)
+                last_module = modules[idx+1]
                 for w in pipeline.run_group_with_yield(
-                    workspace, grouping, image_numbers, self, title, message):
+                    workspace, grouping, image_numbers, 
+                    last_module, title, message):
                     image = w.image_set.get_image(self.image_name.value,
                                                   cache = False)
                     output_image_provider.add_image(image)
+                    w.image_set.clear_cache()
             output_image_provider.serialize(d)
             
         return True

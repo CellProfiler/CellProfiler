98c181be5f1946061205f687c426ea5d0df13a4a
Christoph Sommer <christoph.sommer@imba.oeaw.ac.at>
2015-02-09 17:53:29 +0100

    removed flowSightReader from being used... my installation of bioformats, fails to load/have it

--- cellprofiler/utilities/cpjvm.py
+++ cellprofiler/utilities/cpjvm.py
@@ -216,10 +216,10 @@ def cp_start_vm():
             javabridge.call(stream, "close", "()V")
             os.remove(path)
                     
-        for classname in ("loci.formats.in.FlowSightReader", 
-                          "loci.formats.in.IM3Reader"):
-            klass = javabridge.class_for_name(classname)
-            class_list.add_class(klass)
+#         for classname in ("loci.formats.in.FlowSightReader", 
+#                           "loci.formats.in.IM3Reader"):
+#             klass = javabridge.class_for_name(classname)
+#             class_list.add_class(klass)
         for klass in problem_classes:
             class_list.add_class(klass)
         return class_list

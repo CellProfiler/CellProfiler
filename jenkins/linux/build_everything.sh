a6e9c827a60f019caeaeea453e933ca5e2c0c976
Lee Kamentsky <leek@broadinstitute.org>
2015-07-28 09:06:25 -0400

    The build started to fail when the openjdk rpm was upgraded from 1.7.0.79 to 1.7.0.85

--- jenkins/linux/build_everything.sh
+++ jenkins/linux/build_everything.sh
@@ -12,6 +12,7 @@
 # TMPDIR: location for temporary files
 # SRCDIR: location for downloaded sources (defaults to $TMPDIR/src)
 
+export JAVA_VERSION=1.7.0-openjdk-1.7.0.79
 
 set -e
 set -x
@@ -24,7 +25,7 @@ yum -q -y install python-setuptools gcc gcc-c++ wget vim gtk2-devel git svn \
     unzip tar dos2unix \
     dejavu-lgc-sans-fonts openssl openssl-devel xclock bzip2 \
     bzip2-devel bzip2-libs libXtst make patch readline-devel \
-    java-1.7.0-openjdk java-1.7.0-openjdk-devel
+    "java-$(JAVA_VERSION)" "java-$(JAVA_VERSION)-devel"
 
 echo "---------------------------------------------------------------------"
 echo "-"

375e9b248f0dfd8a402d7465a92ff85a32445bcb
Lee Kamentsky <leek@broadinstitute.org>
2015-07-30 15:23:04 -0400

    issue #1411 - image cache file did not honor temp directory

--- cellprofiler/measurements.py
+++ cellprofiler/measurements.py
@@ -1689,8 +1689,14 @@ class Measurements(object):
             
     def __ensure_cache_file(self):
         if self.__image_cache_file is None:
+            path = cpprefs.get_temporary_directory()
+            if (path is None) or (not os.path.exists(path)) or \
+               (not os.access(path, os.W_OK)):
+                path = None
             h, self.__image_cache_path = tempfile.mkstemp(
-                suffix=".h5", prefix="CellProfilerImageCache")
+                dir = path,
+                suffix=".h5", 
+                prefix="CellProfilerImageCache")
             self.__image_cache_file = h5py.File(
                 self.__image_cache_path, "w")
             self.__hdf5_object_set = HDF5ObjectSet(self.__image_cache_file)

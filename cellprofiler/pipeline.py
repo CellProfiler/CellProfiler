CellProfiler/CellProfiler

commit 128a3e9cd66a56f8bd0389b0803438cb587cf55c

issue #1119 be more friendly to users who open pipeline files using Open Project...

cellprofiler/pipeline.py
@@ -803,6 +803,18 @@ class Pipeline(object):
             module.save_to_handles(handles)
         return handles
     
+    @staticmethod
+    def is_pipeline_txt_file(filename):
+        '''Test a file to see if it can be loaded by Pipeline.loadtxt
+        
+        filename - path to the file
+        
+        returns True if the file starts with the CellProfiler cookie.
+        '''
+        with open(filename, "rb") as fd:
+            header = fd.read(len(COOKIE))
+            return header == COOKIE
+        
     def load(self, fd_or_filename):
         """Load the pipeline from a file
         

cellprofiler/gui/pipelinecontroller.py
@@ -503,6 +503,34 @@ class PipelineController:
                           parent = self.__frame,
                           style = wx.OK | wx.ICON_ERROR)
             return
+        #
+        # Meh, maybe the user loaded a pipeline file...
+        #
+        if not h5py.is_hdf5(filename):
+            if cpp.Pipeline.is_pipeline_txt_file(filename):
+                message = (
+                    "The file, \"%s\", is a pipeline file, not a project file. "
+                    "Do you want to load it as a pipeline?") % \
+                    os.path.split(filename)[-1]
+                result = wx.MessageBox(
+                    message, 
+                    "Cannot load as project file",
+                    style = wx.YES | wx.NO | wx.YES_DEFAULT | wx.ICON_QUESTION,
+                    parent = self.__frame)
+                if result == wx.YES:
+                    self.do_load_pipeline(filename)
+                return
+            else:
+                message = (
+                    "CellProfiler cannot read the file, \"%s\", as a project "
+                    "file. It may be damaged or corrupted or may not be in the "
+                    ".cpproj format") % os.path.split(filename)[-1]
+                wx.MessageBox(
+                    message, "Cannot read %s" % filename,
+                    style = wx.OK | wx.ICON_ERROR,
+                    parent = self.__frame)
+                return
+                
         if self.is_running():
             # Defensive programming - the user shouldn't be able
             # to do this.

commit 0cf57c5bbc26622ed3437b07abb58b1fe67eb4fe

Fixes #1532 - Pipeline.is_pipeline_txt_file was broken by 128a3e9

cellprofiler/pipeline.py
@@ -815,7 +815,7 @@ class Pipeline(object):
         returns True if the file starts with the CellProfiler cookie.
         '''
         with open(filename, "rb") as fd:
-            return self.is_pipeline_text_fd(fd)
+            return Pipeline.is_pipeline_txt_fd(fd)
         
     @staticmethod
     def is_pipeline_txt_fd(fd):
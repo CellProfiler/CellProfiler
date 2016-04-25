088e32f39ccd1b3680efbc481db690526bf9bdc4
Allen Goodman <allen.goodman@icloud.com>
2016-04-25 17:14:56 -0400

    deleted contrib.cell_star

--- contrib/cell_star/parameter_fitting/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'Adam Kaczmarek, Filip Mróz'
-__all__ = ["pf_process", "pf_snake"]
--- contrib/cell_star/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    import scipy.io.matlab
-    import unittest
-    import nose
-    import sys
-
-    nose.main()

2c332291fcd3143f4a125d2130d2087d5a2929b7
Allen Goodman <allen.goodman@icloud.com>
2016-05-18 17:10:17 -0400

    moved cellprofiler.matlab into cellprofiler.pipeline (resolves #1936)

--- cellprofiler/pipeline.py
+++ cellprofiler/pipeline.py
@@ -44,7 +44,6 @@ import cellprofiler.object as cpo
 import cellprofiler.workspace as cpw
 import cellprofiler.setting as cps
 from cellprofiler.utilities.utf16encode import utf16encode, utf16decode
-from cellprofiler.matlab.cputils import make_cell_struct_dtype, new_string_cell_array, encapsulate_strings_in_arrays
 from cellprofiler.utilities.walk_in_background import WalkCollection, THREAD_STOP
 from bioformats.omexml import OMEXML
 import cellprofiler.utilities.version as cpversion
@@ -102,6 +101,16 @@ SETTINGS_DTYPE = np.dtype([(VARIABLE_VALUES, '|O4'),
                            (MODULE_NOTES, '|O4'),
                            (SHOW_WINDOW, '|O4'),
                            (BATCH_STATE, '|O4')])
+
+
+def make_cell_struct_dtype(fields):
+    """Makes the dtype of a struct composed of cells
+
+    fields - the names of the fields in the struct
+    """
+    return numpy.dtype([(str(x), '|O4') for x in fields])
+
+
 CURRENT_DTYPE = make_cell_struct_dtype([NUMBER_OF_IMAGE_SETS,
                                         SET_BEING_ANALYZED, NUMBER_OF_MODULES,
                                         SAVE_OUTPUT_HOW_OFTEN, TIME_STARTED,
@@ -3987,3 +3996,55 @@ class MeasurementDependency(Dependency):
 
     def __str__(self):
         return "Measurement: %s.%s" % (self.object_name, self.feature)
+
+
+def new_string_cell_array(shape):
+    """Return a numpy.ndarray that looks like {NxM cell} to Matlab
+
+    Return a numpy.ndarray that looks like {NxM cell} to Matlab.
+    Each of the cells looks empty.
+    shape - the shape of the array that's generated, e.g. (5,19) for a 5x19 cell array.
+            Currently, this must be a 2-d shape.
+    The object returned is a numpy.ndarray with dtype=dtype('object') and the given shape
+    with each cell in the array filled with a numpy.ndarray with shape = (1,0)
+    and dtype=dtype('float64'). This appears to be the form that's created in matlab
+    for this sort of object.
+    """
+    result = numpy.ndarray(shape, dtype=numpy.dtype('object'))
+    for i in range(0, shape[0]):
+        for j in range(0, shape[1]):
+            result[i, j] = numpy.empty((0, 0))
+    return result
+
+
+def encapsulate_strings_in_arrays(handles):
+    """Recursively descend through the handles structure, replacing strings as arrays packed with strings
+
+    This function makes the handles structure loaded through the sandwich compatible with loadmat. It operates on the array in-place.
+    """
+    if handles.dtype.kind == 'O':
+        # cells - descend recursively
+        flat = handles.flat
+        for i in range(0, len(flat)):
+            if isinstance(flat[i], str) or isinstance(flat[i], unicode):
+                flat[i] = encapsulate_string(flat[i])
+            elif isinstance(flat[i], numpy.ndarray):
+                encapsulate_strings_in_arrays(flat[i])
+    elif handles.dtype.fields:
+        # A structure: iterate over all structure elements.
+        for field in handles.dtype.fields.keys():
+            if isinstance(handles[field], str) or isinstance(handles[field], unicode):
+                handles[field] = encapsulate_string(handles[field])
+            elif isinstance(handles[field], numpy.ndarray):
+                encapsulate_strings_in_arrays(handles[field])
+
+
+def encapsulate_string(s):
+    """Encapsulate a string in an array of shape 1 of the length of the string
+    """
+    if isinstance(s, str):
+        result = numpy.ndarray((1,), '<S%d' % (len(s)))
+    else:
+        result = numpy.ndarray((1,), '<U%d' % (len(s)))
+    result[0] = s
+    return result
--- cellprofiler/tests/test_pipeline.py
+++ cellprofiler/tests/test_pipeline.py
@@ -1672,3 +1672,49 @@ img_942_data = ('eJyVd3VQFN7bLwiKSAtIs7T00p1SUgLSXZIqtdTCLogoISUgHYuggkgo3Q1L'
 
 if __name__ == "__main__":
     unittest.main()
+
+
+class TestUtils(unittest.TestCase):
+    def test_02_001_EncapsulateString(self):
+        a = cellprofiler.pipeline.encapsulate_string('Hello')
+        self.assertTrue(a.shape == (1,))
+        self.assertTrue(a.dtype.kind == 'S')
+        self.assertTrue(a[0] == 'Hello')
+
+    def test_02_001_EncapsulateUnicode(self):
+        a = cellprofiler.pipeline.encapsulate_string(u'Hello')
+        self.assertTrue(a.shape == (1,))
+        self.assertTrue(a.dtype.kind == 'U')
+        self.assertTrue(a[0] == u'Hello')
+
+    def test_02_01_EncapsulateCell(self):
+        cell = numpy.ndarray((1, 1), dtype=object)
+        cell[0, 0] = u'Hello, world'
+        cellprofiler.pipeline.encapsulate_strings_in_arrays(cell)
+        self.assertTrue(isinstance(cell[0, 0], numpy.ndarray))
+        self.assertTrue(cell[0, 0][0] == u'Hello, world')
+
+    def test_02_02_EncapsulateStruct(self):
+        struct = numpy.ndarray((1, 1), dtype=[('foo', object)])
+        struct['foo'][0, 0] = u'Hello, world'
+        cellprofiler.pipeline.encapsulate_strings_in_arrays(struct)
+        self.assertTrue(isinstance(struct['foo'][0, 0], numpy.ndarray))
+        self.assertTrue(struct['foo'][0, 0][0] == u'Hello, world')
+
+    def test_02_03_EncapsulateCellInStruct(self):
+        struct = numpy.ndarray((1, 1), dtype=[('foo', object)])
+        cell = numpy.ndarray((1, 1), dtype=object)
+        cell[0, 0] = u'Hello, world'
+        struct['foo'][0, 0] = cell
+        cellprofiler.pipeline.encapsulate_strings_in_arrays(struct)
+        self.assertTrue(isinstance(cell[0, 0], numpy.ndarray))
+        self.assertTrue(cell[0, 0][0] == u'Hello, world')
+
+    def test_02_04_EncapsulateStructInCell(self):
+        struct = numpy.ndarray((1, 1), dtype=[('foo', object)])
+        cell = numpy.ndarray((1, 1), dtype=object)
+        cell[0, 0] = struct
+        struct['foo'][0, 0] = u'Hello, world'
+        cellprofiler.pipeline.encapsulate_strings_in_arrays(cell)
+        self.assertTrue(isinstance(struct['foo'][0, 0], numpy.ndarray))
+        self.assertTrue(struct['foo'][0, 0][0] == u'Hello, world')

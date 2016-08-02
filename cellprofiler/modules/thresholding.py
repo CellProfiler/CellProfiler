ddc0a4acd206ed84c299816cc72c21ecff344b92
Allen Goodman <allen.goodman@icloud.com>
2016-08-02 11:00:43 -0400

    global thresholding

--- cellprofiler/modules/thresholding.py
+++ cellprofiler/modules/thresholding.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 
 Thresholding
@@ -28,41 +29,114 @@ class Thresholding(cellprofiler.module.Module):
             "OutputImage"
         )
 
-        self.structuring_element = cellprofiler.setting.Choice(
-            "Structuing element",
+        self.method = cellprofiler.setting.Choice(
+            u"Method",
             [
-                "Ball"
+                u"Adaptive",
+                u"ISODATA",
+                u"Li’s Minimum Cross Entropy",
+                u"Otsu’s method",
+                u"Yen’s method"
             ]
         )
 
-        self.radius = cellprofiler.setting.Integer(
-            "Radius",
-            1
+        self.adaptive_block_size = cellprofiler.setting.Integer(
+            u"Block size",
+            3
+        )
+
+        self.adaptive_method = cellprofiler.setting.Choice(
+            u"Adaptive method",
+            [
+                u"Gaussian",
+                u"Generic",
+                u"Mean",
+                u"Median"
+            ],
+            u"Gaussian"
+        )
+
+        self.adaptive_offset = cellprofiler.setting.Float(
+            u"Offset",
+            0.0
+        )
+
+        self.adaptive_mode = cellprofiler.setting.Choice(
+            u"Mode",
+            [
+                u"Constant",
+                u"Mirror",
+                u"Nearest",
+                u"Reflect",
+                u"Wrap"
+            ],
+            u"Reflect"
+        )
+
+        self.isodata_bins = cellprofiler.setting.Integer(
+            u"Bins",
+            256
+        )
+
+        self.otsu_bins = cellprofiler.setting.Integer(
+            u"Bins",
+            256
+        )
+
+        self.yen_bins = cellprofiler.setting.Integer(
+            u"Bins",
+            256
         )
 
     def settings(self):
         return [
             self.x_name,
             self.y_name,
-            self.structuring_element,
-            self.radius
+            self.method,
+            self.adaptive_block_size,
+            self.adaptive_method,
+            self.adaptive_offset,
+            self.adaptive_mode,
+            self.isodata_bins,
+            self.otsu_bins,
+            self.yen_bins
         ]
 
     def visible_settings(self):
-        return [
+        settings = [
             self.x_name,
             self.y_name,
-            self.structuring_element,
-            self.radius
+            self.method
         ]
+
+        if self.method.value == u"Adaptive":
+            settings = settings + [
+                self.adaptive_block_size,
+                self.adaptive_method,
+                self.adaptive_offset,
+                self.adaptive_mode
+            ]
+
+        if self.method.value == u"ISODATA":
+            settings = settings + [
+                self.isodata_bins
+            ]
+
+        if self.method.value == u"Otsu’s method":
+            settings = settings + [
+                self.otsu_bins
+            ]
+
+        if self.method.value == u"Yen’s method":
+            settings = settings + [
+                self.yen_bins
+            ]
+
+        return settings
 
     def run(self, workspace):
         x_name = self.x_name.value
         y_name = self.y_name.value
-
-        radius = self.radius.value
-
-        structuring_element = self.structuring_element.value
 
         images = workspace.image_set
 
@@ -70,12 +144,36 @@ class Thresholding(cellprofiler.module.Module):
 
         x_data = x.pixel_data
 
-        disk = skimage.morphology.disk(radius)
-
         y_data = numpy.zeros_like(x_data)
 
         for plane, image in enumerate(x_data):
-            y_data[plane] = skimage.filters.rank.otsu(image, disk)
+            if self.method.value == u"Adaptive":
+                y_data[plane] = skimage.filters.threshold_adaptive(
+                    image=image,
+                    block_size=self.adaptive_block_size,
+                    method=self.adaptive_method,
+                    offset=self.adaptive_offset,
+                    mode=self.adaptive_mode
+                )
+            elif self.method.value == u"ISODATA":
+                y_data[plane] = skimage.filters.threshold_isodata(
+                    image=image,
+                    nbins=self.isodata_bins
+                )
+            elif self.method.value == u"Li’s Minimum Cross Entropy":
+                y_data[plane] = skimage.filters.threshold_li(
+                    image=image
+                )
+            elif self.method.value == u"Otsu’s method":
+                y_data[plane] = skimage.filters.threshold_otsu(
+                    image=image,
+                    nbins=self.otsu_bins
+                )
+            elif self.method.value == u"Yen’s method":
+                y_data[plane] = skimage.filters.threshold_yen(
+                    image=image,
+                    nbins=self.yen_bins
+                )
 
         y_data = skimage.exposure.rescale_intensity(y_data * 1.0)
 

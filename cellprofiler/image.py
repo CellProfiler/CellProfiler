965dcbe4eeb631a9a8a089e0a1ee5960cce2f04a
Allen Goodman <allen.goodman@icloud.com>
2016-08-03 13:47:15 -0400

    Remove HDF5 image caching.

--- cellprofiler/image.py
+++ cellprofiler/image.py
@@ -93,61 +93,7 @@ class Image(object):
         return self.__image.get()
 
     def set_image(self, image, convert=True):
-        """Set the primary image
-
-        Convert the image to a numpy array of dtype = np.float64.
-        Rescale according to Matlab's rules for im2double:
-        * single/double values: keep the same
-        * uint8/16/32/64: scale 0 to max to 0 to 1
-        * int8/16/32/64: scale min to max to 0 to 1
-        * logical: save as is (and get if must_be_binary)
-        """
-        img = np.asanyarray(image)
-        if img.dtype.name == "bool" or not convert:
-            self.__image = ImageCache(img)
-            return
-        mval = 0.
-        scale = 1.
-        fix_range = False
-        if issubclass(img.dtype.type, np.floating):
-            pass
-        elif img.dtype.type is np.uint8:
-            scale = math.pow(2.0, 8.0) - 1
-        elif img.dtype.type is np.uint16:
-            scale = math.pow(2.0, 16.0) - 1
-        elif img.dtype.type is np.uint32:
-            scale = math.pow(2.0, 32.0) - 1
-        elif img.dtype.type is np.uint64:
-            scale = math.pow(2.0, 64.0) - 1
-        elif img.dtype.type is np.int8:
-            scale = math.pow(2.0, 8.0)
-            mval = -scale / 2.0
-            scale -= 1
-            fix_range = True
-        elif img.dtype.type is np.int16:
-            scale = math.pow(2.0, 16.0)
-            mval = -scale / 2.0
-            scale -= 1
-            fix_range = True
-        elif img.dtype.type is np.int32:
-            scale = math.pow(2.0, 32.0)
-            mval = -scale / 2.0
-            scale -= 1
-            fix_range = True
-        elif img.dtype.type is np.int64:
-            scale = math.pow(2.0, 64.0)
-            mval = -scale / 2.0
-            scale -= 1
-            fix_range = True
-        # Avoid temporaries by doing the shift/scale in place.
-        img = img.astype(np.float32)
-        img -= mval
-        img /= scale
-        if fix_range:
-            # These types will always have ranges between 0 and 1. Make it so.
-            np.clip(img, 0, 1, out=img)
-        check_consistency(img, self.__mask)
-        self.__image = ImageCache(img)
+        self.__image = ImageCache(image)
 
     image = property(get_image, set_image)
     pixel_data = property(get_image, set_image)
@@ -385,56 +331,17 @@ class Image(object):
 
 
 class ImageCache(object):
-    '''An HDF5 cache that can store an image, mask or crop mask
-
-    '''
-    IC_MONOCHROME = "Monochrome"
-    IC_COLOR = "Color"
-    IC_5D = "5D"
-
     def __init__(self, image):
-        '''Initialize with the image to control'''
-        self.__backing_store = None
-        self.__name = None
-        if image.ndim == 2:
-            self.__type = ImageCache.IC_MONOCHROME
-            self.__image = image.reshape(1, 1, 1, image.shape[0], image.shape[1])
-        elif image.ndim == 3:
-            self.__type = ImageCache.IC_COLOR
-            self.__image = image.transpose(2, 0, 1).reshape(
-                    image.shape[2], 1, 1, image.shape[0], image.shape[1])
-        else:
-            self.__type = ImageCache.IC_5D
-            self.__image = image
+        self.__image = image
 
     def is_cached(self):
-        '''Return True if image is already cached by a backing store
-
-        '''
-        return self.__backing_store is not None
+        return True
 
     def cache(self, name, backing_store):
-        '''Cache an image into a backing store
-
-        name - unique channel name of the image
-        backing_store - an HDF5ImageSet
-        '''
-        self.__backing_store = backing_store
-        self.__name = name
-        self.__backing_store.set_image(self.__name, self.__image)
-        del self.__image
+        pass
 
     def get(self):
-        '''Get the image in its original format'''
-        if self.is_cached():
-            image = self.__backing_store.get_image(self.__name)
-        else:
-            image = self.__image
-        if self.__type == ImageCache.IC_MONOCHROME:
-            return image.reshape(image.shape[3], image.shape[4])
-        elif self.__type == ImageCache.IC_COLOR:
-            return image.reshape(
-                    image.shape[0], image.shape[3], image.shape[4]).transpose(1, 2, 0)
+        return self.__image
 
 
 def crop_image(image, crop_mask, crop_internal=False):

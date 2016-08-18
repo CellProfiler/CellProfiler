d7b68484d55656c731d3387421c9ee981f316375
Allen Goodman <allen.goodman@icloud.com>
2016-08-18 14:57:13 -0400

    watershed fixes

--- cellprofiler/modules/watershed.py
+++ cellprofiler/modules/watershed.py
@@ -9,7 +9,9 @@ import cellprofiler.module
 import cellprofiler.object
 import cellprofiler.setting
 import numpy
+import scipy.ndimage
 import skimage.exposure
+import skimage.feature
 import skimage.filters
 import skimage.measure
 import skimage.morphology
@@ -21,12 +23,12 @@ class Watershed(cellprofiler.module.Module):
     variable_revision_number = 1
 
     def create_settings(self):
-        self.x_name = cellprofiler.setting.ImageNameSubscriber(
-            "Input"
+        self.image = cellprofiler.setting.ImageNameSubscriber(
+            "Image"
         )
 
-        self.object_name = cellprofiler.setting.ObjectNameProvider(
-            "Object name",
+        self.object = cellprofiler.setting.ObjectNameProvider(
+            "Object",
             ""
         )
 
@@ -34,89 +36,72 @@ class Watershed(cellprofiler.module.Module):
             "Markers"
         )
 
-        self.connectivity = cellprofiler.setting.ImageNameSubscriber(
-            "Connectivity"
+        self.markers = cellprofiler.setting.ImageNameSubscriber(
+            "Markers"
         )
 
         self.mask = cellprofiler.setting.ImageNameSubscriber(
             "Mask"
         )
 
-
     def settings(self):
         return [
-            self.x_name,
-            self.object_name,
+            self.image,
+            self.object,
             self.markers,
-            self.connectivity,
             self.mask
         ]
 
     def visible_settings(self):
         return [
-            self.x_name,
-            self.object_name,
+            self.image,
+            self.object,
             self.markers,
-            self.connectivity,
             self.mask
         ]
 
     def run(self, workspace):
-        x_name = self.x_name.value
-        object_name = self.object_name.value
-
-        markers_name = self.markers.value
-
-        connectivity_name = self.connectivity.value
-
-        mask_name = self.mask.value
-
         images = workspace.image_set
 
-        x = images.get_image(x_name)
+        image = images.get_image(self.image.value).pixel_data
 
-        x_data = x.pixel_data
+        markers = images.get_image(self.markers.value).pixel_data
 
-        markers = images.get_image(markers_name)
+        mask = images.get_image(self.mask.value).pixel_data
 
-        markers_data = markers.pixel_data
+        labels = skimage.morphology.watershed(image, markers, mask=mask)
 
-        connectivity = images.get_image(connectivity_name)
-
-        connectivity_data = connectivity.pixel_data
-
-        mask = images.get_image(mask_name)
-
-        mask_data = mask.pixel_data
-
-        y_data = numpy.zeros_like(x_data)
-
-        segmentation = skimage.morphology.watershed(
-            image=x_data,
-            markers=markers_data
-        )
-
-        labels = skimage.measure.label(segmentation)
+        labels = skimage.measure.label(labels)
 
         objects = cellprofiler.object.Objects()
 
         objects.segmented = labels
 
-        workspace.object_set.add_objects(objects, object_name)
+        workspace.object_set.add_objects(objects, self.object.value)
 
         if self.show_window:
-            workspace.display_data.x_data = x_data
+            workspace.display_data.image = image
+
+            workspace.display_data.labels = labels
 
     def display(self, workspace, figure):
-        dimensions = (1, 1)
+        dimensions = (2, 1)
 
-        x_data = workspace.display_data.x_data[16]
+        image = workspace.display_data.image[16]
+
+        labels = workspace.display_data.labels[16]
 
         figure.set_subplots(dimensions)
 
         figure.subplot_imshow(
             0,
             0,
-            x_data,
+            image,
             colormap="gray"
+        )
+
+        figure.subplot_imshow_labels(
+            1,
+            0,
+            labels
         )

a65132a1258398a31bb1644b1ac72a1aae7d9a81
Allen Goodman <allen.goodman@icloud.com>
2016-09-21 16:36:12 -0400

    distance transform

--- cellprofiler/modules/distancetransform.py
+++ cellprofiler/modules/distancetransform.py
@@ -2,91 +2,21 @@
 
 """
 
-<strong>Distance transform</strong>
+Distance transform
 
 """
 
-import cellprofiler.image
 import cellprofiler.module
 import cellprofiler.setting
 import scipy.ndimage
 
 
-class DistanceTransform(cellprofiler.module.Module):
-    category = "Image processing"
-    module_name = "Distance transform"
+class DistanceTransform(cellprofiler.module.ImageProcessing):
+    module_name = "DistanceTransform"
+
     variable_revision_number = 1
 
-    def create_settings(self):
-        self.x_name = cellprofiler.setting.ImageNameSubscriber(
-            "Input"
-        )
-
-        self.y_name = cellprofiler.setting.ImageNameProvider(
-            "Output",
-            "DistanceTransform"
-        )
-
-    def settings(self):
-        return [
-            self.x_name,
-            self.y_name
-        ]
-
-    def visible_settings(self):
-        return [
-            self.x_name,
-            self.y_name
-        ]
-
     def run(self, workspace):
-        x_name = self.x_name.value
+        self.function = scipy.ndimage.distance_transform_edt
 
-        y_name = self.y_name.value
-
-        images = workspace.image_set
-
-        x = images.get_image(x_name)
-
-        dimensions = x.dimensions
-
-        x_data = x.pixel_data
-
-        y_data = scipy.ndimage.distance_transform_edt(x_data)
-
-        y = cellprofiler.image.Image(
-            image=y_data,
-            parent_image=x,
-            dimensions=dimensions
-        )
-
-        images.add(y_name, y)
-
-        if self.show_window:
-            workspace.display_data.x_data = x_data
-
-            workspace.display_data.y_data = y_data
-
-            workspace.display_data.dimensions = dimensions
-
-    def display(self, workspace, figure):
-        layout = (2, 1)
-
-        figure.set_subplots(
-            dimensions=workspace.display_data.dimensions,
-            subplots=layout
-        )
-
-        figure.subplot_imshow(
-            dimensions=workspace.display_data.dimensions,
-            image=workspace.display_data.x_data,
-            x=0,
-            y=0
-        )
-
-        figure.subplot_imshow(
-            dimensions=workspace.display_data.dimensions,
-            image=workspace.display_data.y_data,
-            x=1,
-            y=0
-        )
+        super(DistanceTransform, self).run(workspace)

130d338b6efd100ab593f49473d91fbcda46ba08
mcquin <mcquin@broadinstitute.org>
2016-10-26 10:02:12 -0400

    edge detection

--- cellprofiler/modules/edgedetection.py
+++ cellprofiler/modules/edgedetection.py
@@ -1,7 +1,6 @@
 # coding=utf-8
-"""
 
-Edge detection
+"""
 
 """
 
@@ -9,15 +8,15 @@ import cellprofiler.image
 import cellprofiler.module
 import cellprofiler.setting
 import numpy
-import skimage.exposure
-import skimage.feature
+import skimage.color
 import skimage.filters
-import skimage.morphology
 
 
 class EdgeDetection(cellprofiler.module.Module):
-    category = "Feature detection"
-    module_name = "Edge detection"
+    category = "Feature Detection"
+
+    module_name = "EdgeDetection"
+
     variable_revision_number = 1
 
     def create_settings(self):
@@ -27,22 +26,7 @@ class EdgeDetection(cellprofiler.module.Module):
 
         self.y_name = cellprofiler.setting.ImageNameProvider(
             u"Output",
-            u"OutputImage"
-        )
-
-        self.operation = cellprofiler.setting.Choice(
-            u"Operation",
-            [
-                u"Canny edge detection",
-                u"Prewitt operator",
-                u"Roberts’ cross",
-                u"Sobel operator"
-            ]
-        )
-
-        self.sigma = cellprofiler.setting.Float(
-            u"Sigma",
-            1.0
+            u"EdgeDetection"
         )
 
         self.mask = cellprofiler.setting.ImageNameSubscriber(
@@ -54,44 +38,18 @@ class EdgeDetection(cellprofiler.module.Module):
         return [
             self.x_name,
             self.y_name,
-            self.operation,
-            self.sigma,
             self.mask
         ]
 
     def visible_settings(self):
-        settings = [
+        return [
             self.x_name,
             self.y_name,
-            self.operation
+            self.mask
         ]
-
-        if self.operation.value == u"Canny edge detection":
-            settings = settings + [
-                self.sigma,
-                self.mask
-            ]
-
-        if self.operation.value == u"Prewitt operator":
-            settings = settings + [
-                self.mask
-            ]
-
-        if self.operation.value == u"Roberts’ cross":
-            settings = settings + [
-                self.mask
-            ]
-
-        if self.operation.value == u"Sobel operator":
-            settings = settings + [
-                self.mask
-            ]
-
-        return settings
 
     def run(self, workspace):
         x_name = self.x_name.value
-        y_name = self.y_name.value
 
         images = workspace.image_set
 
@@ -99,38 +57,52 @@ class EdgeDetection(cellprofiler.module.Module):
 
         x_data = x.pixel_data
 
-        y_data = numpy.zeros_like(x_data)
+        if x.multichannel:
+            x_data = skimage.color.rgb2gray(x_data)
 
-        mask = self.mask.value
-        if self.mask.is_blank:
-            mask = None
+        mask_data = None
 
-        for plane, image in enumerate(x_data):
-            if self.operation.value == u"Canny edge detection":
-                y_data[plane] = skimage.feature.canny(x_data[plane], sigma=self.sigma.value, mask=mask)
-            elif self.operation.value == u"Prewitt operator":
-                y_data[plane] = skimage.filters.prewitt(x_data[plane], mask=mask)
-            elif self.operation.value == u"Roberts’ cross":
-                y_data[plane] = skimage.filters.roberts(x_data[plane], mask=mask)
-            elif self.operation.value == u"Sobel operator":
-                y_data[plane] = skimage.filters.sobel(x_data[plane], mask=mask)
+        if not self.mask.is_blank:
+            mask_name = self.mask.value
 
-        y_data = skimage.exposure.rescale_intensity(y_data * 1.0)
+            mask = images.get_image(mask_name)
+
+            mask_data = mask.pixel_data
+
+        dimensions = x.dimensions
+
+        if dimensions is 2:
+            y_data = skimage.filters.sobel(x_data, mask=mask_data)
+        else:
+            y_data = numpy.zeros_like(x_data)
+
+            for plane, image in enumerate(x_data):
+                plane_mask = None if mask_data is None else mask_data[plane]
+
+                y_data[plane] = skimage.filters.sobel(image, mask=plane_mask)
 
         y = cellprofiler.image.Image(
             image=y_data,
-            parent_image=x
+            parent_image=x,
+            dimensions=dimensions
         )
+
+        y_name = self.y_name.value
 
         images.add(y_name, y)
 
         if self.show_window:
             workspace.display_data.x_data = x_data
+
             workspace.display_data.y_data = y_data
 
+            workspace.dimensions = dimensions
+
     def display(self, workspace, figure):
-        figure.set_grids((1, 2))
+        dimensions = workspace.display_data.dimensions
 
-        figure.gridshow(0, 0, workspace.display_data.x_data)
+        figure.set_subplots((2, 1))
 
-        figure.gridshow(0, 1, workspace.display_data.y_data)
+        figure.subplot_imshow(0, 0, workspace.display_data.x_data, dimensions=dimensions)
+
+        figure.subplot_imshow(0, 1, workspace.display_data.y_data, dimensions=dimensions)

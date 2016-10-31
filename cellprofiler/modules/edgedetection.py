05981a5c8d5cf54f21e379f99ab69a150599cc5f
mcquin <mcquin@broadinstitute.org>
2016-10-31 09:09:39 -0400

    inherit from ImageProcessing

--- cellprofiler/modules/edgedetection.py
+++ cellprofiler/modules/edgedetection.py
@@ -12,7 +12,7 @@ import skimage.color
 import skimage.filters
 
 
-class EdgeDetection(cellprofiler.module.Module):
+class EdgeDetection(cellprofiler.module.ImageProcessing):
     category = "Feature Detection"
 
     module_name = "EdgeDetection"
@@ -20,20 +20,7 @@ class EdgeDetection(cellprofiler.module.Module):
     variable_revision_number = 1
 
     def create_settings(self):
-        self.x_name = cellprofiler.setting.ImageNameSubscriber(
-            u"Input",
-            doc="""
-            An image or volume to detect edges in.
-            """
-        )
-
-        self.y_name = cellprofiler.setting.ImageNameProvider(
-            u"Output",
-            u"EdgeDetection",
-            doc="""
-            Name the output.
-            """
-        )
+        super(EdgeDetection, self).create_settings()
 
         self.mask = cellprofiler.setting.ImageNameSubscriber(
             u"Mask",
@@ -41,16 +28,16 @@ class EdgeDetection(cellprofiler.module.Module):
         )
 
     def settings(self):
-        return [
-            self.x_name,
-            self.y_name,
+        __settings__ = super(EdgeDetection, self).settings()
+
+        return __settings__ + [
             self.mask
         ]
 
     def visible_settings(self):
-        return [
-            self.x_name,
-            self.y_name,
+        __settings__ = super(EdgeDetection, self).visible_settings()
+
+        return __settings__ + [
             self.mask
         ]
 
@@ -103,12 +90,3 @@ class EdgeDetection(cellprofiler.module.Module):
             workspace.display_data.y_data = y_data
 
             workspace.dimensions = dimensions
-
-    def display(self, workspace, figure):
-        dimensions = workspace.display_data.dimensions
-
-        figure.set_subplots((2, 1))
-
-        figure.subplot_imshow(0, 0, workspace.display_data.x_data, dimensions=dimensions)
-
-        figure.subplot_imshow(0, 1, workspace.display_data.y_data, dimensions=dimensions)

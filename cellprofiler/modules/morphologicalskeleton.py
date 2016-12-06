c4ea87270faf7b541f341522ede9b52e5a9addc8
Allen Goodman <allen.goodman@icloud.com>
2016-12-06 10:32:09 -0500

    morphological skeleton (resolves #2176)

--- cellprofiler/modules/morphologicalskeleton.py
+++ cellprofiler/modules/morphologicalskeleton.py
@@ -1,41 +1,20 @@
 """
 
-<strong>Morphological skeleton<strong> thins an image into a single-pixel wide skeleton.
+Morphological skeleton thins an image into a single-pixel wide skeleton.
 
 """
 
 import cellprofiler.image
 import cellprofiler.module
-import cellprofiler.setting
 import skimage.morphology
 
 
-class MorphologicalSkeleton(cellprofiler.module.Module):
+class MorphologicalSkeleton(cellprofiler.module.ImageProcessing):
     category = "Mathematical morphology"
+
     module_name = "Morphological skeleton"
+
     variable_revision_number = 1
-
-    def create_settings(self):
-        self.x_name = cellprofiler.setting.ImageNameSubscriber(
-            "Input"
-        )
-
-        self.y_name = cellprofiler.setting.ImageNameProvider(
-            "Output",
-            "MorphologicalSkeleton"
-        )
-
-    def settings(self):
-        return [
-            self.x_name,
-            self.y_name
-        ]
-
-    def visible_settings(self):
-        return [
-            self.x_name,
-            self.y_name
-        ]
 
     def run(self, workspace):
         x_name = self.x_name.value
@@ -50,12 +29,15 @@ class MorphologicalSkeleton(cellprofiler.module.Module):
 
         x_data = x.pixel_data
 
-        y_data = skimage.morphology.skeletonize_3d(x_data)
+        if x.volumetric:
+            y_data = skimage.morphology.skeletonize_3d(x_data)
+        else:
+            y_data = skimage.morphology.skeletonize(x_data)
 
         y = cellprofiler.image.Image(
+            dimensions=dimensions,
             image=y_data,
-            parent_image=x,
-            dimensions=dimensions
+            parent_image=x
         )
 
         images.add(y_name, y)
@@ -64,26 +46,5 @@ class MorphologicalSkeleton(cellprofiler.module.Module):
             workspace.display_data.x_data = x_data
 
             workspace.display_data.y_data = y_data
-            
+
             workspace.display_data.dimensions = dimensions
-
-    def display(self, workspace, figure):
-        layout = (1, 2)
-
-        figure.set_subplots(layout)
-
-        figure.subplot_imshow(
-            colormap="gray",
-            dimensions=workspace.display_data.dimensions,
-            image=workspace.display_data.x_data,
-            x=0,
-            y=0
-        )
-
-        figure.subplot_imshow(
-            colormap="gray",
-            dimensions=workspace.display_data.dimensions,
-            image=workspace.display_data.y_data,
-            x=1,
-            y=0
-        )

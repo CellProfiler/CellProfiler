b83b478645fcabb5b67aacd8747aadbafb8ca580
Allen Goodman <allen.goodman@icloud.com>
2017-02-13 11:17:50 -0500

    Upgrade NumPy to 1.12.0 (resolves #2589)

--- setup.py
+++ setup.py
@@ -343,7 +343,7 @@ setuptools.setup(
             "mahotas",
             "matplotlib<2.0.0",
             "MySQL-python",
-            "numpy<1.12.0",
+            "numpy",
             "prokaryote>=1.0.11",
             "pyamg==3.1.1",
             "pytest",

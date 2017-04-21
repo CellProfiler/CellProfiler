5693520acaf8bf8cfa18c5afa703894ee9e85f8f
mcquin <mcquin@broadinstitute.org>
2017-04-21 12:16:43 -0400

    top-hat transform

--- cellprofiler/modules/tophattransform.py
+++ cellprofiler/modules/tophattransform.py
@@ -2,13 +2,15 @@
 
 """
 
-Top-hat transform
+<p>Perform a black or white top-hat transform on grayscale pixel data.</p>
+<p>Top-hat transforms are useful for extracting small elements and details from images and volumes.</p>
 
 """
 
+import skimage.morphology
+
 import cellprofiler.module
 import cellprofiler.setting
-import skimage.morphology
 
 
 class TopHatTransform(cellprofiler.module.ImageProcessing):
@@ -25,14 +27,24 @@ class TopHatTransform(cellprofiler.module.ImageProcessing):
                 "White top-hat transform"
             ],
             text="Operation",
-            value="Black top-hat transform"
+            value="Black top-hat transform",
+            doc="""
+            Select the top-hat transformation:
+            <ul>
+                <li><i>Black top-hat transform</i>: This operation returns the dark spots of the image that are smaller
+                than the structuring element. Note that dark spots in the original image are bright spots after the
+                black top hat.</li>
+                <li><i>White top-hat transform</i>: This operation returns the bright spots of the image that are
+                smaller than the structuring element.</li>
+            </ul>
+            """
         )
 
         self.structuring_element = cellprofiler.setting.StructuringElement()
 
     def settings(self):
         __settings__ = super(TopHatTransform, self).settings()
-        
+
         return __settings__ + [
             self.structuring_element
         ]
--- tests/modules/test_tophattransform.py
+++ tests/modules/test_tophattransform.py
@@ -25,24 +25,51 @@ def image(request):
     return cellprofiler.image.Image(image=data, dimensions=dimensions)
 
 
-def test_run(image, module, image_set, workspace):
+def test_run_black_tophat(image, module, image_set, workspace):
     module.x_name.value = "example"
 
     module.y_name.value = "TopHatTransform"
 
-    module.operation_name.value = "Black"
+    module.operation_name.value = "Black top-hat transform"
 
     if image.volumetric:
+        module.structuring_element.value = "ball,1"
+
         structure = skimage.morphology.ball(1)
     else:
-        structure = skimage.morphology.disk(1)
+        module.structuring_element.value = "disk,1"
 
-    module.structuring_element.value = structure
+        structure = skimage.morphology.disk(1)
 
     module.run(workspace)
 
     actual = image_set.get_image("TopHatTransform")
 
     desired = skimage.morphology.black_tophat(image.pixel_data, structure)
+
+    numpy.testing.assert_array_equal(actual.pixel_data, desired)
+
+
+def test_run_white_tophat(image, module, image_set, workspace):
+    module.x_name.value = "example"
+
+    module.y_name.value = "TopHatTransform"
+
+    module.operation_name.value = "White top-hat transform"
+
+    if image.volumetric:
+        module.structuring_element.value = "ball,1"
+
+        structure = skimage.morphology.ball(1)
+    else:
+        module.structuring_element.value = "disk,1"
+
+        structure = skimage.morphology.disk(1)
+
+    module.run(workspace)
+
+    actual = image_set.get_image("TopHatTransform")
+
+    desired = skimage.morphology.white_tophat(image.pixel_data, structure)
 
     numpy.testing.assert_array_equal(actual.pixel_data, desired)

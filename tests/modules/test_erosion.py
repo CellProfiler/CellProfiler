04c3457ecd75e814774031a81c6bef005d785421
karhohs <gandalfisarockstar@gmail.com>
2017-05-08 15:31:59 -0400

    tests for planewise morphology operations

--- tests/modules/test_erosion.py
+++ tests/modules/test_erosion.py
@@ -1,4 +1,5 @@
 import cellprofiler.modules.erosion
+import numpy
 import numpy.testing
 import skimage.morphology
 
@@ -11,16 +12,41 @@ def test_run(image, module, image_set, workspace):
     module.y_name.value = "erosion"
 
     if image.dimensions == 3 or image.multichannel:
+        # test 3d structuring element
         module.structuring_element.shape = "ball"
 
         selem = skimage.morphology.ball(1)
+
+        module.run(workspace)
+
+        actual = image_set.get_image("erosion")
+
+        desired = skimage.morphology.erosion(image.pixel_data, selem)
+
+        numpy.testing.assert_array_equal(actual.pixel_data, desired)
+
+        # test planewise
+        selem = skimage.morphology.disk(1)
+
+        module.run(workspace)
+
+        actual = image_set.get_image("erosion")
+
+        desired = numpy.zeros_like(image.pixel_data)
+
+        for index, plane in enumerate(image.pixel_data):
+            
+            desired[index] = skimage.morphology.erosion(plane, selem)
+
+        numpy.testing.assert_array_equal(actual.pixel_data, desired)
+
     else:
         selem = skimage.morphology.disk(1)
 
-    module.run(workspace)
+        module.run(workspace)
 
-    actual = image_set.get_image("erosion")
+        actual = image_set.get_image("erosion")
 
-    desired = skimage.morphology.erosion(image.pixel_data, selem)
+        desired = skimage.morphology.erosion(image.pixel_data, selem)
 
-    numpy.testing.assert_array_equal(actual.pixel_data, desired)
+        numpy.testing.assert_array_equal(actual.pixel_data, desired)

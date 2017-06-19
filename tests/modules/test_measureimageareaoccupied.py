b2eac103b9c133f136b11aacfb709682210517e2
mcquin <mcquin@broadinstitute.org>
2017-06-19 09:37:16 -0400

    Formatting changes
    * Remove import aliases and optimize imports
    * Use str.format
    * Remove '% globals'
    * PEP08 compliance

--- tests/modules/test_measureimageareaoccupied.py
+++ tests/modules/test_measureimageareaoccupied.py
@@ -1,52 +1,46 @@
-"""test_measureimagearea.py - test the MeasureImageArea module
-"""
-
-import base64
 import unittest
-import zlib
-from StringIO import StringIO
 
-import numpy as np
+import centrosome.outline
+import numpy
 
-from cellprofiler.preferences import set_headless
+import cellprofiler.image
+import cellprofiler.measurement
+import cellprofiler.module
+import cellprofiler.modules.measureimageareaoccupied
+import cellprofiler.object
+import cellprofiler.pipeline
+import cellprofiler.preferences
+import cellprofiler.workspace
 
-set_headless()
+cellprofiler.preferences.set_headless()
 
-import cellprofiler.pipeline as cpp
-import cellprofiler.workspace as cpw
-import cellprofiler.module as cpm
-import cellprofiler.image as cpi
-import cellprofiler.measurement as cpmm
-import cellprofiler.object as cpo
-from centrosome.outline import outline
-import cellprofiler.modules.measureimageareaoccupied as mia
 
 OBJECTS_NAME = "MyObjects"
 
 
 class TestMeasureImageArea(unittest.TestCase):
     def make_workspace(self, labels, parent_image=None):
-        object_set = cpo.ObjectSet()
-        objects = cpo.Objects()
+        object_set = cellprofiler.object.ObjectSet()
+        objects = cellprofiler.object.Objects()
         objects.segmented = labels
         objects.parent_image = parent_image
         object_set.add_objects(objects, OBJECTS_NAME)
 
-        pipeline = cpp.Pipeline()
-        module = mia.MeasureImageAreaOccupied()
+        pipeline = cellprofiler.pipeline.Pipeline()
+        module = cellprofiler.modules.measureimageareaoccupied.MeasureImageAreaOccupied()
         module.module_num = 1
         module.operands[0].operand_objects.value = OBJECTS_NAME
         pipeline.add_module(module)
-        image_set_list = cpi.ImageSetList()
-        workspace = cpw.Workspace(pipeline, module,
-                                  image_set_list.get_image_set(0),
-                                  object_set,
-                                  cpmm.Measurements(),
-                                  image_set_list)
+        image_set_list = cellprofiler.image.ImageSetList()
+        workspace = cellprofiler.workspace.Workspace(pipeline, module,
+                                                     image_set_list.get_image_set(0),
+                                                     object_set,
+                                                     cellprofiler.measurement.Measurements(),
+                                                     image_set_list)
         return workspace
 
     def test_00_00_zeros(self):
-        workspace = self.make_workspace(np.zeros((10, 10), int))
+        workspace = self.make_workspace(numpy.zeros((10, 10), int))
         module = workspace.module
         module.operands[0].operand_choice.value = "Objects"
         module.run(workspace)
@@ -59,15 +53,15 @@ class TestMeasureImageArea(unittest.TestCase):
         self.assertEqual(m.get_current_measurement("Image", mn("TotalArea")), 100)
 
         columns = module.get_measurement_columns(workspace.pipeline)
-        features = m.get_feature_names(cpmm.IMAGE)
+        features = m.get_feature_names(cellprofiler.measurement.IMAGE)
         self.assertEqual(len(columns), len(features))
         for column in columns:
             self.assertTrue(column[1] in features)
 
     def test_01_01_one_object(self):
-        labels = np.zeros((10, 10), int)
+        labels = numpy.zeros((10, 10), int)
         labels[2:7, 3:8] = 1
-        area_occupied = np.sum(labels)
+        area_occupied = numpy.sum(labels)
         workspace = self.make_workspace(labels)
         module = workspace.module
         module.operands[0].operand_choice.value = "Objects"
@@ -81,14 +75,14 @@ class TestMeasureImageArea(unittest.TestCase):
         self.assertEqual(m.get_current_measurement("Image", mn("TotalArea")), 100)
 
     def test_01_02_object_with_cropping(self):
-        labels = np.zeros((10, 10), int)
+        labels = numpy.zeros((10, 10), int)
         labels[0:7, 3:8] = 1
-        mask = np.zeros((10, 10), bool)
+        mask = numpy.zeros((10, 10), bool)
         mask[1:9, 1:9] = True
-        image = cpi.Image(np.zeros((10, 10)), mask=mask)
-        area_occupied = np.sum(labels[mask])
-        perimeter = np.sum(outline(np.logical_and(labels, mask)))
-        total_area = np.sum(mask)
+        image = cellprofiler.image.Image(numpy.zeros((10, 10)), mask=mask)
+        area_occupied = numpy.sum(labels[mask])
+        perimeter = numpy.sum(centrosome.outline.outline(numpy.logical_and(labels, mask)))
+        total_area = numpy.sum(mask)
         workspace = self.make_workspace(labels, image)
         module = workspace.module
         module.operands[0].operand_choice.value = "Objects"
@@ -103,16 +97,16 @@ class TestMeasureImageArea(unittest.TestCase):
         self.assertEqual(m.get_current_measurement("Image", mn("TotalArea")), total_area)
 
     def test_02_01_get_measurement_columns(self):
-        module = mia.MeasureImageAreaOccupied()
+        module = cellprofiler.modules.measureimageareaoccupied.MeasureImageAreaOccupied()
         module.operands[0].operand_objects.value = OBJECTS_NAME
         module.operands[0].operand_choice.value = "Objects"
         columns = module.get_measurement_columns(None)
-        expected = ((cpmm.IMAGE, "AreaOccupied_AreaOccupied_%s" % OBJECTS_NAME,
-                     cpmm.COLTYPE_FLOAT),
-                    (cpmm.IMAGE, "AreaOccupied_Perimeter_%s" % OBJECTS_NAME,
-                     cpmm.COLTYPE_FLOAT),
-                    (cpmm.IMAGE, "AreaOccupied_TotalArea_%s" % OBJECTS_NAME,
-                     cpmm.COLTYPE_FLOAT))
+        expected = ((cellprofiler.measurement.IMAGE, "AreaOccupied_AreaOccupied_%s" % OBJECTS_NAME,
+                     cellprofiler.measurement.COLTYPE_FLOAT),
+                    (cellprofiler.measurement.IMAGE, "AreaOccupied_Perimeter_%s" % OBJECTS_NAME,
+                     cellprofiler.measurement.COLTYPE_FLOAT),
+                    (cellprofiler.measurement.IMAGE, "AreaOccupied_TotalArea_%s" % OBJECTS_NAME,
+                     cellprofiler.measurement.COLTYPE_FLOAT))
         self.assertEqual(len(columns), len(expected))
         for column in columns:
             self.assertTrue(any([all([cf == ef for cf, ef in zip(column, ex)])

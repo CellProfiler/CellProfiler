2c3484a2963c8f8459b7b4bea426f2bf879af477
Allen Goodman <allen.goodman@icloud.com>
2017-08-24 13:26:17 -0400

    clear border (image segmentation) module (#2799)
    * clear border (image segmentation) module
    
    * clearborder test
    
    * inherit from ObjectProcessing rather than ImageSegmentation
    
    * updated tests

---
+++ cellprofiler/modules/clearborder.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Clear objects connected to the label image border.
+
+"""
+
+import skimage.measure
+import skimage.segmentation
+
+import cellprofiler.module
+import cellprofiler.object
+import cellprofiler.setting
+
+
+class ClearBorder(cellprofiler.module.ObjectProcessing):
+    module_name = "ClearBorder"
+
+    variable_revision_number = 1
+
+    def create_settings(self):
+        super(ClearBorder, self).create_settings()
+
+        self.buffer_size = cellprofiler.setting.Integer(
+            doc="""
+            The width of the border examined. By default, only objects that 
+            touch the outside of the image are removed.
+            """,
+            minval=0,
+            text="Buffer size",
+            value=0
+        )
+
+    def settings(self):
+        __settings__ = super(ClearBorder, self).settings()
+
+        return __settings__ + [
+            self.buffer_size
+        ]
+
+    def visible_settings(self):
+        __settings__ = super(ClearBorder, self).visible_settings()
+
+        return __settings__ + [
+            self.buffer_size
+        ]
+
+    def run(self, workspace):
+        self.function = skimage.segmentation.clear_border
+
+        super(ClearBorder, self).run(workspace)

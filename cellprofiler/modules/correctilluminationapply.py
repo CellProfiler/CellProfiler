5481b3fa40435dbe4cfe3734b8bff2dd3a232e51
vchernys <30272336+vchernys@users.noreply.github.com>
2017-08-29 10:40:36 -0400

    corrected indentations

--- cellprofiler/modules/correctilluminationapply.py
+++ cellprofiler/modules/correctilluminationapply.py
@@ -4,7 +4,7 @@
 CorrectIlluminationApply
 ========================
 
-**Correct Illumination - Apply** applies an illumination function,
+**CorrectIlluminationApply** applies an illumination function,
 usually created by **CorrectIlluminationCalculate**, to an image in
 order to correct for uneven illumination (uneven shading).
 
@@ -64,39 +64,43 @@ class CorrectIlluminationApply(cpm.Module):
         '''Add an image and its settings to the list of images'''
         image_name = cps.ImageNameSubscriber(
             "Select the input image",
-            cps.NONE, doc="""
-            Select the image to be corrected.""")
+            cps.NONE,
+            doc="Select the image to be corrected.")
 
         corrected_image_name = cps.ImageNameProvider(
             "Name the output image",
-            "CorrBlue", doc="""
-            Enter a name for the corrected image.""")
+            "CorrBlue",
+            doc="Enter a name for the corrected image.")
 
         illum_correct_function_image_name = cps.ImageNameSubscriber(
             "Select the illumination function",
-            cps.NONE, doc="""
-            Select the illumination correction function image that will be used to
-            carry out the correction. This image is usually produced by another
-            module or loaded as a .mat format image using the **Images** module or
-            **LoadSingleImage**.""")
+            cps.NONE,
+            doc="""\
+Select the illumination correction function image that will be used to
+carry out the correction. This image is usually produced by another
+module or loaded as a .mat format image using the **Images** module or
+**LoadSingleImage**.
+"""
+        )
 
         divide_or_subtract = cps.Choice(
             "Select how the illumination function is applied",
-            [DOS_DIVIDE, DOS_SUBTRACT], doc="""
-            This choice depends on how the illumination function was calculated and
-            on your physical model of the way illumination variation affects the
-            background of images relative to the objects in images; it is also
-            somewhat empirical.
-            
-            -  *%(DOS_SUBTRACT)s:* Use this option if the background signal is
-               significant relative to the real signal coming from the cells. If you
-               created the illumination correction function using
-               *%(IC_BACKGROUND)s*, then you will want to choose
-               *%(DOS_SUBTRACT)s* here.
-            -  *%(DOS_DIVIDE)s:* Choose this option if the signal to background
-               ratio is high (the cells are stained very strongly). If you created
-               the illumination correction function using *%(IC_REGULAR)s*, then
-               you will want to choose *%(DOS_DIVIDE)s* here.
+            [DOS_DIVIDE, DOS_SUBTRACT],
+            doc="""\
+This choice depends on how the illumination function was calculated and
+on your physical model of the way illumination variation affects the
+background of images relative to the objects in images; it is also
+somewhat empirical.
+
+-  *%(DOS_SUBTRACT)s:* Use this option if the background signal is
+   significant relative to the real signal coming from the cells. If you
+   created the illumination correction function using
+   *%(IC_BACKGROUND)s*, then you will want to choose
+   *%(DOS_SUBTRACT)s* here.
+-  *%(DOS_DIVIDE)s:* Choose this option if the signal to background
+   ratio is high (the cells are stained very strongly). If you created
+   the illumination correction function using *%(IC_REGULAR)s*, then
+   you will want to choose *%(DOS_DIVIDE)s* here.
             """ % globals())
 
         image_settings = cps.SettingsGroup()

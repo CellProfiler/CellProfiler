ccf54129577d24d351663cf51c099759daa5d55c
mcquin <mcquin@broadinstitute.org>
2017-08-31 11:39:45 -0400

    fix printing

--- cellprofiler/gui/cpframe.py
+++ cellprofiler/gui/cpframe.py
@@ -1092,8 +1092,7 @@ class CPFrame(wx.Frame):
     @staticmethod
     def print_help(event, module_name, help_text):
         """Print the help text for a module"""
-        printer = wx.html.HtmlEasyPrinting("Printing %s" % module_name,
-                                           event.GetEventObject())
+        printer = wx.html.HtmlEasyPrinting("Printing %s" % module_name)
         printer.GetPrintData().SetPaperId(wx.PAPER_LETTER)
         printer.PrintText(help_text)
 

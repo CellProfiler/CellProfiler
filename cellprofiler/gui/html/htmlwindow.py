2e3ce7c61c13feaadf04900c7bb69c075eaf6fb4
mcquin <mcquin@broadinstitute.org>
2017-09-02 22:50:59 -0400

    refactor welcome screen

--- cellprofiler/gui/html/htmlwindow.py
+++ cellprofiler/gui/html/htmlwindow.py
@@ -1,118 +1,34 @@
 # coding=utf-8
-# import cellprofiler.gui.help
-# import cellprofiler.gui.html.utils
-from cellprofiler.preferences import get_background_color, set_startup_blurb
 
-import cellprofiler.icons
 import os
-import sys
 import urllib
-import urllib2
 import webbrowser
-import wx
+
+import sys
 import wx.html
 
+import cellprofiler.icons
+import utils
+
 MEMORY_SCHEME = "memory:"
-WELCOME_SCREEN_FRAME = "WelcomeScreenFrame"
 
 
 class HtmlClickableWindow(wx.html.HtmlWindow):
     def __init__(self, *args, **kwargs):
         wx.html.HtmlWindow.__init__(self, *args, **kwargs)
 
-    def load_startup_blurb(self):
-        self.OnLinkClicked(wx.html.HtmlLinkInfo('startup_main', ''))
-
-    # def OnLinkClicked(self, linkinfo):
-    #     href = linkinfo.Href
-    #     if href.startswith("#"):
-    #         super(HtmlClickableWindow, self).OnLinkClicked(linkinfo)
-    #     elif href.startswith('http://'):
-    #         webbrowser.open(href)
-    #     elif href.startswith('pref:'):
-    #         if 'no_display' in href:
-    #             set_startup_blurb(False)
-    #             # Find the parent frame and, if it's the welcome screen frame,
-    #             # "close" it (= hide it)
-    #             #
-    #             parent = self.Parent
-    #             while parent is not None:
-    #                 if parent.Name == WELCOME_SCREEN_FRAME:
-    #                     parent.Close()
-    #                     break
-    #                 parent = parent.Parent
-    #     elif href.startswith('help:'):
-    #         href = linkinfo.Href[7:]
-    #         html_str = cellprofiler.gui.help.WELCOME_HELP[href]
-    #         html_str += '<p>Go <a href="startup_main">back</a> to the welcome screen.</p>'
-    #         self.SetPage(html_str)
-    #     elif href.startswith('load:'):
-    #         pipeline_filename = href[5:]
-    #         try:
-    #             fd = urllib2.urlopen(pipeline_filename)
-    #             if fd.code < 200 or fd.code > 299:
-    #                 wx.MessageBox(
-    #                         "Sorry, the link, \"%s\" is broken, please contact the webmaster" %
-    #                         pipeline_filename,
-    #                         caption="Unable to access pipeline via internet",
-    #                         style=wx.OK | wx.ICON_INFORMATION)
-    #                 return
-    #             wx.CallAfter(wx.GetApp().frame.pipeline.load, fd)
-    #         except:
-    #             wx.MessageBox(
-    #                     'CellProfiler was unable to load %s' %
-    #                     pipeline_filename, "Error loading pipeline",
-    #                     style=wx.OK | wx.ICON_ERROR)
-    #     elif href.startswith('loadexample:'):
-    #         # Same as "Load", but specific for example pipelines so the user can be directed as to what to do next.
-    #         pipeline_filename = href[12:]
-    #
-    #         try:
-    #             import cellprofiler
-    #
-    #             # Try loading an example pipeline corresponding to the running version of CellProfiler.
-    #             fd = urllib.urlopen(pipeline_filename.format(cellprofiler.__version__))
-    #             if fd.code < 200 or fd.code > 200:
-    #                 # Try loading the example pipeline from CellProfiler/examples master branch.
-    #                 fd = urllib.urlopen(pipeline_filename.format("master"))
-    #
-    #             if fd.code < 200 or fd.code > 299:
-    #                 wx.MessageBox(
-    #                         "Sorry, the link, \"%s\" is broken, please contact the webmaster" %
-    #                         pipeline_filename.format("master"),
-    #                         caption="Unable to access pipeline via internet",
-    #                         style=wx.OK | wx.ICON_INFORMATION)
-    #                 return
-    #
-    #             def fn(fd=fd):
-    #                 import cellprofiler.modules.loaddata
-    #                 pipeline = wx.GetApp().frame.pipeline
-    #                 pipeline.load(fd)
-    #                 for module in pipeline.modules():
-    #                     if isinstance(module, cellprofiler.modules.loaddata.LoadData):
-    #                         # Would prefer to call LoadData's do_reload but not sure how at this point
-    #                         global header_cache
-    #                         header_cache = {}
-    #                         try:
-    #                             module.open_csv()
-    #                         except:
-    #                             pass
-    #                 wx.MessageBox(
-    #                         'Now that you have loaded an example pipeline, press the "Analyze images" button to access and process a small image set from the CellProfiler website so you can see how CellProfiler works.',
-    #                         '', wx.ICON_INFORMATION)
-    #
-    #             wx.CallAfter(fn)
-    #         except:
-    #             wx.MessageBox(
-    #                     'CellProfiler was unable to load %s' %
-    #                     pipeline_filename.format("master"), "Error loading pipeline",
-    #                     style=wx.OK | wx.ICON_ERROR)
-    #     else:
-    #         newpage = cellprofiler.gui.html.utils.find_link(href)
-    #         if newpage is not None:
-    #             self.SetPage(newpage)
-    #         else:
-    #             super(HtmlClickableWindow, self).OnLinkClicked(linkinfo)
+    def OnLinkClicked(self, linkinfo):
+        href = linkinfo.Href
+        if href.startswith("#"):
+            super(HtmlClickableWindow, self).OnLinkClicked(linkinfo)
+        elif href.startswith('http://'):
+            webbrowser.open(href)
+        else:
+            newpage = utils.find_link(href)
+            if newpage is not None:
+                self.SetPage(newpage)
+            else:
+                super(HtmlClickableWindow, self).OnLinkClicked(linkinfo)
 
     def OnOpeningURL(self, file_format, url):
         if file_format == wx.html.HTML_URL_IMAGE:

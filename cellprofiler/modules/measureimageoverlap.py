58e587c94c67b5bf53500692c4879af8f814fd24
mcquin <mcquincl@gmail.com>
2017-09-14 09:44:35 -0400

    unique links

--- cellprofiler/modules/measureimageoverlap.py
+++ cellprofiler/modules/measureimageoverlap.py
@@ -99,12 +99,9 @@ References
 
 -  Collins LM, Dent CW (1998) “Omega: A general formulation of the Rand
    Index of cluster recovery suitable for non-disjoint solutions”,
-   *Multivariate Behavioral Research*, 23, 231-242 `(link)`_
-
+   *Multivariate Behavioral Research*, 23, 231-242. `(link) <http://dx.doi.org/10.1207/s15327906mbr2302_6>`__
 -  Pele O, Werman M (2009) “Fast and Robust Earth Mover’s Distances”,
-   *2009 IEEE 12th International Conference on Computer Vision*
-
-.. _(link): http://dx.doi.org/10.1207/s15327906mbr2302_6
+   *2009 IEEE 12th International Conference on Computer Vision*.
 """
 
 import centrosome.cpmorphology

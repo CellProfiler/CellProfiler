a7f20631721284d4042ee9d368df9ef51966c462
Anne Carpenter <anne@broadinstitute.org>
2017-09-20 11:27:23 -0400

    Documentation - DisplayPlatemap clarify saving

--- cellprofiler/modules/displayplatemap.py
+++ cellprofiler/modules/displayplatemap.py
@@ -16,6 +16,12 @@ so the value shown for each well is current up to the image cycle
 currently being processed; wells that have no corresponding
 measurements as yet are shown as blank. 
 
+At this time, the display produced when **DisplayPlatemap** is run as a
+module cannot be saved in the pipeline (e.g., by using SaveImages). The
+display can be saved manually by selecting the window produced by the
+module and clicking the Save icon in its menu bar or by choosing File
+> Save from CellProfiler's main menu bar.
+
 See also other **Display** modules and data tools.
 """
 

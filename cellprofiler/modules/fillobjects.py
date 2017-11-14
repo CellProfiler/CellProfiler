98250aeaa83746588d7cb2b7d68a1a5301a826c2
Matthew Bowden <bowdenm@spu.edu>
2017-11-14 11:46:20 -0800

    Add connectivity as parameter and remove redundant object removal
    Corrections to FillObjects settings

--- cellprofiler/modules/fillobjects.py
+++ cellprofiler/modules/fillobjects.py
@@ -4,7 +4,7 @@
 FillObjects
 ===========
 
-**FillObjects** fills holes within all objects in an image
+**FillObjects** fills holes within all objects in an image.
 
 This module works best on integer-labeled images (i.e., the output of **ConvertObjectsToImage**
 when the color format is *uint16*).
@@ -26,7 +26,6 @@ YES          YES          NO
 
 import numpy
 import skimage.morphology
-import scipy.ndimage.morphology
 
 import cellprofiler.image
 import cellprofiler.module
@@ -44,8 +43,8 @@ class FillObjects(cellprofiler.module.ImageProcessing):
         super(FillObjects, self).create_settings()
 
         self.size = cellprofiler.setting.Float(
-            text="Size",
-            value=1.0,
+            text="Minimum hole size",
+            value=64.,
             doc="Holes smaller than this diameter will be filled."
         )
 
@@ -64,12 +63,13 @@ class FillObjects(cellprofiler.module.ImageProcessing):
         ]
 
     def run(self, workspace):
-        self.function = lambda image, diameter: remove_objects_and_fill_holes(image, diameter)
+        self.function = lambda image, diameter: \
+            fill_object_holes(image, diameter)
 
         super(FillObjects, self).run(workspace)
 
 
-def remove_objects_and_fill_holes(image, diameter):
+def fill_object_holes(image, diameter):
     radius = diameter / 2.0
 
     if image.ndim == 2 or image.shape[-1] in (3, 4):
@@ -79,14 +79,12 @@ def remove_objects_and_fill_holes(image, diameter):
 
     min_obj_size = numpy.pi * factor
 
-    removed = skimage.morphology.remove_small_objects(image, min_obj_size)
-
     # Iterate through each label as a mask, fill holes on the mask, and reapply to original image
-    for n in numpy.unique(removed):
+    for n in numpy.unique(image):
         if n == 0:
             continue
 
-        filled_mask = scipy.ndimage.morphology.binary_fill_holes(removed == n)
-        removed[filled_mask] = n
+        filled_mask = skimage.morphology.remove_small_holes(image == n, min_obj_size)
+        image[filled_mask] = n
 
-    return removed
+    return image

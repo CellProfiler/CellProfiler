7a18bd72311a1b83ffe3f8de18db810e86965bd2
Matthew Bowden <bowdenm@spu.edu>
2017-11-21 14:40:44 -0800

    Add slicewise option to FillObjects

--- cellprofiler/modules/fillobjects.py
+++ cellprofiler/modules/fillobjects.py
@@ -48,43 +48,67 @@ class FillObjects(cellprofiler.module.ObjectProcessing):
             doc="Holes smaller than this diameter will be filled."
         )
 
+        self.slice_wise = cellprofiler.setting.Binary(
+            text="Slice wise fill",
+            value=False,
+            doc="""\
+Select "*{YES}*" to fill objects on a per-slice level. 
+This will perform the hole filling on each slice of a 
+volumetric image, rather than on the image as a whole. 
+This may be helpful for removing seed artifacts that 
+are the result of segmentation.
+**Note**: Slice-wise operations will be considerably slower.
+""".format(**{
+                "YES": cellprofiler.setting.YES
+            })
+        )
+
     def settings(self):
         __settings__ = super(FillObjects, self).settings()
 
         return __settings__ + [
-            self.size
+            self.size,
+            self.slice_wise
         ]
 
     def visible_settings(self):
         __settings__ = super(FillObjects, self).visible_settings()
 
         return __settings__ + [
-            self.size
+            self.size,
+            self.slice_wise
         ]
 
     def run(self, workspace):
-        self.function = lambda labels, diameter: \
-            fill_object_holes(labels, diameter)
+        self.function = lambda labels, diameter, slicewise: \
+            fill_object_holes(labels, diameter, slicewise)
 
         super(FillObjects, self).run(workspace)
 
 
-def fill_object_holes(labels, diameter):
+def _fill_holes(labels, min_obj_size):
+    array = labels.copy()
+    # Iterate through each label as a mask, fill holes on the mask, and reapply to original image
+    for n in numpy.unique(array):
+        if n == 0:
+            continue
+
+        filled_mask = skimage.morphology.remove_small_holes(array == n, min_obj_size)
+        array[filled_mask] = n
+    return array
+
+
+def fill_object_holes(labels, diameter, slicewise):
     radius = diameter / 2.0
 
-    if labels.ndim == 2 or labels.shape[-1] in (3, 4):
+    if labels.ndim == 2 or labels.shape[-1] in (3, 4) or slicewise:
         factor = radius ** 2
     else:
         factor = (4.0 / 3.0) * (radius ** 3)
 
     min_obj_size = numpy.pi * factor
 
-    # Iterate through each label as a mask, fill holes on the mask, and reapply to original image
-    for n in numpy.unique(labels):
-        if n == 0:
-            continue
+    if not slicewise:
+        return _fill_holes(labels, min_obj_size)
+    return numpy.array([_fill_holes(x, min_obj_size) for x in labels])
 
-        filled_mask = skimage.morphology.remove_small_holes(labels == n, min_obj_size)
-        labels[filled_mask] = n
-
-    return labels

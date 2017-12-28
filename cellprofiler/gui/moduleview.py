7eac6aa9b45820d1dd553cc4f1ef9ad38e5a931c
Matthew Bowden <bowdenm@spu.edu>
2017-12-28 13:36:35 -0800

    All moduleview option text should be left-aligned

--- cellprofiler/gui/moduleview.py
+++ cellprofiler/gui/moduleview.py
@@ -472,7 +472,7 @@ class ModuleView(object):
                     control = self.make_binary_control(v, control_name, control)
                     flag = wx.ALIGN_LEFT
                     text_sizer_item.Flag = \
-                        wx.ALIGN_RIGHT | wx.ALIGN_CENTER_VERTICAL | wx.ALL
+                        wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL | wx.ALL
                 elif isinstance(v, cellprofiler.setting.MeasurementMultiChoice):
                     control = self.make_measurement_multichoice_control(
                         v, control_name, control)

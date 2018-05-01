0fc31a73119e201f0ee7b975eae2fd1d91bb9987
Matthew Bowden <bowdenm@spu.edu>
2018-05-01 14:29:36 -0700

    Pass default settings along if nothing changed

--- cellprofiler/modules/watershed.py
+++ cellprofiler/modules/watershed.py
@@ -326,4 +326,7 @@ the image is not downsampled.
 
             variable_revision_number = 2
 
+        else:
+            __settings__ = setting_values
+
         return __settings__, variable_revision_number, from_matlab

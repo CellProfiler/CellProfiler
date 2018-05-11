5a1a1bc9b7eef2c75c99d769293192f363d9a7fb
Anne Carpenter <anne@broadinstitute.org>
2018-05-11 08:32:09 -0400

    Update matchtemplate.py

--- cellprofiler/modules/matchtemplate.py
+++ cellprofiler/modules/matchtemplate.py
@@ -9,7 +9,7 @@ match a template to a single-channel two-or-three dimensional image or
 multi-channel two-dimensional image. The output of the module is an
 image where each pixel corresponds to the `Pearson product-moment
 correlation coefficient`_ between the image and the template. Practically, this 
-allows you to crop a single object of interest (ie a cell) and predict where
+allows you to crop a single object of interest (i.e., a cell) and predict where
 other such objects are in the image.  Note that this is not rotation invariant, so
 this module will perform best when objects are approximately round or are angled
 in a relatively unified direction.

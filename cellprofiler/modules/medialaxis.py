4015f52189b7398bfb08517bbfbab2fc8120891e
Anne Carpenter <anne@broadinstitute.org>
2018-05-11 08:35:34 -0400

    Update medialaxis.py

--- cellprofiler/modules/medialaxis.py
+++ cellprofiler/modules/medialaxis.py
@@ -6,7 +6,7 @@ MedialAxis
 
 **MedialAxis** computes the medial axis or topological skeleton of a binary image. Rather than by sequentially
 removing pixels as in **MorphologicalSkeleton**, the medial axis is computed based on the 
-distance transform of the thresholded image (ie the distance each foreground pixel is 
+distance transform of the thresholded image (i.e., the distance each foreground pixel is 
 from a background pixel).  See `this tutorial`_ for more information. 
 
 |

64e796371b51e1b57e7e2988a314e198096d59e5
Matthew Bowden <bowdenm@spu.edu>
2018-09-13 22:03:38 -0700

    Demote the update exception from warning to debug
    This message is often part of peoples' error messages on
    GitHub/the forum, and it's a bit misleading. Since this site
    is no longer maintained, we shouldn't let users know about this
    error anyway.

--- cellprofiler/utilities/check_for_updates.py
+++ cellprofiler/utilities/check_for_updates.py
@@ -31,7 +31,7 @@ class VersionChecker(threading.Thread):
             if new_version > self.current_version:
                 self.callback(new_version, info)
         except Exception, e:
-            logger.warning("Exception fetching new version information from %s: %s"%(self.url, e))
+            logger.debug("NON-CRITICAL: Exception fetching new version information from %s: %s"%(self.url, e))
             pass # no worries
 
 def check_for_updates(url, current_version, callback, user_agent='CellProfiler_cfu'):

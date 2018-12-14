ad0194dec84e083532c191c157649aae5e329c2c
Beth Cimini <bethac07@users.noreply.github.com>
2018-12-14 14:52:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setuptools.setup(
         "matplotlib>=2.0.0, !=2.1.0",
         "mysqlclient==1.3.13",
         "numpy",
-        "prokaryote==2.4.0",
+        "prokaryote==2.4.1",
         "python-bioformats==1.5.2",
         "pyzmq==15.3.0",
         "raven",

2a5b189f95b0483e6ca952d50a5547f9f5a6a082
Allen Goodman <allen.goodman@icloud.com>
2019-11-04 13:21:03 -0500

    v6

--- nucleus/pipeline/io/__init__.py
+++ nucleus/pipeline/io/__init__.py
@@ -6,8 +6,11 @@ standard library modules.
 """
 
 from ._v5 import dump as dump_v5
+from ._v6 import dump as dump_v6
 
 
 def dump(pipeline, fp, save_image_plane_details=True, version=5):
     if version == 5:
         dump_v5(pipeline, fp, save_image_plane_details)
+    elif version == 6:
+        dump_v6(pipeline, fp, save_image_plane_details)

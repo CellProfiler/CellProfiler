3f81420b9a834959d616ffc9fdd4f5cc5e54aae4
David Stirling <dstirling@broadinstitute.org>
2020-04-13 14:56:46 -0400

    Temp fixes for workers

--- cellprofiler_core/setting/text/text.py
+++ cellprofiler_core/setting/text/text.py
@@ -633,6 +633,31 @@ def filter_duplicate_names(name_list):
     return [name_dict[n[0]] for n in name_list]
 
 
+def get_name_providers(pipeline, last_setting):
+    """Scan the pipeline to find name providers matching the name given in the setting
+    pipeline - pipeline to scan
+    last_setting - scan the modules in order until you arrive at this setting
+    returns a list of providers that provide a correct "thing" with the
+    same name as that of the subscriber
+    """
+    choices = []
+    for module in pipeline.modules(False):
+        module_choices = []
+        for setting in module.visible_settings():
+            if setting.key() == last_setting.key():
+                return choices
+            if (
+                isinstance(setting, NameProvider)
+                and setting != "Do not use"
+                and module.enabled
+                and last_setting.matches(setting)
+                and setting.value == last_setting.value
+            ):
+                module_choices.append(setting)
+        choices += module_choices
+    assert False, "Setting not among visible settings in pipeline"
+
+
 def get_name_provider_choices(pipeline, last_setting, group):
     """Scan the pipeline to find name providers for the given group
 

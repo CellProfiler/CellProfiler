8d397e2a9595f7e6f9af8aa7c60f0de0aeb0a470
Allen Goodman <allen.goodman@icloud.com>
2020-05-08 14:06:40 -0400

    update wxPython to 4.1.0

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setuptools.setup(
         "scikit-learn>=0.20",
         "scipy>=1.4.1",
         "six",
-        "wxPython>=4.0.7",
+        "wxPython>=4.1.0",
     ],
     license="BSD",
     name="CellProfiler",

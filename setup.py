d1b87e7e6b7baaa04bc2d5b1efe2361458d3d57c
Allen Goodman <allen.goodman@icloud.com>
2020-06-08 11:04:44 -0400

    4.0.0rc7

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8, <4",
     url="https://github.com/CellProfiler/core",
-    version="4.0.0rc6",
+    version="4.0.0rc7",
 )

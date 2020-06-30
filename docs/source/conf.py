5c6feae655b0a4d90388778cb4f7cc3b0d38d725
Allen Goodman <allen.goodman@icloud.com>
2020-06-30 16:14:28 -0400

    4.0.0 docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@
 import os
 import sys
 
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 
@@ -34,9 +36,10 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
+    "sphinx.ext.githubpages",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -89,7 +92,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

7a7939e9c1c9831257c5306ece48aee32128ff31
Allen Goodman <allen.goodman@icloud.com>
2020-07-20 15:54:44 -0400

    aliases

---
+++ cellprofiler_core/image/abstract_image_provider/__init__.py
@@ -0,0 +1,7 @@
+from ._abstract_image_provider import AbstractImageProvider
+
+from .load_images_image_provider import LoadImagesFlexFrameProvider
+from .load_images_image_provider import LoadImagesImageProvider
+from .load_images_image_provider import LoadImagesImageProviderURL
+from .load_images_image_provider import LoadImagesMovieFrameProvider
+from .load_images_image_provider import LoadImagesSTKFrameProvider

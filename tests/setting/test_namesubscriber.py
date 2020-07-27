fce01cf814ae96cfd66e63027ced3acd0c36e858
Allen Goodman <allen.goodman@icloud.com>
2020-07-27 15:08:48 -0400

    fixed test_namesubscriber.py

--- tests/setting/test_namesubscriber.py
+++ tests/setting/test_namesubscriber.py
@@ -2,54 +2,54 @@
 
 import unittest
 
-import cellprofiler_core.setting
+from cellprofiler_core.setting.subscriber import ImageListSubscriber, LabelListSubscriber
 
 
 class TestListNameSubscriber(unittest.TestCase):
     def test_load_image_list_empty(self):
-        s = cellprofiler_core.setting.ListImageNameSubscriber("foo")
+        s = ImageListSubscriber("foo")
         self.assertEqual(s.value_text, "")
         self.assertEqual(s.value, [])
 
     def test_load_image_list_single(self):
-        s = cellprofiler_core.setting.ListImageNameSubscriber("foo", value="SampleName")
+        s = ImageListSubscriber("foo", value="SampleName")
         self.assertEqual(s.value_text, "SampleName")
         self.assertEqual(s.value, ["SampleName"])
 
     def test_load_image_list_multiple(self):
-        s = cellprofiler_core.setting.ListImageNameSubscriber(
+        s = ImageListSubscriber(
             "foo", value="SampleName1, SampleName2"
         )
         self.assertEqual(s.value_text, "SampleName1, SampleName2")
         self.assertEqual(s.value, ["SampleName1", "SampleName2"])
 
     def test_set_image_list(self):
-        s = cellprofiler_core.setting.ListImageNameSubscriber("foo")
+        s = ImageListSubscriber("foo")
         s.value = "SampleName3, SampleName4"
         self.assertEqual(s.value_text, "SampleName3, SampleName4")
         self.assertEqual(s.value, ["SampleName3", "SampleName4"])
 
     def test_load_object_list_empty(self):
-        s = cellprofiler_core.setting.ListObjectNameSubscriber("foo")
+        s = LabelListSubscriber("foo")
         self.assertEqual(s.value_text, "")
         self.assertEqual(s.value, [])
 
     def test_load_object_list_single(self):
-        s = cellprofiler_core.setting.ListObjectNameSubscriber(
+        s = LabelListSubscriber(
             "foo", value="SampleName"
         )
         self.assertEqual(s.value_text, "SampleName")
         self.assertEqual(s.value, ["SampleName"])
 
     def test_load_object_list_multiple(self):
-        s = cellprofiler_core.setting.ListObjectNameSubscriber(
+        s = LabelListSubscriber(
             "foo", value="SampleName1, SampleName2"
         )
         self.assertEqual(s.value_text, "SampleName1, SampleName2")
         self.assertEqual(s.value, ["SampleName1", "SampleName2"])
 
     def test_set_object_list(self):
-        s = cellprofiler_core.setting.ListObjectNameSubscriber("foo")
+        s = LabelListSubscriber("foo")
         s.value = "SampleName3, SampleName4"
         self.assertEqual(s.value_text, "SampleName3, SampleName4")
         self.assertEqual(s.value, ["SampleName3", "SampleName4"])

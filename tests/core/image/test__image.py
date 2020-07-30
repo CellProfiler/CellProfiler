c7bbd73f288dd92f2f0c1f269ed36e0e46a04d3f
Allen Goodman <allen.goodman@icloud.com>
2020-07-30 12:21:19 -0400

    tests

--- tests/core/image/test__image.py
+++ tests/core/image/test__image.py
@@ -1,24 +1,60 @@
+import numpy
+import numpy.random
+import numpy.testing
+
+from cellprofiler_core.image import Image
+
+
 class TestImage:
     def test_multichannel(self):
-        pass
+        x = numpy.zeros((224, 224, 3), numpy.float32)
+
+        image = Image(x)
+
+        assert image.multichannel
 
     def test_volumetric(self):
-        pass
+        x = numpy.zeros((100, 224, 224, 3), numpy.float32)
+
+        image = Image(x, dimensions=3)
+
+        assert image.volumetric
 
     def test_spacing(self):
-        pass
+        x = numpy.zeros((100, 224, 224, 3), numpy.float32)
 
-    def test_spacing(self):
-        pass
+        image = Image(x, dimensions=3)
+
+        assert image.spacing == (1.0, 1.0, 1.0)
 
     def test_get_image(self):
-        pass
+        x = numpy.zeros((224, 224, 3), numpy.float32)
+
+        image = Image(x)
+
+        numpy.testing.assert_array_equal(image.get_image(), x)
 
     def test_set_image(self):
-        pass
+        x = numpy.zeros((224, 224, 3), numpy.float32)
+
+        image = Image(x)
+
+        y = numpy.zeros((224, 224, 3), numpy.float32)
+
+        image.set_image(y)
+
+        numpy.testing.assert_array_equal(image.get_image(), y)
 
     def test_has_parent_image(self):
-        pass
+        x = numpy.zeros((224, 224, 3), numpy.float32)
+
+        parent_image = Image(x)
+
+        assert not parent_image.has_parent_image
+
+        image = Image(x, parent_image=parent_image)
+
+        assert image.has_parent_image
 
     def test_has_masking_objects(self):
         pass
@@ -29,13 +65,7 @@ class TestImage:
     def test_mask(self):
         pass
 
-    def test_mask(self):
-        pass
-
     def test_has_mask(self):
-        pass
-
-    def test_crop_mask(self):
         pass
 
     def test_crop_mask(self):

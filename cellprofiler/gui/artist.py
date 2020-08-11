5d49f741a4b77ea40db94675ae2b707894d834ef
David Stirling <dstirling@broadinstitute.org>
2020-08-11 10:06:21 -0400

    Fix wsv "add item" errors

--- cellprofiler/gui/artist.py
+++ cellprofiler/gui/artist.py
@@ -844,7 +844,7 @@ class CPImageArtist(matplotlib.artist.Artist):
         label_fmt = "--- %s ---"
         for key, sequence in breaks:
             label = label_fmt % key
-            if idx >= len(menu_items) or menu_items[idx].Text != label:
+            if idx >= len(menu_items) or menu_items[idx].ItemLabel != label:
                 item = menu.Insert(idx, wx.NewId(), label)
                 item.Enable(False)
                 menu_items.insert(idx, item)
@@ -856,7 +856,7 @@ class CPImageArtist(matplotlib.artist.Artist):
                 name = data.name
                 if (
                     idx == len(menu_items)
-                    or menu_items[idx].Text.startswith("---")
+                    or menu_items[idx].ItemLabel.startswith("---")
                     or menu_items[idx].IsSeparator()
                 ):
                     sub_menu = wx.Menu()

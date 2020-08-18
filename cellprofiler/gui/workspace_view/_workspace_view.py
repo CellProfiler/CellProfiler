79fef9541c6079ecbc525b2a4c4c7ba2707e8004
David Stirling <dstirling@broadinstitute.org>
2020-08-18 16:36:23 -0400

    Fix basic zoom functionality

--- cellprofiler/gui/workspace_view/_workspace_view.py
+++ cellprofiler/gui/workspace_view/_workspace_view.py
@@ -216,22 +216,26 @@ class WorkspaceView:
 
     def scale_axes(self):
         """Set the axes limits appropriate to the images we have"""
+        max_x = max_y = 0
+        for image_row in self.image_rows:
+            if image_row.data.mode != MODE_HIDE:
+                shape = image_row.data.pixel_data.shape
+                max_x = max(shape[1], max_x)
+                max_y = max(shape[0], max_y)
         ax = self.image.axes
-        if self.frame.navtoolbar.is_home():
-            max_x = max_y = 0
-            for image_row in self.image_rows:
-                if image_row.data.mode != MODE_HIDE:
-                    shape = image_row.data.pixel_data.shape
-                    max_x = max(shape[1], max_x)
-                    max_y = max(shape[0], max_y)
-            if max_x > 0 and max_y > 0:
-                max_x -= 0.5
-                max_y -= 0.5
-                ax.set_xlim(-0.5, max_x)
-                ax.set_ylim(-0.5, max_y)
-                ax.invert_yaxis()
-                self.__axes_scale = (max_y, max_x)
-                self.frame.navtoolbar.reset()
+        if self.__axes_scale is not None:
+            init_x, init_y = self.__axes_scale
+            # if float(max_x) != init_x[1] or float(max_y) != init_y[0]:
+            #     self.__axes_scale = None
+            if init_x != ax.get_xlim() or init_y != ax.get_ylim():
+                return
+        if max_x > 0 and max_y > 0:
+            self.image.axes.set_xlim(0, max_x)
+            self.image.axes.set_ylim(0, max_y)
+            self.image.axes.invert_yaxis()
+            self.__axes_scale = ((0.0, float(max_x)), (float(max_y), 0.0))
+            self.frame.navtoolbar.reset()
+            self.image.refresh()
 
     def layout(self):
         self.panel.SetMinSize(

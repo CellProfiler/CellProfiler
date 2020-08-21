7084dce23178f86f7f44dcbd81c09ac974467918
Allen Goodman <allen.goodman@icloud.com>
2020-08-21 16:18:41 -0400

    4.0.0rc10

--- cellprofiler/gui/dialog.py
+++ cellprofiler/gui/dialog.py
@@ -14,7 +14,7 @@ class AboutDialogInfo(wx.adv.AboutDialogInfo):
 
         self.SetName("CellProfiler")
 
-        self.SetVersion("4.0.0rc9")
+        self.SetVersion("4.0.0rc10")
 
 
 class Error(wx.MessageDialog):

becce9b6754f307edbe78355ae61092322700ce5
David Stirling <dstirling@broadinstitute.org>
2020-08-28 15:50:37 -0400

    v4.0.0rc12

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8, <4",
     url="https://github.com/CellProfiler/core",
-    version="4.0.0rc11",
+    version="4.0.0rc12",
     zip_safe=False,
 )

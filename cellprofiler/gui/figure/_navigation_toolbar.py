91deda73f04b2c28112935ed0ba3fee9885a68c7
David Stirling <dstirling@broadinstitute.org>
2020-09-09 10:15:09 -0400

    Move Figure's "Measure Length" feature to the toolbar (#4212)
    * Move Figure's Measure Length feature to toolbar
    
    * Adjust tool position

--- cellprofiler/gui/figure/_navigation_toolbar.py
+++ cellprofiler/gui/figure/_navigation_toolbar.py
@@ -4,6 +4,8 @@ import matplotlib.backends.backend_wxagg
 import wx
 
 from ..constants.figure import NAV_MODE_ZOOM
+from ..constants.figure import NAV_MODE_MEASURE
+from ..constants.figure import BMP_MEASURE
 from ..constants.figure import NAV_MODE_PAN
 from ..constants.figure import EVT_NAV_MODE_CHANGE
 from ..utilities.figure import get_crosshair_cursor
@@ -12,8 +14,21 @@ from ..utilities.figure import get_crosshair_cursor
 class NavigationToolbar(matplotlib.backends.backend_wxagg.NavigationToolbar2WxAgg):
     """Navigation toolbar for EditObjectsDialog"""
 
-    def __init__(self, canvas):
+    def __init__(self, canvas, want_measure=False):
         super(NavigationToolbar, self).__init__(canvas)
+        if want_measure:
+            self.bmp_error = wx.Bitmap(BMP_MEASURE)
+            self.wx_ids["Measure"] = wx.NewId()
+            self.Bind(wx.EVT_TOOL, self.measure, id=self.wx_ids["Measure"])
+            self.measure_tool = self.CreateTool(
+                self.wx_ids["Measure"],
+                "MeasureLength",
+                self.bmp_error,
+                kind=wx.ITEM_CHECK,
+                shortHelp="Measure Length"
+            )
+            self.InsertTool(6, self.measure_tool)
+        self.Realize()
 
     def set_cursor(self, cursor):
         """Set the cursor based on the mode"""
@@ -36,16 +51,51 @@ class NavigationToolbar(matplotlib.backends.backend_wxagg.NavigationToolbar2WxAg
 
             if "Pan" in self.wx_ids:
                 self.ToggleTool(self.wx_ids["Pan"], False)
+        elif self.mode == NAV_MODE_MEASURE:
+            if "Measure" in self.wx_ids:
+                self.ToggleTool(self.wx_ids["Measure"], False)
 
     def zoom(self, *args):
         matplotlib.backends.backend_wxagg.NavigationToolbar2WxAgg.zoom(self, *args)
-
+        if "Measure" in self.wx_ids:
+            self.ToggleTool(self.wx_ids['Measure'], False)
         self.__send_mode_change_event()
 
     def pan(self, *args):
         matplotlib.backends.backend_wxagg.NavigationToolbar2WxAgg.pan(self, *args)
-
+        if "Measure" in self.wx_ids:
+            self.ToggleTool(self.wx_ids['Measure'], False)
         self.__send_mode_change_event()
+
+    def measure(self, *args):
+        self.mode = NAV_MODE_MEASURE
+        if "Pan" in self.wx_ids:
+            self.ToggleTool(self.wx_ids['Pan'], False)
+        if "Zoom" in self.wx_ids:
+            self.ToggleTool(self.wx_ids['Zoom'], False)
+        self.__send_mode_change_event()
+        if self._active == 'MEASURE':
+            self._active = None
+        else:
+            self._active = 'MEASURE'
+
+        if self._idPress is not None:
+            self._idPress = self.canvas.mpl_disconnect(self._idPress)
+            self.mode = ''
+
+        if self._idRelease is not None:
+            self._idRelease = self.canvas.mpl_disconnect(self._idRelease)
+            self.mode = ''
+
+        if self._active:
+            self.canvas.widgetlock(self)
+        else:
+            self.canvas.widgetlock.release(self)
+
+        for a in self.canvas.figure.get_axes():
+            a.set_navigate_mode(self._active)
+
+        self.set_message(self.mode)
 
     def reset(self):
         """Clear out the position stack"""

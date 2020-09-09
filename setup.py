208ee23358b505ee4ded9a394df79067a8594357
Beth Cimini <bethac07@users.noreply.github.com>
2020-09-09 17:33:11 +0000

    4.0.2 (https://github.com/CellProfiler/core/pull/45)
    * Update setup.py
    
    * Update __init__.py
    
    * Update conf.py

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8, <4",
     url="https://github.com/CellProfiler/core",
-    version="4.0.0",
+    version="4.0.2",
     zip_safe=False,
 )

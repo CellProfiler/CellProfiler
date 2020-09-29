d8f8e9b1bcade385c01a767b8e85487e93c5e60b
David Stirling <dstirling@broadinstitute.org>
2020-09-29 14:18:06 -0400

    v4.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,6 @@ html_theme = "sphinx_rtd_theme"
 
 project = "CellProfiler-core"
 
-release = "4.0.4"
+release = "4.0.5"
 
 templates_path = ["_templates"]
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8, <4",
     url="https://github.com/CellProfiler/core",
-    version="4.0.4",
+    version="4.0.5",
     zip_safe=False,
 )

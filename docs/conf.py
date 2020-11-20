747ddc6d2ed74905f0d376859c98eab3317a2158
Beth Cimini <bethac07@users.noreply.github.com>
2020-11-20 12:06:05 -0500

    v4.0.7 (https://github.com/CellProfiler/core/pull/62)

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,6 @@ html_theme = "sphinx_rtd_theme"
 
 project = "CellProfiler-core"
 
-release = "4.0.6"
+release = "4.0.7"
 
 templates_path = ["_templates"]
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8, <4",
     url="https://github.com/CellProfiler/core",
-    version="4.0.6",
+    version="4.0.7",
     zip_safe=False,
 )

fa20bf9352ef0726d824c527c3648fbec0b75fa6
alucas <alicelucas93@gmail.com>
2020-12-02 16:24:06 -0500

    options to export and import pipeline as json

--- cellprofiler/gui/pipelinecontroller.py
+++ cellprofiler/gui/pipelinecontroller.py
@@ -15,6 +15,7 @@ import string
 import sys
 import threading
 from functools import reduce, cmp_to_key
+from json import JSONDecodeError
 from queue import PriorityQueue, Queue, Empty
 from urllib.request import urlretrieve, url2pathname
 
@@ -90,6 +91,7 @@ from cellprofiler_core.pipeline import (
     Listener,
     PrepareRunError,
 )
+from cellprofiler_core.pipeline.io._v6 import load
 from cellprofiler_core.preferences import (
     RECENT_FILE_COUNT,
     add_image_directory_listener,
@@ -1193,7 +1195,11 @@ class PipelineController(object):
                 if not self.load_hdf5_pipeline(pathname):
                     return
             else:
-                self.__pipeline.load(pathname)
+                try:
+                    with open(pathname, "r") as fd:
+                        load(self.__pipeline, fd)
+                except JSONDecodeError:
+                    self.__pipeline.load(pathname)
             self.__pipeline.turn_off_batch_mode()
             self.__clear_errors()
             self.__workspace.save_pipeline_to_measurements()
@@ -1285,14 +1291,14 @@ class PipelineController(object):
                 self.__frame,
             )
 
-    def do_save_pipeline(self):
-        """Save the pipeline, asking the user for the name
+    def do_save_json_pipeline(self):
+        """Save the pipeline as json, asking the user for the name
 
         return True if the user saved the pipeline
         """
         default_filename = get_current_workspace_path()
         if default_filename is None:
-            default_filename = "pipeline.%s" % EXT_PIPELINE
+            default_filename = "pipeline.%s" % EXT_JSON_PIPELINE
             default_path = None
         else:
             default_path, default_filename = os.path.split(default_filename)
@@ -1321,6 +1327,50 @@ class PipelineController(object):
                     dot_ext_pipeline = "." + EXT_PIPELINE
                     if not file_name.endswith(dot_ext_pipeline):
                         # on platforms other than Windows, add the default suffix
+                        pathname += dot_ext_pipeline
+                self.__pipeline.save(
+                    pathname, save_image_plane_details=save_image_plane_details
+                )
+                return True
+            return False
+
+    def do_save_pipeline(self):
+        """Save the pipeline, asking the user for the name
+
+        return True if the user saved the pipeline
+        """
+        default_filename = get_current_workspace_path()
+        if default_filename is None:
+            default_filename = "pipeline.%s" % EXT_PIPELINE
+            default_path = None
+        else:
+            default_path, default_filename = os.path.split(default_filename)
+            default_filename = (
+                os.path.splitext(default_filename)[0] + "." + EXT_PIPELINE
+            )
+        wildcard = (
+            "CellProfiler pipeline (*.%s)|*.%s|"
+            "CellProfiler pipeline and file list (*.%s)|*.%s|"
+            "JSON object (*.json)|*.json"
+        ) % (EXT_PIPELINE, EXT_PIPELINE, EXT_PIPELINE, EXT_PIPELINE)
+        with wx.FileDialog(
+            self.__frame,
+            "Save pipeline",
+            wildcard=wildcard,
+            defaultFile=default_filename,
+            style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT,
+        ) as dlg:
+            assert isinstance(dlg, wx.FileDialog)
+            if default_path is not None:
+                dlg.SetPath(os.path.join(default_path, default_filename))
+            if dlg.ShowModal() == wx.ID_OK:
+                save_image_plane_details = dlg.GetFilterIndex() == 1
+                file_name = dlg.GetFilename()
+                pathname = dlg.GetPath()
+                if not sys.platform.startswith("win"):
+                    dot_ext_pipeline = "." + EXT_PIPELINE
+                    if not file_name.endswith(dot_ext_pipeline) and not file_name.endswith(".json"):
+                        # on platforms other than Windows, add the default suffix, unless user specified json export
                         pathname += dot_ext_pipeline
                 self.__pipeline.save(
                     pathname, save_image_plane_details=save_image_plane_details

2e5cc8b3b21df79651e90dedaae78c2661460c4b
David Stirling <dstirling@broadinstitute.org>
2021-06-08 22:35:09 +0000

    Handle boolean guide images in manual object annotation modules (#4394)
    * Handle boolean images when editing objects
    
    * Handle display of boolean guide image

--- cellprofiler/gui/editobjectsdlg.py
+++ cellprofiler/gui/editobjectsdlg.py
@@ -763,6 +763,8 @@ class EditObjectsDialog(wx.Dialog):
             image, _ = size_similarly(self.orig_labels[0], self.guide_image)
             if image.ndim == 2:
                 image = numpy.dstack((image, image, image))
+            if image.dtype == bool:
+                image = image.astype(int)
             if self.scaling_mode == self.SM_RAW:
                 cimage = image.copy()
             elif self.scaling_mode in (self.SM_NORMALIZED, self.SM_LOG_NORMALIZED):

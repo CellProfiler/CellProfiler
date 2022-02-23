CellProfiler/CellProfiler

commit 7fd8c1c5cc19a2cf337b5f94df6a74d20c1a0087

fixed 3d cmap (#4510)
Co-authored-by: Callum Tromans-Coia <ctromans@wm7c1-0e3.broadinstitute.org>

cellprofiler/gui/figure/_figure.py
@@ -2029,6 +2029,7 @@ class Figure(wx.Frame):
         in_range = (image.min(), image.max())
         image = image.astype(numpy.float32)
         if self.dimensions == 3:
+            orig_image_max = image.max()
             image = image[self.current_plane, :, :]
         if isinstance(colormap, matplotlib.cm.ScalarMappable):
             colormap = colormap.cmap
@@ -2087,12 +2088,16 @@ class Figure(wx.Frame):
             # Apply display bounds
             if vmin is not None and vmax is not None:
                 image = skimage.exposure.rescale_intensity(image, in_range=(vmin, vmax))
+        
         if not self.is_color_image(image):
             if not normalize:
-                if image.max() < 255:
-                    norm = matplotlib.colors.Normalize(vmin=0, vmax=255)
+                if self.dimensions == 3:
+                    norm = matplotlib.colors.Normalize(vmin=0, vmax=orig_image_max)
                 else:
-                    norm = matplotlib.colors.Normalize(vmin=0, vmax=image.max())
+                    if image.max() < 255:
+                        norm = matplotlib.colors.Normalize(vmin=0, vmax=255)
+                    else:
+                        norm = matplotlib.colors.Normalize(vmin=0, vmax=orig_image_max)
             else:
                 norm = None
             mappable = matplotlib.cm.ScalarMappable(cmap=colormap, norm=norm)

commit 3224e95d9af8a7c1b51fe6b33fa4d9ce3804443c

Fix an error introduced in commit 7fd8c1c
orig_image_max should only be used when a 3D image has been loaded. Prior to this fix, orig_image_max could be referenced in 2D images if the image.max() exceeded 255, resulting in an error.

cellprofiler/gui/figure/_figure.py
@@ -2097,7 +2097,7 @@ class Figure(wx.Frame):
                     if image.max() < 255:
                         norm = matplotlib.colors.Normalize(vmin=0, vmax=255)
                     else:
-                        norm = matplotlib.colors.Normalize(vmin=0, vmax=orig_image_max)
+                        norm = matplotlib.colors.Normalize(vmin=0, vmax=image.max())
             else:
                 norm = None
             mappable = matplotlib.cm.ScalarMappable(cmap=colormap, norm=norm)
a75399deca0d4f1e2cfa856ad97d8996e60beddc
Rebecca Senft <28116530+rsenft1@users.noreply.github.com>
2022-04-08 16:00:04 -0400

    Update threshold.py for issue #4436 skimage histogram binning
    Drop nbins kwarg when multi-otsu fails because values are too close together for skimage histogram bins. See issue #6324 scikit-image.

--- cellprofiler/modules/threshold.py
+++ cellprofiler/modules/threshold.py
@@ -1039,7 +1039,10 @@ staining.
                     # Can't run 3-class otsu on only 2 values.
                     threshold_out = skimage.filters.threshold_otsu(block)
                 else:
-                    threshold_out = threshold_method(block, **kwargs)
+                    try: 
+                        threshold_out = threshold_method(block, **kwargs)
+                    except ValueError:
+                        threshold_out = threshold_method(block)
                 if isinstance(threshold_out, numpy.ndarray):
                     # Select correct bin if running multiotsu
                     threshold_out = threshold_out[bin_wanted]

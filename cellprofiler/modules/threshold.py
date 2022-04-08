d108526dc2706b2ee1901f84f70f4230d3ef990b
Rebecca Senft <28116530+rsenft1@users.noreply.github.com>
2022-04-08 13:43:23 -0400

    Fix breaking if there were 3 unique values as well.

--- cellprofiler/modules/threshold.py
+++ cellprofiler/modules/threshold.py
@@ -1039,7 +1039,7 @@ staining.
                     threshold_out = block[0]
                 elif (self.threshold_operation == TM_OTSU and
                       self.two_class_otsu.value == O_THREE_CLASS and
-                      len(numpy.unique(block)) < 3):
+                      len(numpy.unique(block)) <= 3):
                     # Can't run 3-class otsu on only 2 values. Default back to guide_threshold
                     threshold_out = guide_threshold
                 else:

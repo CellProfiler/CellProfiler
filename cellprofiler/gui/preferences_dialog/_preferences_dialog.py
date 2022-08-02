a58d30b4aa37551ef293c0079dba5ba77921ae5e
Nodar Gogoberidze <gnodar01@gmail.com>
2022-08-02 15:25:36 -0400

    Inject widget inspector on set

--- cellprofiler/gui/preferences_dialog/_preferences_dialog.py
+++ cellprofiler/gui/preferences_dialog/_preferences_dialog.py
@@ -491,14 +491,29 @@ class PreferencesDialog(wx.Dialog):
             ],
             [
                 "Enable widget inspector",
-                get_widget_inspector,
-                set_widget_inspector,
+                (lambda: get_widget_inspector(global_only=True)),
+                self._set_widget_inspector,
                 CHOICE,
                 WIDGET_INSPECTOR_HELP,
             ],
 
             ["Pony", get_wants_pony, set_wants_pony, CHOICE, "The end is neigh.",],
         ]
+
+    def _set_widget_inspector(self, val):
+        from cellprofiler.gui.cpframe import ID_FILE_WIDGET_INSPECTOR, ID_DEBUG_HELP
+
+        set_widget_inspector(val, globally=True)
+        debug_menu = wx.GetApp().frame._CPFrame__menu_debug
+        widget_inspector_item = debug_menu.FindItemById(ID_FILE_WIDGET_INSPECTOR)
+        # when setting true, inject menu item if not already present
+        if val and not widget_inspector_item:
+            debug_help_item = debug_menu.FindItemById(ID_DEBUG_HELP)
+            debug_help_pos = debug_menu.MenuItems.index(debug_help_item)
+            debug_menu.Insert(debug_help_pos, ID_FILE_WIDGET_INSPECTOR, "Widget inspector")
+        # when setting false, remove menu item if present
+        elif not val and widget_inspector_item:
+                debug_menu.Remove(ID_FILE_WIDGET_INSPECTOR)
 
     @staticmethod
     def get_title_font():

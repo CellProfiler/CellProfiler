a9e608f81f39e6cf111b7810022cd4d478fe2d62
callum-jpg <callumtc.jpg@gmail.com>
2022-08-30 15:43:08 -0400

    minimal working example of get_threshold in library

--- cellprofiler/library/functions/image_processing.py
+++ cellprofiler/library/functions/image_processing.py
@@ -3,6 +3,7 @@ import skimage.color
 import skimage.morphology
 import numpy
 import centrosome.threshold
+from cellprofiler.library.functions.library_utils import Threshold
 
 def rgb_to_greyscale(image):
     if image.shape[-1] == 4:
@@ -84,4 +85,93 @@ def get_threshold_robust_background(image,
     sd = variance_fn(im)
     return mean + sd * number_of_deviations
 
+def get_threshold(
+        image, 
+        mask=None,
+        threshold_operation="Minimum Cross-Entropy",
+        two_class_otsu="Two classes",
+        assign_middle_to_foreground="Foreground",
+        log_transform=False,
+        manual_threshold=0,
+        thresholding_measurement=None,
+        threshold_scope="Global",
+        threshold_correction_factor=1,
+        threshold_range_min=0,
+        threshold_range_max=1,
+        adaptive_window_size=50,
+        lower_outlier_fraction=0.05,
+        upper_outlier_fraction=0.05,
+        averaging_method="Mean",
+        variance_method="Standard deviation",
+        number_of_deviations=2,
+        volumetric=False,
+        automatic=False,
+        ):
+    """
+    Get the threshold for an image with the provided settings
+    """
+    threshold = Threshold(
+        threshold_operation,
+        two_class_otsu,
+        assign_middle_to_foreground,
+        log_transform,
+        manual_threshold,
+        thresholding_measurement,
+        threshold_scope,
+        threshold_correction_factor,
+        threshold_range_min,
+        threshold_range_max,
+        adaptive_window_size,
+        lower_outlier_fraction,
+        upper_outlier_fraction,
+        averaging_method,
+        variance_method,
+        number_of_deviations,
+        volumetric
+    )
+    # No mask provided, create a dummy mask
+    if mask is not None:
+        mask = numpy.ones(image.shape, dtype=bool)
 
+    need_transform = (
+            not automatic and
+            threshold_operation.casefold() in ("minimum cross-entropy", "otsu") and
+            log_transform
+    )
+
+    if need_transform:
+        image, conversion_dict = centrosome.threshold.log_transform(image)
+
+    if threshold_operation.casefold() == "manual":
+        return manual_threshold, manual_threshold, None
+
+    elif threshold_operation.casefold() == "measurement":
+        # Thresholds are stored as single element arrays.  Cast to float to extract the value.
+        orig_threshold = float(thresholding_measurement)
+
+        return threshold._correct_global_threshold(orig_threshold), orig_threshold, None
+
+    elif threshold_scope.casefold() == "global" or automatic:
+        th_guide = None
+        th_original = threshold.get_global_threshold(image, mask, automatic=automatic)
+
+    elif threshold_scope.casefold() == "adaptive":
+        th_guide = threshold.get_global_threshold(image, mask)
+        th_original = threshold.get_local_threshold(image, mask)
+    else:
+        raise ValueError("Invalid thresholding settings")
+
+    if need_transform:
+        if image.min() < 0 or image.max() > 1:
+            raise ValueError("For log transform, image pixel values must be between [0, 1]")
+        th_original = centrosome.threshold.inverse_log_transform(th_original, conversion_dict)
+        if th_guide is not None:
+            th_guide = centrosome.threshold.inverse_log_transform(th_guide, conversion_dict)
+
+    if threshold_scope.casefold() == "global" or automatic:
+        th_corrected = threshold._correct_global_threshold(th_original)
+    else:
+        th_guide = threshold._correct_global_threshold(th_guide)
+        th_corrected = threshold._correct_local_threshold(th_original, th_guide)
+
+    return th_corrected, th_original, th_guide

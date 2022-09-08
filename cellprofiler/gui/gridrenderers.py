43af68d567c9f74b6d04a03e0326ca3495474187
Nodar Gogoberidze <gnodar01@gmail.com>
2022-09-08 15:48:21 -0400

    Do some cleanup
    - remove comments
    - resolves #4632

--- cellprofiler/gui/gridrenderers.py
+++ cellprofiler/gui/gridrenderers.py
@@ -17,7 +17,6 @@ class GridLabelRenderer(wxglr.GridLabelRenderer):
         dc.DrawLine(right, top, right, bottom)
         dc.DrawLine(left, top, left, bottom)
         dc.DrawLine(left, bottom, right, bottom)
-        # dc.SetPen(wx.WHITE_PEN)
         dc.SetPen(wx.Pen(wx.SystemSettings.GetColour(wx.SYS_COLOUR_3DHIGHLIGHT)))
         if top == 0:
             dc.DrawLine(left + 1, top, left + 1, bottom)
@@ -39,8 +38,6 @@ class CornerLabelRenderer(wxglr.GridDefaultCornerLabelRenderer):
         self._corner = grid.GetGridCornerLabelWindow()
 
         bmp = wx.Bitmap(get_builtin_image("IMG_UPDATE"))
-        # mask = wx.Mask(bmp, wx.BLUE)
-        # bmp.SetMask(mask)
 
         self._bmp_btn = wx.lib.buttons.GenBitmapTextButton(
             self._corner, bitmap=bmp, size=self._corner.GetSize()
@@ -75,21 +72,4 @@ class CornerLabelRenderer(wxglr.GridDefaultCornerLabelRenderer):
         self._bmp_btn.SetLabel(val)
 
     def Draw(self, grid, dc, rect, rc):
-        # top = rect.top
-        # bottom = rect.bottom
-        # left = rect.left
-        # right = rect.right
-        # dc.SetPen(wx.Pen(wx.SystemSettings.GetColour(wx.SYS_COLOUR_3DSHADOW)))
-        # dc.SetPen(wx.RED_PEN)
-        # dc.DrawLine(right, top, right, bottom)
-        # dc.DrawLine(left, top, left, bottom)
-        # dc.DrawLine(left, bottom, right, bottom)
-        # dc.DrawLine(left, top, right, top)
-        # dc.SetPen(wx.RED_PEN)
-        # if top == 0:
-        #     dc.DrawLine(left + 1, top, left + 1, bottom)
-        #     dc.DrawLine(left + 1, top, right+10, top)
-        # else:
-        #     dc.DrawLine(left + 1, top + 1, left + 1, bottom)
-        #     dc.DrawLine(left + 1, top + 1, right - 1, top + 1)
         pass

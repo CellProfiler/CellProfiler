1d7f0d26b69650de50232b64c02bf78614710a89
David Stirling <daves1990@hotmail.co.uk>
2022-09-23 09:52:42 +0100

    Import tests

--- distribution/macos/CellProfilerLauncher.py
+++ distribution/macos/CellProfilerLauncher.py
@@ -5,9 +5,15 @@ if hasattr(sys, "frozen"):
     # allows multiprocessing on Windows after freeze
     multiprocessing.freeze_support()
 
-print(help('modules'))
-
+print("Testing imports")
+import skimage.filters.rank
+print("Skimage OK")
+import cellprofiler_core.constants.object
+print("Core OK")
+import cellprofiler.gui.constants.preferences_view
+print("GUI OK")
 import cellprofiler.__main__
+print("Main OK")
 
 if __name__ == "__main__":
     cellprofiler.__main__.main()

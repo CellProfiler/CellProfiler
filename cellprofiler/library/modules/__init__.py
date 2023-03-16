f203153c095e6ec8aaa4e6171b6ea92ff5526d10
callum-jpg <callumtc.jpg@gmail.com>
2023-03-16 14:03:08 +0000

    Add OverlayObjects to library (#4743)
    * add overlayobjects to library
    
    * add library module

--- cellprofiler/library/modules/__init__.py
+++ cellprofiler/library/modules/__init__.py
@@ -4,3 +4,4 @@ from ._expandorshrinkobjects import expand_or_shrink_objects
 from ._fillobjects import fillobjects
 from ._threshold import threshold
 from ._savecroppedobjects import savecroppedobjects
+from ._overlayobjects import overlayobjects

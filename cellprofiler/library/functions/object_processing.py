6e083c4eec37e776212ae5426d9dc7a9bb24309e
callum-jpg <callumtc.jpg@gmail.com>
2023-03-21 16:57:51 -0400

    revert regional seed labelling to be the same as in the original module

--- cellprofiler/library/functions/object_processing.py
+++ cellprofiler/library/functions/object_processing.py
@@ -361,7 +361,11 @@ def watershed(
     elif seed_method.casefold() == "regional":
         seeds = mahotas.regmax(seed_image, footprint)
         seeds = skimage.morphology.binary_dilation(seeds, strel)
-        seeds, _ = mahotas.label(seeds, footprint)
+        if input_image.ndim == 3:
+            lab_footprint = numpy.ones((16, 16, 16))
+        else:
+            lab_footprint = numpy.ones((16, 16))
+        seeds, _ = mahotas.label(seeds, lab_footprint)
     else:
         raise NotImplementedError(
             f"seed_method {seed_method} is not supported."

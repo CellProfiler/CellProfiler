7ac68102e7e5dc8845822f2501df34bfb26433c5
callum-jpg <callumtc.jpg@gmail.com>
2023-03-31 09:21:38 -0400

    update maxima methods in test and remove none declump tests

--- tests/modules/test_watershed.py
+++ tests/modules/test_watershed.py
@@ -152,9 +152,9 @@ def test_run_distance_declump_intensity(
     footprint = 8
 
     if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint, footprint))
     else:
-        footprint = numpy.ones((footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint))
 
     input_image = skimage.transform.downscale_local_mean(input_image, factors)
     intensity_image = skimage.transform.downscale_local_mean(intensity_image, factors)
@@ -169,8 +169,9 @@ def test_run_distance_declump_intensity(
             distance,
             min_distance=1,
             threshold_rel=0,
-            footprint=footprint,
+            footprint=maxima_footprint,
             num_peaks=numpy.inf,
+            exclude_border=False
         )
         seeds = numpy.zeros(distance.shape, dtype=bool)
         seeds[tuple(seed_coords.T)] = True
@@ -178,9 +179,9 @@ def test_run_distance_declump_intensity(
         seeds = scipy.ndimage.label(seeds)[0]
 
     elif maxima_method.casefold() == "regional":
-        seeds = mahotas.regmax(distance, footprint)
+        seeds = mahotas.regmax(distance, maxima_footprint)
         seeds = skimage.morphology.binary_dilation(seeds, strel)
-        seeds, _ = mahotas.label(seeds, footprint)
+        seeds = scipy.ndimage.label(seeds)[0]
 
     expected = skimage.segmentation.watershed(
         watershed_input_image,
@@ -255,9 +256,9 @@ def test_run_distance_declump_shape(
     footprint = 8
 
     if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint, footprint))
     else:
-        footprint = numpy.ones((footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint))
 
     input_image = skimage.transform.downscale_local_mean(input_image, factors)
     smoothed_input_image = skimage.filters.gaussian(input_image, sigma=gaussian_sigma)
@@ -271,8 +272,9 @@ def test_run_distance_declump_shape(
             distance,
             min_distance=1,
             threshold_rel=0,
-            footprint=footprint,
+            footprint=maxima_footprint,
             num_peaks=numpy.inf,
+            exclude_border=False
         )
         seeds = numpy.zeros(distance.shape, dtype=bool)
         seeds[tuple(seed_coords.T)] = True
@@ -280,108 +282,9 @@ def test_run_distance_declump_shape(
         seeds = scipy.ndimage.label(seeds)[0]
 
     elif maxima_method.casefold() == "regional":
-        seeds = mahotas.regmax(distance, footprint)
-        seeds = skimage.morphology.binary_dilation(seeds, strel)
-        seeds, _ = mahotas.label(seeds, footprint)
-
-    expected = skimage.segmentation.watershed(
-        watershed_input_image,
-        markers=seeds,
-        mask=input_image != 0,
-    )
-    if downsample > 1:
-        expected = skimage.transform.resize(
-            expected, input_shape, mode="edge", order=0, preserve_range=True
-        )
-        expected = numpy.rint(expected).astype(numpy.uint16)
-
-    numpy.testing.assert_array_equal(actual.segmented, expected)
-
-
-def test_run_distance_declump_none(
-    image, module, image_set, workspace, downsample, gaussian_sigma, maxima_method
-):
-    module.use_advanced.value = True
-
-    module.watershed_method.value = "Distance"
-
-    module.declump_method.value = "None"
-
-    module.x_name.value = "input_image"
-
-    module.y_name.value = "watershed"
-
-    module.downsample.value = downsample
-
-    module.gaussian_sigma.value = gaussian_sigma
-
-    module.seed_method.value = maxima_method
-
-    if image.dimensions == 3:
-        module.structuring_element.value = "Ball,1"
-        structuring_element = "Ball"
-        structuring_element_size = 1
-    else:
-        module.structuring_element.value = "Disk,1"
-        structuring_element = "Disk"
-        structuring_element_size = 1
-
-    if image.multichannel:
-        image.pixel_data = skimage.color.rgb2gray(image.pixel_data)
-
-    # Watershed requires a thresholded input
-    input_image = image.pixel_data > skimage.filters.threshold_otsu(image.pixel_data)
-
-    image_set.add(
-        "input_image",
-        cellprofiler_core.image.Image(
-            image=input_image, convert=False, dimensions=image.dimensions
-        ),
-    )
-
-    module.run(workspace)
-
-    actual = workspace.get_objects("watershed")
-
-    # Generate expect output
-    input_shape = input_image.shape
-    if input_image.ndim > 2:
-        # Only scale x and y
-        factors = (1, downsample, downsample)
-    else:
-        factors = (downsample, downsample)
-
-    footprint = 8
-
-    if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
-    else:
-        footprint = numpy.ones((footprint, footprint))
-
-    input_image = skimage.transform.downscale_local_mean(input_image, factors)
-    smoothed_input_image = skimage.filters.gaussian(input_image, sigma=gaussian_sigma)
-    distance = scipy.ndimage.distance_transform_edt(smoothed_input_image)
-    watershed_input_image = input_image
-    strel = getattr(skimage.morphology, structuring_element.casefold())(
-        structuring_element_size
-    )
-    if maxima_method.casefold() == "local":
-        seed_coords = skimage.feature.peak_local_max(
-            distance,
-            min_distance=1,
-            threshold_rel=0,
-            footprint=footprint,
-            num_peaks=numpy.inf,
-        )
-        seeds = numpy.zeros(distance.shape, dtype=bool)
-        seeds[tuple(seed_coords.T)] = True
+        seeds = mahotas.regmax(distance, maxima_footprint)
         seeds = skimage.morphology.binary_dilation(seeds, strel)
         seeds = scipy.ndimage.label(seeds)[0]
-
-    elif maxima_method.casefold() == "regional":
-        seeds = mahotas.regmax(distance, footprint)
-        seeds = skimage.morphology.binary_dilation(seeds, strel)
-        seeds, _ = mahotas.label(seeds, footprint)
 
     expected = skimage.segmentation.watershed(
         watershed_input_image,
@@ -424,6 +327,7 @@ def test_run_markers_declump_shape(
         min_distance=1,
         threshold_rel=0,
         num_peaks=numpy.inf,
+        exclude_border=False
     )
     markers_image = numpy.zeros(image.pixel_data.shape, dtype=bool)
     markers_image[tuple(seed_coords.T)] = True
@@ -470,9 +374,9 @@ def test_run_markers_declump_shape(
     footprint = 8
 
     if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint, footprint))
     else:
-        footprint = numpy.ones((footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint))
 
     input_image = skimage.transform.downscale_local_mean(input_image, factors)
     markers_image = skimage.transform.downscale_local_mean(markers_image, factors)
@@ -529,6 +433,7 @@ def test_run_markers_declump_intensity(
         min_distance=1,
         threshold_rel=0,
         num_peaks=numpy.inf,
+        exclude_border=False
     )
     markers_image = numpy.zeros(image.pixel_data.shape, dtype=bool)
     markers_image[tuple(seed_coords.T)] = True
@@ -585,9 +490,9 @@ def test_run_markers_declump_intensity(
     footprint = 8
 
     if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint, footprint))
     else:
-        footprint = numpy.ones((footprint, footprint))
+        maxima_footprint = numpy.ones((footprint, footprint))
 
     input_image = skimage.transform.downscale_local_mean(input_image, factors)
     markers_image = skimage.transform.downscale_local_mean(markers_image, factors)
@@ -606,126 +511,6 @@ def test_run_markers_declump_intensity(
         watershed_input_image,
         markers=seeds,
         mask=input_image != 0,
-    )
-    if downsample > 1:
-        expected = skimage.transform.resize(
-            expected, input_shape, mode="edge", order=0, preserve_range=True
-        )
-        expected = numpy.rint(expected).astype(numpy.uint16)
-
-    numpy.testing.assert_array_equal(actual.segmented, expected)
-
-
-def test_run_markers_declump_none(
-    image,
-    module,
-    image_set,
-    workspace,
-    connectivity,
-    compactness,
-    watershed_line,
-    downsample,
-    gaussian_sigma,
-):
-    module.use_advanced.value = True
-
-    module.watershed_method.value = "Markers"
-
-    module.declump_method.value = "None"
-
-    module.x_name.value = "input_image"
-
-    module.y_name.value = "watershed"
-
-    module.markers_name.value = "markers"
-
-    module.connectivity.value = connectivity
-
-    module.compactness.value = compactness
-
-    module.watershed_line.value = watershed_line
-
-    module.downsample.value = downsample
-
-    module.gaussian_sigma.value = gaussian_sigma
-
-    if image.multichannel:
-        image.pixel_data = skimage.color.rgb2gray(image.pixel_data)
-
-    seed_coords = skimage.feature.peak_local_max(
-        image.pixel_data,
-        min_distance=1,
-        threshold_rel=0,
-        num_peaks=numpy.inf,
-    )
-    markers_image = numpy.zeros(image.pixel_data.shape, dtype=bool)
-    markers_image[tuple(seed_coords.T)] = True
-    markers_image = scipy.ndimage.label(markers_image)[0]
-
-    if image.dimensions == 3:
-        module.structuring_element.value = "Ball,1"
-        structuring_element = "Ball"
-        structuring_element_size = 1
-    else:
-        module.structuring_element.value = "Disk,1"
-        structuring_element = "Disk"
-        structuring_element_size = 1
-
-    image_set.add(
-        "markers",
-        cellprofiler_core.image.Image(
-            image=markers_image, convert=False, dimensions=image.dimensions
-        ),
-    )
-
-    # Watershed requires a thresholded input
-    input_image = image.pixel_data > skimage.filters.threshold_otsu(image.pixel_data)
-
-    image_set.add(
-        "input_image",
-        cellprofiler_core.image.Image(
-            image=input_image, convert=False, dimensions=image.dimensions
-        ),
-    )
-
-    module.run(workspace)
-
-    actual = workspace.get_objects("watershed")
-
-    # Generate expect output
-    input_shape = input_image.shape
-    if input_image.ndim > 2:
-        # Only scale x and y
-        factors = (1, downsample, downsample)
-    else:
-        factors = (downsample, downsample)
-
-    footprint = 8
-
-    if input_image.ndim == 3:
-        footprint = numpy.ones((footprint, footprint, footprint))
-    else:
-        footprint = numpy.ones((footprint, footprint))
-
-    input_image = skimage.transform.downscale_local_mean(input_image, factors)
-    markers_image = skimage.transform.downscale_local_mean(markers_image, factors)
-    smoothed_input_image = skimage.filters.gaussian(input_image, sigma=gaussian_sigma)
-    distance = scipy.ndimage.distance_transform_edt(smoothed_input_image)
-    watershed_input_image = input_image
-    strel = getattr(skimage.morphology, structuring_element.casefold())(
-        structuring_element_size
-    )
-
-    seeds = markers_image
-    seeds = skimage.morphology.binary_dilation(seeds, strel)
-
-    expected = skimage.segmentation.watershed(
-        watershed_input_image,
-        markers=seeds,
-        mask=input_image != 0,
-        connectivity=connectivity,
-        compactness=compactness,
-        watershed_line=watershed_line,
     )
     if downsample > 1:
         expected = skimage.transform.resize(
@@ -784,33 +569,38 @@ def test_non_advanced(
 
     actual = workspace.get_objects("watershed")
 
-    # Generate expect output
-    input_shape = input_image.shape
-    if input_image.ndim > 2:
-        # Only scale x and y
-        factors = (1, downsample, downsample)
-    else:
-        factors = (downsample, downsample)
-
     if input_image.ndim == 3:
-        footprint = numpy.ones((8, 8, 8))
+        maxima_footprint = numpy.ones((8, 8, 8))
     else:
-        footprint = numpy.ones((8, 8))
+        maxima_footprint = numpy.ones((8, 8))
 
     distance = scipy.ndimage.distance_transform_edt(input_image)
     watershed_input_image = -distance
+    # watershed_input_image = watershed_input_image - watershed_input_image.min()
     strel = getattr(skimage.morphology, structuring_element.casefold())(
         structuring_element_size
     )
 
-    seeds = mahotas.regmax(distance, footprint)
+    seed_coords = skimage.feature.peak_local_max(
+        distance,
+        footprint=maxima_footprint,
+        min_distance=1,
+        threshold_rel=0,
+        num_peaks=numpy.inf,
+        exclude_border=False
+    )
+    seeds = numpy.zeros(image.pixel_data.shape, dtype=bool)
+    seeds[tuple(seed_coords.T)] = True
     seeds = skimage.morphology.binary_dilation(seeds, strel)
-    seeds, _ = mahotas.label(seeds, footprint)
+    seeds = scipy.ndimage.label(seeds)[0]
 
     expected = skimage.segmentation.watershed(
         watershed_input_image,
         markers=seeds,
         mask=input_image != 0,
+        connectivity=1,
+        compactness=0.0,
+        watershed_line=False,
     )
 
     numpy.testing.assert_array_equal(actual.segmented, expected)

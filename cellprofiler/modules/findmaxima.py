93ee51b2728cce7d5336c8512d8fc7d9a3a47d13
callum-jpg <callumtc.jpg@gmail.com>
2023-04-03 13:04:32 -0400

    format + remove unnecessary maxval

--- cellprofiler/modules/findmaxima.py
+++ cellprofiler/modules/findmaxima.py
@@ -52,7 +52,7 @@ class FindMaxima(ImageProcessing):
             text="Minimum distance between maxima",
             value=5,
             minval=0,
-            doc="""Choose the minimum distance between accepted local maxima"""
+            doc="Choose the minimum distance between accepted local maxima",
         )
 
         self.exclude_mode = Choice(
@@ -60,46 +60,49 @@ class FindMaxima(ImageProcessing):
             [MODE_THRESHOLD, MODE_MASK, MODE_OBJECTS],
             value="Threshold",
             doc=f"""\
-By default, local maxima will be searched for across the whole image. This means that maxima will be found in 
-areas that consist entirely of background. To resolve this we have several methods to exclude background.
+By default, local maxima will be searched for across the whole image. This means
+that maxima will be found in areas that consist entirely of background. To
+resolve this we have several methods to exclude background.
 
-**{MODE_THRESHOLD}** allows you to specify a minimum pixel intensity to be considered as a peak. Setting this to 0
-effectively uses no threshold.
+**{MODE_THRESHOLD}** allows you to specify a minimum pixel intensity to be
+considered as a peak. Setting this to 0 effectively uses no threshold.
 
-**{MODE_MASK}** will restrict peaks to areas which are within a provided mask image. This mask will typically come from 
-the threshold module or another means of finding background.
+**{MODE_MASK}** will restrict peaks to areas which are within a provided mask
+image. This mask will typically come from the threshold module or another means
+of finding background.
 
-**{MODE_OBJECTS}** will restrict peaks to areas within an existing set of objects.
-"""
-
+**{MODE_OBJECTS}** will restrict peaks to areas within an existing set of
+objects.
+""",
         )
 
         self.min_intensity = Float(
             "Specify the minimum intensity of a peak",
             0,
             minval=0,
-            maxval=99,
             doc="""\
-Intensity peaks below this threshold value will be excluded. Use this to ensure that your local 
-maxima are within objects of interest."""
-
+Intensity peaks below this threshold value will be excluded. Use this to ensure
+that your local maxima are within objects of interest.
+""",
         )
 
         self.mask_image = ImageSubscriber(
-            "Select the image to use as a mask", doc="Select the image you want to use. This should be a binary image."
+            "Select the image to use as a mask",
+            doc="Select the image you want to use. This should be a binary image.",
         )
 
         self.mask_objects = LabelSubscriber(
-            "Select the objects to search within", doc="Select the objects within which to search for peaks."
+            "Select the objects to search within",
+            doc="Select the objects within which to search for peaks.",
         )
 
         self.label_maxima = Binary(
             "Individually label maxima?",
             value=True,
             doc="""\
-Assign unique labels to each identified maxima. This is requried if you intend to use
-the labelled maxima as markers in the *Watershed* module.
-            """
+Assign unique labels to each identified maxima. This is requried if you intend
+to use the labelled maxima as markers in the *Watershed* module.
+            """,
         )
 
         self.maxima_color = Color(
@@ -112,22 +115,32 @@ the labelled maxima as markers in the *Watershed* module.
             "Select maxima preview size",
             value=1,
             minval=1,
-            doc="Size of the markers for each maxima in the preview. Positive pixels will be"
-                "expanded by this radius."
-                "You may want to increase this when working with large images.",
+            doc="""\
+Size of the markers for each maxima in the preview. Positive pixels will be
+expanded by this radius. You may want to increase this when working with large
+images.
+""",
         )
-
 
     def settings(self):
         __settings__ = super(FindMaxima, self).settings()
 
-        return __settings__ + [self.min_distance, self.exclude_mode,
-                               self.min_intensity, self.mask_image, self.mask_objects]
+        return __settings__ + [
+            self.min_distance,
+            self.exclude_mode,
+            self.min_intensity,
+            self.mask_image,
+            self.mask_objects,
+        ]
 
     def visible_settings(self):
         __settings__ = super(FindMaxima, self).visible_settings()
 
-        result = __settings__ + [self.label_maxima, self.min_distance, self.exclude_mode]
+        result = __settings__ + [
+            self.label_maxima,
+            self.min_distance,
+            self.exclude_mode,
+        ]
 
         if self.exclude_mode == MODE_THRESHOLD:
             result.append(self.min_intensity)
@@ -200,16 +213,16 @@ the labelled maxima as markers in the *Watershed* module.
         layout = (2, 2)
         dimensions = workspace.display_data.dimensions
 
-        figure.set_subplots(
-            dimensions=dimensions, subplots=layout
-        )
+        figure.set_subplots(dimensions=dimensions, subplots=layout)
 
         title = "Input image, cycle #%d" % (workspace.measurements.image_number,)
         image = workspace.display_data.x_data
         maxima_image = workspace.display_data.y_data.astype(int)
 
         ax = figure.subplot_imshow_grayscale(0, 0, image, title)
-        figure.subplot_imshow_grayscale(1, 0, maxima_image, self.y_name.value, sharexy=ax)
+        figure.subplot_imshow_grayscale(
+            1, 0, maxima_image, self.y_name.value, sharexy=ax
+        )
 
         if self.maxima_size.value > 1:
             if dimensions == 2:
@@ -230,10 +243,7 @@ the labelled maxima as markers in the *Watershed* module.
                 0, 1, labels, "Detected maxima", sharexy=ax, colormap=cmap
             )
         else:
-            figure.subplot_imshow_labels(
-                0, 1, labels, "Detected maxima", sharexy=ax
-            )
-
+            figure.subplot_imshow_labels(0, 1, labels, "Detected maxima", sharexy=ax)
 
     def update_settings(self, settings):
         return settings

c21073dcc45e9e8cdcb6f00b50ab32c602f4e88a
Nodar Gogoberidze <gnodar01@gmail.com>
2023-11-15 08:37:21 -0500

    add IDTertiary objects to library

--- src/subpackages/library/cellprofiler_library/functions/object_processing.py
+++ src/subpackages/library/cellprofiler_library/functions/object_processing.py
@@ -883,3 +883,35 @@ def size_similarly(labels, secondary):
     mask = numpy.zeros(labels.shape, bool)
     mask[:i_max, :j_max] = 1
     return result, mask
+
+def outline(labels):
+    """Given a label matrix, return a matrix of the outlines of the labeled objects
+    
+    If a pixel is not zero and has at least one neighbor with a different
+    value, then it is part of the outline.
+    """
+
+    output = numpy.zeros(labels.shape, labels.dtype)
+    lr_different = labels[1:, :] != labels[:-1, :]
+    ud_different = labels[:, 1:] != labels[:, :-1]
+    d1_different = labels[1:, 1:] != labels[:-1, :-1]
+    d2_different = labels[1:, :-1] != labels[:-1, 1:]
+    different = numpy.zeros(labels.shape, bool)
+    different[1:, :][lr_different] = True
+    different[:-1, :][lr_different] = True
+    different[:, 1:][ud_different] = True
+    different[:, :-1][ud_different] = True
+    different[1:, 1:][d1_different] = True
+    different[:-1, :-1][d1_different] = True
+    different[1:, :-1][d2_different] = True
+    different[:-1, 1:][d2_different] = True
+    #
+    # Labels on edges need outlines
+    #
+    different[0, :] = True
+    different[:, 0] = True
+    different[-1, :] = True
+    different[:, -1] = True
+
+    output[different] = labels[different]
+    return output
--- src/subpackages/library/cellprofiler_library/modules/__init__.py
+++ src/subpackages/library/cellprofiler_library/modules/__init__.py
@@ -16,3 +16,4 @@ from ._watershed import watershed
 from ._measureimageoverlap import measureimageoverlap
 from ._identifyprimaryobjects import identifyprimaryobjects
 from ._identifysecondaryobjects import identifysecondaryobjects
+from ._identifytertiaryobjects import identifytertiaryobjects

0027cc8f7f32836f25d3f0c9bbea299ee13a7078
callum-jpg <callumtc.jpg@gmail.com>
2024-01-10 14:55:45 +0000

    initially working - feature names not attributes in frontend module, though

--- src/frontend/cellprofiler/modules/measureobjectsizeshape.py
+++ src/frontend/cellprofiler/modules/measureobjectsizeshape.py
@@ -12,6 +12,9 @@ from cellprofiler_core.setting.subscriber import LabelListSubscriber
 import cellprofiler.gui.help.content
 import cellprofiler.icons
 
+from cellprofiler_library.modules._measureobjectsizeshape import measureobjectsizeshape
+from cellprofiler_library.opts.objectsizeshapefeatures import ObjectSizeShapeFeatures
+
 __doc__ = """\
 MeasureObjectSizeShape
 ======================
@@ -193,190 +196,190 @@ References
 """The category of the per-object measurements made by this module"""
 AREA_SHAPE = "AreaShape"
 
-"""Calculate Zernike features for N,M where N=0 through ZERNIKE_N"""
-ZERNIKE_N = 9
+# """Calculate Zernike features for N,M where N=0 through ZERNIKE_N"""
+# ZERNIKE_N = 9
 
-F_AREA = "Area"
-F_PERIMETER = "Perimeter"
-F_VOLUME = "Volume"
-F_SURFACE_AREA = "SurfaceArea"
-F_ECCENTRICITY = "Eccentricity"
-F_SOLIDITY = "Solidity"
-F_CONVEX_AREA = "ConvexArea"
-F_EXTENT = "Extent"
-F_CENTER_X = "Center_X"
-F_CENTER_Y = "Center_Y"
-F_CENTER_Z = "Center_Z"
-F_BBOX_AREA = "BoundingBoxArea"
-F_BBOX_VOLUME = "BoundingBoxVolume"
-F_MIN_X = "BoundingBoxMinimum_X"
-F_MAX_X = "BoundingBoxMaximum_X"
-F_MIN_Y = "BoundingBoxMinimum_Y"
-F_MAX_Y = "BoundingBoxMaximum_Y"
-F_MIN_Z = "BoundingBoxMinimum_Z"
-F_MAX_Z = "BoundingBoxMaximum_Z"
-F_EULER_NUMBER = "EulerNumber"
-F_FORM_FACTOR = "FormFactor"
-F_MAJOR_AXIS_LENGTH = "MajorAxisLength"
-F_MINOR_AXIS_LENGTH = "MinorAxisLength"
-F_ORIENTATION = "Orientation"
-F_COMPACTNESS = "Compactness"
-F_INERTIA = "InertiaTensor"
-F_MAXIMUM_RADIUS = "MaximumRadius"
-F_MEDIAN_RADIUS = "MedianRadius"
-F_MEAN_RADIUS = "MeanRadius"
-F_MIN_FERET_DIAMETER = "MinFeretDiameter"
-F_MAX_FERET_DIAMETER = "MaxFeretDiameter"
+# F_AREA = "Area"
+# F_PERIMETER = "Perimeter"
+# F_VOLUME = "Volume"
+# F_SURFACE_AREA = "SurfaceArea"
+# F_ECCENTRICITY = "Eccentricity"
+# F_SOLIDITY = "Solidity"
+# F_CONVEX_AREA = "ConvexArea"
+# F_EXTENT = "Extent"
+# F_CENTER_X = "Center_X"
+# F_CENTER_Y = "Center_Y"
+# F_CENTER_Z = "Center_Z"
+# F_BBOX_AREA = "BoundingBoxArea"
+# F_BBOX_VOLUME = "BoundingBoxVolume"
+# F_MIN_X = "BoundingBoxMinimum_X"
+# F_MAX_X = "BoundingBoxMaximum_X"
+# F_MIN_Y = "BoundingBoxMinimum_Y"
+# F_MAX_Y = "BoundingBoxMaximum_Y"
+# F_MIN_Z = "BoundingBoxMinimum_Z"
+# F_MAX_Z = "BoundingBoxMaximum_Z"
+# F_EULER_NUMBER = "EulerNumber"
+# F_FORM_FACTOR = "FormFactor"
+# F_MAJOR_AXIS_LENGTH = "MajorAxisLength"
+# F_MINOR_AXIS_LENGTH = "MinorAxisLength"
+# F_ORIENTATION = "Orientation"
+# F_COMPACTNESS = "Compactness"
+# F_INERTIA = "InertiaTensor"
+# F_MAXIMUM_RADIUS = "MaximumRadius"
+# F_MEDIAN_RADIUS = "MedianRadius"
+# F_MEAN_RADIUS = "MeanRadius"
+# F_MIN_FERET_DIAMETER = "MinFeretDiameter"
+# F_MAX_FERET_DIAMETER = "MaxFeretDiameter"
 
-F_CENTRAL_MOMENT_0_0 = "CentralMoment_0_0"
-F_CENTRAL_MOMENT_0_1 = "CentralMoment_0_1"
-F_CENTRAL_MOMENT_0_2 = "CentralMoment_0_2"
-F_CENTRAL_MOMENT_0_3 = "CentralMoment_0_3"
-F_CENTRAL_MOMENT_1_0 = "CentralMoment_1_0"
-F_CENTRAL_MOMENT_1_1 = "CentralMoment_1_1"
-F_CENTRAL_MOMENT_1_2 = "CentralMoment_1_2"
-F_CENTRAL_MOMENT_1_3 = "CentralMoment_1_3"
-F_CENTRAL_MOMENT_2_0 = "CentralMoment_2_0"
-F_CENTRAL_MOMENT_2_1 = "CentralMoment_2_1"
-F_CENTRAL_MOMENT_2_2 = "CentralMoment_2_2"
-F_CENTRAL_MOMENT_2_3 = "CentralMoment_2_3"
-F_EQUIVALENT_DIAMETER = "EquivalentDiameter"
-F_HU_MOMENT_0 = "HuMoment_0"
-F_HU_MOMENT_1 = "HuMoment_1"
-F_HU_MOMENT_2 = "HuMoment_2"
-F_HU_MOMENT_3 = "HuMoment_3"
-F_HU_MOMENT_4 = "HuMoment_4"
-F_HU_MOMENT_5 = "HuMoment_5"
-F_HU_MOMENT_6 = "HuMoment_6"
-F_INERTIA_TENSOR_0_0 = "InertiaTensor_0_0"
-F_INERTIA_TENSOR_0_1 = "InertiaTensor_0_1"
-F_INERTIA_TENSOR_1_0 = "InertiaTensor_1_0"
-F_INERTIA_TENSOR_1_1 = "InertiaTensor_1_1"
-F_INERTIA_TENSOR_EIGENVALUES_0 = "InertiaTensorEigenvalues_0"
-F_INERTIA_TENSOR_EIGENVALUES_1 = "InertiaTensorEigenvalues_1"
-F_NORMALIZED_MOMENT_0_0 = "NormalizedMoment_0_0"
-F_NORMALIZED_MOMENT_0_1 = "NormalizedMoment_0_1"
-F_NORMALIZED_MOMENT_0_2 = "NormalizedMoment_0_2"
-F_NORMALIZED_MOMENT_0_3 = "NormalizedMoment_0_3"
-F_NORMALIZED_MOMENT_1_0 = "NormalizedMoment_1_0"
-F_NORMALIZED_MOMENT_1_1 = "NormalizedMoment_1_1"
-F_NORMALIZED_MOMENT_1_2 = "NormalizedMoment_1_2"
-F_NORMALIZED_MOMENT_1_3 = "NormalizedMoment_1_3"
-F_NORMALIZED_MOMENT_2_0 = "NormalizedMoment_2_0"
-F_NORMALIZED_MOMENT_2_1 = "NormalizedMoment_2_1"
-F_NORMALIZED_MOMENT_2_2 = "NormalizedMoment_2_2"
-F_NORMALIZED_MOMENT_2_3 = "NormalizedMoment_2_3"
-F_NORMALIZED_MOMENT_3_0 = "NormalizedMoment_3_0"
-F_NORMALIZED_MOMENT_3_1 = "NormalizedMoment_3_1"
-F_NORMALIZED_MOMENT_3_2 = "NormalizedMoment_3_2"
-F_NORMALIZED_MOMENT_3_3 = "NormalizedMoment_3_3"
-F_SPATIAL_MOMENT_0_0 = "SpatialMoment_0_0"
-F_SPATIAL_MOMENT_0_1 = "SpatialMoment_0_1"
-F_SPATIAL_MOMENT_0_2 = "SpatialMoment_0_2"
-F_SPATIAL_MOMENT_0_3 = "SpatialMoment_0_3"
-F_SPATIAL_MOMENT_1_0 = "SpatialMoment_1_0"
-F_SPATIAL_MOMENT_1_1 = "SpatialMoment_1_1"
-F_SPATIAL_MOMENT_1_2 = "SpatialMoment_1_2"
-F_SPATIAL_MOMENT_1_3 = "SpatialMoment_1_3"
-F_SPATIAL_MOMENT_2_0 = "SpatialMoment_2_0"
-F_SPATIAL_MOMENT_2_1 = "SpatialMoment_2_1"
-F_SPATIAL_MOMENT_2_2 = "SpatialMoment_2_2"
-F_SPATIAL_MOMENT_2_3 = "SpatialMoment_2_3"
+# F_CENTRAL_MOMENT_0_0 = "CentralMoment_0_0"
+# F_CENTRAL_MOMENT_0_1 = "CentralMoment_0_1"
+# F_CENTRAL_MOMENT_0_2 = "CentralMoment_0_2"
+# F_CENTRAL_MOMENT_0_3 = "CentralMoment_0_3"
+# F_CENTRAL_MOMENT_1_0 = "CentralMoment_1_0"
+# F_CENTRAL_MOMENT_1_1 = "CentralMoment_1_1"
+# F_CENTRAL_MOMENT_1_2 = "CentralMoment_1_2"
+# F_CENTRAL_MOMENT_1_3 = "CentralMoment_1_3"
+# F_CENTRAL_MOMENT_2_0 = "CentralMoment_2_0"
+# F_CENTRAL_MOMENT_2_1 = "CentralMoment_2_1"
+# F_CENTRAL_MOMENT_2_2 = "CentralMoment_2_2"
+# F_CENTRAL_MOMENT_2_3 = "CentralMoment_2_3"
+# F_EQUIVALENT_DIAMETER = "EquivalentDiameter"
+# F_HU_MOMENT_0 = "HuMoment_0"
+# F_HU_MOMENT_1 = "HuMoment_1"
+# F_HU_MOMENT_2 = "HuMoment_2"
+# F_HU_MOMENT_3 = "HuMoment_3"
+# F_HU_MOMENT_4 = "HuMoment_4"
+# F_HU_MOMENT_5 = "HuMoment_5"
+# F_HU_MOMENT_6 = "HuMoment_6"
+# F_INERTIA_TENSOR_0_0 = "InertiaTensor_0_0"
+# F_INERTIA_TENSOR_0_1 = "InertiaTensor_0_1"
+# F_INERTIA_TENSOR_1_0 = "InertiaTensor_1_0"
+# F_INERTIA_TENSOR_1_1 = "InertiaTensor_1_1"
+# F_INERTIA_TENSOR_EIGENVALUES_0 = "InertiaTensorEigenvalues_0"
+# F_INERTIA_TENSOR_EIGENVALUES_1 = "InertiaTensorEigenvalues_1"
+# F_NORMALIZED_MOMENT_0_0 = "NormalizedMoment_0_0"
+# F_NORMALIZED_MOMENT_0_1 = "NormalizedMoment_0_1"
+# F_NORMALIZED_MOMENT_0_2 = "NormalizedMoment_0_2"
+# F_NORMALIZED_MOMENT_0_3 = "NormalizedMoment_0_3"
+# F_NORMALIZED_MOMENT_1_0 = "NormalizedMoment_1_0"
+# F_NORMALIZED_MOMENT_1_1 = "NormalizedMoment_1_1"
+# F_NORMALIZED_MOMENT_1_2 = "NormalizedMoment_1_2"
+# F_NORMALIZED_MOMENT_1_3 = "NormalizedMoment_1_3"
+# F_NORMALIZED_MOMENT_2_0 = "NormalizedMoment_2_0"
+# F_NORMALIZED_MOMENT_2_1 = "NormalizedMoment_2_1"
+# F_NORMALIZED_MOMENT_2_2 = "NormalizedMoment_2_2"
+# F_NORMALIZED_MOMENT_2_3 = "NormalizedMoment_2_3"
+# F_NORMALIZED_MOMENT_3_0 = "NormalizedMoment_3_0"
+# F_NORMALIZED_MOMENT_3_1 = "NormalizedMoment_3_1"
+# F_NORMALIZED_MOMENT_3_2 = "NormalizedMoment_3_2"
+# F_NORMALIZED_MOMENT_3_3 = "NormalizedMoment_3_3"
+# F_SPATIAL_MOMENT_0_0 = "SpatialMoment_0_0"
+# F_SPATIAL_MOMENT_0_1 = "SpatialMoment_0_1"
+# F_SPATIAL_MOMENT_0_2 = "SpatialMoment_0_2"
+# F_SPATIAL_MOMENT_0_3 = "SpatialMoment_0_3"
+# F_SPATIAL_MOMENT_1_0 = "SpatialMoment_1_0"
+# F_SPATIAL_MOMENT_1_1 = "SpatialMoment_1_1"
+# F_SPATIAL_MOMENT_1_2 = "SpatialMoment_1_2"
+# F_SPATIAL_MOMENT_1_3 = "SpatialMoment_1_3"
+# F_SPATIAL_MOMENT_2_0 = "SpatialMoment_2_0"
+# F_SPATIAL_MOMENT_2_1 = "SpatialMoment_2_1"
+# F_SPATIAL_MOMENT_2_2 = "SpatialMoment_2_2"
+# F_SPATIAL_MOMENT_2_3 = "SpatialMoment_2_3"
 
-"""The non-Zernike features"""
-F_STD_2D = [
-    F_AREA,
-    F_PERIMETER,
-    F_MAXIMUM_RADIUS,
-    F_MEAN_RADIUS,
-    F_MEDIAN_RADIUS,
-    F_MIN_FERET_DIAMETER,
-    F_MAX_FERET_DIAMETER,
-    F_ORIENTATION,
-    F_ECCENTRICITY,
-    F_FORM_FACTOR,
-    F_SOLIDITY,
-    F_CONVEX_AREA,
-    F_COMPACTNESS,
-    F_BBOX_AREA,
-]
-F_STD_3D = [
-    F_VOLUME,
-    F_SURFACE_AREA,
-    F_CENTER_Z,
-    F_BBOX_VOLUME,
-    F_MIN_Z,
-    F_MAX_Z,
-]
-F_ADV_2D = [
-    F_SPATIAL_MOMENT_0_0,
-    F_SPATIAL_MOMENT_0_1,
-    F_SPATIAL_MOMENT_0_2,
-    F_SPATIAL_MOMENT_0_3,
-    F_SPATIAL_MOMENT_1_0,
-    F_SPATIAL_MOMENT_1_1,
-    F_SPATIAL_MOMENT_1_2,
-    F_SPATIAL_MOMENT_1_3,
-    F_SPATIAL_MOMENT_2_0,
-    F_SPATIAL_MOMENT_2_1,
-    F_SPATIAL_MOMENT_2_2,
-    F_SPATIAL_MOMENT_2_3,
-    F_CENTRAL_MOMENT_0_0,
-    F_CENTRAL_MOMENT_0_1,
-    F_CENTRAL_MOMENT_0_2,
-    F_CENTRAL_MOMENT_0_3,
-    F_CENTRAL_MOMENT_1_0,
-    F_CENTRAL_MOMENT_1_1,
-    F_CENTRAL_MOMENT_1_2,
-    F_CENTRAL_MOMENT_1_3,
-    F_CENTRAL_MOMENT_2_0,
-    F_CENTRAL_MOMENT_2_1,
-    F_CENTRAL_MOMENT_2_2,
-    F_CENTRAL_MOMENT_2_3,
-    F_NORMALIZED_MOMENT_0_0,
-    F_NORMALIZED_MOMENT_0_1,
-    F_NORMALIZED_MOMENT_0_2,
-    F_NORMALIZED_MOMENT_0_3,
-    F_NORMALIZED_MOMENT_1_0,
-    F_NORMALIZED_MOMENT_1_1,
-    F_NORMALIZED_MOMENT_1_2,
-    F_NORMALIZED_MOMENT_1_3,
-    F_NORMALIZED_MOMENT_2_0,
-    F_NORMALIZED_MOMENT_2_1,
-    F_NORMALIZED_MOMENT_2_2,
-    F_NORMALIZED_MOMENT_2_3,
-    F_NORMALIZED_MOMENT_3_0,
-    F_NORMALIZED_MOMENT_3_1,
-    F_NORMALIZED_MOMENT_3_2,
-    F_NORMALIZED_MOMENT_3_3,
-    F_HU_MOMENT_0,
-    F_HU_MOMENT_1,
-    F_HU_MOMENT_2,
-    F_HU_MOMENT_3,
-    F_HU_MOMENT_4,
-    F_HU_MOMENT_5,
-    F_HU_MOMENT_6,
-    F_INERTIA_TENSOR_0_0,
-    F_INERTIA_TENSOR_0_1,
-    F_INERTIA_TENSOR_1_0,
-    F_INERTIA_TENSOR_1_1,
-    F_INERTIA_TENSOR_EIGENVALUES_0,
-    F_INERTIA_TENSOR_EIGENVALUES_1,
-]
-F_ADV_3D = [F_SOLIDITY]
-F_STANDARD = [
-    F_EXTENT,
-    F_EULER_NUMBER,
-    F_EQUIVALENT_DIAMETER,
-    F_MAJOR_AXIS_LENGTH,
-    F_MINOR_AXIS_LENGTH,
-    F_CENTER_X,
-    F_CENTER_Y,
-    F_MIN_X,
-    F_MIN_Y,
-    F_MAX_X,
-    F_MAX_Y,
-]
+# """The non-Zernike features"""
+# F_STD_2D = [
+#     F_AREA,
+#     F_PERIMETER,
+#     F_MAXIMUM_RADIUS,
+#     F_MEAN_RADIUS,
+#     F_MEDIAN_RADIUS,
+#     F_MIN_FERET_DIAMETER,
+#     F_MAX_FERET_DIAMETER,
+#     F_ORIENTATION,
+#     F_ECCENTRICITY,
+#     F_FORM_FACTOR,
+#     F_SOLIDITY,
+#     F_CONVEX_AREA,
+#     F_COMPACTNESS,
+#     F_BBOX_AREA,
+# ]
+# F_STD_3D = [
+#     F_VOLUME,
+#     F_SURFACE_AREA,
+#     F_CENTER_Z,
+#     F_BBOX_VOLUME,
+#     F_MIN_Z,
+#     F_MAX_Z,
+# ]
+# F_ADV_2D = [
+#     F_SPATIAL_MOMENT_0_0,
+#     F_SPATIAL_MOMENT_0_1,
+#     F_SPATIAL_MOMENT_0_2,
+#     F_SPATIAL_MOMENT_0_3,
+#     F_SPATIAL_MOMENT_1_0,
+#     F_SPATIAL_MOMENT_1_1,
+#     F_SPATIAL_MOMENT_1_2,
+#     F_SPATIAL_MOMENT_1_3,
+#     F_SPATIAL_MOMENT_2_0,
+#     F_SPATIAL_MOMENT_2_1,
+#     F_SPATIAL_MOMENT_2_2,
+#     F_SPATIAL_MOMENT_2_3,
+#     F_CENTRAL_MOMENT_0_0,
+#     F_CENTRAL_MOMENT_0_1,
+#     F_CENTRAL_MOMENT_0_2,
+#     F_CENTRAL_MOMENT_0_3,
+#     F_CENTRAL_MOMENT_1_0,
+#     F_CENTRAL_MOMENT_1_1,
+#     F_CENTRAL_MOMENT_1_2,
+#     F_CENTRAL_MOMENT_1_3,
+#     F_CENTRAL_MOMENT_2_0,
+#     F_CENTRAL_MOMENT_2_1,
+#     F_CENTRAL_MOMENT_2_2,
+#     F_CENTRAL_MOMENT_2_3,
+#     F_NORMALIZED_MOMENT_0_0,
+#     F_NORMALIZED_MOMENT_0_1,
+#     F_NORMALIZED_MOMENT_0_2,
+#     F_NORMALIZED_MOMENT_0_3,
+#     F_NORMALIZED_MOMENT_1_0,
+#     F_NORMALIZED_MOMENT_1_1,
+#     F_NORMALIZED_MOMENT_1_2,
+#     F_NORMALIZED_MOMENT_1_3,
+#     F_NORMALIZED_MOMENT_2_0,
+#     F_NORMALIZED_MOMENT_2_1,
+#     F_NORMALIZED_MOMENT_2_2,
+#     F_NORMALIZED_MOMENT_2_3,
+#     F_NORMALIZED_MOMENT_3_0,
+#     F_NORMALIZED_MOMENT_3_1,
+#     F_NORMALIZED_MOMENT_3_2,
+#     F_NORMALIZED_MOMENT_3_3,
+#     F_HU_MOMENT_0,
+#     F_HU_MOMENT_1,
+#     F_HU_MOMENT_2,
+#     F_HU_MOMENT_3,
+#     F_HU_MOMENT_4,
+#     F_HU_MOMENT_5,
+#     F_HU_MOMENT_6,
+#     F_INERTIA_TENSOR_0_0,
+#     F_INERTIA_TENSOR_0_1,
+#     F_INERTIA_TENSOR_1_0,
+#     F_INERTIA_TENSOR_1_1,
+#     F_INERTIA_TENSOR_EIGENVALUES_0,
+#     F_INERTIA_TENSOR_EIGENVALUES_1,
+# ]
+# F_ADV_3D = [F_SOLIDITY]
+# F_STANDARD = [
+#     F_EXTENT,
+#     F_EULER_NUMBER,
+#     F_EQUIVALENT_DIAMETER,
+#     F_MAJOR_AXIS_LENGTH,
+#     F_MINOR_AXIS_LENGTH,
+#     F_CENTER_X,
+#     F_CENTER_Y,
+#     F_MIN_X,
+#     F_MIN_Y,
+#     F_MAX_X,
+#     F_MAX_Y,
+# ]
 
 
 class MeasureObjectSizeShape(Module):
@@ -469,7 +472,7 @@ module.""".format(
     def get_zernike_numbers(self):
         """The Zernike numbers measured by this module"""
         if self.calculate_zernikes.value:
-            return centrosome.zernike.get_zernike_indexes(ZERNIKE_N + 1)
+            return centrosome.zernike.get_zernike_indexes(ObjectSizeShapeFeatures.ZERNIKE_N.value + 1)
         else:
             return []
 
@@ -482,20 +485,20 @@ module.""".format(
 
     def get_feature_names(self, pipeline):
         """Return the names of the features measured"""
-        feature_names = list(F_STANDARD)
+        feature_names = list(ObjectSizeShapeFeatures.F_STANDARD.value)
 
         if pipeline.volumetric():
-            feature_names += list(F_STD_3D)
+            feature_names += list(ObjectSizeShapeFeatures.F_STD_3D.value)
             if self.calculate_advanced.value:
-                feature_names += list(F_ADV_3D)
+                feature_names += list(ObjectSizeShapeFeatures.F_ADV_3D.value)
         else:
-            feature_names += list(F_STD_2D)
+            feature_names += list(ObjectSizeShapeFeatures.F_STD_2D.value)
             if self.calculate_zernikes.value:
                 feature_names += [
                     self.get_zernike_name(index) for index in self.get_zernike_numbers()
                 ]
             if self.calculate_advanced.value:
-                feature_names += list(F_ADV_2D)
+                feature_names += list(get_feature_names.F_ADV_2D.values)
 
         return feature_names
 
@@ -524,7 +527,23 @@ module.""".format(
 
             workspace.display_data.statistics = []
         for object_name in self.objects_list.value:
-            self.run_on_objects(object_name, workspace)
+            # self.run_on_objects(object_name, workspace)
+            
+            objects = workspace.get_objects(object_name)
+            
+            features_to_record = measureobjectsizeshape(
+                objects = objects.dense,
+                calculate_advanced = self.calculate_advanced.value,
+                calculate_zernikes = self.calculate_zernikes.value,
+                volumetric = workspace.pipeline.volumetric(),
+                spacing = objects.parent_image.spacing
+                    if objects.has_parent_image
+                    else (1.0,) * objects.dimensions, # TODO: Check this change is OK
+            )
+            
+            for f, m in features_to_record.items():
+                self.record_measurement(workspace, object_name, f, m)
+                
 
     def run_on_objects(self, object_name, workspace):
         """Determine desired measurements and pass in object arrays for analysis"""

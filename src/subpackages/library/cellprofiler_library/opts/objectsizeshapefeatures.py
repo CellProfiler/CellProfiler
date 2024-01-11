f4bee3ad4a6a6e4a52eb2dd20ba35fbd43308a9a
callum-jpg <callumtc.jpg@gmail.com>
2024-01-11 10:41:20 +0000

    add description of features

--- src/subpackages/library/cellprofiler_library/opts/objectsizeshapefeatures.py
+++ src/subpackages/library/cellprofiler_library/opts/objectsizeshapefeatures.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class ObjectSizeShapeFeatures(Enum):
+    """The category of the per-object measurements made by the MeasureObjectSizeShape module"""
     AREA_SHAPE = "AreaShape"
     
     ZERNIKE_N = 9

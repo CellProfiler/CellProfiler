d8671ea11959d34eab4ad9e4030fac6444015007
Nodar Gogoberidze <gnodar01@gmail.com>
2024-08-06 16:03:56 -0400

    Fail fast and try print

--- tests/core/analysis/test_analysis.py
+++ tests/core/analysis/test_analysis.py
@@ -348,6 +348,8 @@ class TestAnalysis(unittest.TestCase):
             "Entering %s" % inspect.getframeinfo(inspect.currentframe()).function
         )
         pipeline, m = self.make_pipeline_and_measurements_and_start()
+        
+        print("I AM STARTING THE HEARTBEAT TEST")
 
         with self.FakeWorker() as worker:
             heartbeat_address = self.analysis.runner.boundary.keepalive_address

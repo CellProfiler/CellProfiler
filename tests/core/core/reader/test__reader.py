fe106c337dfa2391e30403f6e355183e7d21e73b
Nodar Gogoberidze <gnodar01@gmail.com>
2024-11-20 16:30:04 -0500

    Complete uint8 tests

--- tests/core/core/reader/test__reader.py
+++ tests/core/core/reader/test__reader.py
@@ -18,7 +18,7 @@ def readers():
     # post-filtered readers
     return AVAILABLE_READERS
 
-def create_provider(img_data, path, rescale=True):
+def create_image(reader, img_data, path, rescale=True):
     io.imsave(path, img_data)
 
     provider = MonochromeImage(
@@ -33,11 +33,60 @@ def create_provider(img_data, path, rescale=True):
         z=None,
         t=None
     )
-    return provider
+
+    provider._FileImage__preferred_reader = reader
+
+    # check if image reader supports ext
+    image_file = provider.get_image_file()
+    if ALL_READERS[reader].supports_format(image_file) == -1:
+        return None
+
+    return provider.provide_image(None) # img.pixel_data for array
+
 
 class TestReaders:
-    def test_simple(self, readers, tmp_path):
-        img_data = numpy.array([0,0,0,0], dtype=numpy.dtype("uint8")).reshape(2,2)
-        provider = create_provider(img_data, tmp_path / "uint8_all_zero.tiff", rescale=True)
-        img = provider.provide_image(None) # img.pixel_data for array
-        print("Did the thing")
+    def test_uint8_tif_full_range(self, readers, tmp_path):
+        img_ext = "tiff"
+        img_name = "uint8_full_range"
+        img_data = numpy.array([0,128,255,0], dtype=numpy.dtype("uint8")).reshape(2,2)
+
+        for reader in readers:
+
+            test_img = create_image(reader, img_data, tmp_path / f"{img_name}.{img_ext}", rescale=True)
+
+            # ext not supported, skip testing reader for this image
+            if test_img == None:
+                continue
+
+            test_img_data = test_img.pixel_data
+            ref_img_data = (img_data / (2**8-1)).astype("float32")
+
+            assert test_img_data.dtype == numpy.dtype("float32"), "dtype mismatch"
+            assert test_img_data.shape == ref_img_data.shape, "shape mismatch"
+            assert test_img_data.min() == ref_img_data.min(), "min mismatch"
+            assert test_img_data.max() == ref_img_data.max(), "max mismatch"
+            # test_img intensity values should range from 0 to 1
+            assert numpy.all(test_img_data == ref_img_data), "data mismatch"
+
+    def test_uint8_tif_partial_range(self, readers, tmp_path):
+        img_ext = "tiff"
+        img_name = "uint8_partial_range"
+        img_data = numpy.array([64,65,127,128], dtype=numpy.dtype("uint8")).reshape(2,2)
+
+        for reader in readers:
+
+            test_img = create_image(reader, img_data, tmp_path / f"{img_name}.{img_ext}", rescale=True)
+
+            # ext not supported, skip testing reader for this image
+            if test_img == None:
+                continue
+
+            test_img_data = test_img.pixel_data
+            ref_img_data = (img_data / (2**8-1)).astype("float32")
+
+            assert test_img_data.dtype == numpy.dtype("float32"), "dtype mismatch"
+            assert test_img_data.shape == ref_img_data.shape, "shape mismatch"
+            assert test_img_data.min() == ref_img_data.min(), "min mismatch"
+            assert test_img_data.max() == ref_img_data.max(), "max mismatch"
+            # test_img intensity values should range from 64/255 to 128/255
+            assert numpy.all(test_img_data == ref_img_data), "data mismatch"

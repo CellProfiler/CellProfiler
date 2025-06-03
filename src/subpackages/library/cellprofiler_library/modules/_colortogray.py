cfdd6c938d3b35dda00b01d7cabcbd0d58849a15
sspathak <suraj.s.pathak@gmail.com>
2025-06-03 11:20:17 -0400

    replaced Any types with less generic types and added validator decorator

--- src/subpackages/library/cellprofiler_library/modules/_colortogray.py
+++ src/subpackages/library/cellprofiler_library/modules/_colortogray.py
@@ -1,26 +1,29 @@
-from pydantic import Field
-from typing import Annotated, Any, Iterable
+from pydantic import Field, validate_call, ConfigDict
+from typing import Annotated, Any, Iterable, Sequence
 import numpy as np
 import matplotlib.colors
+from ..types import Image2DColor, Image2DGrayscale
 
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def combine_colortogray(
-    image: Annotated[Any, Field(description="Pixel data of image to threshold")],
-    channels: Annotated[Iterable[int], Field(description="Array of integer identifier ")],
-    contributions: Annotated[Iterable[float], Field(description="Array of contribution values")],
-    ):
+    image: Annotated[Image2DColor, Field(description="Pixel data of image to threshold")],
+    channels: Annotated[Sequence[int], Field(description="Array of integer identifier ")],
+    contributions: Annotated[Sequence[float], Field(description="Array of contribution values")],
+    ) -> Image2DGrayscale:
     denominator = sum(contributions)
-    channels = np.array(channels, int)
-    contributions = np.array(contributions) / denominator
+    _channels = np.array(channels, int)
+    _contributions = np.array(contributions) / denominator
 
     output_image = np.sum(
-        image[:, :, channels]
-        * contributions[np.newaxis, np.newaxis, :],
+        image[:, :, _channels]
+        * _contributions[np.newaxis, np.newaxis, :],
         2
     )
     return output_image
 
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def split_hsv(
-        input_image: Annotated[Any, Field(description="Pixel data of image to be split")],
-):
+        input_image: Annotated[Image2DColor, Field(description="Pixel data of image to be split")],
+) -> Image2DGrayscale:
      output_image = matplotlib.colors.rgb_to_hsv(input_image)
      return output_image

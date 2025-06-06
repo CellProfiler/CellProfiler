707d83e12343f1e868ed681a6e81c8a93bf9d22e
sspathak <suraj.s.pathak@gmail.com>
2025-06-06 14:04:11 -0400

    colortogray renamed functions, moved functions to functions/image_processing.py, renamed some helper functions

--- src/subpackages/library/cellprofiler_library/functions/image_processing.py
+++ src/subpackages/library/cellprofiler_library/functions/image_processing.py
@@ -9,6 +9,10 @@ from typing import Annotated, Any, Optional, Tuple, Callable, Union, Sequence
 from pydantic import Field, BeforeValidator
 from ..types import ImageGrayscale, ImageGrayscaleMask, Image2DColor, Image2DGrayscale
 from ..opts import threshold as Threshold
+from typing import Annotated, Any, Optional, Tuple, Callable, Union, Sequence
+from pydantic import Field, validate_call, BeforeValidator, ConfigDict
+from ..types import ImageGrayscale, ImageGrayscaleMask, Image2DColor, Image2DGrayscale
+
 
 
 def rgb_to_greyscale(image):
@@ -567,6 +571,7 @@ def gaussian_filter(image, sigma):
 # ColorToGray
 ################################################################################
 
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def combine_colortogray(
     image:          Annotated[Image2DColor, Field(description="Pixel data of image to threshold")],
     channels:       Annotated[Sequence[int], Field(description="Array of integer identifier ")],
@@ -582,16 +587,22 @@ def combine_colortogray(
         2
     )
     return output_image
+
      
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def split_hsv(
         input_image: Annotated[Image2DColor, Field(description="Pixel data of image to be split. Input shape is (x, y, 3) where c is the color channel.")],
 ) -> Annotated[Sequence[Image2DGrayscale], Field(description="Output is a list of images where each image is a channel in the HSV color space. ")]:
      output_image = matplotlib.colors.rgb_to_hsv(input_image)
      return [i for i in output_image.transpose(2, 0, 1)]
 
+
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def split_rgb(input_image: Image2DColor) -> Annotated[Sequence[Image2DGrayscale], Field(description="Output is a list of images where each image is a channel of the RGB color space. ")]:  
      return [i for i in input_image.transpose(2, 0, 1)]
 
+
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def split_multichannel(input_image: Image2DColor) -> Annotated[Sequence[Image2DGrayscale], Field(description="Output is a list of images where each image is a channel")]:
      return split_rgb(input_image)
 

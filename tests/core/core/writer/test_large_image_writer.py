b21974e5f747aa90393066f67177cab7a0e4695a
Nodar Gogoberidze <gnodar01@gmail.com>
2025-06-20 10:53:56 -0400

    [wip] cont

--- tests/core/core/writer/test_large_image_writer.py
+++ tests/core/core/writer/test_large_image_writer.py
@@ -6,6 +6,7 @@ from cellprofiler_core.utilities.pathname import pathname2url
 from cellprofiler_core.pipeline import Pipeline
 from cellprofiler_core.pipeline.event import LoadException
 from cellprofiler_core.pipeline import ImageFile, ImagePlane
+from cellprofiler_core.image import Image
 from cellprofiler_core.writers.tiled_writer import TiledImageWriter
 from cellprofiler_core.modules.namesandtypes import NamesAndTypes, LOAD_AS_GRAYSCALE_IMAGE, INTENSITY_RESCALING_BY_DATATYPE, ASSIGN_ALL
 import dask.array
@@ -15,9 +16,9 @@ def get_data_directory():
     folder = os.path.dirname(tests.core.__file__)
     return os.path.abspath(os.path.join(folder, "data/"))
 
-def get_pipeline():
+def get_pipeline(name):
     pipeline_file = os.path.join(
-        get_data_directory(), "tiled/tiled.cppipe"
+        get_data_directory(), f"tiled/{name}.cppipe"
     )
     with open(pipeline_file, "r") as fd:
         data = fd.read()
@@ -38,8 +39,8 @@ def make_ipd(url, metadata, series=0, index=0, channel=None):
         plane.set_metadata(key, value)
     return plane
 
-def run_pipeline():
-    pipeline = get_pipeline()
+def run_pipeline(name):
+    pipeline = get_pipeline(name)
     url = pathname2url(os.path.join(get_data_directory(), "tiled/largeimg.ome.tiff"))
     pipeline.add_urls([url])
     for module in pipeline.modules():
@@ -48,7 +49,7 @@ def run_pipeline():
     return pipeline, measurements
 
 def test_01_load_pipeline():
-    pipeline = get_pipeline()
+    pipeline = get_pipeline("tiled")
 
     assert len(pipeline.modules()) == 4
     module = pipeline.modules()[2]
@@ -69,11 +70,13 @@ def test_02_ome_tiff_read():
     fd, filepath = make_temporary_file(prefix="largeimg", suffix=".ome.zarr")
     os.close(fd)
 
-    pipeline, _ = run_pipeline()
+    pipeline, measurements = run_pipeline("tiled_gaussian")
     image_plane_list: list[ImagePlane] = pipeline.image_plane_list
 
     for plane in image_plane_list:
         file = ImageFile(filepath)
+        img_name: list[str] = measurements.get_names()[0] # e.g. 'DNA'
+        img: Image = measurements.get_image(img_name)
         writer = TiledImageWriter(file)
         # provider -> get data
         writer.write_tiled()

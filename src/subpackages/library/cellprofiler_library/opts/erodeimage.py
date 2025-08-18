896e5ec6bdccac50874c9b7d5cd1f6dbc375b9e3
sspathak <suraj.s.pathak@gmail.com>
2025-08-18 10:47:41 -0400

    [erodeimage]: moved to library

---
+++ src/subpackages/library/cellprofiler_library/opts/erodeimage.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+"""
+Options and enums for ErodeImage module
+"""
+
+# Note: ErodeImage is a simple morphological operation module that doesn't require
+# complex enums. The main configuration is handled through the StructuringElement
+# setting which is managed by the core framework. This file is created for 
+# consistency with the refactoring pattern but may be minimal.
+
+# Currently no custom enums needed for ErodeImage as it uses standard
+# StructuringElement configuration from cellprofiler_core

cdcfd1fc382df5cc577c701ad3a881c1189738dc
sspathak <suraj.s.pathak@gmail.com>
2025-09-19 16:37:30 -0400

    [imagemath]: code cleanup, made types more specific

--- src/subpackages/library/cellprofiler_library/modules/_imagemath.py
+++ src/subpackages/library/cellprofiler_library/modules/_imagemath.py
@@ -1,16 +1,11 @@
 import numpy
-from typing import Callable, Optional, List, Tuple, cast
+from typing import Callable, Optional, List, Tuple, cast, Dict
 from skimage.util import invert as _invert
 from cellprofiler_library.opts.imagemath import Operator, BINARY_OUTPUT_OPS, UNARY_INPUT_OPS, BINARY_INPUT_OPS
 from ..types import ImageAny, ImageMaskAny
 from numpy import isscalar as _isscalar
 invert = cast(Callable[[ImageAny], ImageAny], _invert)
-isscalar = cast(Callable[[ImageAny], bool], _isscalar)
-
-def use_logical_operation(pixel_data: List[ImageAny]) -> bool:
-    return all(
-        [pd.dtype == bool for pd in pixel_data if not isscalar(pd)]
-    )
+isscalar = cast(Callable[[Optional[ImageAny]], bool], _isscalar)
 
 
 def apply_on_image(
@@ -19,7 +14,6 @@ def apply_on_image(
         comparitor: ImageAny, 
         op: Callable[[ImageAny, ImageAny], ImageAny], 
         opval: Operator,
-        use_logical: bool
         ) -> ImageAny:
     assert isinstance(output_pixel_data, numpy.ndarray), "output_pixel_data must be a numpy array" # Pylance needs to understand this is a numpy array
     if not isscalar(pd) and output_pixel_data.ndim != pd.ndim:
@@ -31,83 +25,90 @@ def apply_on_image(
             pd = pd[:, :, numpy.newaxis]
     if opval == Operator.EQUALS:
         output_pixel_data = output_pixel_data & (comparitor == pd)
-    # elif opval == Operator.SUBTRACT and use_logical:
-    #     output_pixel_data[pd] = False
     else:
         output_pixel_data = op(output_pixel_data, pd)
     return output_pixel_data
-
-
-def apply_on_mask():
-    pass
 
 
 def logical_subtract(output_pixel_data: ImageAny, pd: ImageAny) -> ImageAny:
     output_pixel_data[pd] = False
     return output_pixel_data
 
+
 def apply_binary_operation(
     opval: Operator, 
     pixel_data: List[ImageAny], 
-    masks: List[ImageMaskAny], 
+    masks: Optional[List[Optional[ImageMaskAny]]], 
     output_pixel_data: ImageAny, 
-    output_mask: ImageMaskAny,
+    output_mask: Optional[ImageMaskAny],
     image_factors: Optional[List[float]], # TODO: can this be removed?
     ignore_mask: bool, # TODO: can this be removed?
     ) -> Tuple[
         ImageAny, 
-        ImageMaskAny, 
+        Optional[ImageMaskAny], 
     ]:
+    #
+    # Helper function to determine if logical operations should be used
+    #
+    def use_logical_operation(pixel_data: List[ImageAny]) -> bool:
+        return all(
+            [pd.dtype == bool for pd in pixel_data if not isscalar(pd)]
+        )
     # initialize op to return martix of ones by default
-    op: Callable[[ImageAny, ImageAny], ImageAny] = lambda im1, im2 : numpy.ones(im1.shape)
     comparitor = pixel_data[0] # fix pylance error
     use_logical = use_logical_operation(pixel_data)
+    op_fn_dispatch: Dict[Operator, Callable[[ImageAny, ImageAny], ImageAny]] = {
+        Operator.ADD: numpy.add,
+        Operator.SUBTRACT: logical_subtract if use_logical else numpy.subtract,
+        Operator.DIFFERENCE: numpy.logical_xor if use_logical else lambda x, y: numpy.abs(numpy.subtract(x, y)),
+        Operator.MULTIPLY: numpy.logical_and if use_logical else numpy.multiply,
+        Operator.MINIMUM: numpy.minimum,
+        Operator.MAXIMUM: numpy.maximum,
+        Operator.AVERAGE: numpy.add,
+        Operator.MAXIMUM: numpy.maximum,
+        Operator.AND: numpy.logical_and,
+        Operator.OR: numpy.logical_or,
+        Operator.EQUALS: numpy.equal,
+        Operator.NONE: lambda x, y: x,
+        Operator.DIVIDE: numpy.divide,
+    }
+    if opval not in op_fn_dispatch:
+        raise NotImplementedError(f"Unimplemented operation: {opval}")
+    #
     # Binary operations
-    if opval in (Operator.ADD, Operator.AVERAGE):
-        op = numpy.add
-    
-    elif opval == Operator.SUBTRACT:
-        if use_logical:
-            output_pixel_data = pixel_data[0].copy()
-            op = logical_subtract
-        else:
-            op = numpy.subtract
-    
-    elif opval == Operator.DIFFERENCE:
-        op = numpy.logical_xor if use_logical else lambda x, y: numpy.abs(numpy.subtract(x, y))
+    #
+    op = op_fn_dispatch[opval]
 
-    elif opval == Operator.MULTIPLY:
-        op = numpy.logical_and if use_logical else numpy.multiply
-    
-    elif opval == Operator.MINIMUM:
-        op = numpy.minimum
-    
-    elif opval == Operator.MAXIMUM:
-        op = numpy.maximum
-    
-    elif opval == Operator.AND:
-        op = numpy.logical_and
-    
-    elif opval == Operator.OR:
-        op = numpy.logical_or
-    
-    elif opval == Operator.EQUALS:
+    #
+    # Equals and Subtract operations need additional handling
+    #
+    if opval == Operator.EQUALS:
         output_pixel_data = numpy.ones(pixel_data[0].shape, bool)
         comparitor = pixel_data[0]
-    else:
-        op = numpy.divide
+    elif opval == Operator.SUBTRACT and use_logical:
+        output_pixel_data = pixel_data[0].copy()
 
+    
+    # _masks is a list of Nones if masks is None. Fixes type warnings.
+    if masks is None:
+        masks = [None for _ in pixel_data]
+
+    #
+    # Apply the operation to each image in the list
+    #
     for pd, mask in zip(pixel_data[1:], masks[1:]):
-        output_pixel_data = apply_on_image(output_pixel_data, pd, comparitor, op, opval, use_logical)
-        if ignore_mask:
-            continue
-        else:
+        output_pixel_data = apply_on_image(output_pixel_data, pd, comparitor, op, opval)
+        if not ignore_mask:
             if output_mask is None:
                 output_mask = mask
             elif mask is not None:
                 output_mask = output_mask & mask
+    #
+    # Average operation needs additional handling
+    #
     if opval == Operator.AVERAGE:
-        if not use_logical_operation(pixel_data):
+        if not use_logical:
+            assert image_factors is not None, "image_factors must be provided for average operation"
             output_pixel_data /= sum(image_factors)
     return output_pixel_data, output_mask
 
@@ -115,13 +116,13 @@ def apply_binary_operation(
 def apply_unary_operation(
     opval: Operator, 
     pixel_data: List[ImageAny], 
-    masks: List[ImageMaskAny], 
+    masks: Optional[List[Optional[ImageMaskAny]]], 
     output_pixel_data: ImageAny,
-    output_mask: ImageMaskAny,
+    output_mask: Optional[ImageMaskAny],
     ignore_mask: bool,
     ) -> Tuple[
         ImageAny, 
-        ImageMaskAny,
+        Optional[ImageMaskAny],
     ]:
     if opval == Operator.STDEV:
         pixel_array = numpy.array(pixel_data)
@@ -149,9 +150,9 @@ def apply_unary_operation(
 def image_math(
         opval: Operator, 
         pixel_data: List[ImageAny], 
-        masks: List[ImageMaskAny], 
+        masks: Optional[List[Optional[ImageMaskAny]]], 
         output_pixel_data: ImageAny, 
-        output_mask: ImageMaskAny,
+        output_mask: Optional[ImageMaskAny],
         ignore_mask: bool = False,
         image_factors: Optional[List[float]] = None,
         exponent: Optional[float] = None,
@@ -161,7 +162,7 @@ def image_math(
         truncate_high: Optional[bool] = None,
         replace_nan: Optional[bool] = None,
         ) -> Tuple[
-            ImageAny, ImageMaskAny
+            ImageAny, Optional[ImageMaskAny]
         ]:
     if opval in BINARY_INPUT_OPS:
         output_pixel_data, output_mask = apply_binary_operation(opval, pixel_data, masks, output_pixel_data, output_mask, image_factors, ignore_mask)

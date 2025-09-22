e6244a567da76090bae6d4b273ededf704353a6f
sspathak <suraj.s.pathak@gmail.com>
2025-09-22 10:20:34 -0400

    [imagemath]: Added type annotations, fixed typing error caused by weaving of image and measurement operands into one array, renamed variable names to be more descriptive, code cleanup

--- src/subpackages/library/cellprofiler_library/modules/_imagemath.py
+++ src/subpackages/library/cellprofiler_library/modules/_imagemath.py
@@ -1,35 +1,36 @@
 import numpy
-from typing import Callable, Optional, List, Tuple, cast
-from cellprofiler_library.opts.imagemath import Operator, BINARY_OUTPUT_OPS, UNARY_INPUT_OPS, BINARY_INPUT_OPS
-from ..types import ImageAny, ImageMaskAny
+from typing import Callable, Optional, List, Tuple, cast, Union, Annotated
 from numpy import isscalar as _isscalar
+from pydantic import validate_call, ConfigDict, Field
+from cellprofiler_library.opts.imagemath import Operator, BINARY_OUTPUT_OPS, UNARY_INPUT_OPS, BINARY_INPUT_OPS
+from cellprofiler_library.types import ImageAny, ImageAnyMask
 from cellprofiler_library.functions.image_processing import imagemath_apply_binary_operation, imagemath_apply_unary_operation
 isscalar = cast(Callable[[Optional[ImageAny]], bool], _isscalar)
 
 
-
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def image_math(
-        opval: Operator, 
-        pixel_data: List[ImageAny], 
-        masks: Optional[List[Optional[ImageMaskAny]]], 
-        output_pixel_data: ImageAny, 
-        output_mask: Optional[ImageMaskAny],
-        ignore_mask: bool = False,
-        image_factors: Optional[List[float]] = None,
-        exponent: Optional[float] = None,
-        after_factor: Optional[float] = None,
-        addend: Optional[float] = None,
-        truncate_low: Optional[bool] = None,
-        truncate_high: Optional[bool] = None,
-        replace_nan: Optional[bool] = None,
+        opval:              Annotated[Operator, Field(description=" Operator to apply to the images or measurements")],
+        operands:           Annotated[List[Union[ImageAny, float]], Field(description=" List of images and/or measurements to apply the operator to")],
+        masks:              Annotated[Optional[List[Optional[Union[ImageAnyMask, bool]]]], Field(description=" List of masks for the images. Use `True` for measurement operands")],
+        output_pixel_data:  Annotated[ImageAny, Field(description=" Output image or measurement")],
+        output_mask:        Annotated[Optional[ImageAnyMask], Field(description=" Output mask")],
+        ignore_mask:        Annotated[bool, Field(description=" Ignore the masks")],
+        image_factors:      Annotated[Optional[List[float]], Field(description=" List of image factors to multiply the images by")],
+        exponent:           Annotated[Optional[float], Field(description=" Exponent to raise the images by")],
+        after_factor:       Annotated[Optional[float], Field(description=" Multiply the images by this value after the operation")],
+        addend:             Annotated[Optional[float], Field(description=" Add this value to the images")],
+        truncate_low:       Annotated[Optional[bool], Field(description=" Truncate the images below 0")],
+        truncate_high:      Annotated[Optional[bool], Field(description=" Truncate the images above 0")],
+        replace_nan:        Annotated[Optional[bool], Field(description=" Replace invalid values with 0")],
         ) -> Tuple[
-            ImageAny, Optional[ImageMaskAny]
+            ImageAny, Optional[ImageAnyMask]
         ]:
     if opval in BINARY_INPUT_OPS:
-        output_pixel_data, output_mask = imagemath_apply_binary_operation(opval, pixel_data, masks, output_pixel_data, output_mask, image_factors, ignore_mask)
+        output_pixel_data, output_mask = imagemath_apply_binary_operation(opval, operands, masks, output_pixel_data, output_mask, image_factors, ignore_mask)
 
     elif opval in UNARY_INPUT_OPS:
-        output_pixel_data, output_mask = imagemath_apply_unary_operation(opval, pixel_data, masks, output_pixel_data, output_mask, ignore_mask)
+        output_pixel_data, output_mask = imagemath_apply_unary_operation(opval, operands, masks, output_pixel_data, output_mask, ignore_mask)
 
     else:
         raise NotImplementedError(

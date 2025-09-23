9d1f71f61cfc55cc28297fa46ff52a9a1aa3957a
sspathak <suraj.s.pathak@gmail.com>
2025-09-23 15:27:29 -0400

    [convertimagetoobjects]: Updated types, removed redundant imports

--- src/subpackages/library/cellprofiler_library/functions/image_processing.py
+++ src/subpackages/library/cellprofiler_library/functions/image_processing.py
@@ -6,7 +6,7 @@ import centrosome.threshold
 import scipy
 import matplotlib
 from typing import Any, Optional, Tuple, Callable, Union, List
-from ..types import ImageGrayscale, ImageGrayscaleMask, Image2DColor, Image2DGrayscale
+from ..types import ImageGrayscale, ImageGrayscaleMask, Image2DColor, Image2DGrayscale, ImageAny, ObjectLabelsDense
 from ..opts import threshold as Threshold
 
 
@@ -595,12 +595,12 @@ def split_multichannel(input_image: Image2DColor) -> List[Image2DGrayscale]:
      return split_rgb(input_image)
 
 def convert_image_to_objects(
-        data:           Annotated[Any, Field(description="Image to be converted to Objects")], 
+        data:           Annotated[ImageAny, Field(description="Image to be converted to Objects")], 
         cast_to_bool:   Annotated[bool, Field(description="Convert a grayscale image to binary before converting it to an object")], 
         preserve_label: Annotated[bool, Field(description="Preserve original labels of objects")], 
         background:     Annotated[int, Field(description="Pixel value of the background")], 
         connectivity:   Annotated[Union[int, None], Field(description="Maximum number of orthogonal hops to consider a pixel/voxel as a neighbor")]
-        ) -> Any:
+        ) -> ObjectLabelsDense:
     # Compatibility with skimage
     connectivity = None if connectivity == 0 else connectivity
 
--- src/subpackages/library/cellprofiler_library/modules/_convertimagetoobjects.py
+++ src/subpackages/library/cellprofiler_library/modules/_convertimagetoobjects.py
@@ -1,7 +1,6 @@
-from typing import Annotated, Any, Optional, Tuple, Callable, Union
-from pydantic import Field, validate_call, BeforeValidator, ConfigDict
-
-from cellprofiler_library.types import ImageGrayscale
+from typing import Annotated, Union
+from pydantic import Field, validate_call, ConfigDict
+from cellprofiler_library.types import ImageGrayscale, ObjectLabelsDense
 from ..functions.image_processing import convert_image_to_objects
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
@@ -11,5 +10,5 @@ def convert_image_to_objects(
         preserve_label: Annotated[bool, Field(description="Preserve original labels of objects")], 
         background:     Annotated[int, Field(description="Pixel value of the background")], 
         connectivity:   Annotated[Union[int, None], Field(description="Maximum number of orthogonal hops to consider a pixel/voxel as a neighbor")]
-        ) -> Any:
+        ) -> ObjectLabelsDense:
     return convert_image_to_objects(data, cast_to_bool, preserve_label, background, connectivity)

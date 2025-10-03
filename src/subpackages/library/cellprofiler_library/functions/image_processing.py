b0f6a029dde92b45e08d04cdadebdcca7d01ebbd
sspathak <suraj.s.pathak@gmail.com>
2025-10-03 10:10:41 -0400

    [graytocolor]: Updated tests to use enums, moved funcitons from library/modules to library/functions/image_processing, updated enum usage in graytocolor module frontend

--- src/subpackages/library/cellprofiler_library/functions/image_processing.py
+++ src/subpackages/library/cellprofiler_library/functions/image_processing.py
@@ -1850,3 +1850,88 @@ def imagemath_apply_unary_operation(
             "The operation %s has not been implemented" % opval
         )
     return output_pixel_data, output_mask
+
+
+###############################################################################
+# GrayToColor
+###############################################################################
+
+def gray_to_rgb(
+        pixel_data_arr: List[Optional[Image2DGrayscale]],
+        adjustment_factor_array: List[float],
+        intensities: List[Tuple[float, ...]]=[(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)],
+        wants_rescale: bool=True,
+        ) -> Image2DColor:
+    assert(len(pixel_data_arr) == len(adjustment_factor_array)), f"pixel_data_arr and adjustment_factor_array must be the same length. pixel_data_arr has {len(pixel_data_arr)} elements, adjustment_factor_array has {len(adjustment_factor_array)} elements."
+    assert(len(pixel_data_arr) == len(intensities)), f"pixel_data_arr and intensities must be the same length. pixel_data_arr has {len(pixel_data_arr)} elements, intensities has {len(intensities)} elements."
+
+    parent_image = None
+    rgb_pixel_data = None
+    for pixel_data, adjustment_factor, intensity_triplet in zip(pixel_data_arr, adjustment_factor_array, intensities):
+        if pixel_data is None:
+            continue
+        multiplier = numpy.array(intensity_triplet) * adjustment_factor
+        if wants_rescale:
+            pixel_data = pixel_data /numpy.max(pixel_data)
+        if parent_image is not None:
+            if parent_image.shape != pixel_data.shape:
+                raise ValueError(
+                    "The input images have different sizes (%s vs %s)"
+                    % (
+                        parent_image.shape, 
+                        pixel_data.shape,
+                    )
+                )
+            rgb_pixel_data += numpy.dstack([pixel_data] * 3) * multiplier
+        else:
+            parent_image = pixel_data
+            rgb_pixel_data = numpy.dstack([pixel_data] * 3) * multiplier
+    return rgb_pixel_data
+
+def gray_to_cmyk(*args, **kwargs):
+    # gray to cmyk has the same implementation as gray to rgb but with different intensities
+    return gray_to_rgb(*args, **kwargs)
+
+
+def gray_to_composite_color(
+        pixel_data_arr: List[Optional[Image2DGrayscale]],
+        color_array: List[Tuple[int, int, int]],
+        weight_array: List[float],
+        wants_rescale: bool,
+) -> Image2DColor:
+    source_channels = pixel_data_arr
+    parent_image = pixel_data_arr[0]
+    for idx, pd in enumerate(source_channels):
+        if pd is None:
+            continue
+        if pd.shape != source_channels[0].shape:
+            raise ValueError(
+                "The input images have different sizes (%s vs %s)"
+                % (
+                    source_channels[0].shape,
+                    pd.shape,
+                )
+            )
+
+    colors: List[NDArray[numpy.float32]] = []
+    pixel_data = parent_image
+    if wants_rescale:
+        pixel_data = pixel_data / numpy.max(pixel_data)
+    for color_tuple, weight in zip(color_array, weight_array):
+        color = (weight * numpy.array(color_tuple).astype(pixel_data.dtype) / 255)
+        colors += [color[numpy.newaxis, numpy.newaxis, :]]
+    rgb_pixel_data = pixel_data[:, :, numpy.newaxis] * colors[0]
+    for image, color in zip(source_channels[1:], colors[1:]):
+        if wants_rescale:
+            image = image / numpy.max(image)
+        rgb_pixel_data = rgb_pixel_data + image[:, :, numpy.newaxis] * color
+
+    return rgb_pixel_data
+    
+
+def gray_to_stacked_color(
+    pixel_data_arr: List[Optional[Image2DGrayscale]]
+) -> Image2DColor:
+    source_channels = pixel_data_arr
+    rgb_pixel_data = numpy.dstack(source_channels)
+    return rgb_pixel_data

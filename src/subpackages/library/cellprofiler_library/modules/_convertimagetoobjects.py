54628284e57209efb27e6a80aae00190872f93fe
sspathak <suraj.s.pathak@gmail.com>
2025-10-08 10:06:31 -0400

    [convertimagetoobjects]: changed type in _convertimagetoobjects.py to use Optional type

--- src/subpackages/library/cellprofiler_library/modules/_convertimagetoobjects.py
+++ src/subpackages/library/cellprofiler_library/modules/_convertimagetoobjects.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Union
+from typing import Annotated, Optional
 from pydantic import Field, validate_call, ConfigDict
 from cellprofiler_library.types import ImageGrayscale, ObjectLabelsDense
 from ..functions.image_processing import convert_image_to_objects
@@ -9,6 +9,6 @@ def convert_image_to_objects(
         cast_to_bool:   Annotated[bool, Field(description="Convert a grayscale image to binary before converting it to an object")], 
         preserve_label: Annotated[bool, Field(description="Preserve original labels of objects")], 
         background:     Annotated[int, Field(description="Pixel value of the background")], 
-        connectivity:   Annotated[Union[int, None], Field(description="Maximum number of orthogonal hops to consider a pixel/voxel as a neighbor")]
+        connectivity:   Annotated[Optional[int], Field(description="Maximum number of orthogonal hops to consider a pixel/voxel as a neighbor")]
         ) -> ObjectLabelsDense:
     return convert_image_to_objects(data, cast_to_bool, preserve_label, background, connectivity)

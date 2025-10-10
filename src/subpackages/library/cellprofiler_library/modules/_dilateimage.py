1b87774f6b34538e32103fa11337c9f55717ed15
sspathak <suraj.s.pathak@gmail.com>
2025-10-10 10:45:57 -0400

    [dilateimage]: Updated library module function signature to allow passing structuring_elements opts to create structuring elements without needing to use skimage, reverted frontend module logic to use ImageProcessing (super class) functionality to process images.

--- src/subpackages/library/cellprofiler_library/modules/_dilateimage.py
+++ src/subpackages/library/cellprofiler_library/modules/_dilateimage.py
@@ -7,23 +7,22 @@ This module contains the core algorithms for morphological dilation operations.
 """
 
 from pydantic import validate_call, ConfigDict
-from typing import Union
-import numpy as np
-
-from ..types import Image2D, Image2DGrayscale, Image3DGrayscale, ImageGrayscale
-from ..functions.image_processing import morphology_dilation
+from typing import Union, Tuple
+from cellprofiler_library.types import ImageAny, StructuringElement
+from cellprofiler_library.functions.image_processing import morphology_dilation, get_structuring_element
+from cellprofiler_library.opts.structuring_elements import StructuringElementShape2D, StructuringElementShape3D
 
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def dilate_image(
-    image: ImageGrayscale,
-    structuring_element: np.ndarray
-) -> ImageGrayscale:
+    image: ImageAny,
+    structuring_element: Union[StructuringElement, Tuple[Union[StructuringElementShape2D, StructuringElementShape3D], int]]
+) -> ImageAny:
     """Apply morphological dilation to an image.
     
     Args:
         image: Input image (2D or 3D grayscale)
-        structuring_element: Structuring element for dilation operation
+        structuring_element: Structuring element for dilation operation as an NDArray or a tuple of (StructuringElement[N]D, size)
         
     Returns:
         Dilated image with same dimensions and type as input
@@ -31,4 +30,6 @@ def dilate_image(
     Raises:
         NotImplementedError: If trying to apply 3D structuring element to 2D image
     """
+    if isinstance(structuring_element, tuple):
+        structuring_element = get_structuring_element(structuring_element[0], structuring_element[1])
     return morphology_dilation(image, structuring_element)

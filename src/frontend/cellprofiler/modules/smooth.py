5bfbf13601a2215afad1dd9f150bbdd480562743
sspathak <suraj.s.pathak@gmail.com>
2025-10-31 16:54:43 -0400

    [smooth]: moved to library

--- src/frontend/cellprofiler/modules/smooth.py
+++ src/frontend/cellprofiler/modules/smooth.py
@@ -22,9 +22,6 @@ See also several related modules in the *Advanced* category (e.g.,
 **MedianFilter** and **GaussianFilter**).
 """
 
-import numpy
-import scipy.ndimage
-import skimage.restoration
 from cellprofiler_core.constants.module import (
     HELP_ON_MEASURING_DISTANCES,
     HELP_ON_PIXEL_INTENSITIES,
@@ -35,16 +32,9 @@ from cellprofiler_core.setting import Binary
 from cellprofiler_core.setting.choice import Choice
 from cellprofiler_core.setting.subscriber import ImageSubscriber
 from cellprofiler_core.setting.text import ImageName, Float
-from centrosome.filter import median_filter, circular_average_filter
-from centrosome.smooth import fit_polynomial
-from centrosome.smooth import smooth_with_function_and_mask
 
-FIT_POLYNOMIAL = "Fit Polynomial"
-MEDIAN_FILTER = "Median Filter"
-GAUSSIAN_FILTER = "Gaussian Filter"
-SMOOTH_KEEPING_EDGES = "Smooth Keeping Edges"
-CIRCULAR_AVERAGE_FILTER = "Circular Average Filter"
-SM_TO_AVERAGE = "Smooth to Average"
+from cellprofiler_library.modules._smooth import smooth
+from cellprofiler_library.opts.smooth import SmoothingMethod
 
 
 class Smooth(Module):
@@ -68,19 +58,19 @@ class Smooth(Module):
         self.smoothing_method = Choice(
             "Select smoothing method",
             [
-                FIT_POLYNOMIAL,
-                GAUSSIAN_FILTER,
-                MEDIAN_FILTER,
-                SMOOTH_KEEPING_EDGES,
-                CIRCULAR_AVERAGE_FILTER,
-                SM_TO_AVERAGE,
+                SmoothingMethod.FIT_POLYNOMIAL,
+                SmoothingMethod.GAUSSIAN_FILTER,
+                SmoothingMethod.MEDIAN_FILTER,
+                SmoothingMethod.SMOOTH_KEEPING_EDGES,
+                SmoothingMethod.CIRCULAR_AVERAGE_FILTER,
+                SmoothingMethod.SM_TO_AVERAGE,
             ],
             doc="""\
 This module smooths images using one of several filters. Fitting a
 polynomial is fastest but does not allow a very tight fit compared to
 the other methods:
 
--  *%(FIT_POLYNOMIAL)s:* This method is fastest but does not allow
+-  *{FIT_POLYNOMIAL}:* This method is fastest but does not allow
    a very tight “fit” compared to the other methods. Thus, it will usually be less
    accurate. The method treats the intensity of the image
    pixels as a polynomial function of the x and y position of each
@@ -92,53 +82,68 @@ the other methods:
    view), this method will produce an image with a bright central region
    and dimmer edges. But, in some cases the peak/trough of the
    polynomial may actually occur outside of the image itself.
--  *%(GAUSSIAN_FILTER)s:* This method convolves the image with a
+-  *{GAUSSIAN_FILTER}:* This method convolves the image with a
    Gaussian whose full width at half maximum is the artifact diameter
    entered. Its effect is to blur and obscure features smaller than the
    specified diameter and spread bright or dim features larger than the
    specified diameter.
--  *%(MEDIAN_FILTER)s:* This method finds the median pixel value within
+-  *{MEDIAN_FILTER}:* This method finds the median pixel value within
    the diameter you specify. It removes bright or dim features
    that are significantly smaller than the specified diameter.
--  *%(SMOOTH_KEEPING_EDGES)s:* This method uses a bilateral filter
+-  *{SMOOTH_KEEPING_EDGES}:* This method uses a bilateral filter
    which limits Gaussian smoothing across an edge while applying
    smoothing perpendicular to an edge. The effect is to respect edges in
-   an image while smoothing other features. *%(SMOOTH_KEEPING_EDGES)s*
+   an image while smoothing other features. *{SMOOTH_KEEPING_EDGES}*
    will filter an image with reasonable speed for artifact diameters
    greater than 10 and for intensity differences greater than 0.1. The
    algorithm will consume more memory and operate more slowly as you
    lower these numbers.
--  *%(CIRCULAR_AVERAGE_FILTER)s:* This method convolves the image with
+-  *{CIRCULAR_AVERAGE_FILTER}:* This method convolves the image with
    a uniform circular averaging filter whose size is the artifact
    diameter entered. This filter is useful for re-creating an
    out-of-focus blur to an image.
--  *%(SM_TO_AVERAGE)s:* Creates a flat, smooth image where every pixel
+-  *{SM_TO_AVERAGE}:* Creates a flat, smooth image where every pixel
    of the image equals the average value of the original image.
 
-*Note, when deciding between %(MEDIAN_FILTER)s and %(GAUSSIAN_FILTER)s
+*Note, when deciding between {MEDIAN_FILTER} and {GAUSSIAN_FILTER}
 we typically recommend
-%(MEDIAN_FILTER)s over %(GAUSSIAN_FILTER)s because the
+{MEDIAN_FILTER} over {GAUSSIAN_FILTER} because the
 median is less sensitive to outliers, although the results are also
 slightly less smooth and the fact that images are in the range of 0
 to 1 means that outliers typically will not dominate too strongly
 anyway.*
-"""
-            % globals(),
+""".format(
+    **{
+        "FIT_POLYNOMIAL": SmoothingMethod.FIT_POLYNOMIAL.value,
+        "GAUSSIAN_FILTER": SmoothingMethod.GAUSSIAN_FILTER.value,
+        "MEDIAN_FILTER": SmoothingMethod.MEDIAN_FILTER.value,
+        "SMOOTH_KEEPING_EDGES": SmoothingMethod.SMOOTH_KEEPING_EDGES.value,
+        "CIRCULAR_AVERAGE_FILTER": SmoothingMethod.CIRCULAR_AVERAGE_FILTER.value,
+        "SM_TO_AVERAGE": SmoothingMethod.SM_TO_AVERAGE.value,
+    }
+),
         )
 
         self.wants_automatic_object_size = Binary(
             "Calculate artifact diameter automatically?",
             True,
             doc="""\
-*(Used only if “%(GAUSSIAN_FILTER)s”, “%(MEDIAN_FILTER)s”, “%(SMOOTH_KEEPING_EDGES)s” or “%(CIRCULAR_AVERAGE_FILTER)s” is selected)*
+*(Used only if “{GAUSSIAN_FILTER}”, “{MEDIAN_FILTER}”, “{SMOOTH_KEEPING_EDGES}” or “{CIRCULAR_AVERAGE_FILTER}” is selected)*
 
 Select *Yes* to choose an artifact diameter based on the size of
 the image. The minimum size it will choose is 30 pixels, otherwise the
 size is 1/40 of the size of the image.
 
 Select *No* to manually enter an artifact diameter.
-"""
-            % globals(),
+""".format(
+    **{
+        "GAUSSIAN_FILTER": SmoothingMethod.GAUSSIAN_FILTER.value,
+        "MEDIAN_FILTER": SmoothingMethod.MEDIAN_FILTER.value,
+        "SMOOTH_KEEPING_EDGES": SmoothingMethod.SMOOTH_KEEPING_EDGES.value,
+        "CIRCULAR_AVERAGE_FILTER": SmoothingMethod.CIRCULAR_AVERAGE_FILTER.value,
+        "SM_TO_AVERAGE": SmoothingMethod.SM_TO_AVERAGE.value,
+    }
+),
         )
 
         self.object_size = Float(
@@ -171,7 +176,7 @@ these changes. A lower number will preserve weaker edges. A higher
 number will preserve only stronger edges. Values should be between zero
 and one. {pixel_help}
 """.format(
-                smooth_help=SMOOTH_KEEPING_EDGES, pixel_help=HELP_ON_PIXEL_INTENSITIES
+                smooth_help=SmoothingMethod.SMOOTH_KEEPING_EDGES, pixel_help=HELP_ON_PIXEL_INTENSITIES
             ),
         )
 
@@ -192,7 +197,7 @@ and all pixels greater than one to one.
 Select *No* to allow values less than zero and greater than one in
 the output image.
 """.format(
-                fit=FIT_POLYNOMIAL
+                fit=SmoothingMethod.FIT_POLYNOMIAL
             ),
         )
 
@@ -209,13 +214,13 @@ the output image.
 
     def visible_settings(self):
         result = [self.image_name, self.filtered_image_name, self.smoothing_method]
-        if self.smoothing_method.value not in [FIT_POLYNOMIAL, SM_TO_AVERAGE]:
+        if self.smoothing_method.value not in [SmoothingMethod.FIT_POLYNOMIAL, SmoothingMethod.SM_TO_AVERAGE]:
             result.append(self.wants_automatic_object_size)
             if not self.wants_automatic_object_size.value:
                 result.append(self.object_size)
-            if self.smoothing_method.value == SMOOTH_KEEPING_EDGES:
+            if self.smoothing_method.value == SmoothingMethod.SMOOTH_KEEPING_EDGES:
                 result.append(self.sigma_range)
-        if self.smoothing_method.value == FIT_POLYNOMIAL:
+        if self.smoothing_method.value == SmoothingMethod.FIT_POLYNOMIAL:
             result.append(self.clip)
         return result
 
@@ -224,46 +229,15 @@ the output image.
             self.image_name.value, must_be_grayscale=True
         )
         pixel_data = image.pixel_data
-        if self.wants_automatic_object_size.value:
-            object_size = min(30, max(1, numpy.mean(pixel_data.shape) / 40))
-        else:
-            object_size = float(self.object_size.value)
-        sigma = object_size / 2.35
-        if self.smoothing_method.value == GAUSSIAN_FILTER:
-
-            def fn(image):
-                return scipy.ndimage.gaussian_filter(
-                    image, sigma, mode="constant", cval=0
-                )
-
-            output_pixels = smooth_with_function_and_mask(pixel_data, fn, image.mask)
-        elif self.smoothing_method.value == MEDIAN_FILTER:
-            output_pixels = median_filter(pixel_data, image.mask, object_size / 2 + 1)
-        elif self.smoothing_method.value == SMOOTH_KEEPING_EDGES:
-            sigma_range = float(self.sigma_range.value)
-
-            output_pixels = skimage.restoration.denoise_bilateral(
-                image=pixel_data.astype(float),
-                channel_axis=2 if image.multichannel else None,
-                sigma_color=sigma_range,
-                sigma_spatial=sigma,
-            )
-        elif self.smoothing_method.value == FIT_POLYNOMIAL:
-            output_pixels = fit_polynomial(pixel_data, image.mask, self.clip.value)
-        elif self.smoothing_method.value == CIRCULAR_AVERAGE_FILTER:
-            output_pixels = circular_average_filter(
-                pixel_data, object_size / 2 + 1, image.mask
-            )
-        elif self.smoothing_method.value == SM_TO_AVERAGE:
-            if image.has_mask:
-                mean = numpy.mean(pixel_data[image.mask])
-            else:
-                mean = numpy.mean(pixel_data)
-            output_pixels = numpy.ones(pixel_data.shape, pixel_data.dtype) * mean
-        else:
-            raise ValueError(
-                "Unsupported smoothing method: %s" % self.smoothing_method.value
-            )
+        output_pixels = smooth(
+            pixel_data = pixel_data, 
+            mask = image.mask, 
+            multichannel = image.multichannel, 
+            object_size = None if self.wants_automatic_object_size else self.object_size.value,
+            smoothing_method = self.smoothing_method.value, 
+            sigma_range = self.sigma_range.value, 
+            clip = self.clip.value,
+        )
         output_image = Image(output_pixels, parent_image=image)
         workspace.image_set.add(self.filtered_image_name.value, output_image)
         workspace.display_data.pixel_data = pixel_data

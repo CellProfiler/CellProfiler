2db5616ef20932ecda4e2bd460c8e35fa93553d4
sspathak <suraj.s.pathak@gmail.com>
2025-11-04 17:11:15 -0500

    [measureimageintensity]: Fixed incorrect typing, renamed function get_intensity_measurements -> measure_image_intensities

--- src/subpackages/library/cellprofiler_library/modules/_measureimageintensity.py
+++ src/subpackages/library/cellprofiler_library/modules/_measureimageintensity.py
@@ -1,13 +1,15 @@
 import numpy as np
 from numpy.typing import NDArray
-from typing import List, Annotated
+from typing import List, Annotated, Optional
 from pydantic import Field, validate_call, ConfigDict
-from cellprofiler_library.functions.measurement import get_intensity_measurements
+from cellprofiler_library.functions.measurement import measure_image_intensities
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def measure_image_intensity(
-        pixels: Annotated[NDArray[np.float32], Field(description="Image pixel data")],
-        percentiles: Annotated[List[int], Field(description="Percentiles to measure")]=[],
+        pixels:         Annotated[NDArray[np.float32], Field(description="Image pixel data")],
+        percentiles:    Annotated[Optional[List[int]], Field(description="Percentiles to measure")]=[],
         ):
-    statistics, percentile_measures = get_intensity_measurements(pixels, percentiles)
+    if percentiles is None:
+        percentiles = []
+    statistics, percentile_measures = measure_image_intensities(pixels, percentiles)
     return statistics, percentile_measures

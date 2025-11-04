d2f2d14335686abab12a887f1baaf4f3648105a3
sspathak <suraj.s.pathak@gmail.com>
2025-11-04 16:54:08 -0500

    [measureimageareaoccupied]: moved more code to library/functions/mesaurement

--- src/subpackages/library/cellprofiler_library/modules/_measureimageareaoccupied.py
+++ src/subpackages/library/cellprofiler_library/modules/_measureimageareaoccupied.py
@@ -1,62 +1,37 @@
-from typing import Annotated, Optional, Tuple
-from pydantic import Field, validate_call, ConfigDict
 import skimage.measure
 import numpy as np
-from ..types import ImageBinary, ObjectLabelsDense, ImageAny
-from ..functions.measurement import measure_surface_area
+from typing import Annotated, Optional, Tuple
+from pydantic import Field, validate_call, ConfigDict
+from cellprofiler_library.types import ImageBinary, ObjectSegmentation, ImageAny
+from cellprofiler_library.functions.measurement import measure_area_occupied, measure_total_area, measure_perimeter, measure_object_perimeter, measure_objects_area_occupied, measure_objects_total_area
 
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def measure_image_area_perimeter(
-    im_pixel_data: Annotated[ImageBinary, Field(description="Binary image pixel data")],
-    im_volumetric: Annotated[bool, Field(description="Image is volumetric")],
-    im_spacing: Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None
-    ) -> Tuple[float, float, float]:
+    im_pixel_data:  Annotated[ImageBinary, Field(description="Binary image pixel data")],
+    im_volumetric:  Annotated[bool, Field(description="Image is volumetric")],
+    im_spacing:     Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None
+    ) -> Tuple[np.float_, np.float_, np.int_]:
 
-    area_occupied = np.sum(im_pixel_data > 0)
-
-    if area_occupied > 0:
-        if im_volumetric:
-            perimeter = measure_surface_area(im_pixel_data > 0, spacing=im_spacing)
-        else:
-            perimeter = skimage.measure.perimeter(im_pixel_data > 0)
-    else:
-        perimeter = 0
-        
-    total_area = np.prod(np.shape(im_pixel_data))
+    area_occupied = measure_area_occupied(im_pixel_data)
+    perimeter = measure_perimeter(im_pixel_data, im_volumetric, im_spacing)  if area_occupied > 0 else np.float64(0.0)
+    total_area = measure_total_area(im_pixel_data)
  
     return area_occupied, perimeter, total_area
 
-
-
+@validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def measure_objects_area_perimeter(
-    label_image: ObjectLabelsDense, 
-    mask: Optional[ImageAny], 
-    volumetric: bool, 
-    spacing: Optional[Tuple[float, ...]] = None
-    ) -> Tuple[float, float, float]:
+    label_image:    Annotated[ObjectSegmentation, Field(description="Object labels to measure")],
+    mask:           Annotated[Optional[ImageAny], Field(description="Mask of the image")] = None,
+    volumetric:     Annotated[bool, Field(description="True if the image is volumetric")] = False,
+    spacing:        Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None
+    ) -> Tuple[np.float_, np.float_, np.int_]:
     if mask is not None:
         label_image[~mask] = 0
-        total_area = np.sum(mask)
+    regionprops = skimage.measure.regionprops(label_image)
 
-    else:
-        total_area = np.product(label_image.shape)
+    total_area = measure_objects_total_area(label_image, mask)
+    area_occupied = measure_objects_area_occupied(None, regionprops=regionprops)
+    perimeter = measure_object_perimeter(label_image, regionprops=regionprops, volumetric=volumetric, spacing=spacing) if area_occupied > 0 else np.float64(0.0)
 
-    region_properties = skimage.measure.regionprops(label_image)
-    area_occupied = np.sum([region["area"] for region in region_properties])
-    
-    if area_occupied > 0:
-        if volumetric:
-            labels = np.unique(label_image)
-            if labels[0] == 0:
-                labels = labels[1:]
-            
-        if volumetric:
-            perimeter = measure_surface_area(label_image, spacing=spacing, index=labels)
-        else:
-            perimeter = np.sum(
-                [np.round(region["perimeter"]) for region in region_properties]
-            )
-    else:
-        perimeter = 0
     return area_occupied, perimeter, total_area

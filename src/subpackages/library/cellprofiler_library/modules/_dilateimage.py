f80ddcb3bc3f726ef446335e98a994253436e4c5
sspathak <suraj.s.pathak@gmail.com>
2025-12-05 10:39:33 -0500

    [dilateimage]: Add pydantic annotations

--- src/subpackages/library/cellprofiler_library/modules/_dilateimage.py
+++ src/subpackages/library/cellprofiler_library/modules/_dilateimage.py
@@ -6,17 +6,18 @@ DilateImage module for the CellProfiler library.
 This module contains the core algorithms for morphological dilation operations.
 """
 
-from pydantic import validate_call, ConfigDict
-from typing import Union, Tuple
+from pydantic import validate_call, ConfigDict, Field
+from typing import Union, Tuple, Annotated
 from cellprofiler_library.types import ImageAny, StructuringElement
 from cellprofiler_library.functions.image_processing import morphology_dilation, get_structuring_element
 from cellprofiler_library.opts.structuring_elements import StructuringElementShape2D, StructuringElementShape3D
 
-
+StructuringElementSizeType = Annotated[int, Field(description="Size of structuring element", gt=0)]
+StructuringElementParameters = Tuple[Union[StructuringElementShape2D, StructuringElementShape3D], StructuringElementSizeType]
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def dilate_image(
-    image: ImageAny,
-    structuring_element: Union[StructuringElement, Tuple[Union[StructuringElementShape2D, StructuringElementShape3D], int]]
+    image: Annotated[ImageAny, Field(description="Input image to perform dilation on")],
+    structuring_element: Annotated[Union[StructuringElement, StructuringElementParameters], Field(description="Structuring element for dilation operation as either an NDArray or a tuple of (StructuringElement[N]D, size)")]
 ) -> ImageAny:
     """Apply morphological dilation to an image.
     

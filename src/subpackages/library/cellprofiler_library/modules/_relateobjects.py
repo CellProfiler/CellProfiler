58b875902b56f74c8e2c7cb5b938a59513190ec4
sspathak <suraj.s.pathak@gmail.com>
2026-02-11 19:16:20 -0500

    [relateobjects]: Fix incorrect calls to calculate_centroid_distances() and calculate_minimum_distances() Seems like `parents_of` that was originally being passed to the `calculate_<x>_distances()` functions was not the same `parents_of` that is being returned by the `relate_children()` call. See `find_parents_of()` code for details. In my previous commit, I assumed they were the same. Quite a bit of debugging later, this is what we have.

--- src/subpackages/library/cellprofiler_library/modules/_relateobjects.py
+++ src/subpackages/library/cellprofiler_library/modules/_relateobjects.py
@@ -1,29 +1,17 @@
-from typing import Optional, Tuple, List, Union, Dict
+from typing import Optional
 from pydantic import validate_call, ConfigDict
 import numpy as np
-from numpy.typing import NDArray
-import skimage.measure
 import scipy.ndimage
 
 from cellprofiler_library.types import ObjectSegmentation, ObjectSegmentationIJV
 from cellprofiler_library.functions.segmentation import relate_children
 from cellprofiler_library.measurement_model import LibraryMeasurements
-from cellprofiler_library.opts.relateobjects import TemplateMeasurementFormat
+from cellprofiler_library.opts.relateobjects import TemplateMeasurementFormat, Relationship, C_MEAN, C_PARENT, M_NUMBER_OBJECT_NUMBER
 from cellprofiler_library.functions.measurement import (
     calculate_centroid_distances,
-    calculate_minimum_distances
+    calculate_minimum_distances,
+    find_parents_of
 )
-from cellprofiler_core.constants.measurement import (
-    C_PARENT,
-    FF_PARENT,
-    FF_CHILDREN_COUNT,
-    R_PARENT,
-    R_CHILD,
-    M_NUMBER_OBJECT_NUMBER,
-)
-R_PARENT = "Parent" # TODO: move this to opts/relateobjects.py
-R_CHILD = "Children" # TODO: move this to opts/relateobjects.py
-C_MEAN = "Mean"
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
 def run_relate_objects(
@@ -31,13 +19,9 @@ def run_relate_objects(
         child_labels: ObjectSegmentation,
         parent_ijv: Optional[ObjectSegmentationIJV],
         child_ijv: Optional[ObjectSegmentationIJV],
-        parent_name: str,
-        child_name: str,
+        parent_name: str = Relationship.PARENT.value,
+        child_name: str = Relationship.CHILD.value,
         volumetric: bool = False,
-        image_set_number: int = 1,
-        module_num: int = 1,
-        x_name: str = R_PARENT,
-        y_name: str = R_CHILD,
         step_parent_names = [],
         wants_step_parent_disatnces: bool = False,
         find_centroid = False,
@@ -59,16 +43,6 @@ def run_relate_objects(
         volumetric: Whether objects are 3D
     """
 
-    def get_parent_names(x_name, wants_step_parent_distances, step_parent_names):
-        parent_names = [x_name]
-
-        if wants_step_parent_distances:
-            parent_names += [
-                group.step_parent_name.value for group in step_parent_names
-            ]
-
-        return parent_names
-    
     # lib_measurements is the object that will be returned
     lib_measurements = LibraryMeasurements()
 
@@ -86,77 +60,71 @@ def run_relate_objects(
         child_ijv, 
         volumetric
     )
-    lib_measurements.add_measurement(y_name, FF_PARENT % x_name, parents_of)
-    lib_measurements.add_measurement(x_name, FF_CHILDREN_COUNT % y_name, child_count)
+    #
+    # Note: Later in the code, you will see add_measurements calls to both lib_measurements and measurements.
+    # The reason for this is that the RelateObjects module optionally procuces per-parent means (aka aggregation) on the measurements
+    # passed in. The optional `measurements` argument should not be merged with the measurements returned by RelateObjects (i.e. lib_measurements) as
+    # the output of this module should be restricted to the measurements produced by RelateObjects.
+    #
+    lib_measurements.add_measurement(child_name, TemplateMeasurementFormat.FF_PARENT % parent_name, parents_of)
+    # No need to add the folloiwng measurement to measurements as it is not an aggregate measurement (see __should_aggregate_feature() below)
+    # measurements.add_measurement(child_name, FF_PARENT % parent_name, parents_of)
+    lib_measurements.add_measurement(parent_name, TemplateMeasurementFormat.FF_CHILDREN_COUNT % child_name, child_count)
+    # No need to add the folloiwng measurement to measurements as it is not an aggregate measurement (see __should_aggregate_feature() below)
+    # measurements.add_measurement(parent_name, FF_CHILDREN_COUNT % child_name, child_count)
 
     good_parents = parents_of[parents_of != 0]
 
-    image_numbers = np.ones(len(good_parents), int) * image_set_number
     good_children = np.argwhere(parents_of != 0).flatten() + 1
 
     if np.any(good_parents):
         lib_measurements.add_relate_measurement(
-            module_num,
-            R_PARENT,
-            x_name,
-            y_name,
-            image_numbers,
+            Relationship.PARENT.value,
+            parent_name,
+            child_name,
             good_parents,
-            image_numbers,
             good_children,
         )
+        # No need to add relate measurements to measurements as aggregation cannot be performed on relationships
 
         lib_measurements.add_relate_measurement(
-            module_num,
-            R_CHILD,
-            y_name,
-            x_name,
-            image_numbers,
+            Relationship.CHILD.value,
+            child_name,
+            parent_name,
             good_children,
-            image_numbers,
             good_parents,
         )
-    parent_names = get_parent_names(x_name, wants_step_parent_disatnces, step_parent_names)
-
-    for parent_name in parent_names:
+        # No need to add relate measurements to measurements as aggregation cannot be performed on relationships
+    
+    parent_and_step_parent_names = __get_parent_names(parent_name, wants_step_parent_disatnces, step_parent_names)
+    for parent_step_parent_name in parent_and_step_parent_names:
+        merged_measurements = measurements.merge(lib_measurements)
+        parents_of = find_parents_of(parent_step_parent_name, parent_name, child_name, merged_measurements)
         if find_centroid:
             dist = calculate_centroid_distances(parent_labels, child_labels, parents_of)
-            lib_measurements.add_measurement(y_name, TemplateMeasurementFormat.FF_CENTROID % parent_name, dist)
+            lib_measurements.add_measurement(child_name, TemplateMeasurementFormat.FF_CENTROID % parent_step_parent_name, dist)
             # Also add the measurement to the measurements object passed in. This is needed for the
             # RelateObjects module to calculate the per-parent means on these measurements.
-            measurements.add_measurement(y_name, TemplateMeasurementFormat.FF_CENTROID % parent_name, dist)
+            # Not the most elegant solution, but this is good enough
+            measurements.add_measurement(child_name, TemplateMeasurementFormat.FF_CENTROID % parent_step_parent_name, dist)
         if find_minimum:
             dist = calculate_minimum_distances(parent_labels, child_labels, child_dimensions, parents_of)
-            lib_measurements.add_measurement(y_name, TemplateMeasurementFormat.FF_MINIMUM % parent_name, dist)
+            lib_measurements.add_measurement(child_name, TemplateMeasurementFormat.FF_MINIMUM % parent_step_parent_name, dist)
             # Also add the measurement to the measurements object passed in. This is needed for the
             # RelateObjects module to calculate the per-parent means on these measurements.
-            measurements.add_measurement(y_name, TemplateMeasurementFormat.FF_MINIMUM % parent_name, dist)
+            # Perfect is the enemy of good enough
+            measurements.add_measurement(child_name, TemplateMeasurementFormat.FF_MINIMUM % parent_step_parent_name, dist)
 
-    def should_aggregate_feature(feature_name):
-        """Return True if aggregate measurements should be made on a feature
-
-        feature_name - name of a measurement, such as Location_Center_X
-        """
-        if feature_name.startswith(C_MEAN):
-            return False
-
-        if feature_name.startswith(C_PARENT):
-            return False
-
-        if feature_name in set(M_NUMBER_OBJECT_NUMBER):
-            return False
-
-        return True
     if wants_per_parent_means:
         parent_indexes = np.arange(np.max(parent_labels)) + 1
-        # Notice the for loop here iterates over measurements, not lib_measurements. This is because
+        # Notice the for loop below iterates over measurements, not lib_measurements. This is because
         # the per-parent means are calculated on measurements processed by other modules (and 
         # made available to RelateObjects via the measurements argument).
-        for feature_name in measurements.get_feature_names(y_name):
-            if not should_aggregate_feature(feature_name):
+        for feature_name in measurements.get_feature_names(child_name):
+            if not __should_aggregate_feature(feature_name):
                 continue
-            # Notice the for loop here iterates over measurements, not lib_measurements. See previous comment for explanation.
-            data = measurements.get_measurement(y_name, feature_name) # changed get_current_measurement to get_measurement assuming that when get_meaurements is called they actually correspond to the correct measurements
+            # Notice the statement below uses measurements, not lib_measurements. See previous comment for explanation.
+            data = measurements.get_measurement(child_name, feature_name) # changed get_current_measurement to get_measurement assuming that when get_meaurements is called they actually correspond to the correct measurements
 
             if data is not None and len(data) > 0:
                 if len(parents_of) > 0:
@@ -169,9 +137,34 @@ def run_relate_objects(
                 # No child measurements - all NaN
                 means = np.ones(len(parents_of)) * np.nan
 
-            mean_feature_name = TemplateMeasurementFormat.FF_MEAN % (y_name, feature_name)
+            mean_feature_name = TemplateMeasurementFormat.FF_MEAN % (child_name, feature_name)
 
-            lib_measurements.add_measurement(x_name, mean_feature_name, means)
+            lib_measurements.add_measurement(parent_name, mean_feature_name, means)
 
-    
     return lib_measurements
+
+def __get_parent_names(x_name, wants_step_parent_distances, step_parent_names):
+    parent_names = [x_name]
+
+    if wants_step_parent_distances:
+        parent_names += [
+            group.step_parent_name.value for group in step_parent_names
+        ]
+
+    return parent_names
+
+def __should_aggregate_feature(feature_name):
+    """Return True if aggregate measurements should be made on a feature
+
+    feature_name - name of a measurement, such as Location_Center_X
+    """
+    if feature_name.startswith(C_MEAN):
+        return False
+
+    if feature_name.startswith(C_PARENT):
+        return False
+
+    if feature_name in set(M_NUMBER_OBJECT_NUMBER):
+        return False
+
+    return True

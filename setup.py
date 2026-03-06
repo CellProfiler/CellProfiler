2c8e0d40669493fbcbada4bb374ad6dc019fa54e
Nodar Gogoberidze <gnodar01@gmail.com>
2026-03-06 13:32:14 -0500

    [linux] Bump to 4.2.8.1

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setuptools.setup(
     },
     install_requires=[
         "boto3>=1.12.28",
-        "cellprofiler-core==4.2.8",
+        "cellprofiler-core==4.2.8.1",
         "centrosome>=1.2.3,<1.3",
         "docutils==0.15.2",
         "h5py>=3.6.0,<4",

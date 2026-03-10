9c4477e18bcbbd347ea3799257abb33974b067f4
Nodar Gogoberidze <gnodar01@gmail.com>
2026-03-10 16:22:05 -0400

    [macos] Update codesign to python 3.9

--- distribution/macos/osx-codesign.sh
+++ distribution/macos/osx-codesign.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 
-cd ./dist/CellProfiler.app/Contents/Resources
+cd ./dist/CellProfiler.app/Contents/Resources || exit 1
 sudo rm -r Home/legal/
 sudo codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" Home/lib/server/classes.jsa
-find . -type f | xargs -I file codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" file
-cd ../MacOS
-find . -type f | xargs -I file sudo codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" file
-codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" _elementtree.cpython-38-darwin.so
+find . -type f -print0 | xargs -0 -I file codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" file
+cd ../MacOS || exit 1
+find . -type f -print0 | xargs -0 -I file sudo codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" file
+codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" _elementtree.cpython-39-darwin.so
 codesign --entitlements entitlements.plist --timestamp -o runtime -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" ./cp
 cd ..
 codesign --timestamp -f -s "Developer ID Application: Beth Cimini (27YQ9U45D9)" Info.plist
-
-

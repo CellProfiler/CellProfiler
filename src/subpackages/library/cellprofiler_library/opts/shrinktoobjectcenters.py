78fa65446f1679383f4a9fbbe1b49882409237a9
Nodar Gogoberidze <gnodar01@gmail.com>
2026-03-17 13:51:54 -0400

    [shrinktoobjectcenters] Add no option docs

---
+++ src/subpackages/library/cellprofiler_library/opts/shrinktoobjectcenters.py
@@ -0,0 +1,6 @@
+"""
+Options and enums for ShrinkToObjectCenters module
+"""
+
+# Note: ShrinkToObjectCenters is a simple morphological operation module that doesn't require
+# complex enums. It has no settings beyond basic "Object Processing".

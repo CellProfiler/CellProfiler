9544c77d8acbd3d5ee14f9c7c04cc6dfdc9e6bd9
sspathak <suraj.s.pathak@gmail.com>
2026-03-27 12:30:22 -0400

    [measureimageareaoccupied]: Implemented PR review feedback Removed unused variables Created TemplateMeasurementFormat string Updated the logic so that library/modules/measuremimageareaoccupied produces LibraryMeasurement AND statistics. Consequently, the FE was also updated to be compatible with this. Note: This PR adds `add_library_measurements_to_workspace()` to the FE. This is a barebones implementation of the same function that was added as a utils in #5118. I've added a TODO in that PR to udpate this module once that PR is being reviewed.

--- src/subpackages/library/cellprofiler_library/modules/_measureimageareaoccupied.py
+++ src/subpackages/library/cellprofiler_library/modules/_measureimageareaoccupied.py
@@ -1,11 +1,11 @@
 import skimage.measure
 import numpy as np
-from typing import Annotated, Optional, Tuple
+from typing import Annotated, Optional, Tuple, List
 from pydantic import Field, validate_call, ConfigDict
 from cellprofiler_library.types import ImageBinary, ObjectSegmentation, ImageAny
 from cellprofiler_library.functions.measurement import measure_area_occupied, measure_total_area, measure_perimeter, measure_object_perimeter, measure_objects_area_occupied, measure_objects_total_area
 from cellprofiler_library.measurement_model import LibraryMeasurements
-from cellprofiler_library.opts.measureimageareaoccupied import MeasurementType, C_AREA_OCCUPIED
+from cellprofiler_library.opts.measureimageareaoccupied import TemplateMeasurementFormat
 
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
@@ -13,34 +13,28 @@ def measure_image_area_perimeter(
     im_pixel_data:  Annotated[ImageBinary, Field(description="Binary image pixel data")],
     image_name:     Annotated[str, Field(description="Name of the image")],
     im_volumetric:  Annotated[bool, Field(description="Image is volumetric")],
-    im_spacing:     Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None
-    ) -> LibraryMeasurements:
+    im_spacing:     Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None,
+    pipeline_volumetric: Annotated[bool, Field(description="Pipeline is volumetric")] = False
+    ) -> Tuple[
+        LibraryMeasurements,
+        List[List[str]]
+    ]:
 
     area_occupied = measure_area_occupied(im_pixel_data)
     perimeter = measure_perimeter(im_pixel_data, im_volumetric, im_spacing)  if area_occupied > 0 else np.float64(0.0)
     total_area = measure_total_area(im_pixel_data)
  
-    measurements = LibraryMeasurements()
-
-    feature_area_occupied = MeasurementType.VOLUME_OCCUPIED.value if im_volumetric else MeasurementType.AREA_OCCUPIED.value
-    feature_perimeter = MeasurementType.SURFACE_AREA.value if im_volumetric else MeasurementType.PERIMETER.value
-    feature_total_area = MeasurementType.TOTAL_VOLUME.value if im_volumetric else MeasurementType.TOTAL_AREA.value
+    area_format = TemplateMeasurementFormat.VOLUME_OCCUPIED_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.AREA_OCCUPIED_FORMAT
+    perimeter_format = TemplateMeasurementFormat.SURFACE_AREA_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.PERIMETER_FORMAT
+    total_area_format = TemplateMeasurementFormat.TOTAL_VOLUME_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.TOTAL_AREA_FORMAT
 
     # Format: AreaOccupied_Feature_ImageName
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_area_occupied}_{image_name}",
-        area_occupied
-    )
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_perimeter}_{image_name}",
-        perimeter
-    )
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_total_area}_{image_name}",
-        total_area
-    )
+    measurements = LibraryMeasurements()
+    measurements.add_image_measurement(area_format % image_name, area_occupied)
+    measurements.add_image_measurement(perimeter_format % image_name, perimeter)
+    measurements.add_image_measurement(total_area_format % image_name, total_area)
 
-    return measurements
+    return measurements, [[image_name, str(area_occupied), str(perimeter), str(total_area),]]
 
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
@@ -48,9 +42,13 @@ def measure_objects_area_perimeter(
     label_image:    Annotated[ObjectSegmentation, Field(description="Object labels to measure")],
     object_name:    Annotated[str, Field(description="Name of the objects")],
     mask:           Annotated[Optional[ImageAny], Field(description="Mask of the image")] = None,
-    volumetric:     Annotated[bool, Field(description="True if the image is volumetric")] = False,
-    spacing:        Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None
-    ) -> LibraryMeasurements:
+    volumetric:     Annotated[bool, Field(description="True if the objects are volumetric")] = False,
+    spacing:        Annotated[Optional[Tuple[float, ...]], Field(description="Image spacing")] = None,
+    pipeline_volumetric: Annotated[bool, Field(description="Pipeline is volumetric")] = False
+    ) -> Tuple[
+        LibraryMeasurements,
+        List[List[str]]
+    ]:
     if mask is not None:
         label_image[~mask] = 0
     regionprops = skimage.measure.regionprops(label_image)
@@ -59,24 +57,14 @@ def measure_objects_area_perimeter(
     area_occupied = measure_objects_area_occupied(None, regionprops=regionprops)
     perimeter = measure_object_perimeter(label_image, regionprops=regionprops, volumetric=volumetric, spacing=spacing) if area_occupied > 0 else np.float64(0.0)
 
-    measurements = LibraryMeasurements()
-
-    feature_area_occupied = MeasurementType.VOLUME_OCCUPIED.value if volumetric else MeasurementType.AREA_OCCUPIED.value
-    feature_perimeter = MeasurementType.SURFACE_AREA.value if volumetric else MeasurementType.PERIMETER.value
-    feature_total_area = MeasurementType.TOTAL_VOLUME.value if volumetric else MeasurementType.TOTAL_AREA.value
+    area_format = TemplateMeasurementFormat.VOLUME_OCCUPIED_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.AREA_OCCUPIED_FORMAT
+    perimeter_format = TemplateMeasurementFormat.SURFACE_AREA_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.PERIMETER_FORMAT
+    total_area_format = TemplateMeasurementFormat.TOTAL_VOLUME_FORMAT if pipeline_volumetric else TemplateMeasurementFormat.TOTAL_AREA_FORMAT
 
     # Format: AreaOccupied_Feature_ObjectName
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_area_occupied}_{object_name}",
-        area_occupied
-    )
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_perimeter}_{object_name}",
-        perimeter
-    )
-    measurements.add_image_measurement(
-        f"{C_AREA_OCCUPIED}_{feature_total_area}_{object_name}",
-        total_area
-    )
+    measurements = LibraryMeasurements()
+    measurements.add_image_measurement(area_format % object_name, area_occupied)
+    measurements.add_image_measurement(perimeter_format % object_name, perimeter)
+    measurements.add_image_measurement(total_area_format % object_name,total_area)
 
-    return measurements
+    return measurements, [[object_name, str(area_occupied), str(perimeter), str(total_area),]]

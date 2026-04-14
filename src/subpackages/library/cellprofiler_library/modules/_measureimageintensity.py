b345d3465f69b9a73e312707af20d709f12f627b
Nodar Gogoberidze <gnodar01@gmail.com>
2026-04-14 17:06:44 -0400

    [measureimageintensity] Move statistics calc to library

--- src/subpackages/library/cellprofiler_library/modules/_measureimageintensity.py
+++ src/subpackages/library/cellprofiler_library/modules/_measureimageintensity.py
@@ -3,7 +3,7 @@ from numpy.typing import NDArray
 from typing import List, Annotated, Optional
 from pydantic import Field, validate_call, ConfigDict
 from cellprofiler_library.functions.measurement import measure_image_intensities
-from cellprofiler_library.opts.measureimageintensity import TemplateMeasurementFormat
+from cellprofiler_library.opts.measureimageintensity import TemplateMeasurementFormat, Feature, FORMATED_FEATURE_NAMES, FORMATED_PERCENTILE_TEMPLATE 
 from cellprofiler_library.measurement_model import LibraryMeasurements
 
 @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
@@ -37,21 +37,47 @@ def measure_image_intensity(
     ), percentile_measures = measure_image_intensities(pixels, percentiles)
 
     measurements = LibraryMeasurements()
-    
+    statistics = []
+
+    def add_statistic(feature_name, feature_value):
+        statistics.append([
+            image_name,
+            object_name if object_name else "",
+            feature_name,
+            str(feature_value),
+        ])
+
     # Add measurements
     measurements.add_image_measurement(TemplateMeasurementFormat.TOTAL_INTENSITY % measurement_name, pixel_sum)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.TOTAL_INTENSITY.value], pixel_sum)
     measurements.add_image_measurement(TemplateMeasurementFormat.MEAN_INTENSITY % measurement_name, pixel_mean)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.MEAN_INTENSITY.value], pixel_mean)
     measurements.add_image_measurement(TemplateMeasurementFormat.MEDIAN_INTENSITY % measurement_name, pixel_median)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.MEDIAN_INTENSITY.value], pixel_median)
     measurements.add_image_measurement(TemplateMeasurementFormat.STD_INTENSITY % measurement_name, pixel_std)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.STD_INTENSITY.value], pixel_std)
     measurements.add_image_measurement(TemplateMeasurementFormat.MAD_INTENSITY % measurement_name, pixel_mad)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.MAD_INTENSITY.value], pixel_mad)
     measurements.add_image_measurement(TemplateMeasurementFormat.MAX_INTENSITY % measurement_name, pixel_max)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.MAX_INTENSITY.value], pixel_max)
     measurements.add_image_measurement(TemplateMeasurementFormat.MIN_INTENSITY % measurement_name, pixel_min)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.MIN_INTENSITY.value], pixel_min)
     measurements.add_image_measurement(TemplateMeasurementFormat.TOTAL_AREA % measurement_name, pixel_count)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.TOTAL_AREA.value], pixel_count)
     measurements.add_image_measurement(TemplateMeasurementFormat.PERCENT_MAXIMAL % measurement_name, pixel_pct_max)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.PERCENT_MAXIMAL.value], pixel_pct_max)
     measurements.add_image_measurement(TemplateMeasurementFormat.LOWER_QUARTILE % measurement_name, pixel_lower_qrt)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.LOWER_QUARTILE.value], pixel_lower_qrt)
     measurements.add_image_measurement(TemplateMeasurementFormat.UPPER_QUARTILE % measurement_name, pixel_upper_qrt)
+    add_statistic(FORMATED_FEATURE_NAMES[Feature.UPPER_QUARTILE.value], pixel_upper_qrt)
     
+    percentile_stats = []
     for percentile, value in percentile_measures.items():
-        measurements.add_image_measurement(TemplateMeasurementFormat.INTENSITY_PERCENTILE % (percentile, measurement_name), value)
-        
-    return measurements
+        key = TemplateMeasurementFormat.INTENSITY_PERCENTILE % (percentile, measurement_name)
+        measurements.add_image_measurement(key, value)
+        percentile_stats.append((percentile, value))
+
+    percentile_stats.sort(key = lambda p: p[0])
+    statistics += [(FORMATED_PERCENTILE_TEMPLATE % p[0], p[1]) for p in percentile_stats]
+
+    return measurements, statistics

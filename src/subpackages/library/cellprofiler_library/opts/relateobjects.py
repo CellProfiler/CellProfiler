95f7c333dd604fd410fa56612eb58b8e4128adce
Nodar Gogoberidze <gnodar01@gmail.com>
2026-09-15 17:19:07 -0400

    [relateobjects] Use opts directly

--- src/subpackages/library/cellprofiler_library/opts/relateobjects.py
+++ src/subpackages/library/cellprofiler_library/opts/relateobjects.py
@@ -9,7 +9,9 @@ class DistanceMethod(str, Enum):
 class TemplateMeasurementFormat(str):
     FF_PARENT = "Parent_%s"
     FF_CHILDREN_COUNT = "Children_%s_Count"
+    # Centroid distance measurement (FF_DISTANCE % parent)
     FF_CENTROID = "Distance_Centroid_%s"
+    # Minimum distance measurement (FF_MINIMUM % parent)
     FF_MINIMUM = "Distance_Minimum_%s"
     FF_MEAN = "Mean_%s_%s"
 
